af30e06f3f30a376488b1224ac22fd1f684f1d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:55 +0000

    Add package date-convert-ad-bs/1.1.3/date-convert-ad-bs-1.1.3.tar.gz

---
+++ packages/date-convert-ad-bs/date-convert-ad-bs-1.1.3.tar.gz/date-convert-ad-bs-1.1.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.1.3'
+DESCRIPTION = 'Convert AD to BS and vice versa'
+
+# Setting up
+setup(
+    name="date-convert-ad-bs",
+    version=VERSION,
+    author="Ulleri Tech (Binij Shrestha)",
+    author_email="<uller@ulleri.tech>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Ulleri-Tech/date-convert-AD-BS',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['python', "Convert Date",
+              "AD to BS",
+              "BS to AD"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
