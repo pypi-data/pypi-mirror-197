925a8b5352ba197e7090d10290803df38e159b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:25 +0000

    Add package minet/1.0.0a10/minet-1.0.0a10.tar.gz

---
+++ packages/minet/minet-1.0.0a10.tar.gz/minet-1.0.0a10/minet/crawl/__init__.py
@@ -0,0 +1,11 @@
+from minet.crawl.types import CrawlResult, CrawlJob, UrlOrCrawlJob
+from minet.crawl.state import CrawlerState
+from minet.crawl.spiders import (
+    Spider,
+    SpiderResult,
+    SpiderNextJobs,
+    FunctionSpider,
+    DefinitionSpider,
+    DefinitionSpiderOutput,
+)
+from minet.crawl.crawler import Crawler
