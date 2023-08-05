ca433abc8fba1e68975e416ac2f917c557ca7410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:14 +0000

    Add package carmine/0.0.2/carmine-0.0.2-py3-none-any.whl

---
+++ packages/carmine/carmine-0.0.2-py3-none-any.whl/carmine/cli.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright 2023 David Seaward and contributors
+
+
+def invoke():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    invoke()
