c2af9d0214044d897ffe2c5148b6145962337329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:51 +0000

    Add package Telegant/0.1.4/Telegant-0.1.4.tar.gz

---
+++ packages/Telegant/Telegant-0.1.4.tar.gz/Telegant-0.1.4/telegant/api.py
@@ -0,0 +1,17 @@
+import aiohttp  
+
+class Api:
+    async def request(self, action, params=None):
+        async with aiohttp.ClientSession() as session:
+            try:
+                url = f"{self.base_url}{action}"
+                response = await session.post(url, params=params)
+                return await response.json()
+            except Exception as e:
+                print(f"Error sending request: {e}")
+
+    async def sendMessage(self, **params):
+        return await self.request("sendMessage", params)
+
+    async def sendDice(self, **params):  
+        return await self.request("sendDice", params)
