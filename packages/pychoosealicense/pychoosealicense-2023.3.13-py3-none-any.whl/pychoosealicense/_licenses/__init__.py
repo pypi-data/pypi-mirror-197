852596f9bdbf844df37614c979bf960be9c9b6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:34 +0000

    Add package pychoosealicense/2023.3.13/pychoosealicense-2023.3.13-py3-none-any.whl

---
+++ packages/pychoosealicense/pychoosealicense-2023.3.13-py3-none-any.whl/pychoosealicense/_licenses/__init__.py
@@ -0,0 +1,17 @@
+"""
+License metadata used by ``pychoosealicense``.
+"""
+
+CHOOSEALICENSE_COMMIT_INFO = """
+commit 0ccd6c5d0ce1c0f68e088ec0c936322e43b2fbfb
+Merge: 2b3259f b62ee46
+Author: Mike Linksvayer <mlinksva@github.com>
+Date:   Tue Dec 27 17:22:18 2022 -0800
+
+    Merge pull request #1066 from github/bsd-3-nickname
+
+    Add "Modified" nickname to BSD-3-Clause
+"""
+
+CHOOSEALICENSE_COMMIT_HASH = "0ccd6c5d0ce1c0f68e088ec0c936322e43b2fbfb"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "0ccd6c5"
