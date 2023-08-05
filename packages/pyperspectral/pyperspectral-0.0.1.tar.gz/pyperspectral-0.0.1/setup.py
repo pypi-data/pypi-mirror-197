563e394f60cd013e51c4ce1b2eb08e8f15cca8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:13 +0000

    Add package pyperspectral/0.0.1/pyperspectral-0.0.1.tar.gz

---
+++ packages/pyperspectral/pyperspectral-0.0.1.tar.gz/pyperspectral-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import pyperspectral
+
+setup(
+    name='pyperspectral',
+    version=pyperspectral.__version__,
+    packages=['pyperspectral'],
+    url='',
+    license='',
+    author='Roland Pfeiffer',
+    author_email='',
+    description=''
+)
