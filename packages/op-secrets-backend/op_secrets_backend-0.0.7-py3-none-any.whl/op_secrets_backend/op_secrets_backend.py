844424e76018252139f05d4ad337abcf3472b0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:17 +0000

    Add package op-secrets-backend/0.0.7/op_secrets_backend-0.0.7-py3-none-any.whl

---
+++ packages/op-secrets-backend/op_secrets_backend-0.0.7-py3-none-any.whl/op_secrets_backend/op_secrets_backend.py
@@ -0,0 +1,37 @@
+# this is a library used by dag users
+from airflow.secrets import BaseSecretsBackend
+
+import requests
+import os
+import json
+
+
+class OpSecretsBackend(BaseSecretsBackend):
+    def get_connection(self, conn_id: str): # conn_id here is OP connection name
+        from airflow.models.taskinstance import _CURRENT_CONTEXT
+        from airflow.configuration import ensure_secrets_loaded
+        if _CURRENT_CONTEXT is None or len(_CURRENT_CONTEXT) == 0:
+            raise Exception("failed to get _CURRENT_CONTEXT") 
+        run_id = _CURRENT_CONTEXT[0]["dag_run"].run_id
+        dag_id = _CURRENT_CONTEXT[0]["dag_run"].dag_id
+        id = _CURRENT_CONTEXT[0]["dag_run"].id
+        AC_URL = os.getenv('AC_URL')
+        param_values={'run_id':run_id, 'dag_id':dag_id, 'id':id, 'user_connection_name':conn_id}
+        url=AC_URL+"/airflow-connection-id"
+        response = requests.get(url, params = param_values)
+        if response is None or response.status_code != 200:
+            raise Exception("Cannot connect with AC") 
+        result = json.loads(response.text)
+        if result[0] == 200:
+            for secrets_backend in ensure_secrets_loaded():
+                print(secrets_backend)
+                result = isinstance(secrets_backend, OpSecretsBackend)
+                if result:
+                    print("yes")
+                    print(result)
+                else:
+                    print("No")
+            return super().get_connection(result[1])
+        else:
+            raise Exception(result[1])
+    
