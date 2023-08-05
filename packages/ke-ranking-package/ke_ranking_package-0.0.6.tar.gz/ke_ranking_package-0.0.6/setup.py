442482e93b5384e4600c7245c343140d86be3f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package ke-ranking-package/0.0.6/ke_ranking_package-0.0.6.tar.gz

---
+++ packages/ke-ranking-package/ke_ranking_package-0.0.6.tar.gz/ke_ranking_package-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ke_ranking_package",
+    version="0.0.6",
+    description="Useful Functions for search & ranking",
+    py_modules=["helpers", "paths", "visualize", "elastic_client"],
+    install_requires=[
+        "clickhouse-driver~=0.2.4",
+        "elasticsearch~=8.4.1"],
+    package_dir={"": "src"},
+    url = "https://github.com/KazanExpress/ke_ranking_package"
+)
