aabe46731fc15d5dea8e7bede1305acf55f43d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:11 +0000

    Add package Uname-sridhar/0.1.2/Uname_sridhar-0.1.2.tar.gz

---
+++ packages/Uname-sridhar/Uname_sridhar-0.1.2.tar.gz/Uname_sridhar-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+requirements = os.popen("/usr/local/bin/pipreqs uname --print").read().splitlines()
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Uname_sridhar',
+    version='0.1.2',
+    author='Sridhar',
+    author_email='dcsvsridhar@gmail.com',
+    description='Uname_sridhar is a wrapper of uname Command',
+    packages=find_packages(),
+    url='https://git.selfmade.ninja/SRIDHARDSCV/package_uname',
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'Uname=uname.uname:main',
+        ],
+    },
+)
