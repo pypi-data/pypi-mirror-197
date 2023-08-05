50e7dc6302b158f76f675463e638308eca4f132d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:59 +0000

    Add package greeting-package/0.1.1/greeting_package-0.1.1.tar.gz

---
+++ packages/greeting-package/greeting_package-0.1.1.tar.gz/greeting_package-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="greeting_package",
+    version="0.1.1",
+    author="Jeff Osundwa",
+    author_email="mulamajeff@tuta.io",
+    description="A package for generating greeting messages",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifier=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
