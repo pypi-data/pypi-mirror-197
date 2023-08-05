f9e732e6f1155adbbd164d13c93f58c093bfdade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:56 +0000

    Add package hcai-datasets-nightly/0.1.5.dev202303131412/hcai-datasets-nightly-0.1.5.dev202303131412.tar.gz

---
+++ packages/hcai-datasets-nightly/hcai-datasets-nightly-0.1.5.dev202303131412.tar.gz/hcai-datasets-nightly-0.1.5.dev202303131412/hcai_datasets/__init__.py
@@ -0,0 +1,12 @@
+'''Image Datasets'''
+from hcai_datasets import hcai_affectnet
+from hcai_datasets import hcai_ckplus
+from hcai_datasets import hcai_faces
+
+'''Audio Datasets'''
+from hcai_datasets import hcai_audioset
+from hcai_datasets import hcai_librispeech
+from hcai_datasets import hcai_is2021_ess
+
+'''Nova Datasets'''
+from hcai_datasets import hcai_nova_dynamic
