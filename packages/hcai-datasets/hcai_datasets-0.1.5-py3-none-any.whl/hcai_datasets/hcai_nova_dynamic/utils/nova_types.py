1bbed97312742b62ff31e806834fd75ce392664b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:58 +0000

    Add package hcai-datasets/0.1.5/hcai_datasets-0.1.5-py3-none-any.whl

---
+++ packages/hcai-datasets/hcai_datasets-0.1.5-py3-none-any.whl/hcai_datasets/hcai_nova_dynamic/utils/nova_types.py
@@ -0,0 +1,23 @@
+from enum import Enum
+import numpy as np
+
+class DataTypes(Enum):
+  VIDEO = 0
+  AUDIO  = 1
+  FEATURE = 2
+
+
+class AnnoTypes(Enum):
+  DISCRETE = 0
+  CONTINUOUS = 1
+  FREE = 2
+  POINT = 3
+  DISCRETE_POLYGON = 4
+
+
+'''Helper'''
+def string_to_enum(enum, string):
+  for e in enum:
+    if e.name == string.upper():
+      return e
+  raise ValueError('{} not part of enumeration  {}'.format(string, enum))
