a5977cd887d3c58941884146e988e96c1c25b6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:37 +0000

    Add package PdfFileM/0.0.1/PdfFileM-0.0.1.tar.gz

---
+++ packages/PdfFileM/PdfFileM-0.0.1.tar.gz/PdfFileM-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='PdfFileM',
+    version='0.0.1',
+    license='MIT License',
+    author='Francisco Aparicio & Pedro Tercio',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='faparicionc1996@gmail.com',
+    keywords='Pdf',
+    description=u'Traduzir paginas de um pdf e leitura do arquivo com voz do sistema',
+    packages=['PdfFileM'],
+    install_requires=["requests", "pyttsx3", "PyPDF2", "pdfplumber", "googletrans"],)
