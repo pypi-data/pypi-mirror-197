f9607f2e79b1f35c5c78daf6a892f03a231df638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:22 +0000

    Add package rrllm/0.0.17/rrllm-0.0.17.tar.gz

---
+++ packages/rrllm/rrllm-0.0.17.tar.gz/rrllm-0.0.17/setup.py
@@ -0,0 +1,43 @@
+import os 
+os.chdir('/Users/kaficifci/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/Political-Reaction-to-Economic-Globalization/package_paper/rrllm')
+
+from setuptools import setup, find_packages
+import os
+import sys
+
+if sys.version_info[0] != 3:
+    raise RuntimeError('Unsupported python version "{0}"'.format(
+        sys.version_info[0]))
+
+def _get_file_content(file_name):
+    with open(file_name, 'r') as file_handler:
+        return str(file_handler.read())
+      
+def get_long_description():
+    return _get_file_content('README.md')
+
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    for line in fh:
+        requirements.append(line.strip())
+
+setup(
+    name="rrllm",
+    version='0.0.17 ',
+    author="Muhammed Cifci",
+    author_email="mkcifci@psu.edu",
+    description="Rubin's rules to account for performance instability of LLMs",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    keywords='nlp transformers classification rubinsrules',
+    url="https://github.com/mkcifci/RR_LLM.git",
+    packages=['rrllm'],
+    py_modules=['rrllm'],
+    license="MIT",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=requirements
+)
