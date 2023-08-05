f43c20889d4b63ebcbe1675e69e5464427bf373b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package astro-provider-databricks/0.1.1/astro_provider_databricks-0.1.1-py3-none-any.whl

---
+++ packages/astro-provider-databricks/astro_provider_databricks-0.1.1-py3-none-any.whl/astro_databricks/__init__.py
@@ -0,0 +1,10 @@
+"""Description of the package"""
+from astro_databricks.operators.notebook import DatabricksNotebookOperator
+from astro_databricks.operators.workflow import DatabricksWorkflowTaskGroup
+
+__version__ = "0.1.1"
+__all__ = [
+    "DatabricksNotebookOperator",
+    "DatabricksWorkflowTaskGroup"
+]
+
