f90834aa5cc3123f1a2b62c00b33b6ba522e5268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:21 +0000

    Add package unified-planning/0.5.0.309.dev1/unified_planning-0.5.0.309.dev1-py3-none-any.whl

---
+++ packages/unified-planning/unified_planning-0.5.0.309.dev1-py3-none-any.whl/unified_planning/engines/compilers/utils.py
@@ -0,0 +1,272 @@
+# Copyright 2021 AIPlan4EU project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""This module defines different utility functions for the compilers."""
+
+
+import unified_planning as up
+from unified_planning.exceptions import UPConflictingEffectsException, UPUsageError
+from unified_planning.model import (
+    FNode,
+    TimeInterval,
+    Action,
+    InstantaneousAction,
+    DurativeAction,
+    Problem,
+    Effect,
+    Expression,
+    SimulatedEffect,
+    Parameter,
+    DurationInterval,
+)
+from unified_planning.plans import ActionInstance
+from typing import Dict, Iterable, List, Optional, Tuple, cast
+
+
+def check_and_simplify_conditions(
+    problem: Problem, action: DurativeAction, simplifier
+) -> Tuple[bool, List[Tuple[TimeInterval, FNode]]]:
+    """
+    Simplifies conditions and if it is False (a contraddiction)
+    returns False, otherwise returns True.
+    If the simplification is True (a tautology) removes all conditions at the given timing.
+    If the simplification is still an AND rewrites back every "arg" of the AND
+    in the conditions
+    If the simplification is not an AND sets the simplification as the only
+    condition at the given timing.
+    Then, the new conditions are returned as a List[Tuple[Timing, FNode]] and the user can
+    decide how to use the new conditions.
+    """
+    # new action conditions
+    nac: List[Tuple[TimeInterval, FNode]] = []
+    # t = timing, lc = list condition
+    for i, lc in action.conditions.items():
+        # conditions (as an And FNode)
+        c = problem.environment.expression_manager.And(lc)
+        # conditions simplified
+        cs = simplifier.simplify(c)
+        if cs.is_bool_constant():
+            if not cs.bool_constant_value():
+                return (
+                    False,
+                    [],
+                )
+        else:
+            if cs.is_and():
+                for new_cond in cs.args:
+                    nac.append((i, new_cond))
+            else:
+                nac.append((i, cs))
+    return (True, nac)
+
+
+def check_and_simplify_preconditions(
+    problem: Problem, action: InstantaneousAction, simplifier
+) -> Tuple[bool, List[FNode]]:
+    """
+    Simplifies preconditions and if it is False (a contraddiction)
+    returns False, otherwise returns True.
+    If the simplification is True (a tautology) removes all preconditions.
+    If the simplification is still an AND rewrites back every "arg" of the AND
+    in the preconditions
+    If the simplification is not an AND sets the simplification as the only
+    precondition.
+    Then, the new preconditions are returned as a List[FNode] and the user can
+    decide how to use the new preconditions.
+    """
+    # action preconditions
+    ap = action.preconditions
+    if len(ap) == 0:
+        return (True, [])
+    # preconditions (as an And FNode)
+    p = problem.environment.expression_manager.And(ap)
+    # preconditions simplified
+    ps = simplifier.simplify(p)
+    # new action preconditions
+    nap: List[FNode] = []
+    if ps.is_bool_constant():
+        if not ps.bool_constant_value():
+            return (False, [])
+    else:
+        if ps.is_and():
+            nap.extend(ps.args)
+        else:
+            nap.append(ps)
+    action._set_preconditions(nap)
+    return (True, nap)
+
+
+def create_effect_with_given_subs(
+    problem: Problem,
+    old_effect: Effect,
+    simplifier,
+    subs: Dict[Expression, Expression],
+) -> Optional[Effect]:
+    new_fluent = old_effect.fluent.substitute(subs)
+    new_value = old_effect.value.substitute(subs)
+    new_condition = simplifier.simplify(old_effect.condition.substitute(subs))
+    if new_condition == problem.environment.expression_manager.FALSE():
+        return None
+    else:
+        return Effect(new_fluent, new_value, new_condition, old_effect.kind)
+
+
+def create_action_with_given_subs(
+    problem: Problem,
+    old_action: Action,
+    simplifier,
+    subs: Dict[Expression, Expression],
+) -> Optional[Action]:
+    """This method is used to instantiate the actions parameters to a constant."""
+    naming_list: List[str] = []
+    for param, value in subs.items():
+        assert isinstance(param, Parameter)
+        assert isinstance(value, FNode)
+        naming_list.append(str(value))
+    c_subs = cast(Dict[Parameter, FNode], subs)
+    if isinstance(old_action, InstantaneousAction):
+        new_action = InstantaneousAction(
+            get_fresh_name(problem, old_action.name, naming_list)
+        )
+        for p in old_action.preconditions:
+            new_action.add_precondition(p.substitute(subs))
+        for e in old_action.effects:
+            new_effect = create_effect_with_given_subs(problem, e, simplifier, subs)
+            if new_effect is not None:
+                # We try to add the new effect, but a compiler might generate conflicting effects,
+                # so the action is just considered invalid
+                try:
+                    new_action._add_effect_instance(new_effect)
+                except UPConflictingEffectsException:
+                    return None
+        se = old_action.simulated_effect
+        if se is not None:
+            new_fluents = []
+            for f in se.fluents:
+                new_fluents.append(f.substitute(subs))
+
+            def fun(_problem, _state, _):
+                assert se is not None
+                return se.function(_problem, _state, c_subs)
+
+            # We try to add the new simulated effect, but a compiler might generate conflicting effects,
+            # so the action is just considered invalid
+            try:
+                new_action.set_simulated_effect(SimulatedEffect(new_fluents, fun))
+            except UPConflictingEffectsException:
+                return None
+        is_feasible, new_preconditions = check_and_simplify_preconditions(
+            problem, new_action, simplifier
+        )
+        if not is_feasible:
+            return None
+        new_action._set_preconditions(new_preconditions)
+        return new_action
+    elif isinstance(old_action, DurativeAction):
+        new_durative_action = DurativeAction(
+            get_fresh_name(problem, old_action.name, naming_list)
+        )
+        old_duration = old_action.duration
+        new_duration = DurationInterval(
+            old_duration.lower.substitute(subs),
+            old_duration.upper.substitute(subs),
+            old_duration.is_left_open(),
+            old_duration.is_right_open(),
+        )
+        new_durative_action.set_duration_constraint(new_duration)
+        for i, cl in old_action.conditions.items():
+            for c in cl:
+                new_durative_action.add_condition(i, c.substitute(subs))
+        for t, el in old_action.effects.items():
+            for e in el:
+                new_effect = create_effect_with_given_subs(problem, e, simplifier, subs)
+                if new_effect is not None:
+                    # We try to add the new simulated effect, but a compiler might generate conflicting effects,
+                    # so the action is just considered invalid
+                    try:
+                        new_durative_action._add_effect_instance(t, new_effect)
+                    except UPConflictingEffectsException:
+                        return None
+        for t, se in old_action.simulated_effects.items():
+            new_fluents = []
+            for f in se.fluents:
+                new_fluents.append(f.substitute(subs))
+
+            def fun(_problem, _state, _):
+                assert se is not None
+                return se.function(_problem, _state, c_subs)
+
+            # We try to add the new simulated effect, but a compiler might generate conflicting effects,
+            # so the action is just considered invalid
+            try:
+                new_durative_action.set_simulated_effect(
+                    t, SimulatedEffect(new_fluents, fun)
+                )
+            except UPConflictingEffectsException:
+                return None
+        is_feasible, new_conditions = check_and_simplify_conditions(
+            problem, new_durative_action, simplifier
+        )
+        if not is_feasible:
+            return None
+        new_durative_action.clear_conditions()
+        for interval, c in new_conditions:
+            new_durative_action.add_condition(interval, c)
+        return new_durative_action
+    else:
+        raise NotImplementedError
+
+
+def get_fresh_name(
+    problem: Problem, original_name: str, parameters_names: Iterable[str] = []
+) -> str:
+    """This method returns a fresh name for the problem, given a name and an iterable of names in input."""
+    name_list = [original_name]
+    name_list.extend(parameters_names)
+    new_name = "_".join(name_list)
+    base_name = new_name
+    count = 0
+    while problem.has_name(new_name):
+        new_name = f"{base_name}_{str(count)}"
+        count += 1
+    return new_name
+
+
+def lift_action_instance(
+    action_instance: ActionInstance,
+    map: Dict["up.model.Action", Tuple["up.model.Action", List["up.model.FNode"]]],
+) -> ActionInstance:
+    """ "map" is a map from every action in the "grounded_problem" to the tuple
+    (original_action, parameters).
+
+    Where the grounded action is obtained by grounding
+    the "original_action" with the specific "parameters"."""
+    lifted_action, parameters = map[action_instance.action]
+    return ActionInstance(lifted_action, tuple(parameters))
+
+
+def replace_action(
+    action_instance: ActionInstance,
+    map: Dict["up.model.Action", Optional["up.model.Action"]],
+) -> Optional[ActionInstance]:
+    try:
+        replaced_action = map[action_instance.action]
+    except KeyError:
+        raise UPUsageError(
+            "The Action of the given ActionInstance does not have a valid replacement."
+        )
+    if replaced_action is not None:
+        return ActionInstance(replaced_action, action_instance.actual_parameters)
+    else:
+        return None
---
+++ packages/unified-planning/unified_planning-0.5.0.309.dev1-py3-none-any.whl/unified_planning/engines/plan_validator.py
@@ -0,0 +1,138 @@
+# Copyright 2021 AIPlan4EU project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import unified_planning as up
+import unified_planning.environment
+import unified_planning.engines as engines
+import unified_planning.engines.mixins as mixins
+import unified_planning.model.walkers as walkers
+from unified_planning.model import (
+    AbstractProblem,
+    Problem,
+    ProblemKind,
+    COWState,
+    UPCOWState,
+)
+from unified_planning.engines.results import (
+    ValidationResult,
+    ValidationResultStatus,
+    LogMessage,
+    LogLevel,
+)
+from unified_planning.engines.sequential_simulator import (
+    SequentialSimulator,
+    InstantaneousEvent,
+)
+from unified_planning.plans import SequentialPlan, PlanKind
+
+
+class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
+    """
+    Performs :class:`~unified_planning.plans.Plan` validation.
+
+    If the given :class:`~unified_planning.model.Problem` has any quality metric,
+    the metric is simply ignored because it predicates over the Optimality of
+    the Plan, but not the Validity!
+    """
+
+    def __init__(self, **options):
+        engines.engine.Engine.__init__(self)
+        self._env: "unified_planning.environment.Environment" = (
+            unified_planning.environment.get_environment(
+                options.get("environment", None)
+            )
+        )
+        self.manager = self._env.expression_manager
+        self._substituter = walkers.Substituter(self._env)
+
+    @property
+    def name(self):
+        return "sequential_plan_validator"
+
+    @staticmethod
+    def supports_plan(plan_kind: "up.plans.PlanKind") -> bool:
+        return plan_kind == PlanKind.SEQUENTIAL_PLAN
+
+    @staticmethod
+    def supported_kind() -> ProblemKind:
+        supported_kind = ProblemKind()
+        supported_kind.set_problem_class("ACTION_BASED")
+        supported_kind.set_typing("FLAT_TYPING")
+        supported_kind.set_typing("HIERARCHICAL_TYPING")
+        supported_kind.set_numbers("CONTINUOUS_NUMBERS")
+        supported_kind.set_numbers("DISCRETE_NUMBERS")
+        supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
+        supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
+        supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("EQUALITY")
+        supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
+        supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
+        supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
+        supported_kind.set_effects_kind("INCREASE_EFFECTS")
+        supported_kind.set_effects_kind("DECREASE_EFFECTS")
+        supported_kind.set_fluents_type("NUMERIC_FLUENTS")
+        supported_kind.set_fluents_type("OBJECT_FLUENTS")
+        supported_kind.set_quality_metrics("ACTIONS_COST")
+        supported_kind.set_quality_metrics("PLAN_LENGTH")
+        supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
+        supported_kind.set_quality_metrics("MAKESPAN")
+        supported_kind.set_quality_metrics("FINAL_VALUE")
+        return supported_kind
+
+    @staticmethod
+    def supports(problem_kind):
+        return problem_kind <= SequentialPlanValidator.supported_kind()
+
+    def _validate(
+        self, problem: "AbstractProblem", plan: "unified_planning.plans.Plan"
+    ) -> "up.engines.results.ValidationResult":
+        """
+        Returns True if and only if the plan given in input is a valid plan for the problem given in input.
+        This means that from the initial state of the problem, by following the plan, you can reach the
+        problem goal. Otherwise False is returned.
+
+        :param problem: The problem for which the plan to validate was generated.
+        :param plan: The plan that must be validated.
+        :return: The generated up.engines.results.ValidationResult; a data structure containing the information
+            about the plan validity and eventually some additional log messages for the user.
+        """
+        assert isinstance(plan, SequentialPlan)
+        assert isinstance(problem, Problem)
+        simulator = SequentialSimulator(problem)
+        current_state: "COWState" = UPCOWState(problem.initial_values)
+        count = 0  # used for better error indexing
+        for ai in plan.actions:
+            action = ai.action
+            assert isinstance(action, unified_planning.model.InstantaneousAction)
+            count = count + 1
+            events = simulator.get_events(action, ai.actual_parameters)
+            assert len(events) < 2, f"{str(ai)} + {len(events)}"
+            for event in events:
+                if not simulator.is_applicable(event, current_state):
+                    error = f"Preconditions {event.conditions} of {str(count)}-th action instance {str(ai)} are not satisfied."
+                    logs = [LogMessage(LogLevel.ERROR, error)]
+                    return ValidationResult(
+                        ValidationResultStatus.INVALID, self.name, logs
+                    )
+                current_state = simulator.apply_unsafe(event, current_state)
+        unsatisfied_goals = simulator.get_unsatisfied_goals(current_state)
+        if len(unsatisfied_goals) == 0:
+            return ValidationResult(ValidationResultStatus.VALID, self.name, [])
+        else:
+            error = f"Goals {unsatisfied_goals} are not satisfied by the plan."
+            logs = [LogMessage(LogLevel.ERROR, error)]
+            return ValidationResult(ValidationResultStatus.INVALID, self.name, logs)
