2a41afe1d34be208a933e1d0b4d993b5ad62d330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:11 +0000

    Add package profile-photo/0.1.3/profile_photo-0.1.3-py2.py3-none-any.whl

---
+++ packages/profile-photo/profile_photo-0.1.3-py2.py3-none-any.whl/profile_photo/__version__.py
@@ -0,0 +1,12 @@
+"""
+Profile Photo - Center + Crop Image to create a Profile Pic or Headshot
+"""
+
+__title__ = 'profile-photo'
+__description__ = 'Center + Crop Image to create a Profile Pic or Headshot'
+__url__ = 'https://github.com/rnag/profile-photo'
+__version__ = '0.1.3'
+__author__ = 'Ritvik Nag'
+__author_email__ = 'rv.kvetch@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 Ritvik Nag'
