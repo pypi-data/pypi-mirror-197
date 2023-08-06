56abb686af2c220162e2f761cf2201c93391c378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:29 +0000

    Add package Ordenamiento/0.0.1/Ordenamiento-0.0.1.tar.gz

---
+++ packages/Ordenamiento/Ordenamiento-0.0.1.tar.gz/Ordenamiento-0.0.1/README.md
@@ -0,0 +1,15 @@
+# Librería de Métodos de Ordenamiento de Arreglos (MOA)
+## Creado por Abdiel López Villa
+
+
+## Métodos
+```convertir_arreglo(arreglo):```
+```burbuja_optimus(arreglo)```
+```shellSort(arreglo)```
+```quickSort(areglo, low, high)```
+```radixSort(arreglo):```
+
+## Licencia
+MIT
+
+**Software Gratis, ¡Genial! 😎**
---
+++ packages/Ordenamiento/Ordenamiento-0.0.1.tar.gz/Ordenamiento-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.1'
+PACKAGE_NAME = 'Ordenamiento'
+AUTHOR = 'Abdiel López Villa'
+AUTHOR_EMAIL = '212t0218@itsm.edu.mx'
+URL = 'https://github.com/AbdielLopezVilla'
+
+LICENSE = 'MIT'
+DESCRIPTION = ''
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding = 'utf-8')
+LONG_DESC_TYPE = "text/markdown"
+
+
+INSTALL_REQUIRES = [
+      'numpy'
+      ]
+
+setup(
+    name = PACKAGE_NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = LONG_DESC_TYPE,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    install_requires = INSTALL_REQUIRES,
+    license = LICENSE,
+    packages = find_packages(),
+    include_package_data = True
+)
