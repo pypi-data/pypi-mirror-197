887e9004c6e1f643ca863b09addd529b3505a2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:52 +0000

    Add package zyte-common-items/0.3.1/zyte-common-items-0.3.1.tar.gz

---
+++ packages/zyte-common-items/zyte-common-items-0.3.1.tar.gz/zyte-common-items-0.3.1/zyte_common_items/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+from .adapter import ZyteItemAdapter
+from .base import Item, is_data_container
+from .components import (
+    AdditionalProperty,
+    AggregateRating,
+    Brand,
+    Breadcrumb,
+    Gtin,
+    Image,
+    Link,
+    Metadata,
+)
+from .items import Product, ProductFromList, ProductList, ProductVariant
+from .pages import (
+    BasePage,
+    BaseProductListPage,
+    BaseProductPage,
+    Page,
+    ProductListPage,
+    ProductPage,
+)
