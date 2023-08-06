3720bf3c30dcddcd386aa53e2b9cc98abf984fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:40 +0000

    Add package qsonic/0.2.0/qsonic-0.2.0-py3-none-any.whl

---
+++ packages/qsonic/qsonic-0.2.0-py3-none-any.whl/qsonic/mathtools.py
@@ -0,0 +1,151 @@
+"""Mathematical utility objects and functions."""
+import numpy as np
+from numba import njit
+
+
+class Fast1DInterpolator():
+    """Fast interpolator class for equally spaced data. Out of domain points
+    are linearly extrapolated without producing any warnings or errors.
+
+    Example::
+
+        one_interp = Fast1DInterpolator(0., 1., np.ones(3))
+        one_interp(5) # = 1
+
+    Parameters
+    ----------
+    xp0: float
+        Initial x point for interpolation data.
+    dxp0: float
+        Spacing of x points.
+    fp: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Function calculated at interpolation points.
+    ep: :external+numpy:py:class:`ndarray <numpy.ndarray>`, optional
+        Error on fp points. Not used! Bookkeeping purposes only.
+    copy: bool, default: False
+        Copy input data, specifically fp.
+    """
+
+    def __init__(self, xp0, dxp, fp, copy=False, ep=None):
+        self.xp0 = float(xp0)
+        self.dxp = float(dxp)
+        if copy:
+            self.fp = fp.copy()
+        else:
+            self.fp = fp
+        self.ep = ep
+
+    def __call__(self, x):
+        return _fast_eval_interp1d(x, self.xp0, self.dxp, self.fp)
+
+
+@njit("f8[:](f8[:], f8, f8, f8[:])")
+def _fast_eval_interp1d(x, xp0, dxp, fp):
+    xx = (x - xp0) / dxp
+    idx = np.clip(xx, 0, fp.size - 1 - 1e-8).astype(np.int_)
+
+    d_idx = xx - idx
+    y1, y2 = fp[idx], fp[idx + 1]
+
+    return y1 * (1 - d_idx) + y2 * d_idx
+
+
+@njit("f8[:](f8[:], f8[:])")
+def mypoly1d(coef, x):
+    """ My simple power series polynomial calculator.
+
+    Arguments
+    ---------
+    coef: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Coefficient array in increasing power starting with the constant.
+    x: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Array to calculate polynomial.
+
+    Returns
+    ---------
+    results: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Polynomial calculated at x.
+    """
+    results = np.zeros_like(x)
+    for i, a in enumerate(coef):
+        results += a * x**i
+    return results
+
+
+def fft_gaussian_smooth(x, sigma_pix=20, pad_size=25, mode='edge'):
+    """ My Gaussian smoother using FFTs. Input array is padded with edge
+    values at the boundary by default.
+
+    Arguments
+    ---------
+    x: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        1D array to smooth.
+    sigma_pix: float, default: 20
+        Smoothing Gaussian sigma
+    pad_size: int, default: 25
+        Number of pixels to pad the array x at the boundary.
+    mode: str
+        Padding method. See :external+numpy:func:`numpy.pad` for options.
+
+    Returns
+    ---------
+    y: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Smoothed x values. Same size as x.
+    """
+    # Pad the input array to get rid of annoying edge effects
+    # Pad values are set to the edge value
+    arrsize = x.size + 2 * pad_size
+    padded_arr = np.pad(x, pad_size, mode=mode)
+
+    kvals = np.fft.rfftfreq(arrsize)
+    smerror_k = np.fft.rfft(padded_arr) * np.exp(-(kvals * sigma_pix)**2 / 2.)
+
+    y = np.fft.irfft(smerror_k, n=arrsize)[pad_size:-pad_size]
+
+    return y
+
+
+def get_smooth_ivar(ivar, sigma_pix=20, pad_size=25, esigma=3.5):
+    """ Smoothing ``ivar`` values to reduce signal-noise coupling.
+
+    Smoothing is done on ``error=1/sqrt(ivar)``, while replacing ``ivar=0`` and
+    outliers in ``error`` values with the median. These replaced values are put
+    back in in the final result.
+
+    Arguments
+    ---------
+    ivar: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Inverse variance array.
+    sigma_pix: float, default: 20
+        Smoothing Gaussian sigma.
+    pad_size: int, default: 25
+        Number of pixels to pad the array at the boundary.
+    esigma: float, default: 3.5
+        Sigma to identify outliers via MAD.
+
+    Returns
+    ---------
+    ivar2: :external+numpy:py:class:`ndarray <numpy.ndarray>`
+        Smoothed ivar values. Outliers and masked values are put back in.
+    """
+    error = np.empty_like(ivar)
+    w1 = ivar > 0
+    error[w1] = 1 / np.sqrt(ivar[w1])
+    median_err = np.median(error[w1])
+    error[~w1] = median_err
+
+    # Isolate high noise pixels
+    mad = np.median(np.abs(error[w1] - median_err)) * 1.4826
+    w2 = (error - median_err) > esigma * mad
+    err_org = error[w2].copy()
+
+    # Replace them with the median
+    error[w2] = median_err
+    error = fft_gaussian_smooth(error, sigma_pix, pad_size)
+
+    # Restore values of bad pixels
+    error[w2] = err_org
+    ivar2 = 1 / error**2
+    ivar2[~w1] = 0
+
+    return ivar2
