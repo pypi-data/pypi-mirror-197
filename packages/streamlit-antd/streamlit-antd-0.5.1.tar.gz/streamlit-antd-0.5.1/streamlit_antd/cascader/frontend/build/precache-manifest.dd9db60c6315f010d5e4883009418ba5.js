addbc4ddde377d930b00b5a554f0f706ac372eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:48 +0000

    Add package streamlit-antd/0.5.1/streamlit-antd-0.5.1.tar.gz

---
+++ packages/streamlit-antd/streamlit-antd-0.5.1.tar.gz/streamlit-antd-0.5.1/streamlit_antd/cascader/frontend/build/precache-manifest.dd9db60c6315f010d5e4883009418ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f122fd03e9d0e9f042286869eedaba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "594d59c83d2e17341ba2",
+    "url": "./static/css/2.57ba64fa.chunk.css"
+  },
+  {
+    "revision": "594d59c83d2e17341ba2",
+    "url": "./static/js/2.8ab5cdce.chunk.js"
+  },
+  {
+    "revision": "481ffa692589faecfc9e92cb6d7fba92",
+    "url": "./static/js/2.8ab5cdce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaef3c3834b5bd5231c",
+    "url": "./static/js/main.2118186e.chunk.js"
+  },
+  {
+    "revision": "37b79e6a04a52cb5bb77",
+    "url": "./static/js/runtime-main.1b03bd3a.js"
+  }
+]);
