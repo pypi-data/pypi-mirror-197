044f474f935bf3070212d5503a974ebb66406611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:25 +0000

    Add package hcai-nova-server/0.1.2/hcai_nova_server-0.1.2-py3-none-any.whl

---
+++ packages/hcai-nova-server/hcai_nova_server-0.1.2-py3-none-any.whl/nova_server/utils/import_utils.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+import subprocess
+
+def assert_or_install_dependencies(packages, trainer_name):
+    exec_path = Path(sys.executable)
+    site_package_path = exec_path / '..' / ".." / 'Lib' / 'nova-server-site-packages' / trainer_name
+    site_package_path.mkdir(parents=True, exist_ok=True)
+
+    for i,pkg in enumerate(packages):
+        params = []
+        pk = pkg.split(' ')
+        if len(pk) > 1:
+            params.extend(pk[1:])
+        name = pk[0].split('==')
+
+        params.append("--target={}".format(site_package_path))
+        adjusted_name = str(name[0]).replace('-', '_')
+        dirs = [1 if x.name.startswith(adjusted_name) else 0 for x in site_package_path.iterdir() if x.is_dir()]
+
+        if sum(dirs) > 0:
+            print(f'skip installation of {site_package_path}. Package already installed')
+            pass
+        else:
+            install_package(pk[0], params)
+
+    sys.path.insert(0, str(site_package_path.resolve()))
+
+def install_package(pkg, params):
+    call = [sys.executable, "-m", "pip", "install", pkg, *params]
+    print(call)
+    return subprocess.check_call(call)
