5152e882c4910c8acd890be83bcfe5fb00b8efd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:39 +0000

    Add package SciAssist/0.0.28/SciAssist-0.0.28.tar.gz

---
+++ packages/SciAssist/SciAssist-0.0.28.tar.gz/SciAssist-0.0.28/src/SciAssist/pipelines/__init__.py
@@ -0,0 +1,90 @@
+# main developer: Yixi Ding <dingyixi@hotmail.com>
+
+from typing import Dict
+
+import torch
+
+from SciAssist import BASE_CACHE_DIR
+from SciAssist.models.components.bart_summarization import BartForSummarization
+from SciAssist.models.components.bert_token_classifier import BertForTokenClassifier
+from SciAssist.models.components.flant5_summarization import FlanT5ForSummarization
+from SciAssist.utils.data_utils import (
+    DataUtilsForTokenClassification,
+    DataUtilsForSeq2Seq, DataUtilsForFlanT5, DataUtilsForT5,
+)
+
+# Provided models for each task
+TASKS = {
+    "reference-string-parsing": {
+        "scibert-on-cora":  {
+            "model": BertForTokenClassifier,
+            "model_dict_url": "https://huggingface.co/spaces/wing-nus/SciAssist/resolve/main/scibert-uncased.pt",
+            "data_utils": DataUtilsForTokenClassification
+        },
+        "default": {
+            "model": BertForTokenClassifier,
+            "model_dict_url": "https://huggingface.co/spaces/wing-nus/SciAssist/resolve/main/scibert-uncased.pt",
+            "data_utils": DataUtilsForTokenClassification,
+        },
+    },
+
+    "single-doc-summarization": {
+        "bart-cnn-on-mup": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": "https://huggingface.co/spaces/dyxohjl666/Controlled-summarization/resolve/main/flant5-mup-scisumm.pt",
+            "data_utils": DataUtilsForSeq2Seq,
+        },
+        "default": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": "https://huggingface.co/spaces/dyxohjl666/Controlled-summarization/resolve/main/flant5-mup-scisumm.pt",
+            "data_utils": DataUtilsForSeq2Seq,
+        },
+        "t5": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": None,
+            "data_utils": DataUtilsForT5,
+        },
+        "flan-t5": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": None,
+            "data_utils": DataUtilsForFlanT5,
+        }
+    },
+
+    "controlled-summarization": {
+        "default": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": "https://huggingface.co/spaces/dyxohjl666/Controlled-summarization/resolve/main/flant5-mup-scisumm.pt",
+            # "model_dict_url": None,
+            "data_utils": DataUtilsForFlanT5,
+        }
+    }
+
+}
+
+def load_model(config: Dict, cache_dir=BASE_CACHE_DIR, device="gpu"):
+    '''
+
+    Args:
+        config (Dict):
+            A dictionary including the model class and the url to download the weights dict.
+            For example:
+                {
+                    "model": BertForTokenClassifier,
+                    "model_dict_url": "https://huggingface.co/spaces/wing-nus/SciAssist/resolve/main/scibert-uncased.pt",
+                }
+
+    Returns: A loaded model.
+
+    '''
+
+    print("Loading the model...")
+    model_class = config["model"]
+    model = model_class(cache_dir=cache_dir)
+    map_location = None if torch.cuda.is_available() and device in ["gpu","cuda"] else torch.device("cpu")
+    if config["model_dict_url"]!=None:
+        state_dict = torch.hub.load_state_dict_from_url(config["model_dict_url"], model_dir=cache_dir, map_location=map_location)
+        model.load_state_dict(state_dict)
+    model.eval()
+
+    return model
---
+++ packages/SciAssist/SciAssist-0.0.28.tar.gz/SciAssist-0.0.28/src/SciAssist/utils/xml2txt.py
@@ -0,0 +1,43 @@
+import xml.etree.ElementTree as ET
+import os
+file_list = []
+
+root_dir = "/home/dingyx/project/SciAssist/data/Task2/From-ScisummNet-2019/"
+for dirpath,dirnames,files in os.walk(root_dir):
+    file_list = dirnames
+    break
+
+
+def parse_rec(filename):
+    file = os.path.join(root_dir,filename,"Reference_XML",filename+".xml")
+    tree = ET.parse(file)  # 解析读取xml函数
+
+
+    # Element.findall()查找当前元素的直接子元素中带有指定标签的元素
+    # Element.find()找带有特定标签的第一个子级
+    # Elemtn.text 访问元素的文本内容
+    # Element.get 访问元素的属性。
+    body_text = ""
+    if len(tree.findall('SECTION'))>0:
+        for section in tree.findall('SECTION'):
+            if "Introduction" in section.get("title") or "Conclusion" in section.get("title"):
+                for s in section.findall("S"):
+                    body_text += s.text
+                    body_text += " "
+    else:
+        for s in tree.findall("S"):
+            if s.text is not None:
+                body_text += s.text
+                body_text += " "
+
+    with open(os.path.join(root_dir,filename,filename+".txt"),"w") as f:
+        f.write(body_text)
+
+    return body_text
+
+for file in file_list:
+    s = parse_rec(file)
+
+print("Finished.")
+
+
