74fb12c7df61aa67e4c59f64ce132f1a2373ec39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:50 +0000

    Add package simplegcd/1.1.0/simplegcd-1.1.0.tar.gz

---
+++ packages/simplegcd/simplegcd-1.1.0.tar.gz/simplegcd-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.0'
+DESCRIPTION = 'A package for simple deployment of Google cloud instance groups'
+
+# Setting up
+setup(
+    name="simplegcd",
+    version=VERSION,
+    author="Cabe Towers",
+    author_email="<cabe.towers@gmail.com>",
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['google-cloud-compute'],
+    keywords=['python', 'google-cloud', 'google-cloud-compute'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
