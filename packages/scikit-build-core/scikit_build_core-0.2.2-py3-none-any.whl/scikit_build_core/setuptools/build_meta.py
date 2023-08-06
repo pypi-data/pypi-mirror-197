fe7d643fa68dd5bba64dbe550c470eeb91eca404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:26 +0000

    Add package scikit-build-core/0.2.2/scikit_build_core-0.2.2-py3-none-any.whl

---
+++ packages/scikit-build-core/scikit_build_core-0.2.2-py3-none-any.whl/scikit_build_core/setuptools/build_meta.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from ..builder.get_requires import cmake_ninja_for_build_wheel
+
+__all__ = [
+    "prepare_metadata_for_build_wheel",
+    "build_wheel",
+    "build_sdist",
+    "get_requires_for_build_sdist",
+    "get_requires_for_build_wheel",
+]
+
+
+def __dir__() -> list[str]:
+    return __all__
+
+
+def get_requires_for_build_sdist(
+    config_settings: dict[str, str | list[str]] | None = None  # noqa: ARG001
+) -> list[str]:
+    return ["setuptools"]
+
+
+def get_requires_for_build_wheel(
+    config_settings: dict[str, str | list[str]] | None = None
+) -> list[str]:
+    return ["setuptools", "wheel", *cmake_ninja_for_build_wheel(config_settings)]
+
+
+def build_sdist(
+    sdist_directory: str,
+    config_settings: dict[str, str | list[str]] | None = None,
+) -> str:
+    import setuptools.build_meta
+
+    return setuptools.build_meta.build_sdist(  # type: ignore[no-any-return]
+        sdist_directory, config_settings
+    )
+
+
+def build_wheel(
+    wheel_directory: str,
+    config_settings: dict[str, str | list[str]] | None = None,
+    metadata_directory: str | None = None,
+) -> str:
+    import setuptools.build_meta
+
+    return setuptools.build_meta.build_wheel(  # type: ignore[no-any-return]
+        wheel_directory, config_settings, metadata_directory
+    )
+
+
+def prepare_metadata_for_build_wheel(
+    metadata_directory: str,
+    config_settings: dict[str, str | list[str]] | None = None,
+) -> str:
+    import setuptools.build_meta
+
+    return setuptools.build_meta.prepare_metadata_for_build_wheel(  # type: ignore[no-any-return]
+        metadata_directory, config_settings
+    )
