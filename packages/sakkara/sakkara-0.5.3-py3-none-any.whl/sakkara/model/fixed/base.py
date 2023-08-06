f17a27450b9d022ee7f3eb4a28391df85621d70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:35 +0000

    Add package sakkara/0.5.3/sakkara-0.5.3-py3-none-any.whl

---
+++ packages/sakkara/sakkara-0.5.3-py3-none-any.whl/sakkara/model/fixed/base.py
@@ -0,0 +1,57 @@
+from abc import ABC
+from copy import deepcopy
+from typing import Any, Union, Tuple, Optional, Set
+
+import numpy as np
+
+from sakkara.model.math_op import MathOpBase
+from sakkara.relation.groupset import GroupSet
+
+from sakkara.relation.representation import UnrepeatableRepresentation
+
+
+class FixedValueComponent(MathOpBase, ABC):
+    """
+    Class for non-random fixed values that appear in model that are assigned to a group.
+
+    :param value: Value to wrap into the component.
+    :param name: Name of the corresponding variable to register in PyMC.
+    :param group: Group of which the component is defined for.
+
+    """
+
+    def __init__(self, value: Any, group: Union[str, Tuple[str, ...]], name: Optional[str] = None):
+        super().__init__()
+        self.group = (group,) if isinstance(group, str) else group
+        self.name = name
+        self.values = value
+
+    def set_name(self, name: str) -> None:
+        self.name = name
+
+    def get_name(self) -> Optional[str]:
+        return self.name if self.name is not None else 'fixed'
+
+    def clear(self):
+        self.variable = None
+        self.representation = None
+
+    def prebuild(self, groupset: GroupSet) -> None:
+        pass
+
+    def build_variable(self) -> None:
+        self.variable = deepcopy(self.values)
+
+    def retrieve_groups(self) -> Set[str]:
+        return set(self.group)
+
+
+class UnrepeatableComponent(FixedValueComponent, ABC):
+    """
+    Class for components that are fixed and cannot be repeated
+    """
+    def __init__(self, value: Any):
+        super().__init__(value, 'global')
+
+    def build_representation(self, groupset: GroupSet) -> None:
+        self.representation = UnrepeatableRepresentation()
