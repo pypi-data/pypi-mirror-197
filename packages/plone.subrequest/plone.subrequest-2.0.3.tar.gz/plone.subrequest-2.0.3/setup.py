6ddbb8267af5659adfd632626daebb87ad729ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:52 +0000

    Add package plone.subrequest/2.0.3/plone.subrequest-2.0.3.tar.gz

---
+++ packages/plone.subrequest/plone.subrequest-2.0.3.tar.gz/plone.subrequest-2.0.3/setup.py
@@ -0,0 +1,70 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import os.path
+
+
+version = "2.0.3"
+
+setup(
+    name="plone.subrequest",
+    version=version,
+    description="Subrequests for Zope2",
+    long_description=(
+        open("README.rst").read()
+        + "\n\n"
+        + open(os.path.join("plone", "subrequest", "usage.rst")).read()
+        + "\n\n"
+        + open("CHANGES.rst").read()
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope",
+        "Framework :: Zope :: 5",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plone",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.subrequest",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone"],
+    include_package_data=True,
+    platforms="Any",
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "AccessControl",
+        "Acquisition",
+        "plone.protect",
+        "setuptools",
+        "zope.component",
+        "zope.globalrequest",
+        "zope.interface",
+        "zope.publisher",
+    ],
+    extras_require={
+        "test": [
+            "five.localsitemanager",
+            "manuel",
+            "plone.testing[zope]",
+            "zope.configuration",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
