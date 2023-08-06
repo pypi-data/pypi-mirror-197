ce739c57aaccab3be210fe60c42b865ec68afe34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:28 +0000

    Add package aligned/0.0.2/aligned-0.0.2.tar.gz

---
+++ packages/aligned/aligned-0.0.2.tar.gz/aligned-0.0.2/aligned/redis/config.py
@@ -0,0 +1,193 @@
+import logging
+from dataclasses import dataclass, field
+
+import polars as pl
+
+try:
+    from redis.asyncio import ConnectionPool, Redis, StrictRedis  # type: ignore
+except ModuleNotFoundError:
+
+    class Redis:  # type: ignore
+        pass
+
+    StrictRedis = Redis
+    ConnectionPool = Redis
+
+from aligned.data_source.stream_data_source import SinkableDataSource, StreamDataSource
+from aligned.feature_source import FeatureSource, WritableFeatureSource
+from aligned.online_source import OnlineSource
+from aligned.request.retrival_request import FeatureRequest, RetrivalRequest
+from aligned.retrival_job import RetrivalJob
+from aligned.schemas.codable import Codable
+from aligned.schemas.feature import Feature, FeatureType
+from aligned.schemas.feature_view import CompiledFeatureView
+
+logger = logging.getLogger(__name__)
+
+
+redis_manager: dict[str, ConnectionPool] = {}
+
+
+@dataclass
+class RedisConfig(Codable):
+
+    env_var: str
+
+    @property
+    def url(self) -> str:
+        import os
+
+        return os.environ[self.env_var]
+
+    @staticmethod
+    def from_url(url: str) -> 'RedisConfig':
+        import os
+
+        os.environ['REDIS_URL'] = url
+        return RedisConfig(env_var='REDIS_URL')
+
+    @staticmethod
+    def localhost() -> 'RedisConfig':
+        import os
+
+        if 'REDIS_URL' not in os.environ:
+            os.environ['REDIS_URL'] = 'redis://localhost:6379'
+
+        return RedisConfig(env_var='REDIS_URL')
+
+    def redis(self) -> Redis:
+        if self.env_var not in redis_manager:
+            redis_manager[self.env_var] = ConnectionPool.from_url(self.url, decode_responses=True)
+
+        return StrictRedis(connection_pool=redis_manager[self.env_var])
+
+    def online_source(self) -> 'RedisOnlineSource':
+        return RedisOnlineSource(config=self)
+
+    def stream(self, topic: str) -> 'RedisStreamSource':
+        return RedisStreamSource(topic_name=topic, config=self)
+
+
+@dataclass
+class RedisOnlineSource(OnlineSource):
+
+    config: RedisConfig
+    source_type = 'redis'
+
+    def feature_source(self, feature_views: set[CompiledFeatureView]) -> 'RedisSource':
+        return RedisSource(self.config)
+
+
+@dataclass
+class RedisSource(FeatureSource, WritableFeatureSource):
+
+    config: RedisConfig
+    batch_size = 1_000_000
+
+    def all_for(self, request: FeatureRequest, limit: int | None = None) -> RetrivalJob:
+        raise NotImplementedError()
+
+    def features_for(self, facts: RetrivalJob, request: FeatureRequest) -> RetrivalJob:
+        from aligned.redis.job import FactualRedisJob
+
+        return FactualRedisJob(self.config, request.needed_requests, facts)
+
+    async def write(self, job: RetrivalJob, requests: list[RetrivalRequest]) -> None:
+
+        redis = self.config.redis()
+        data = await job.to_polars()
+
+        async with redis.pipeline(transaction=True) as pipe:
+
+            for request in requests:
+                # Run one query per row
+                data = data.with_column(
+                    (
+                        pl.lit(request.location.identifier)
+                        + pl.lit(':')
+                        + pl.concat_str(sorted(request.entity_names))
+                    ).alias('id')
+                )
+
+                features = ['id']
+
+                for feature in request.all_features:
+
+                    expr = pl.col(feature.name).cast(pl.Utf8).alias(feature.name)
+
+                    if feature.dtype == FeatureType('').bool:
+                        # Redis do not support bools
+                        expr = (
+                            pl.col(feature.name).cast(pl.Int8, strict=False).cast(pl.Utf8).alias(feature.name)
+                        )
+                    elif feature.dtype == FeatureType('').datetime:
+                        expr = pl.col(feature.name).dt.timestamp('ms').cast(pl.Utf8).alias(feature.name)
+                    elif feature.dtype == FeatureType('').embedding or feature.dtype == FeatureType('').array:
+                        import json
+
+                        expr = pl.col(feature.name).apply(lambda x: json.dumps(x.to_list()))
+
+                    data = data.with_column(expr)
+                    features.append(feature.name)
+
+                redis_frame = data.select(features).collect()
+
+                for record in redis_frame.to_dicts():
+                    pipe.hset(record['id'], mapping={key: value for key, value in record.items() if value})
+                    for key in record.items():
+                        if record[key] is None:
+                            pipe.hdel(record['id'], key)
+                await pipe.execute()
+
+
+@dataclass
+class RedisStreamSource(StreamDataSource, SinkableDataSource):
+
+    topic_name: str
+    config: RedisConfig
+
+    mappings: dict[str, str] = field(default_factory=dict)
+
+    name: str = 'redis'
+
+    def map_values(self, mappings: dict[str, str]) -> 'RedisStreamSource':
+        return RedisStreamSource(
+            topic_name=self.topic_name, config=self.config, mappings=self.mappings | mappings
+        )
+
+    def make_redis_friendly(self, data: pl.LazyFrame, features: set[Feature]) -> pl.LazyFrame:
+        # Run one query per row
+        for feature in features:
+
+            expr = pl.col(feature.name)
+
+            if feature.dtype == FeatureType('').bool:
+                # Redis do not support bools
+                expr = pl.col(feature.name).cast(pl.Int8, strict=False)
+            elif feature.dtype == FeatureType('').datetime:
+                expr = pl.col(feature.name).dt.timestamp('ms')
+            elif feature.dtype == FeatureType('').embedding or feature.dtype == FeatureType('').array:
+                import json
+
+                expr = pl.col(feature.name).apply(lambda x: json.dumps(x.to_list()))
+
+            data = data.with_column(expr.cast(pl.Utf8).alias(feature.name))
+
+        return data
+
+    async def write_to_stream(self, job: RetrivalJob) -> None:
+        redis = self.config.redis()
+        df = await job.to_polars()
+
+        df = self.make_redis_friendly(df, job.request_result.features.union(job.request_result.entities))
+        values = df.collect()
+
+        async with redis.pipeline(transaction=True) as pipe:
+            _ = [
+                pipe.xadd(
+                    self.topic_name,
+                    {key: value for key, value in record.items() if value},
+                )
+                for record in values.to_dicts()
+            ]
+            await pipe.execute()
