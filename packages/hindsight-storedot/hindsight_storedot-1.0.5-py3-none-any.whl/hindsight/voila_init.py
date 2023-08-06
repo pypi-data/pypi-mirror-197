55c00863f0009522a39e30ddb024b7c9ef2af268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:30 +0000

    Add package hindsight-storedot/1.0.5/hindsight_storedot-1.0.5-py3-none-any.whl

---
+++ packages/hindsight-storedot/hindsight_storedot-1.0.5-py3-none-any.whl/hindsight/voila_init.py
@@ -0,0 +1,35 @@
+from IPython import get_ipython
+from IPython.display import display
+from ipywidgets import Output
+
+from hindsight.utils.voila import QueryParams, IS_RUNNING_VOILA, LoadingSpinner
+
+def set_query_params(global_ref):
+    ipython = get_ipython()
+    ipython.events.register('post_execute', QueryParams(global_ref).set_variables) #post_run_cell
+
+
+def add_loading_indicator():
+    if not IS_RUNNING_VOILA():
+        return
+    
+    __header = Output()
+    __header.layout.margin = '0px 0px 0px 0px'
+    __header.layout.padding = '0px 0px 0px 0px'
+    
+    loading_container = LoadingSpinner.get_loading_indicator()
+    
+    with __header:
+        display(loading_container)
+    
+    def display_loading():
+        __header.layout.visibility = 'visible'
+
+    def hide_loading():
+        __header.layout.visibility = 'hidden'
+
+    display(__header)
+    
+    ipython = get_ipython()
+    ipython.events.register('pre_execute', display_loading)
+    ipython.events.register('post_execute', hide_loading)
