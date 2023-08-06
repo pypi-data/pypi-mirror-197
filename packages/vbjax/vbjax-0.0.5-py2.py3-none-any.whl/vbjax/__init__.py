567b878d8fd556d992764ce4ff21cb145574a3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:17 +0000

    Add package vbjax/0.0.5/vbjax-0.0.5-py2.py3-none-any.whl

---
+++ packages/vbjax/vbjax-0.0.5-py2.py3-none-any.whl/vbjax/__init__.py
@@ -0,0 +1,9 @@
+from .loops import make_sde, make_ode, make_dde, make_sdde
+from .shtlc import make_shtdiff
+from .neural_mass import JRState, JRTheta, jr_dfun, jr_default_theta
+from .regmap import make_region_mapping
+from .coupling import make_diff_cfun, make_linear_cfun
+from .connectome import make_conn_latent_mvnorm
+from .sparse import make_spmv
+from .layers import make_dense_layers
+from ._version import __version__
