b3ec1f1f10e640c030d4f3fd048da3b12b36a9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:20 +0000

    Add package paramaterial/0.0.10/paramaterial-0.0.10-py3-none-any.whl

---
+++ packages/paramaterial/paramaterial-0.0.10-py3-none-any.whl/paramaterial/preparing.py
@@ -0,0 +1,129 @@
+"""Functions to be used for preparing the experimental data for batch processing."""
+
+import os
+import shutil
+from pathlib import Path
+from typing import List, Dict, Union
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from paramaterial.plug import DataSet
+
+
+def copy_data_and_rename_by_test_id(data_in: str, data_out: str, info_table: pd.DataFrame, test_id_col='test_id'):
+    """Rename files in data directory by test_id in info table."""
+    # make data directory if it doesn't exist
+    if not os.path.exists(data_out):
+        os.mkdir(data_out)
+
+    # check info table
+    if 'old_filename' not in info_table.columns:
+        raise ValueError(f'There is no "old_filename" column in the info table.')
+    if test_id_col not in info_table.columns:
+        raise ValueError(f'There is no "{test_id_col}" column in the info table.')
+    if info_table[test_id_col].duplicated().any():
+        raise ValueError(f'There are duplicate test_ids.')
+    if info_table['old_filename'].duplicated().any():
+        raise ValueError(f'There are duplicate old_filenames.')
+
+    for filename, test_id in zip(info_table['old_filename'], info_table[test_id_col]):
+        # check that file exists
+        if not os.path.exists(f'{data_in}/{filename}'):
+            raise FileNotFoundError(f'File {filename} does not exist in {data_in}.')
+        # copy and rename file
+        shutil.copy(f'{data_in}/{filename}', f'{data_out}/{test_id}.csv')
+
+    print(f'Copied {len(info_table)} files in {data_in} to {data_out}.')
+
+
+def check_data_formatting(ds: DataSet):
+    pass
+
+
+def check_column_headers(data_dir: str, exception_headers: List[str] = None):
+    """Check that all files in a directory have the same column headers and that column headers don't contain spaces."""
+    file_list = os.listdir(data_dir)
+    first_file = pd.read_csv(f'{data_dir}/{file_list[0]}')
+    first_columns = first_file.columns
+    if exception_headers is not None:
+        for exception_header in exception_headers:
+            if exception_header in first_columns:
+                first_columns = first_columns.drop(exception_header)
+    print("Checking column headers...")
+    for column_header in first_file.columns:
+        if len(column_header.split(' ')) > 1:
+            raise ValueError(f'Column header "{column_header}" contains a space.')
+    print(f'First file headers:\n\t{list(first_file.columns)}')
+    for file in file_list[1:]:
+        df = pd.read_csv(f'{data_dir}/{file}')
+        df_columns = df.columns
+        if exception_headers is not None:
+            for exception_header in exception_headers:
+                if exception_header in df_columns:
+                    df_columns = df_columns.drop(exception_header)
+        if not df_columns.equals(first_columns):
+            raise ValueError(f'Column headers in {file} don\'t match column headers of first file.'
+                             f'{file} headers:\n\t{list(df.columns)}')
+    print(f'Headers in all files are the same as in the first file, except for {exception_headers}.')
+
+
+def check_for_duplicate_files(data_dir: str):
+    """Check that there are no duplicate files in the data directory by hashing the contents."""
+    print('Checking for duplicate files...')
+    hashes = [hash(open(f'{data_dir}/{file}', 'rb').read()) for file in os.listdir(data_dir)]
+    if len(hashes) != len(set(hashes)):
+        duplicates = [file for file, filehash in zip(os.listdir(data_dir), hashes) if hashes.count(filehash) > 1]
+        raise ValueError(f'There are duplicate files in {data_dir}.\n'
+                         'The duplicates are:' + '\n\t'.join(duplicates))
+    else:
+        print(f'No duplicate files found in "{data_dir}".')
+
+
+def make_experimental_matrix(info_table: pd.DataFrame, index: Union[str, List[str]], columns: Union[str, List[str]],
+                             as_heatmap: bool = False, title: str = None, xlabel: str = None,
+                             ylabel: str = None, tick_params: Dict = None, **kwargs) -> pd.DataFrame|plt.Axes:
+    if isinstance(index, str):
+        index = [index]
+    if isinstance(columns, str):
+        columns = [columns]
+    exp_matrix = info_table.groupby(index + columns).size().unstack(columns).fillna(0).astype(int)
+    if not as_heatmap:
+        return exp_matrix
+    else:
+        ax = sns.heatmap(exp_matrix, **kwargs)
+        if title:
+            ax.set_title(title)
+        if xlabel:
+            ax.set_xlabel(xlabel)
+        if ylabel:
+            ax.set_ylabel(ylabel)
+        if tick_params:
+            ax.tick_params(**tick_params)
+        return ax
+
+
+def convert_files_in_directory_to_csv(directory_path: str):
+    for file in os.listdir(directory_path):
+        if not file.endswith('.csv'):
+            df = pd.read_csv(f'{directory_path}/{file}', header=[0, 1], delimiter='\t')
+            df.columns = \
+                [col[0] if str(col[1]).startswith('Unnamed') else ' '.join(col).strip() for col in df.columns]
+            df.to_csv(f'{directory_path}/{file[:-4]}.csv', index=False)
+
+
+def extract_info(in_dir, info_path):
+    info_df = pd.DataFrame(columns=['filename', 'test type', 'temperature', 'material'])
+    for filename in os.listdir(in_dir):
+        info_row = pd.Series()
+        info_row['filename'] = filename
+        name_list = filename.split('_')
+        if name_list[0] == 'P':
+            info_row['test type'] = 'PST'
+        else:
+            info_row['test type'] = 'UT'
+        info_row['temperature'] = float(name_list[1])
+        info_row['material'] = 'AA6061-T651_' + name_list[2]
+        info_df = info_df.append(info_row, ignore_index=True)
+    info_df.to_excel(info_path, index=False)
---
+++ packages/paramaterial/paramaterial-0.0.10-py3-none-any.whl/paramaterial/screening.py
@@ -0,0 +1,145 @@
+"""Module with functions for generating and reading a screening pdf file."""
+
+import os
+from io import BytesIO
+from typing import Callable
+from typing import Tuple
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from PyPDF2 import PdfReader
+from reportlab.graphics import renderPDF
+from reportlab.lib.colors import black
+from reportlab.lib.colors import magenta, pink, blue
+from reportlab.pdfgen import canvas
+from svglib.svglib import svg2rlg
+
+from paramaterial.plug import DataSet, DataItem
+
+
+def make_screening_pdf(
+        ds: DataSet,
+        plot_func: Callable[[DataItem], None],
+        pdf_path: str = 'screening_pdf.pdf',
+        pagesize: Tuple[float, float] = (900, 600),
+) -> None:
+    """Make a screening pdf where each page contains a plot, a check box and a commentbox.
+
+    Args:
+        ds: DataSet object
+        plot_func: Function that takes a DataItem and generates a plot
+        pdf_path: Path where pdf will be saved
+        pagesize: Size of the pdf pages
+
+    Returns: None
+    """
+    # setup canvas
+    pdf_canvas = canvas.Canvas(pdf_path, pagesize=(pagesize[0], pagesize[1]))
+
+    # loop through dataitems
+    for di in ds:
+        # make plot for dataitem
+        plot_func(di)
+
+        # add plot to page
+        imgdata = BytesIO()
+        plt.savefig(imgdata, format='svg')
+        imgdata.seek(0)
+        drawing = svg2rlg(imgdata)
+        renderPDF.draw(drawing, pdf_canvas, 0.001*pagesize[0], 0.15*pagesize[1])
+
+        # setup form
+        form = pdf_canvas.acroForm
+        pdf_canvas.setFont("Courier", plt.rcParams['font.size'] + 6)
+
+        # add test_id
+        pdf_canvas.drawString(0.05*pagesize[0], 0.95*pagesize[1], f'{di.test_id}')
+
+        # add checkbox
+        pdf_canvas.drawString(0.05*pagesize[0], 0.14*pagesize[1], 'REJECT?:')
+        form.checkbox(name=f'reject_box_{di.test_id}', buttonStyle='check',
+                      x=0.15*pagesize[0], y=0.13*pagesize[1],
+                      borderColor=magenta, fillColor=pink, textColor=blue, forceBorder=True)
+
+        # add text field
+        pdf_canvas.drawString(0.05*pagesize[0], 0.08*pagesize[1], 'COMMENT:')
+        form.textfield(name=f'comment_box_{di.test_id}', maxlen=10000,
+                       x=0.15*pagesize[0], y=0.05*pagesize[1], width=0.7*pagesize[0], height=0.05*pagesize[1],
+                       borderColor=magenta, fillColor=pink, textColor=black, forceBorder=True, fieldFlags='multiline')
+
+        # add page to canvas and close plot
+        # make name of page the test_id
+        pdf_canvas.showPage()
+        plt.close()
+
+    pdf_canvas.save()
+    print(f'Screening pdf saved to {pdf_path}.')
+
+
+def read_screening_pdf_fields(ds: DataSet, screening_pdf_path: str) -> DataSet:
+    """Screen data using marked pdf file.
+
+    Args:
+        ds: DataSet object
+        screening_pdf_path: Path to screening pdf file
+
+    Returns: DataSet object with checkbox and comment fields added to each DataItem's info
+    """
+    new_ds = ds.copy()
+    _info_table = new_ds.copy().info_table
+    test_id_key = ds.test_id_key
+
+    # drop reject and comment cols if they exist
+    if 'reject' in _info_table.columns:
+        _info_table.drop(columns=['reject'], inplace=True)
+    if 'comment' in _info_table.columns:
+        _info_table.drop(columns=['comment'], inplace=True)
+
+    # dataframe for screening results
+    screening_df = pd.DataFrame(columns=[test_id_key, 'reject', 'comment'])
+
+    with open(screening_pdf_path, 'rb') as f:
+        pdf_fields = PdfReader(f).get_fields()
+
+    # get comment and reject fields
+    comment_fields = [field for field in pdf_fields if 'comment' in field]
+    reject_fields = [field for field in pdf_fields if 'reject' in field]
+
+    # get test_ids from comment fields
+    test_ids = [field.split('_box_')[1] for field in comment_fields]
+
+    # get comments and rejects
+    comments = [pdf_fields[field]['/V'] for field in comment_fields]
+    rejects = [pdf_fields[field]['/V'] for field in reject_fields]
+
+    # add to dataframe
+    screening_df[test_id_key] = test_ids
+    screening_df['reject'] = rejects
+    screening_df['comment'] = comments
+
+    _info_table = _info_table.merge(screening_df, on=test_id_key, how='left')
+
+    new_ds.info_table = _info_table
+
+    return new_ds
+
+
+def remove_rejected_items(ds: DataSet, screening_pdf_path: str) -> DataSet:
+    """Reject data items that were marked as reject in the screening pdf."""
+    new_ds = ds.copy()
+    screened_ds = read_screening_pdf_fields(new_ds, screening_pdf_path)
+    screened_ds.info_table = screened_ds.info_table[screened_ds.info_table['reject'] != '/Yes']
+    # print a list of the rejected items with a detailed message
+    rejected_items = screened_ds.info_table[screened_ds.info_table['reject'] == '/Yes']
+    for i, row in rejected_items.iterrows():
+        print(f'Item {row[screened_ds.test_id_key]} was rejected because {row["comment"]}')
+    return screened_ds
+
+
+if __name__ == '__main__':
+    ds = DataSet(r'C:\Users\DS\msc-case-studies\baron study\info\01 raw info.xlsx',
+                 r'C:\Users\DS\msc-case-studies\baron study\data\01 raw data')
+    ds2 = read_screening_pdf_fields(
+        ds,
+        r'C:\Users\DS\msc-case-studies\baron study\info\04 trimmed data screening marked 7Dec22.pdf')
+    print(ds2.info_table.reject.unique())
