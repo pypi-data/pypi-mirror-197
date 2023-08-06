a018e58715de215d2bec87dac98b5c5df4459a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:26 +0000

    Add package psdm-analysis/0.1.2.dev3/psdm_analysis-0.1.2.dev3-py3-none-any.whl

---
+++ packages/psdm-analysis/psdm_analysis-0.1.2.dev3-py3-none-any.whl/psdm_analysis/models/input/participant/wec.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import List
+
+from psdm_analysis.models.input.container.mixins import HasTypeMixin, SpTypeMixin
+from psdm_analysis.models.input.enums import SystemParticipantsEnum
+from psdm_analysis.models.input.participant.participant import SystemParticipants
+
+
+@dataclass(frozen=True)
+class WindEnergyConverters(SpTypeMixin, SystemParticipants):
+    @staticmethod
+    def get_enum() -> SystemParticipantsEnum:
+        return SystemParticipantsEnum.WIND_ENERGY_CONVERTER
+
+    @property
+    def market_reaction(self):
+        return self.data["market_reaction"]
+
+    @property
+    def eta_conv(self):
+        return self.data["eta_conv"]
+
+    @property
+    def rotor_area(self):
+        return self.data["rotor_area"]
+
+    @property
+    def hub_height(self):
+        return self.data["hub_height"]
+
+    @staticmethod
+    def entity_attributes() -> [str]:
+        return ["market_reaction"]
+
+    @staticmethod
+    def type_attributes() -> [str]:
+        return SpTypeMixin.type_attributes() + [
+            "cp_characteristic",
+            "eta_conv",
+            "rotor_area",
+            "hub_height",
+        ]
