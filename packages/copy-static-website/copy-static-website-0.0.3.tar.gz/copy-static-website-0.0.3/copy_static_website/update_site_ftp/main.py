437f6a3f5795beb083fa3da031dff2109525fd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:34 +0000

    Add package copy-static-website/0.0.3/copy-static-website-0.0.3.tar.gz

---
+++ packages/copy-static-website/copy-static-website-0.0.3.tar.gz/copy-static-website-0.0.3/copy_static_website/update_site_ftp/main.py
@@ -0,0 +1,20 @@
+import os
+from ..download import download_full_site
+from ..deploy.ftp import deploy_to_ftp
+
+def update_site_ftp(project_root_folder: str, site_url: str, ftp_host: str, ftp_user: str, ftp_pwd: str, site_relative_path: str = None, force_download: bool = False, google_analytics_id: str = None, save_html_as: str = 'index.html', links_to_force_open_in_current_tab: list[list] = []):
+    download_full_site(
+        url=site_url,
+        project_root_folder=project_root_folder,
+        site_relative_path=site_relative_path,
+        force_download=force_download,
+        google_analytics_id=google_analytics_id,
+        save_html_as=save_html_as,
+        links_to_force_open_in_current_tab=links_to_force_open_in_current_tab
+    )
+    deploy_to_ftp(
+        host=ftp_host,
+        user=ftp_user,
+        password=ftp_pwd,
+        base_folder=project_root_folder
+    )
