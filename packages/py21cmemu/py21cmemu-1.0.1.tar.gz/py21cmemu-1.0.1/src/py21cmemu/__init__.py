8c30219f46df428469ed4f9ec28af000a6c6c2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:13 +0000

    Add package py21cmemu/1.0.1/py21cmemu-1.0.1.tar.gz

---
+++ packages/py21cmemu/py21cmemu-1.0.1.tar.gz/py21cmemu-1.0.1/src/py21cmemu/__init__.py
@@ -0,0 +1,7 @@
+"""21cmEMU: An emulator of 21cmFAST summaries."""
+
+__version__ = "1.0.0"
+from .emulator import Emulator
+from .get_emulator import add_emulator_data
+from .get_emulator import download_emu_data
+from .get_emulator import move_emulator_data
