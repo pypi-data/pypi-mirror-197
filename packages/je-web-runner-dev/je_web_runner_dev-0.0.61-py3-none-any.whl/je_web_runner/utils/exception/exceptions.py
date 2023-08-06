47430e7765aea0296cc1f95c91dc99ff79c5d8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:55 +0000

    Add package je-web-runner-dev/0.0.61/je_web_runner_dev-0.0.61-py3-none-any.whl

---
+++ packages/je-web-runner-dev/je_web_runner_dev-0.0.61-py3-none-any.whl/je_web_runner/utils/exception/exceptions.py
@@ -0,0 +1,48 @@
+class WebRunnerException(Exception):
+    pass
+
+
+class WebRunnerWebDriverNotFoundException(WebRunnerException):
+    pass
+
+
+class WebRunnerOptionsWrongTypeException(WebRunnerException):
+    pass
+
+
+class WebRunnerArgumentWrongTypeException(WebRunnerException):
+    pass
+
+
+class WebRunnerWebDriverIsNoneException(WebRunnerException):
+    pass
+
+
+class WebRunnerExecuteException(WebRunnerException):
+    pass
+
+
+class WebRunnerJsonException(WebRunnerException):
+    pass
+
+
+class WebRunnerAssertException(WebRunnerException):
+    pass
+
+
+class WebRunnerHTMLException(WebRunnerException):
+    pass
+
+
+class WebRunnerAddCommandException(WebRunnerException):
+    pass
+
+
+# XML
+
+class XMLException(WebRunnerException):
+    pass
+
+
+class XMLTypeException(XMLException):
+    pass
