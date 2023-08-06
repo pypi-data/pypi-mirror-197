a5bae46dfeee6763dc682c651d67ea48aed323d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:19 +0000

    Add package wizata-dsapi/0.0.95/wizata-dsapi-0.0.95.tar.gz

---
+++ packages/wizata-dsapi/wizata-dsapi-0.0.95.tar.gz/wizata-dsapi-0.0.95/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='wizata-dsapi',
+    version='0.0.95',
+    description='Wizata Data Science Toolkit',
+    author='Wizata S.A.',
+    author_email='info@wizata.com',
+    packages=['wizata_dsapi'],
+    install_requires=[
+        'flask',
+        'pandas',
+        'numpy',
+        'dill'
+    ]
+)
