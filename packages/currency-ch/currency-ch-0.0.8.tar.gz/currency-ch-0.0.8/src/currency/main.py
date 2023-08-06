942665a0820389609d57d5610e1beab86d73661a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:42 +0000

    Add package currency-ch/0.0.8/currency-ch-0.0.8.tar.gz

---
+++ packages/currency-ch/currency-ch-0.0.8.tar.gz/currency-ch-0.0.8/src/currency/main.py
@@ -0,0 +1,23 @@
+import currency.functions as functions
+import requests
+
+def convert(input_currency, output_currency, amount=1, roundTo=2):
+    if not functions.check(input_currency.upper()):
+        raise ValueError("Invalid input currency " + input_currency)
+    if not functions.check(output_currency.upper()):
+        raise ValueError("Invalid output currency " + output_currency)
+    if amount <= 0:
+        raise ValueError("Input amount must be positive.")
+    url = "http://floatrates.com/daily/" + input_currency + ".json"
+    data = requests.get(url).json()[output_currency.lower()]
+    return format(round(amount * data["rate"], roundTo), '.' + roundTo + 'f')
+
+
+def rate(input_currency, output_currency, roundTo=2):
+    if not functions.check(input_currency.upper()):
+        raise ValueError("Invalid input currency " + input_currency)
+    if not functions.check(output_currency.upper()):
+        raise ValueError("Invalid output currency " + output_currency)
+    url = "http://floatrates.com/daily/" + input_currency + ".json"
+    data = requests.get(url).json()[output_currency.lower()]
+    return format(round(data["rate"], roundTo), '.' + roundTo + 'f') 
