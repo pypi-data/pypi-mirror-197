c4b4e740c06e66c5b44a7ce53802cfbe9a391e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:23 +0000

    Add package mage-ai/0.8.25/mage_ai-0.8.25-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.8.25-py3-none-any.whl/mage_ai/data_preparation/models/utils.py
@@ -0,0 +1,62 @@
+from mage_ai.shared.parsers import encode_complex
+from typing import Dict
+import dask.dataframe as dd
+import pandas as pd
+import simplejson
+
+MAX_PARTITION_BYTE_SIZE = 100 * 1024 * 1024
+JSON_SERIALIZABLE_COLUMN_TYPES = [
+    dict.__name__,
+    list.__name__,
+]
+STRING_SERIALIZABLE_COLUMN_TYPES = [
+    'ObjectId',
+]
+
+
+def serialize_columns(row: pd.Series, column_types: Dict) -> pd.Series:
+    for column, column_type in column_types.items():
+        if column_type in JSON_SERIALIZABLE_COLUMN_TYPES:
+            val = row[column]
+            if val is not None:
+                row[column] = simplejson.dumps(
+                    val,
+                    default=encode_complex,
+                    ignore_nan=True,
+                    use_decimal=True,
+                )
+        elif column_type in STRING_SERIALIZABLE_COLUMN_TYPES:
+            val = row[column]
+            if val is not None:
+                row[column] = str(val)
+
+    return row
+
+
+def deserialize_columns(row: pd.Series, column_types: Dict) -> pd.Series:
+    for column, column_type in column_types.items():
+        if column_type not in JSON_SERIALIZABLE_COLUMN_TYPES:
+            continue
+
+        val = row[column]
+        if val is not None:
+            row[column] = simplejson.loads(val)
+
+    return row
+
+
+def dask_from_pandas(df: pd.DataFrame) -> dd:
+    ddf = dd.from_pandas(df, npartitions=1)
+    npartitions = 1 + ddf.memory_usage(deep=True).sum().compute() // MAX_PARTITION_BYTE_SIZE
+    ddf = ddf.repartition(npartitions=npartitions)
+
+    return ddf
+
+
+def apply_transform(ddf: dd, apply_function) -> dd:
+    res = ddf.apply(apply_function, axis=1, meta=ddf)
+    return res.compute()
+
+
+def apply_transform_pandas(df: pd.DataFrame, apply_function) -> pd.DataFrame:
+    return df.apply(apply_function, axis=1)
