1fb722ce88648ca9cfe05a1ed4a451b62a7cccf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:39 +0000

    Add package techzapi/1.1.9/techzapi-1.1.9.tar.gz

---
+++ packages/techzapi/techzapi-1.1.9.tar.gz/techzapi-1.1.9/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", encoding="utf8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="techzapi",
+    packages=["techzapi"],
+    version="1.1.9",
+    license="MIT",
+    description="Python Wrapper For TechZApi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="TechShreyash",
+    author_email="techshreyash123@gmail.com",
+    url="https://github.com/TechShreyash/TechZApi",
+    keywords=[
+        "API",
+        "TechZApi",
+        "TechZAPI",
+        "TechZBots",
+        "GogoAnime",
+        "Anime",
+        "AnimeAPI",
+    ],
+    install_requires=["requests"],
+    zip_safe=False,
+)
