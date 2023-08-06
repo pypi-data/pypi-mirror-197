aadaff5b0b969295638a85accc725c54be53d3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:51 +0000

    Add package apispec-plugins/1.0.1/apispec_plugins-1.0.1-py3-none-any.whl

---
+++ packages/apispec-plugins/apispec_plugins-1.0.1-py3-none-any.whl/apispec_plugins/base/types.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+__all__ = (
+    "AuthSchemes",
+    "HTTPResponse",
+    "Server",
+    "Tag",
+)
+
+
+class AuthSchemes:
+    @dataclass
+    class BasicAuth:
+        type: str = "http"
+        scheme: str = "basic"
+
+
+@dataclass
+class HTTPResponse:
+    code: int
+    description: str | None
+
+
+@dataclass
+class Server:
+    url: str
+    description: str | None
+
+
+@dataclass
+class Tag:
+    name: str
+    description: str | None
