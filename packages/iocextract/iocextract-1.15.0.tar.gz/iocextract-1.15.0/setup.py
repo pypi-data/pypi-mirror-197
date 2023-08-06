43d1a729d531a6c96c86f4031a417dd051965e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:59 +0000

    Add package iocextract/1.15.0/iocextract-1.15.0.tar.gz

---
+++ packages/iocextract/iocextract-1.15.0.tar.gz/iocextract-1.15.0/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+# README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='iocextract',
+    version='1.15.0',
+    include_package_data=True,
+    py_modules=['iocextract',],
+    install_requires=['regex',],
+    extras_require = {
+        ':python_version <= "2.7"': [
+            'ipaddress',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'iocextract = iocextract:main'
+        ]
+    },
+    license='GPL',
+    description='Advanced Indicator of Compromise (IOC) extractor.',
+    long_description="README.md",
+    url='https://github.com/InQuest/python-iocextract',
+    author='InQuest Labs',
+    author_email='labs@inquest.net',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Internet',
+    ],
+)
