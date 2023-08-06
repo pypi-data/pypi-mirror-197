b6b95f34c20b6071c717118bfedc1d566957c171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:05 +0000

    Add package now-lms/0.0.1a120230314/now_lms-0.0.1a120230314-py3-none-any.whl

---
+++ packages/now-lms/now_lms-0.0.1a120230314-py3-none-any.whl/now_lms/misc.py
@@ -0,0 +1,81 @@
+# Copyright 2022 -2023 BMO Soluciones, S.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+# - William José Moreno Reyes
+
+
+"""Utilerias varias."""
+
+ICONOS_RECURSOS: dict = {
+    "html": "bi bi-code-square",
+    "img": "bi bi-image",
+    "link": "bi bi-share",
+    "meet": "bi bi-broadcast",
+    "mp3": "bi bi-soundwave",
+    "pdf": "bi bi-file-pdf-fill",
+    "slides": "bi bi-file-slides",
+    "text": "bi bi-blockquote-left",
+    "youtube": "bi bi-youtube",
+}
+
+TEMPLATES_BY_TYPE: dict = {
+    "html": "type_html.html",
+    "img": "type_img.html",
+    "link": "type_link.html",
+    "meet": "type_meet.html",
+    "mp3": "type_audio.html",
+    "pdf": "type_pdf.html",
+    "slides": "type_slides.html",
+    "text": "type_text.html",
+    "youtube": "type_youtube.html",
+}
+
+HTML_TAGS = [
+    "a",
+    "abbr",
+    "acronym",
+    "b",
+    "blockquote",
+    "br",
+    "code",
+    "dd",
+    "del",
+    "div",
+    "dl",
+    "dt",
+    "em",
+    "em",
+    "h1",
+    "h2",
+    "h3",
+    "hr",
+    "i",
+    "img",
+    "li",
+    "ol",
+    "p",
+    "pre",
+    "s",
+    "strong",
+    "sub",
+    "sup",
+    "table",
+    "tbody",
+    "td",
+    "th",
+    "thead",
+    "tr",
+    "ul",
+]
