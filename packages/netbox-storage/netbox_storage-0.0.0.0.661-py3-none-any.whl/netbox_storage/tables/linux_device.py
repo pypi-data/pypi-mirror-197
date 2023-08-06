b36b3f64891e06d48a0b7a0a57523799efd14e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:36 +0000

    Add package netbox-storage/0.0.0.0.661/netbox_storage-0.0.0.0.661-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.661-py3-none-any.whl/netbox_storage/tables/linux_device.py
@@ -0,0 +1,33 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ToggleColumn,
+)
+from netbox_storage.models import LinuxDevice
+
+
+class LinuxDeviceTable(NetBoxTable):
+
+    pk = ToggleColumn()
+    device = tables.Column(
+        linkify=True,
+        verbose_name="Device"
+    )
+    type = tables.Column(
+        linkify=True,
+        verbose_name="Type"
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = LinuxDevice
+        fields = (
+            "pk",
+            "device",
+            "type",
+        )
+        default_columns = (
+            "pk",
+            "device",
+            "type",
+        )
