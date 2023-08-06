ba87839e321a9aa0e1ed75159bdb3d60c658ec3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:32 +0000

    Add package goodgame/0.2.0/goodgame-0.2.0.tar.gz

---
+++ packages/goodgame/goodgame-0.2.0.tar.gz/goodgame-0.2.0/goodgame/__init__.py
@@ -0,0 +1,27 @@
+from .exceptions import FlagNotFoundError, SDLError
+from .app import App
+from .clock import Clock, Timer, Animation, Counter
+from .events import CommonEvent, QuitEvent, AudioDeviceEvent, DropEvent, TouchFingerEvent, KeyboardEvent,\
+    MouseMotionEvent, MouseButtonEvent, MouseWheelEvent, TextEditingEvent, TextInputEvent, DisplayEvent, WindowEvent,\
+    JoyAxisEvent, JoyBallEvent, JoyButtonEvent, JoyDeviceEvent, JoyHatEvent, JoyBatteryEvent, ControllerAxisEvent,\
+    ControllerButtonEvent, ControllerDeviceEvent, ControllerTouchpadEvent, ControllerSensorEvent
+from .audio import AudioDeviceManager, AudioSpec, AudioDevice
+from .video import BackendManager, Backend, DisplaysManager, Display, DisplayMode, PixelFormat
+from .window import Window
+from .surface import Surface, SurfaceAnimation
+from .texture import Texture
+from .renderer import Renderer
+from .cursor import CursorManager, Cursor
+from .touch import TouchDevice, Finger
+from .sensor import Sensor
+from .gamecontroller import GameController
+from .joystick import Joystick
+from .mixer import Mixer, Music, Chunk
+from .ttf import TTF
+from .loader import Loader
+from .math import Math
+from .opengl import GLContext
+from .bmf import BMFont, BMChar
+
+
+__version__ = '0.2.0'
