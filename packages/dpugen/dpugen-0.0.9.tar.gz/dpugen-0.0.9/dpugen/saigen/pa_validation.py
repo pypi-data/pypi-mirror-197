21f3f64978f0fd7629d3869227a0903c1afdc8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:13 +0000

    Add package dpugen/0.0.9/dpugen-0.0.9.tar.gz

---
+++ packages/dpugen/dpugen-0.0.9.tar.gz/dpugen-0.0.9/dpugen/saigen/pa_validation.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+from saigen.confbase import *
+from saigen.confutils import *
+
+ipa = ipaddress.ip_address  # optimization so the . does not get executed multiple times
+
+class PaValidation(ConfBase):
+
+    def __init__(self, params={}):
+        super().__init__(params)
+
+    def items(self):
+        print('  Generating %s ...' % os.path.basename(__file__), file=sys.stderr)
+        self.num_yields = 0
+        p = self.params
+
+        for eni_index, eni in enumerate(range(p.ENI_START, p.ENI_START + p.ENI_COUNT * p.ENI_STEP, p.ENI_STEP)):
+            vtep_remote = str(ipa(p.PAR) + int(ipa(p.IP_STEP1)) * eni_index)
+
+            self.num_yields += 1
+            yield {
+                'name': 'pa_validation_#eni%d' % eni,
+                'op': 'create',
+                'type' : 'SAI_OBJECT_TYPE_PA_VALIDATION_ENTRY',
+                'key' : {
+                    'switch_id': '$SWITCH_ID',
+                    'sip': vtep_remote,
+                    'vnet_id': '$vnet_#eni%d' % eni
+                },
+                'attributes' : [
+                    'SAI_PA_VALIDATION_ENTRY_ATTR_ACTION', 'SAI_PA_VALIDATION_ENTRY_ACTION_PERMIT'
+                ]
+            }
+
+
+if __name__ == '__main__':
+    conf = PaValidation()
+    common_main(conf)
