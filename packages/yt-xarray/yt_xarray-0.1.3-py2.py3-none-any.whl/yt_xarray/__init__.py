6f4666688aaf59fe1b1b3cabd9f60ea441ec69f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:59 +0000

    Add package yt-xarray/0.1.3/yt_xarray-0.1.3-py2.py3-none-any.whl

---
+++ packages/yt-xarray/yt_xarray-0.1.3-py2.py3-none-any.whl/yt_xarray/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for yt_xarray."""
+
+__author__ = """Chris Havlin"""
+__email__ = "chris.havlin@gmail.com"
+__version__ = "0.1.3"
+
+
+# import the yt frontend and the xarray accessor so they are registered with
+# their respective codes
+
+from .accessor import YtAccessor
+from .accessor._xr_to_yt import known_coord_aliases
+from .yt_xarray import open_dataset
