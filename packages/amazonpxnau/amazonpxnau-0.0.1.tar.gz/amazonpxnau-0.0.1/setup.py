cdd4fa4fdb17e0984f613062b6b79bb4179d0d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:01 +0000

    Add package amazonpxnau/0.0.1/amazonpxnau-0.0.1.tar.gz

---
+++ packages/amazonpxnau/amazonpxnau-0.0.1.tar.gz/amazonpxnau-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='amazonpxnau',
+    version='0.0.1',
+    packages=['amazonpxnau'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
