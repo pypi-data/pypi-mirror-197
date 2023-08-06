cb84519278a87f117218d19f58677826a838096b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:50 +0000

    Add package aws-service-catalog-factory/0.100.0/aws_service_catalog_factory-0.100.0.tar.gz

---
+++ packages/aws-service-catalog-factory/aws_service_catalog_factory-0.100.0.tar.gz/aws_service_catalog_factory-0.100.0/servicecatalog_factory/environmental_variables.py
@@ -0,0 +1,7 @@
+#  Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+
+
+CACHE_INVALIDATOR = "SCT_CACHE_INVALIDATOR"
+
+AWS_URL_SUFFIX = "AWS_URL_SUFFIX"
