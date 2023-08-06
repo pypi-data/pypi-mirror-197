a04ce97f5b63bd88342fd19626d2b6a5f7e27f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:22 +0000

    Add package arthurai/3.23.post1/arthurai-3.23.post1.tar.gz

---
+++ packages/arthurai/arthurai-3.23.post1.tar.gz/arthurai-3.23.post1/arthurai/version.py
@@ -0,0 +1,11 @@
+# STICK TO SEMANTIC VERSIONING:
+# https://semver.org/
+# TLDR:
+# Given a version number MAJOR.MINOR.PATCH, increment the:
+#
+# MAJOR version when you make incompatible API changes,
+# MINOR version when you add functionality in a backwards compatible manner, and
+# PATCH version when you make backwards compatible bug fixes.
+
+# NEXT PERSON PLEASE: delete this comment and update the version to '3.23.1' or '3.24.0', removing the '.post1'
+__version__ = "3.23.post1"
