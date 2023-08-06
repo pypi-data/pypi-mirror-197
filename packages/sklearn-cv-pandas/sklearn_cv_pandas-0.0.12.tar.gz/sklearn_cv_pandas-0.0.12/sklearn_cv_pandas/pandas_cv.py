cfb282023047b975d985f3b0a40a2cafaff10020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:01 +0000

    Add package sklearn-cv-pandas/0.0.12/sklearn_cv_pandas-0.0.12.tar.gz

---
+++ packages/sklearn-cv-pandas/sklearn_cv_pandas-0.0.12.tar.gz/sklearn_cv_pandas-0.0.12/sklearn_cv_pandas/pandas_cv.py
@@ -0,0 +1,174 @@
+import logging
+from functools import reduce
+from operator import mul
+
+import numpy
+from sklearn import model_selection
+
+from . import (
+    model,
+    preprocessing
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+class RandomizedSearchCV(model_selection.RandomizedSearchCV):
+    """
+    sklearn.model_selection.RandomizedSearchCV with pandas DataFrame interface
+    """
+    def __init__(self, estimator, param_distributions, n_iter=10, scoring=None, n_jobs=None, pre_dispatch='2*n_jobs',
+                 cv=None, refit=True, verbose=10, random_state=None, error_score=numpy.nan, return_train_score=True):
+        """
+        The same manner as [sklearn.model_selection.RandomizedSearchCV](
+        https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.RandomizedSearchCV.html)
+        """
+        super(RandomizedSearchCV, self).__init__(
+            estimator, param_distributions, n_iter=n_iter, scoring=scoring, n_jobs=n_jobs, pre_dispatch=pre_dispatch,
+            cv=cv, refit=refit, verbose=verbose, random_state=random_state, error_score=error_score,
+            return_train_score=return_train_score
+        )
+
+    def fit_holdout_pandas(self, df_training, target_column, feature_columns,
+                      df_validation=None, ratio_training=None, **kwargs):
+        """
+        `fit` for pandas DataFrame to perform one set of holdout validation
+        Args:
+            df_training (pandas.DataFrame): training data set
+            target_column (str): column name of prediction target
+            feature_columns (list of str): column names of features
+            df_validation (pandas.DataFrame): if specified, used as validation data set
+            ratio_training (float): if specified, `df_training` is split for training / validation
+            **kwargs: Other keyword arguments for original `fit`
+
+        Returns:
+            conjurer.ml.Model
+        """
+        logger.warning("start learning with {} hyper parameters".format(self.n_iter))
+        return _holdout_logic(
+            self, df_training, target_column, feature_columns, df_validation, ratio_training, **kwargs
+        )
+
+    def fit_cv_pandas(self, df, target_column, feature_columns, cv, **kwargs):
+        """
+        `fit` for pandas DataFrame to perform cross validation
+        Args:
+            df (pandas.DataFrame): training data set
+            target_column (str): column name of prediction target
+            feature_columns (list of str): column names of features
+            cv (int or cross-validation generator): The number of fold for CV
+            **kwargs: Other keyword arguments for original `fit`
+
+        Returns:
+            conjurer.ml.Model
+        """
+        logger.warning("start learning with {} hyper parameters".format(self.n_iter))
+        return _cv_logic(
+            self, df, target_column, feature_columns, cv, **kwargs
+        )
+
+
+class GridSearchCV(model_selection.GridSearchCV):
+    def __init__(self, estimator, param_grid, scoring=None,
+                 n_jobs=None, pre_dispatch='2*n_jobs', cv=None, refit=True,
+                 verbose=10, error_score=numpy.nan, return_train_score=True, random_state=None):
+        super(GridSearchCV, self).__init__(
+            estimator, param_grid, scoring=scoring, n_jobs=n_jobs, pre_dispatch=pre_dispatch,
+            cv=cv, refit=refit, verbose=verbose, error_score=error_score, return_train_score=return_train_score
+        )
+        self.random_state = random_state
+
+    def fit_holdout_pandas(self, df_training, target_column, feature_columns,
+                      df_validation=None, ratio_training=None, **kwargs):
+        """
+        `fit` for pandas DataFrame to perform one set of holdout validation
+        Args:
+            df_training (pandas.DataFrame): training data set
+            target_column (str): column name of prediction target
+            feature_columns (list of str): column names of features
+            df_validation (pandas.DataFrame): if specified, used as validation data set
+            ratio_training (float): if specified, `df_training` is split for training / validation
+            **kwargs: Other keyword arguments for original `fit`
+
+        Returns:
+            conjurer.ml.Model
+        """
+        logger.warning("start learning with {} parameters".format(_get_num_parameters(self.param_grid)))
+        return _holdout_logic(
+            self, df_training, target_column, feature_columns, df_validation, ratio_training, **kwargs
+        )
+
+    def fit_cv_pandas(self, df, target_column, feature_columns, cv, **kwargs):
+        """
+        `fit` for pandas DataFrame to perform cross validation
+        Args:
+            df (pandas.DataFrame): training data set
+            target_column (str): column name of prediction target
+            feature_columns (list of str): column names of features
+            cv (int or cross-validation generator): The number of fold for CV
+            **kwargs: Other keyword arguments for original `fit`
+
+        Returns:
+            conjurer.ml.Model
+        """
+        logger.warning("start learning with {} parameters".format(_get_num_parameters(self.param_grid)))
+        return _cv_logic(
+            self, df, target_column, feature_columns, cv, **kwargs
+        )
+
+
+def _split_for_holdout(df_training, target_column, feature_columns, df_validation, ratio_training, random_state):
+    if df_validation is not None:
+        x = numpy.concatenate(
+            (
+                preprocessing.get_x(df_training, feature_columns), 
+                preprocessing.get_x(df_validation, feature_columns)
+            ),
+            axis=0
+        )
+        y = numpy.concatenate(
+            (
+                preprocessing.get_y(df_training, target_column), 
+                preprocessing.get_y(df_validation, target_column)
+            ),
+            axis=0
+        )
+        num_training = len(df_training)
+        num_validation = len(df_validation)
+    else:
+        shuffled_df = df_training.sample(len(df_training), random_state=random_state)
+        x = preprocessing.get_x(shuffled_df, feature_columns)
+        y = preprocessing.get_y(shuffled_df, target_column)
+        num_training = int(ratio_training * len(df_training))
+        num_validation = len(df_training) - num_training
+    return x, y, num_training, num_validation
+
+
+def _get_num_parameters(param_grid):
+    product = lambda list_values: reduce(mul, list_values, 1)
+    return len(param_grid) if isinstance(param_grid, list) \
+        else product([len(param_grid[elem]) for elem in param_grid.keys()])
+
+
+def _holdout_logic(cv_obj, df_training, target_column, feature_columns, df_validation, ratio_training, **kwargs):
+    x, y, num_training, num_validation = _split_for_holdout(
+        df_training, target_column, feature_columns, df_validation, ratio_training, cv_obj.random_state)
+    cv_obj.cv = model_selection.PredefinedSplit(
+        numpy.array([-1] * num_training + [0] * num_validation))
+    cv_obj.fit(x, y, **kwargs)
+    return model.Model(cv_obj, feature_columns=feature_columns, target_column=target_column)
+
+
+def _cv_logic(cv_obj, df, target_column, feature_columns, cv, **kwargs):
+    x = preprocessing.get_x(df, feature_columns)
+    y = preprocessing.get_y(df, target_column)
+    if isinstance(cv, int):
+        if set(y) == {0, 1}:
+            cv_obj.cv = model_selection.StratifiedKFold(n_splits=cv, shuffle=True, random_state=cv_obj.random_state)
+        else:
+            cv_obj.cv = model_selection.KFold(n_splits=cv, shuffle=True, random_state=cv_obj.random_state)
+    else:
+        cv_obj.cv = cv
+    cv_obj.fit(x, y, **kwargs)
+    return model.Model(cv_obj, feature_columns=feature_columns, target_column=target_column)
---
+++ packages/sklearn-cv-pandas/sklearn_cv_pandas-0.0.12.tar.gz/sklearn_cv_pandas-0.0.12/tests/test_pandas_cv.py
@@ -0,0 +1,146 @@
+import pytest
+
+import pandas
+from pandas import testing
+from scipy import stats
+from sklearn import (
+    linear_model,
+    tree,
+    pipeline,
+    impute,
+    preprocessing
+)
+
+from sklearn_cv_pandas import (
+    RandomizedSearchCV,
+    GridSearchCV
+)
+from tests import utils
+
+
+def execute_scenario(model_type, is_cl, with_prep, cv_type, holdout_type):
+    cv = _get_cv(model_type, is_cl, with_prep, cv_type)
+    df_training = utils.get_input_df(100, with_prep)
+    df_validation = utils.get_input_df(100, with_prep)
+    df_test = utils.get_input_df(10, with_prep)
+    target_column = "target_cl" if is_cl else "target_rg"
+    feature_columns = ["column{}".format(i) for i in range(6)]
+    model = cv.fit_cv_pandas(df_training, target_column, feature_columns, cv=3) \
+        if holdout_type == "cv" \
+        else cv.fit_holdout_pandas(df_training, target_column, feature_columns, ratio_training=0.8) \
+        if holdout_type == "holdout_ratio" \
+        else cv.fit_holdout_pandas(df_training, target_column, feature_columns, df_validation)
+    _assert_prediction(model, df_test, is_cl)
+
+
+def test_random_linear_holdout_ratio_cl():
+    execute_scenario("linear", True, False, "random", "holdout_ratio")
+
+
+def test_random_tree_holdout_2dfs_rg():
+    execute_scenario("tree", False, False, "random", "holdout_2dfs")
+
+
+def test_random_tree_with_prep_cv_cl():
+    execute_scenario("tree", True, True, "random", "cv")
+
+
+def test_grid_linear_holdout_ratio_cl():
+    execute_scenario("linear", True, False, "grid", "holdout_ratio")
+
+
+def test_grid_tree_with_prep_holdout_2dfs_rg():
+    execute_scenario("tree", False, True, "grid", "holdout_2dfs")
+
+
+def test_grid_tree_cv_cl():
+    execute_scenario("tree", True, False, "grid", "cv")
+
+
+def test_fix_random_seed():
+    is_cl = False
+    with_prep = False
+    estimator = _get_estimator("tree", is_cl, with_prep)
+    params = _get_params_random("tree", is_cl, with_prep)
+    metric = "roc_auc" if is_cl else "neg_root_mean_squared_error"
+    target_column = "target_cl" if is_cl else "target_rg"
+    cv1 = RandomizedSearchCV(estimator, params, scoring=metric, random_state=0)
+    cv2 = RandomizedSearchCV(estimator, params, scoring=metric, random_state=0)
+    df_training = utils.get_input_df(100, with_prep)
+    df_test = utils.get_input_df(10, with_prep)
+    feature_columns = ["column{}".format(i) for i in range(6)]
+    model1 = cv1.fit_cv_pandas(df_training, target_column, feature_columns, cv=3)
+    model2 = cv2.fit_cv_pandas(df_training, target_column, feature_columns, cv=3)
+    pred1 = model1.predict(df_test)
+    pred2 = model2.predict(df_test)
+    columns = ["param_max_depth", "mean_test_score", "std_test_score", "mean_train_score", "std_train_score"]
+    testing.assert_frame_equal(
+        pandas.DataFrame(cv1.cv_results_)[columns], pandas.DataFrame(cv2.cv_results_)[columns]
+    )
+    testing.assert_frame_equal(pred1, pred2)
+
+
+def _get_cv(model_type, is_cl, with_prep, cv_type):
+    estimator = _get_estimator(model_type, is_cl, with_prep)
+    metric = "roc_auc" if is_cl else "neg_root_mean_squared_error"
+    if cv_type == "random":
+        params = _get_params_random(model_type, is_cl, with_prep)
+        return RandomizedSearchCV(estimator, params, scoring=metric)
+    else:
+        params = _get_params_grid(model_type, is_cl, with_prep)
+        return GridSearchCV(estimator, params, scoring=metric)
+
+
+def _get_estimator(model_type, is_cl, with_preprocessing):
+    if model_type == "linear":
+        ml_estimator = linear_model.LogisticRegression(solver="liblinear", random_state=0) \
+            if is_cl else linear_model.Lasso(random_state=0)
+    else:
+        ml_estimator = tree.DecisionTreeClassifier(random_state=0) if is_cl \
+            else tree.DecisionTreeRegressor(random_state=0)
+    return _add_preprocessing(ml_estimator) if with_preprocessing else ml_estimator
+
+
+def _add_preprocessing(estimator):
+    return pipeline.Pipeline(
+        steps=[
+            ("mvi", impute.SimpleImputer()),
+            ("std", preprocessing.StandardScaler()),
+            ("ml", estimator)
+        ]
+    )
+
+
+def _get_params_random(model_type, is_cl, with_preprocessing):
+    if model_type == "linear":
+        ml_params = dict(
+            penalty=["l1", "l2"],
+            C=stats.loguniform(1e-5, 10)
+        ) if is_cl else dict(alpha=stats.loguniform(1e-5, 10))
+    else:
+        ml_params = dict(max_depth=list(range(5, 16)))
+    return _convert_ml_params(ml_params) if with_preprocessing else ml_params
+
+
+def _get_params_grid(model_type, is_cl, with_preprocessing):
+    if model_type == "linear":
+        ml_params = dict(
+            penalty=["l1", "l2"],
+            C=[1e-5, 1e-3]
+        ) if is_cl else dict(alpha=[1e-5, 1e-3, 1e-1, 10])
+    else:
+        ml_params = dict(max_depth=[5, 8, 11, 14])
+    return _convert_ml_params(ml_params) if with_preprocessing else ml_params
+
+
+def _convert_ml_params(ml_params):
+    return {"{}__{}".format("ml", k): v for k, v in ml_params.items()}
+
+
+def _assert_prediction(model, df_test, is_cl):
+    pred_df = model.predict(df_test)
+    expected_columns = ["score", "id1", "id2", "target_cl", "target_rg"]
+    if is_cl:
+        expected_columns.insert(1, "predicted_class")
+    assert list(pred_df.columns) == expected_columns
+    assert len(pred_df) == 10
