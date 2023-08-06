de55e3f4632ac9c31e896be837f08038b28197a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:45 +0000

    Add package datalad-next/1.0.0b2/datalad_next-1.0.0b2.tar.gz

---
+++ packages/datalad-next/datalad_next-1.0.0b2.tar.gz/datalad_next-1.0.0b2/datalad_next/constraints/__init__.py
@@ -0,0 +1,53 @@
+"""Data validation, coercion, and parameter documentation
+
+.. currentmodule:: datalad_next.constraints
+.. autosummary::
+   :toctree: generated
+
+   base
+   basic
+   compound
+   formats
+   parameter
+   git
+   dataset
+   exceptions
+"""
+from .base import (
+    AllOf,
+    AnyOf,
+    Constraint,
+    DatasetParameter,
+)
+# expose constraints with direct applicability, but not
+# base and helper classes
+from .basic import (
+    EnsureBool,
+    EnsureCallable,
+    EnsureChoice,
+    EnsureFloat,
+    EnsureInt,
+    EnsureKeyChoice,
+    EnsureNone,
+    EnsurePath,
+    EnsureStr,
+    EnsureRange,
+    EnsureValue,
+    NoConstraint,
+)
+from .compound import (
+    EnsureIterableOf,
+    EnsureListOf,
+    EnsureTupleOf,
+    EnsureMapping,
+    EnsureGeneratorFromFileLike,
+    WithDescription,
+)
+# this is the key type, almost all consuming code will want to
+# have this for `except` clauses
+from .exceptions import ConstraintError
+from .formats import (
+    EnsureJSON,
+    EnsureURL,
+    EnsureParsedURL,
+)
---
+++ packages/datalad-next/datalad_next-1.0.0b2.tar.gz/datalad_next-1.0.0b2/datalad_next/url_operations/tests/test_http.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import pytest
+
+from ..http import (
+    HttpUrlOperations,
+    UrlOperationsRemoteError,
+    UrlOperationsResourceUnknown,
+)
+
+
+def test_http_url_operations(credman, httpbin, tmp_path):
+    hbsurl = httpbin['standard']
+    hbscred = (
+        'hbscred',
+        dict(user='mike', secret='dummy', type='user_password',
+             realm=f'{hbsurl}/Fake Realm'),
+    )
+
+    credman.set(hbscred[0], **hbscred[1])
+    ops = HttpUrlOperations()
+    # authentication after redirect
+    target_url = f'{hbsurl}/basic-auth/mike/dummy'
+    props = ops.stat(f'{hbsurl}/redirect-to?url={target_url}')
+    # we get the resolved URL after redirect back
+    assert props['url'] == target_url
+    # same again, but credentials are wrong
+    target_url = f'{hbsurl}/basic-auth/mike/WRONG'
+    with pytest.raises(UrlOperationsRemoteError):
+        ops.stat(f'{hbsurl}/redirect-to?url={target_url}')
+    # make sure we get the size info
+    assert ops.stat(f'{hbsurl}/bytes/63')['content-length'] == 63
+
+    # download
+    # SFRUUEJJTiBpcyBhd2Vzb21l == 'HTTPBIN is awesome'
+    props = ops.download(f'{hbsurl}/base64/SFRUUEJJTiBpcyBhd2Vzb21l',
+                         tmp_path / 'mydownload',
+                         hash=['md5'])
+    assert (tmp_path / 'mydownload').read_text() == 'HTTPBIN is awesome'
+
+    # 404s
+    with pytest.raises(UrlOperationsResourceUnknown):
+        ops.stat(f'{hbsurl}/status/404')
+    with pytest.raises(UrlOperationsResourceUnknown):
+        ops.download(f'{hbsurl}/status/404', tmp_path / 'dontmatter')
