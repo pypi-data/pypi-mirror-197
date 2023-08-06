7c69b70b889149d6507dd0d3e2373890d7ed7b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:25 +0000

    Add package jittok/0.0.3/jittok-0.0.3.tar.gz

---
+++ packages/jittok/jittok-0.0.3.tar.gz/jittok-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup
+
+from jittok import __author__, __description__, __email__, __package_name__, __version__
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"), "r") as fin:
+    __long_description__ = fin.read()
+
+setup(
+    name=__package_name__,
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    license="MIT",
+    url="https://github.com/osoken/jittok",
+    description=__description__,
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    package_data={__package_name__: ["py.typed"]},
+    packages=[__package_name__, f"{__package_name__}.jptext"],
+    install_requires=["regex", "pykakasi"],
+    extras_require={
+        "dev": ["flake8", "pytest", "black", "mypy", "tox", "isort", "types-regex", "pytest-mock"],
+    },
+)
