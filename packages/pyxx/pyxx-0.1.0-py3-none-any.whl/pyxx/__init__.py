3fd20577fc6aee3b665ea9b038327e41312aad2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:22 +0000

    Add package pyxx/0.1.0/pyxx-0.1.0-py3-none-any.whl

---
+++ packages/pyxx/pyxx-0.1.0-py3-none-any.whl/pyxx/__init__.py
@@ -0,0 +1,12 @@
+"""Collection of useful Python utilities and tools"""
+
+# PROGRAM VERSION ------------------------------------------------------------
+__version__ = '0.1.0'
+
+
+# PACKAGE MODULES ------------------------------------------------------------
+from . import arrays
+from . import files
+from . import numbers
+from . import strings
+from . import units
