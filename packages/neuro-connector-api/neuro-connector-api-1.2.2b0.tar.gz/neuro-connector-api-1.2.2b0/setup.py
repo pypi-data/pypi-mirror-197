f6d61143e08baef0201e19ad79037ec283505703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:38 +0000

    Add package neuro-connector-api/1.2.2b0/neuro-connector-api-1.2.2b0.tar.gz

---
+++ packages/neuro-connector-api/neuro-connector-api-1.2.2b0.tar.gz/neuro-connector-api-1.2.2b0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='neuro-connector-api',
+   version='v1.2.2-beta',
+   description='Connects to app.myneuro.ai',
+   long_description='Intended to be used in the command line. NeuroConnector.py -c [connectonId] -o [organizationId] -u [baseUrl] -a [appToken] -f [function] -p [filePath]\nFunctions [1=sendTestResultsJson]\n',
+   author='Ben Hesketh',
+   author_email='bhesketh@wearedragonfly.co',
+   packages=['neuro-connector-api'],  #same as name
+   install_requires=['wheel', 'bar', 'greek','urllib3','requests'], #external packages as dependencies
+)
