deeea1cb739d1fc7e862916d4806e682abe27af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:48 +0000

    Add package mo-gymnasium/0.3.4/mo-gymnasium-0.3.4.tar.gz

---
+++ packages/mo-gymnasium/mo-gymnasium-0.3.4.tar.gz/mo-gymnasium-0.3.4/mo_gymnasium/envs/mujoco/__init__.py
@@ -0,0 +1,20 @@
+from gymnasium.envs.registration import register
+
+
+register(
+    id="mo-halfcheetah-v4",
+    entry_point="mo_gymnasium.envs.mujoco.half_cheetah:MOHalfCheehtahEnv",
+    max_episode_steps=1000,
+)
+
+register(
+    id="mo-hopper-v4",
+    entry_point="mo_gymnasium.envs.mujoco.hopper:MOHopperEnv",
+    max_episode_steps=1000,
+)
+
+register(
+    id="mo-reacher-v4",
+    entry_point="mo_gymnasium.envs.mujoco.reacher:MOReacherEnv",
+    max_episode_steps=50,
+)
