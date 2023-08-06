cd122174e4f29dfca79abd0361c7fbb58cfe43e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:07 +0000

    Add package codat-files/0.4.0/codat-files-0.4.0.tar.gz

---
+++ packages/codat-files/codat-files-0.4.0.tar.gz/codat-files-0.4.0/src/codat/models/operations/__init__.py
@@ -0,0 +1,5 @@
+from .download_files import *
+from .list_files import *
+from .upload_files import *
+
+__all__ = ["DownloadFilesRequest","DownloadFilesResponse","ListFilesFile","ListFilesRequest","ListFilesResponse","UploadFilesRequest","UploadFilesResponse"]
