6029aa034bbaa33229d3ffc8d73b11b8edd7a168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:05 +0000

    Add package satip/2.10.65/satip-2.10.65.tar.gz

---
+++ packages/satip/satip-2.10.65.tar.gz/satip-2.10.65/tests/test_eumetsat.py
@@ -0,0 +1,11 @@
+"""Tests for satip.eumetsat."""
+
+
+def test_filename_to_datetime():
+    """If there were a test here, there would also be a docstring here."""
+    pass
+
+
+def test_data_tailor():
+    """If there were a test here, there would also be a docstring here."""
+    pass
