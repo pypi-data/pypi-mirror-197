30cfa8ec388bccf3975f533d014c3422eb832d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:01 +0000

    Add package langwhat/2.0.3/langwhat-2.0.3.tar.gz

---
+++ packages/langwhat/langwhat-2.0.3.tar.gz/langwhat-2.0.3/README.md
@@ -0,0 +1,119 @@
+# LangWhat
+
+Answer "What is it?" on the command line with the power of large language models (LLMs).
+
+[pyWhat](https://github.com/bee-san/pyWhat) LLM version, leveraging OpenAI API and Sydney (Bing AI Chat).
+
+- [LangWhat](#langwhat)
+  - [Screenshots](#screenshots)
+    - [OpenAI](#openai)
+    - [Sydney](#sydney)
+    - [Caching](#caching)
+  - [Features](#features)
+  - [Installation](#installation)
+    - [pipx](#pipx)
+    - [pip](#pip)
+  - [Usage](#usage)
+  - [Develop](#develop)
+  - [Credits](#credits)
+
+## Screenshots
+
+### OpenAI
+
+```bash
+langwhat 'f7316ffccd4d2d555a7522328cf792dd73bfbcd9'
+
+langwhat 'f7316ffccd4d2d555a7522328cf792dd73bfbcd9' --zh
+```
+
+![](images/screenshot-hash.png)
+
+### Sydney
+
+Sydney fixed my type "marry" automatically.
+
+```bash
+langwhat 'marry ball washington' -s
+
+langwhat 'marry ball washington' -s -z
+```
+
+
+![](images/screenshot-sydney-mary.png)
+
+### Caching
+
+Responses are much faster when cache hits, and token usage becomes 0.
+
+Note that Sydney doesn't support counting token usage, and always shows 0.
+
+```bash
+lw teddy --show-token-usage
+
+lw teddy --show-token-usage
+```
+
+![](images/screenshot-caching-teddy.png)
+## Features
+- Uses few-shot prompting to reduce model mis-behavior
+- English by default for superior response speed and accuracy
+- Supports supplying API Key via either environment variable or config file at `~/.config/langwhat/api_key.txt`
+- Supports using [Sydney](https://www.bing.com/search?q=Bing+AI&showconv=1) with `-s` flag
+- Caching responses in a local sqlite database
+
+
+## Installation
+
+### pipx
+
+This is the recommended installation method.
+
+```
+$ pipx install langwhat
+
+# python 3.11 or higher is required, if your pipx uses a lower version of python by default,
+# you could run the following command to install langwhat with python 3.11
+# pipx install --python "$(which python3.11)"
+```
+
+### [pip](https://pypi.org/project/langwhat/)
+
+```
+$ pip install langwhat
+```
+
+
+## Usage
+
+```
+$ langwhat --help
+
+usage: lw [-h] [-z] [-s] [-C] [--show-token-usage] [-V] what
+
+positional arguments:
+  what                what is it
+
+options:
+  -h, --help          show this help message and exit
+  -z, --zh            Use Mandarin to prompt and answer
+  -s, --sydney        Use Sydney (Bing AI) instead of OpenAI
+  -C, --no-cache      Disable cache
+  --show-token-usage  Show token usage
+  -V, --version       show program's version number and exit
+```
+
+
+
+## Develop
+
+```
+$ git clone https://github.com/tddschn/langwhat.git
+$ cd langwhat
+$ poetry install
+```
+
+## Credits
+- LangChain
+- [EdgeGPT](https://github.com/acheong08/EdgeGPT)
+- [Yihong](https://github.com/yihong0618/iWhat)
---
+++ packages/langwhat/langwhat-2.0.3.tar.gz/langwhat-2.0.3/langwhat/cli.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import argparse
+from os import environ
+from . import __version__
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("what", help="what is it")
+    # parser.add_argument(
+    #     "--openai-key", dest="openai_key", type=str, default="", help="OpenAI api key"
+    # )
+    # parser.add_argument(
+    #     '--bing-cookie-json', dest='bing_cookie_json', type=str, default='', help='Bing cookie json'
+    # )
+    # args to change api_base
+    # parser.add_argument(
+    #     "--api_base",
+    #     dest="api_base",
+    #     type=str,
+    #     help="specify base url other than the OpenAI's official API address",
+    # )
+    parser.add_argument(
+        "-z",
+        "--zh",
+        dest="zh",
+        action="store_true",
+        help="Use Mandarin to prompt and answer",
+    )
+
+    parser.add_argument(
+        '-s',
+        '--sydney',
+        dest='sydney',
+        action='store_true',
+        help='Use Sydney (Bing AI) instead of OpenAI',
+    )
+
+    parser.add_argument(
+        '-C', '--no-cache', dest='no_cache', action='store_true', help='Disable cache'
+    )
+
+    parser.add_argument(
+        '--show-token-usage',
+        dest='show_token_usage',
+        action='store_true',
+        help='Show token usage',
+    )
+
+    parser.add_argument(
+        '-V',
+        '--version',
+        action='version',
+        version=f'%(prog)s {__version__}',
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    from langwhat.langwhat import LangWhat
+
+    if not args.sydney:
+        # OPENAI_API_KEY = args.openai_key or env.get("OPENAI_API_KEY")
+        # BING_COOKIE_JSON = args.bing_cookie_json or env.get("BING_COOKIE_JSON") or env.get("COOKIE_FILE")
+        OPENAI_API_KEY = environ.get("OPENAI_API_KEY")
+        if not OPENAI_API_KEY:
+            from .config import get_api_key
+
+            OPENAI_API_KEY = get_api_key()
+            environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+            if not OPENAI_API_KEY:
+                raise Exception(
+                    "OpenAI API key not provided, please provide it by running `export OPENAI_API_KEY=<sk-XXXX>`"
+                )
+        BING_COOKIE_JSON = None
+    else:
+        from .config import get_cookies_file
+
+        BING_COOKIE_JSON = get_cookies_file()
+
+    if not args.no_cache:
+        from .utils import use_langchain_sqlite_cache
+
+        use_langchain_sqlite_cache()
+
+    # what = What(args.what, is_en=args.en, api_base=args.api_base)
+    langwhat = LangWhat(
+        args.what,
+        is_zh=args.zh,
+        bing_cookie_json_path=BING_COOKIE_JSON,
+        sydney=args.sydney,
+        api_base=None,
+        show_token_usage=args.show_token_usage,
+    )
+    langwhat.show()
+
+
+if __name__ == "__main__":
+    main()
