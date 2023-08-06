16319bc798794bbd06ca171c59b16c5044e5846a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:04 +0000

    Add package katsdptelstate/0.13/katsdptelstate-0.13-py3-none-any.whl

---
+++ packages/katsdptelstate/katsdptelstate-0.13-py3-none-any.whl/katsdptelstate/test/__init__.py
@@ -0,0 +1,36 @@
+
+# Automatically added by katversion
+__version__ = '0.9'
+
+# Automatically added by katversion
+__version__ = '0.10'
+
+# Automatically added by katversion
+__version__ = '0.11'
+
+# Automatically added by katversion
+__version__ = '0.11.dev545+master.4c1580c.dirty'
+
+# Automatically added by katversion
+__version__ = '0.12.dev582+master.05c9738'
+
+# Automatically added by katversion
+__version__ = '0.12.dev582+spr1.2807.prepare.for.v0.12.05c9738.dirty'
+
+# Automatically added by katversion
+__version__ = '0.12.dev586+spr1.2766.redis.regression.a23629a'
+
+# Automatically added by katversion
+__version__ = '0.12'
+
+# Automatically added by katversion
+__version__ = '0.13.dev589+spr1.2766.prepare.for.0.13.3512d58.dirty'
+
+# Automatically added by katversion
+__version__ = '0.13.dev589+spr1.2766.prepare.for.0.13.ecf0c58.dirty'
+
+# Automatically added by katversion
+__version__ = '0.13.dev593+spr1.2766.prepare.for.0.13.af3e7a6'
+
+# Automatically added by katversion
+__version__ = '0.13'
