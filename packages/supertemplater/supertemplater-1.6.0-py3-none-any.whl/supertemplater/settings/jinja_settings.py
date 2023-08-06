311397ca31b235c46bf404efcf7babae028c4c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:06 +0000

    Add package supertemplater/1.6.0/supertemplater-1.6.0-py3-none-any.whl

---
+++ packages/supertemplater/supertemplater-1.6.0-py3-none-any.whl/supertemplater/settings/jinja_settings.py
@@ -0,0 +1,8 @@
+from supertemplater.models.base import BaseModel
+
+
+class JinjaSettings(BaseModel):
+    block_start_string: str = "{*"
+    block_end_string: str = "*}"
+    variable_start_string: str = "{@"
+    variable_end_string: str = "@}"
