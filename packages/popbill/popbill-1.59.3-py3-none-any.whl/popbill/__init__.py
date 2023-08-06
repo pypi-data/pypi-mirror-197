9ce540f204468551133f25a10b4e3dc19070d4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:36 +0000

    Add package popbill/1.59.3/popbill-1.59.3-py3-none-any.whl

---
+++ packages/popbill/popbill-1.59.3-py3-none-any.whl/popbill/__init__.py
@@ -0,0 +1,74 @@
+__version__ = "1.59.3"
+Version = __version__  # for backward compatibility
+__all__ = [
+    "PopbillException",
+    "JoinForm",
+    "ContactInfo",
+    "CorpInfo",
+    "TaxinvoiceService",
+    "Taxinvoice",
+    "TaxinvoiceDetail",
+    "Contact",
+    "FaxService",
+    "FaxReceiver",
+    "FileData",
+    "StatementService",
+    "Statement",
+    "StatementDetail",
+    "CashbillService",
+    "Cashbill",
+    "MessageService",
+    "MessageReceiver",
+    "HTTaxinvoiceService",
+    "HTCashbillService",
+    "ClosedownService",
+    "BizInfoCheckService",
+    "KakaoService",
+    "KakaoReceiver",
+    "KakaoButton",
+    "EasyFinBankService",
+    "BankAccountInfo",
+    "AccountCheckService",
+    "Response",
+    "UseHistory",
+    "RefundHistoryResult",
+    "RefundHistory",
+    "RefundForm",
+    "PaymentHistoryResult",
+    "PaymentResponse",
+    "PaymentForm",
+    "PaymentHistory",
+    "IssueResponse",
+    "BulkCashbillResult",
+    "BulkCashbillIssueResult",
+    "CashbillInfo",
+    "CBSearchResult",
+]
+
+from .accountCheckService import *
+from .base import (
+    ContactInfo,
+    CorpInfo,
+    JoinForm,
+    PaymentForm,
+    PaymentHistory,
+    PaymentHistoryResult,
+    PaymentResponse,
+    PopbillException,
+    RefundForm,
+    RefundHistory,
+    RefundHistoryResult,
+    Response,
+    UseHistory,
+)
+from .bizInfoCheckService import *
+from .cashbillService import *
+from .closedownService import *
+from .easyFinBankService import *
+from .faxService import *
+from .htCashbillService import *
+from .htTaxinvoiceService import *
+from .kakaoService import *
+from .messageService import *
+from .statementService import *
+from .taxinvoiceService import *
