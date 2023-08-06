48513698aff1195ebc814af976c1d0ab5fa9b246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package GeneticEngine/0.7.0/GeneticEngine-0.7.0-py3-none-any.whl

---
+++ packages/GeneticEngine/GeneticEngine-0.7.0-py3-none-any.whl/geneticengine/algorithms/callbacks/csv_callback.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import csv
+from typing import Any
+from typing import Callable
+
+from geneticengine.algorithms.callbacks.callback import Callback
+from geneticengine.algorithms.gp.individual import Individual
+
+
+class CSVCallback(Callback):
+    """Callback that outputs to a given CSV file."""
+
+    def __init__(
+        self,
+        filename: str = "evolution_results.csv",
+        filter_population: Callable[[list[Individual]], list[Individual]] = lambda x: x,
+        only_record_best_ind: bool = True,
+        extra_columns: dict[
+            str,
+            Callable[[int, list[Individual], float, Any, Individual], Any],
+        ]
+        | None = None,
+    ):
+        self.filename = filename
+        self.filter_population = filter_population
+        self.time = 0.0
+        self.only_record_best_ind = only_record_best_ind
+        self.extra_columns: dict[str, Callable[[int, list[Individual], float, Any, Individual], Any]] = (
+            extra_columns if extra_columns else {}
+        )
+        self.write_header()
+
+    def end_evolution(self):
+        self.outfile.close()
+
+    def write_header(self):
+        self.outfile = open(f"{self.filename}", "w", newline="")
+        self.writer = csv.writer(self.outfile)
+        row = [
+            "Fitness",
+            "Depth",
+            "Nodes",
+            "Generations",
+            "Execution Time",
+            "Seed",
+        ]
+        for name in self.extra_columns:
+            row.append(name)
+        self.writer.writerow(row)
+
+    def process_iteration(self, generation: int, population, time: float, gp):
+        pop = self.filter_population(population)
+        if self.only_record_best_ind:
+            pop = [gp.get_best_individual(gp.problem, population)]
+        self.time = time
+        for ind in pop:
+            phenotype = ind.get_phenotype()
+            if hasattr(phenotype, "gengy_distance_to_term"):
+                depth = phenotype.gengy_distance_to_term
+            else:
+                depth = -1
+            if hasattr(phenotype, "gengy_nodes"):
+                nodes = phenotype.gengy_nodes
+            else:
+                nodes = -1
+            row = [
+                ind.get_fitness(gp.problem),
+                depth,
+                nodes,
+                generation,
+                self.time,
+                gp.random_source.seed,
+            ]
+
+            for (name, fun) in self.extra_columns.items():
+                row.append(fun(generation, population, time, gp, ind))
+
+            self.writer.writerow([str(x) for x in row])
---
+++ packages/GeneticEngine/GeneticEngine-0.7.0-py3-none-any.whl/geneticengine/algorithms/gp/individual.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import Callable
+from typing import Generic
+from typing import TypeVar
+import weakref
+from geneticengine.core.generic_utils import GenericWrapper
+
+from geneticengine.core.problems import Fitness, Problem
+
+G = TypeVar("G")
+P = TypeVar("P")
+
+
+class IndividualNotEvaluatedException(Exception):
+    pass
+
+
+class Individual(Generic[G, P]):
+    genotype: G
+    genotype_to_phenotype: GenericWrapper[Callable[[G], P]]
+    phenotype: P | None = None
+
+    def __init__(self, genotype: G, genotype_to_phenotype: Callable[[G], P]):
+        self.genotype = genotype
+        self.genotype_to_phenotype = GenericWrapper(genotype_to_phenotype)
+        self.fitness_store: weakref.WeakKeyDictionary[Problem, Fitness] = weakref.WeakKeyDictionary()
+
+    def get_phenotype(self):
+        if self.phenotype is None:
+            self.phenotype = self.genotype_to_phenotype.get()(self.genotype)
+        return self.phenotype
+
+    def has_fitness(self, problem: Problem) -> bool:
+        return problem in self.fitness_store
+
+    def set_fitness(self, problem: Problem, fitness: Fitness):
+        self.fitness_store[problem] = fitness
+
+    def get_fitness(self, problem: Problem) -> Fitness:
+        if problem in self.fitness_store:
+            return self.fitness_store[problem]
+        else:
+            raise IndividualNotEvaluatedException()
+
+    @staticmethod
+    def key_function(problem: Problem):
+        def kf(ind):
+            return problem.key_function(ind.get_phenotype())
+
+        return kf
+
+    def __str__(self) -> str:
+        return f"{self.genotype}"
