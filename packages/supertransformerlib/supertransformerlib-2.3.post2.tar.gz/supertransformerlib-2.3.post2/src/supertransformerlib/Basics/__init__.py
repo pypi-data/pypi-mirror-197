a9fb3dec83d3fe49a9ccf3ef5da97758d98be3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:48 +0000

    Add package supertransformerlib/2.3.post2/supertransformerlib-2.3.post2.tar.gz

---
+++ packages/supertransformerlib/supertransformerlib-2.3.post2.tar.gz/supertransformerlib-2.3.post2/src/supertransformerlib/Basics/__init__.py
@@ -0,0 +1,7 @@
+
+
+from .local_sample import *
+from .linear import *
+from .memory_tensor import *
+from .multiheaded_attention import *
+from .feedforward import *
---
+++ packages/supertransformerlib/supertransformerlib-2.3.post2.tar.gz/supertransformerlib-2.3.post2/src/supertransformerlib/__init__.py
@@ -0,0 +1,10 @@
+"""
+A library for fast, smart transformers.
+
+
+"""
+
+__version__ = "2.3r2"
+
+from . import Core # noqa
+from . import Basics #noqa
