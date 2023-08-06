8315a0ff4fd5e627ae0514412360f0b7ee6536df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:43 +0000

    Add package hierarchybuilder/0.0.1/hierarchybuilder-0.0.1.tar.gz

---
+++ packages/hierarchybuilder/hierarchybuilder-0.0.1.tar.gz/hierarchybuilder-0.0.1/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hierarchybuilder",
+    version="0.0.1",
+    author="Itay Yair",
+    author_email="iy24592@gmail.com",
+    description="implementation of hierarchy builder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/itayair//hierarchybuilder",
+    install_requires=[
+        'fastapi==0.94.1',
+        'nltk==3.8.1',
+        'pandas==1.5.3',
+        'quantulum3==0.8.1',
+        'requests==2.28.2',
+        'scikit_learn==1.2.2',
+        'setuptools==60.2.0',
+        'spacy==3.0.9',
+        'torch==1.13.1',
+        'tqdm==4.64.1',
+        'transformers==4.26.1',
+        'uvicorn==0.21.0'
+    ],
+    dependency_links=['en-ud-model-sm @ https://storage.googleapis.com/en_ud_model/en_ud_model_sm-2.0.0.tar.gz'],
+    project_urls={
+        "Bug Tracker": "https://github.com/itayair/hierarchybuilder/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7"
+)
