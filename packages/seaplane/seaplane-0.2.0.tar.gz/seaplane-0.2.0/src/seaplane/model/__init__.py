4b8c2d1b21dca3701ac835593167bff66a702762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:47 +0000

    Add package seaplane/0.2.0/seaplane-0.2.0.tar.gz

---
+++ packages/seaplane/seaplane-0.2.0.tar.gz/seaplane-0.2.0/src/seaplane/model/__init__.py
@@ -0,0 +1,6 @@
+from .locks import *  # noqa
+from .metadata import *  # noqa
+from .provider import *  # noqa
+from .region import *  # noqa
+from .restrict import *  # noqa
+from .compute import * #noqa
