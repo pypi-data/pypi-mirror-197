6c38b5e0fad0fc56bbbc691a9b4d785621a7249f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:01 +0000

    Add package mael/0.0.3.4/mael-0.0.3.4.tar.gz

---
+++ packages/mael/mael-0.0.3.4.tar.gz/mael-0.0.3.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from codecs import open
+import os
+
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+# def _requirements():
+#     requirements_file_path = os.path.join(root_dir, 'requirements.txt')
+#     return [name.rstrip() for name in open(requirements_file_path).readlines()]
+#     install_requires = _requirements()
+
+# Get the long description from the README file
+readme_file_path = os.path.join(root_dir, 'README.rst')
+with open(readme_file_path, encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mael',
+    packages=['mael'],
+    # package_dir={'': 'mael'},
+    # packages=find_packages(where='mael'),
+    version='0.0.3.4',
+    py_modules=['mael/main', 'initializer', 'excel_builder', 'column_config'],
+    install_requires=[
+        'pyyaml',
+        'openpyxl'
+    ],
+    entry_points={
+        'console_scripts': ['mael=main:main'],
+    },
+    license='MIT',
+    author='Kenji Otsuka',
+    author_email='kok.fdcm@gmail.com',
+    description='A tool to convert markdown file to excel.',
+    long_description=long_description,
+    url='https://github.com/KenjiOhtsuka/mael'
+    # entry_points='''
+    #     [console_scripts]
+    #     myproject=myproject:cli
+    # ''',
+)
+
