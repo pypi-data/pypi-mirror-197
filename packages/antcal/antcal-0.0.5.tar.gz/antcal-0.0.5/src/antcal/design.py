7fb41d577e1cbdcdedd39d77a40cefb0f4298ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:33 +0000

    Add package antcal/0.0.5/antcal-0.0.5.tar.gz

---
+++ packages/antcal/antcal-0.0.5.tar.gz/antcal-0.0.5/src/antcal/design.py
@@ -0,0 +1,18 @@
+# %% Import
+from antcal.sim.hfss import HFSS, SOLUTIONS
+
+
+# %% Class
+class EMDesign:
+    """Represents a EM design"""
+
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def variables(self):
+        pass
+
+    @variables.setter
+    def variables(self):
+        pass
