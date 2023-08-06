1251bb653d969972a33bedf3da65658182b0fe86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:55 +0000

    Add package rna-seq-normalization/0.3.0/rna_seq_normalization-0.3.0.tar.gz

---
+++ packages/rna-seq-normalization/rna_seq_normalization-0.3.0.tar.gz/rna_seq_normalization-0.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rna_seq_normalization',
+    version='0.3.0',
+    description='Python package used for performing RNA-Seq normalization. It contains the three most popular techniques: TPM, RPKM and CPM.',
+    author='Sergio Vazquez Montes de Oca',
+    author_email='sergiovazquezmontesdeoca@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords=['rna-seq', 'rna', 'tpm', 'normalization', 'rpkm', 'cpm', 'rna-sequencing', 'statistics', 'bioinformatics'
+              'data analysis', 'data science'],
+    install_requires=[],
+    python_requires='>=3.6',
+)
