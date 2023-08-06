09eb1f886263eb985911799dc576e18b0d36330b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:15 +0000

    Add package donv/0.1.13/donv-0.1.13-py3-none-any.whl

---
+++ packages/donv/donv-0.1.13-py3-none-any.whl/donv/attach.py
@@ -0,0 +1,19 @@
+from . import option
+from .base import Docker_Base
+
+class Docker_Attach(Docker_Base):
+    def __init__(self, opt):
+        super(Docker_Attach, self).__init__(opt)
+        self.set_cmd()
+
+    def set_cmd(self):
+        self.add_option(f'docker attach {self.opt.name}')
+
+def main():
+    opt = option.Options('name').get_option()
+    docker = Docker_Attach(opt)
+    docker.print_cmd()
+    docker.do_cmd()
+
+if __name__ == '__main__':
+    main()
