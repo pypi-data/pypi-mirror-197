67d0bc264f4b282f9b85a4d497ea42af32e7d089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:03 +0000

    Add package oaklib/0.2.3/oaklib-0.2.3.tar.gz

---
+++ packages/oaklib/oaklib-0.2.3.tar.gz/oaklib-0.2.3/src/oaklib/implementations/sparql/sparql_implementation.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+import rdflib
+
+from oaklib.implementations.sparql.abstract_sparql_implementation import (
+    AbstractSparqlImplementation,
+)
+from oaklib.interfaces.differ_interface import DifferInterface
+from oaklib.interfaces.mapping_provider_interface import MappingProviderInterface
+from oaklib.interfaces.obograph_interface import OboGraphInterface
+from oaklib.interfaces.patcher_interface import PatcherInterface
+from oaklib.interfaces.search_interface import SearchInterface
+from oaklib.interfaces.semsim_interface import SemanticSimilarityInterface
+from oaklib.interfaces.taxon_constraint_interface import TaxonConstraintInterface
+
+
+@dataclass
+class SparqlImplementation(
+    AbstractSparqlImplementation,
+    DifferInterface,
+    SearchInterface,
+    MappingProviderInterface,
+    OboGraphInterface,
+    PatcherInterface,
+    SemanticSimilarityInterface,
+    TaxonConstraintInterface,
+):
+    """
+    Wraps any local or remote sparql endpoint
+    """
+
+    def __post_init__(self):
+        if self.graph is None:
+            resource = self.resource
+            # print(resource)
+            if resource.url:
+                super(SparqlImplementation, self).__post_init__()
+            else:
+                graph = rdflib.Graph()
+                if resource is not None:
+                    graph.parse(str(resource.local_path), format=resource.format)
+                    self.graph = graph
