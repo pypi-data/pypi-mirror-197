c96a2763c033b1a230baff3a7dcc75762b58f67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:00 +0000

    Add package edx-arch-experiments/1.1.2/edx_arch_experiments-1.1.2-py2.py3-none-any.whl

---
+++ packages/edx-arch-experiments/edx_arch_experiments-1.1.2-py2.py3-none-any.whl/edx_arch_experiments/__init__.py
@@ -0,0 +1,7 @@
+"""
+A plugin to include applications under development by the architecture team at edx .
+"""
+
+__version__ = '1.1.2'
+
+default_app_config = 'edx_arch_experiments.apps.EdxArchExperimentsConfig'  # pylint: disable=invalid-name
