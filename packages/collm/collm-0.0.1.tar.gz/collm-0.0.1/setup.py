9ad7f732fa51383d149cea5a33cb2bc9baf502ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:54 +0000

    Add package collm/0.0.1/collm-0.0.1.tar.gz

---
+++ packages/collm/collm-0.0.1.tar.gz/collm-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="collm",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Razvan Dinu",
+    author_email="rdinu@nvidia.com",
+    description="CoLLM",
+    long_description="To be provided.",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
