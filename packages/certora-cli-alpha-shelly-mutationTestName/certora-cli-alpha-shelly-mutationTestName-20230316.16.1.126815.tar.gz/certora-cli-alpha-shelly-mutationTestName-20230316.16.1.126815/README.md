02fd3e06908c0a7bd7e6bff25cf6bcbdb9312e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:55 +0000

    Add package certora-cli-alpha-shelly-mutationTestName/20230316.16.1.126815/certora-cli-alpha-shelly-mutationTestName-20230316.16.1.126815.tar.gz

---
+++ packages/certora-cli-alpha-shelly-mutationTestName/certora-cli-alpha-shelly-mutationTestName-20230316.16.1.126815.tar.gz/certora-cli-alpha-shelly-mutationTestName-20230316.16.1.126815/README.md
@@ -0,0 +1,1 @@
+Commit ff59b25. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.
---
+++ packages/certora-cli-alpha-shelly-mutationTestName/certora-cli-alpha-shelly-mutationTestName-20230316.16.1.126815.tar.gz/certora-cli-alpha-shelly-mutationTestName-20230316.16.1.126815/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="certora-cli-alpha-shelly-mutationTestName",
+        version="20230316.16.1.126815",
+        author="Certora",
+        author_email="support@certora.com",
+        description="Runner for the Certora Prover",
+        long_description="Commit ff59b25. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.",
+        long_description_content_type="text/markdown",
+        url="https://pypi.org/project/certora-cli-alpha-shelly-mutationTestName",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=['tabulate', 'requests', 'pycryptodome', 'tqdm', 'click', 'sly', 'argcomplete'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={
+            "console_scripts": [
+                "certoraRun = certora_cli.certoraRun:entry_point",
+                "certoraMutate = certora_cli.certoraMutate:ext_gambit_entry_point"
+            ]
+        },
+        python_requires='>=3.8',
+    )
+        
