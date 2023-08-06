63635f1067fc85ee6adee0ce4fbca83b34a62cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:51 +0000

    Add package cdk-cloudformation-newrelic-agent-configuration/1.1.0a7/cdk-cloudformation-newrelic-agent-configuration-1.1.0a7.tar.gz

---
+++ packages/cdk-cloudformation-newrelic-agent-configuration/cdk-cloudformation-newrelic-agent-configuration-1.1.0a7.tar.gz/cdk-cloudformation-newrelic-agent-configuration-1.1.0a7/setup.py
@@ -0,0 +1,65 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-cloudformation-newrelic-agent-configuration",
+    "version": "1.1.0.a7",
+    "description": "Manage New Relic Server-Side Agent Configuration",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws-ia/cloudformation-newrelic-resource-providers",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cdklabs/cdk-cloudformation.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_cloudformation_newrelic_agent_configuration",
+        "cdk_cloudformation_newrelic_agent_configuration._jsii"
+    ],
+    "package_data": {
+        "cdk_cloudformation_newrelic_agent_configuration._jsii": [
+            "newrelic-agent-configuration@1.1.0-alpha.7.jsii.tgz"
+        ],
+        "cdk_cloudformation_newrelic_agent_configuration": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "aws-cdk-lib>=2.69.0, <3.0.0",
+        "constructs>=10.1.280, <11.0.0",
+        "jsii>=1.78.1, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
