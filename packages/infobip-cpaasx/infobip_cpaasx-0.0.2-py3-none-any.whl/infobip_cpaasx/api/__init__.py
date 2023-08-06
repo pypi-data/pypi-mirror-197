50fbdfd07d31b6e8289152b2e15f185b4c0f1fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:52 +0000

    Add package infobip-cpaasx/0.0.2/infobip_cpaasx-0.0.2-py3-none-any.whl

---
+++ packages/infobip-cpaasx/infobip_cpaasx-0.0.2-py3-none-any.whl/infobip_cpaasx/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from infobip_cpaasx.api.application_api import ApplicationApi
+from infobip_cpaasx.api.entity_api import EntityApi
+from infobip_cpaasx.api.mms_api import MmsApi
+from infobip_cpaasx.api.number_registration_api import NumberRegistrationApi
+from infobip_cpaasx.api.numbers_api import NumbersApi
+from infobip_cpaasx.api.resource_association_api import ResourceAssociationApi
+from infobip_cpaasx.api.sms_api import SmsApi
