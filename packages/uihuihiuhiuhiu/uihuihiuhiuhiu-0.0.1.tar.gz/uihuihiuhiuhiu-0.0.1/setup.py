ba1e31dc5a6668988b87efa18d677ad5dbce7ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:03 +0000

    Add package uihuihiuhiuhiu/0.0.1/uihuihiuhiuhiu-0.0.1.tar.gz

---
+++ packages/uihuihiuhiuhiu/uihuihiuhiuhiu-0.0.1.tar.gz/uihuihiuhiuhiu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='uihuihiuhiuhiu',
+    version='0.0.1',
+    packages=['uihuihiuhiuhiu'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
