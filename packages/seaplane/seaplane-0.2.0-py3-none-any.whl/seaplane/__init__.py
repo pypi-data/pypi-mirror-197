56c357ea86d813b9b1c807f8f21ca68f9b8498e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:46 +0000

    Add package seaplane/0.2.0/seaplane-0.2.0-py3-none-any.whl

---
+++ packages/seaplane/seaplane-0.2.0-py3-none-any.whl/seaplane/__init__.py
@@ -0,0 +1,34 @@
+from .api.lock_api import LockAPI
+from .api.metadata_api import MetadataAPI
+from .api.restrict_api import RestrictAPI
+from .api.compute_api import ComputeAPI
+from .api.token_api import TokenAPI
+from .configuration import Configuration, config
+
+
+class Seaplane:
+    @property
+    def config(self) -> Configuration:
+        return config
+
+    @property
+    def auth(self) -> TokenAPI:
+        return config._token_api
+
+    @property
+    def metadata(self) -> MetadataAPI:
+        return MetadataAPI(config)
+
+    @property
+    def locks(self) -> LockAPI:
+        return LockAPI(config)
+
+    @property
+    def restrict(self) -> RestrictAPI:
+        return RestrictAPI(config)
+
+    @property
+    def compute(self) -> ComputeAPI:
+        return ComputeAPI(config)
+
+sea = Seaplane()
