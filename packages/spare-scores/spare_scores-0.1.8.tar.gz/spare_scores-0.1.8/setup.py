f19a2281d5a411337c3cb81ecf5961a47332d693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:25 +0000

    Add package spare-scores/0.1.8/spare_scores-0.1.8.tar.gz

---
+++ packages/spare-scores/spare_scores-0.1.8.tar.gz/spare_scores-0.1.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='spare_scores',
+      version='0.1.8',
+      description='Compute characteristic brain signatures of your case population.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Gyujoon Hwang',
+      author_email='ghwang1106@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={'spare_scores':['mdl/*.pkl.gz','data/*.csv']},
+      include_package_data=True,
+      install_requires=['numpy==1.20.3',
+                        'pandas==1.5.3',
+                        'scikit-learn==0.23.2']
+      )
