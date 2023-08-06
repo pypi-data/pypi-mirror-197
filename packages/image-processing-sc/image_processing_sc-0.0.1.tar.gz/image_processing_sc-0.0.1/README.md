020e7d7f499e2b01c1dbc2f657c3cb5622eba99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package image-processing-sc/0.0.1/image_processing_sc-0.0.1.tar.gz

---
+++ packages/image-processing-sc/image_processing_sc-0.0.1.tar.gz/image_processing_sc-0.0.1/README.md
@@ -0,0 +1,22 @@
+# package_name
+
+Description. 
+The package package_name is used to:
+	- 
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install image_processing_sc
+```
+
+
+
+## Author
+SauloCarvalho
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/image-processing-sc/image_processing_sc-0.0.1.tar.gz/image_processing_sc-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing_sc",
+    version="0.0.1",
+    author="SauloCarvalho",
+    description="Image Processing Program",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.5',
+)
