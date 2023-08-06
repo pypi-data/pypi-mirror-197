da74ae902247f2ca37fe3d244d44961fa99ce4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:23 +0000

    Add package typernexrad-cli/0.1.0/typernexrad_cli-0.1.0-py3-none-any.whl

---
+++ packages/typernexrad-cli/typernexrad_cli-0.1.0-py3-none-any.whl/pages/01_login.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import requests
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import webbrowser
+
+load_dotenv()
+
+
+
+no_sidebar_style = """
+    <style> 
+        div[data-testid="stSidebarNav"] {display: none;}
+        </style>
+        
+        """
+block_sidebar_style = """
+    <style> 
+        div[data-testid="stSidebarNav"] {display: block;}
+        </style>
+        
+        """
+
+st.set_page_config(page_title="Login", initial_sidebar_state="collapsed")
+st.markdown(no_sidebar_style, unsafe_allow_html=True)
+with st.container():
+        Username = st.text_input('Enter your username')
+        Password = st.text_input('Enter your password', type='password')
+        if st.button('Verify'):
+            if Username == "" or Password == "":
+                st.error('Username or Password value is empty')
+            else:
+                data = {
+                "grant_type": "password",
+                "username": Username,
+                "password": Password
+                }
+                response = requests.post('http://3.235.95.244:8000/login',data=data)
+                if int(response.json()['status_code']) == 200:
+                    os.environ["access_token"] = response.json()["access_token"]
+                    requests.post('http://localhost:8000/update_login',headers={"Authorization": f"Bearer {response.json()['access_token']}"})
+                    # with open(".env", "a") as f:
+                    #     f.write(f"access_token={response.json()['access_token']}\n")
+
+                    with open(".env", "r") as f:
+                        lines = f.readlines()
+
+                    # Find the line that contains the access token
+                    for i, line in enumerate(lines):
+                        if line.startswith("access_token="):
+                            # Replace the access token with the new value
+                            lines[i] = "access_token=" + response.json()['access_token'] + "\n"
+                            break
+
+                    # Write the modified lines back to the file
+                    with open(".env", "w") as f:
+                        f.writelines(lines)
+                    st.success('Login Successful')
+                    # show sidebar
+                    st.markdown(block_sidebar_style, unsafe_allow_html=True)
+
+
+                elif int(response.json()['status_code']) == 404:
+                    st.error('Username not found in the database')
+                else:
+                    st.error('Password is incorrect')
+
+if st.button("Signup"):
+     webbrowser.open("http://localhost:8501/User_Signup")
---
+++ packages/typernexrad-cli/typernexrad_cli-0.1.0-py3-none-any.whl/pages/Nexrad_plot.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import plotly.express as px
+import sqlite3
+import os
+import requests
+import streamlit as st
+import os
+from dotenv import load_dotenv
+import webbrowser
+
+load_dotenv()
+
+def create_plot_table():
+    response = requests.post('http://3.235.95.244:8000/create_plot_table')
+    return response
+
+with st.sidebar:
+    if st.button("Logout"):
+        webbrowser.open("http://streamlit:8501/login")
+
+create_plot_table()
+ACCESS_TOKEN = os.environ["access_token"]
+headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+
+st.title("NEXRAD Station Locations")
+FASTAPI_URL='http://3.235.95.244:8000/user_api_status'
+input={'api_name':'nexrad_plot'}
+response=requests.post(FASTAPI_URL,json=input,headers=headers)
+            
+if response.status_code==200:
+    response = requests.post('http://3.235.95.244:8000/retrieve_plot_data',headers=headers)
+
+        
+
+response = requests.post('http://3.235.95.244:8000/retrieve_plot_data',headers=headers)
+
+if response.status_code == 200:
+    df = pd.DataFrame(response.json()['df_dict'])
+    #sql_query = pd.read_sql('SELECT * FROM NEXRAD_PLOT', con)
+    #df = pd.DataFrame(sql_query, columns = ['Id', 'State', 'City','ICAO_Location_Identifier','Coordinates','Lat','Lon'])
+    fig = px.scatter_geo(df,lat='Lat',lon='Lon')
+    fig.update_layout(title = 'Nexrad Locations', title_x=0.5)
+    st.plotly_chart(fig, use_container_width=True)
+else:
+    st.error('Either you have not logged in or else your session has expired.', icon="🚨")
