a128dbef8f89101bf02ecaa4dc2e5b04bfe15c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:15 +0000

    Add package hil-core/0.0.1/hil_core-0.0.1-cp37-cp37m-win_amd64.whl

---
+++ packages/hil-core/hil_core-0.0.1-cp37-cp37m-win_amd64.whl/hil/core/__init__.py
@@ -0,0 +1,16 @@
+'''
+Copyright (c) 2023 by HIL Group
+Author: hufeng.mao@carota.ai
+Date: 2023-03-16 16:13:35
+LastEditors: hufeng.mao@carota.ai
+LastEditTime: 2023-03-16 17:24:32
+Description: 
+
+==========  =============  ================
+When        Who            What and why
+==========  =============  ================
+
+==========  =============  ================
+'''
+from .uds_client import UdsClient
+from .doip_client import DoipClient
