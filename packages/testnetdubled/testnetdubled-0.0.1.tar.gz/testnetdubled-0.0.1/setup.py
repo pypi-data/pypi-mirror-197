f3f998e1dd4376dee781f0dd60c778f55731494c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:40 +0000

    Add package testnetdubled/0.0.1/testnetdubled-0.0.1.tar.gz

---
+++ packages/testnetdubled/testnetdubled-0.0.1.tar.gz/testnetdubled-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='testnetdubled',
+    version='0.0.1',
+    packages=['testnetdubled'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
