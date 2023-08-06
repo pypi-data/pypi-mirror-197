c0dca78d40a64849f2f16cb9e74e24367f4d4b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:57 +0000

    Add package simpleworkspace/1.1.153/SimpleWorkspace-1.1.153.tar.gz

---
+++ packages/simpleworkspace/SimpleWorkspace-1.1.153.tar.gz/SimpleWorkspace-1.1.153/src/SimpleWorkspace/Utility/Strings.py
@@ -0,0 +1,26 @@
+
+
+import hashlib as _hashlib
+import string as _string
+
+def Hash(text:str, hashFunc=_hashlib.sha256()) -> str:
+    hashFunc.update(text.encode())
+    return hashFunc.hexdigest()
+
+def Random(length:int, charset=_string.ascii_letters + _string.digits):
+    '''
+    Generates random string with a specified length and consisting of specified charset
+    @param charset: 
+        * a simple string of different characters to sample the random string from.
+            repeating same characters multiple times, increases probability of that character being picked more often.
+        * You can enter your own characters or/and combine with defaults below.
+            * string.ascii_lowercase - a-z
+            * string.ascii_uppercase - A-Z
+            * string.ascii_letters - lowercase + uppercase
+            * string.digits - 0-9
+            * string.punctuation -- all symbols
+            * string.printable -- a string containing all ASCII characters considered printable
+    '''
+    import random
+
+    return ''.join([random.choice(charset) for _ in range(length)])
