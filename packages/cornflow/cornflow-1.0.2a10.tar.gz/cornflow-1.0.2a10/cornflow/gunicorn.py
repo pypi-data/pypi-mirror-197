0a70348c5f9fb7081f224576f5a27d44a7db590a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:37 +0000

    Add package cornflow/1.0.2a10/cornflow-1.0.2a10.tar.gz

---
+++ packages/cornflow/cornflow-1.0.2a10.tar.gz/cornflow-1.0.2a10/cornflow/gunicorn.py
@@ -0,0 +1,27 @@
+"""gunicorn WSGI server configuration."""
+from gevent import monkey
+
+monkey.patch_all(ssl=False)
+
+import os
+from multiprocessing import cpu_count
+
+
+def max_workers():
+    return cpu_count()
+
+
+pidfile = "/usr/src/app/gunicorn.pid"
+chdir = "/usr/src/app"
+bind = "0.0.0.0:5000"
+max_requests = 1000
+worker_class = "gevent"
+workers = 3
+timeout = 300
+keepalive = 300
+graceful_timeout = 300
+log_level = "info"
+
+if os.getenv("CORNFLOW_LOGGING") == "file":
+    accesslog = "/usr/src/app/log/info.log"
+    errorlog = "/usr/src/app/log/error.log"
