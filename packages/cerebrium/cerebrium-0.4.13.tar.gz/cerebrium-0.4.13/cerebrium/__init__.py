b7ca19255f6b80814bc673ff27662c9323e04e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:54 +0000

    Add package cerebrium/0.4.13/cerebrium-0.4.13.tar.gz

---
+++ packages/cerebrium/cerebrium-0.4.13.tar.gz/cerebrium-0.4.13/cerebrium/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.4.13"
+
+from cerebrium.core import deploy, model_api_request
+from cerebrium.flow import ModelType as model_type
+from cerebrium.conduit import Conduit, Hardware as hardware
+from cerebrium.logging.base import LoggingPlatform as logging_platform
