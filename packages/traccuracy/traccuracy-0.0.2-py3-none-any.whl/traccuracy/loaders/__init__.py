8502394b8ae60774c6071e4bbf7b87d40caa743e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:39 +0000

    Add package traccuracy/0.0.2/traccuracy-0.0.2-py3-none-any.whl

---
+++ packages/traccuracy/traccuracy-0.0.2-py3-none-any.whl/traccuracy/loaders/__init__.py
@@ -0,0 +1,10 @@
+""" Subpackage for loading tracking data into memory
+
+This subpackage contains functions for loading ground
+truth or tracking method outputs into memory as TrackingData objects.
+Each loading function must return one TrackingData object which has a
+track graph and optionally contains a corresponding segmentation.
+"""
+from ._ctc import load_ctc_data
+
+__all__ = ["load_ctc_data"]
---
+++ packages/traccuracy/traccuracy-0.0.2-py3-none-any.whl/traccuracy/metrics/__init__.py
@@ -0,0 +1,4 @@
+from ._ctc import AOGMMetrics, CTCMetrics
+from ._divisions import DivisionMetrics
+
+__all__ = ["CTCMetrics", "DivisionMetrics", "AOGMMetrics"]
