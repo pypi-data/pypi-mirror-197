a2456160a738a328ee565b48303fa35d8e96322b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:07 +0000

    Add package pyCftool/0.0.22/pyCftool-0.0.22.tar.gz

---
+++ packages/pyCftool/pyCftool-0.0.22.tar.gz/pyCftool-0.0.22/pyCftool/loadgui.py
@@ -0,0 +1,13 @@
+
+from PySide6 import QtCore, QtUiTools
+from . import mplwidget
+from mplwidget import MplWidget
+
+def loadUiWidget(uifilename, parent=None):
+    loader = QtUiTools.QUiLoader()
+    uifile = QtCore.QFile(uifilename)
+    uifile.open(QtCore.QFile.ReadOnly)
+    loader.registerCustomWidget(MplWidget)
+    ui = loader.load(uifile, parent)
+    uifile.close()
+    return ui
