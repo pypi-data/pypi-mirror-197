818517f30db370585dcfb4d276f80fd5e0c5dfc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:28 +0000

    Add package pyeoskit/1.1.12/pyeoskit-1.1.12-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyeoskit/pyeoskit-1.1.12-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyeoskit/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from .http_client import HttpClient
+from .rpc_interface import RPCInterface, WalletClient
+from .chainapi_sync import ChainApi
+from . import _pyeoskit
+
+__version__='1.1.12'
+
+_pyeoskit.init()
+
+eosapi = ChainApi()
