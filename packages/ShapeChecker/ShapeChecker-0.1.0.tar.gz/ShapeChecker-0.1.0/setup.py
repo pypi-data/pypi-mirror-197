ca20006fb48c57a784ea8867a0b71272d6907558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:35 +0000

    Add package ShapeChecker/0.1.0/ShapeChecker-0.1.0.tar.gz

---
+++ packages/ShapeChecker/ShapeChecker-0.1.0.tar.gz/ShapeChecker-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='ShapeChecker',
+    version='0.1.0',
+    description='ShapeChecker assist you when doing tensors manipulation',
+    url='https://github.com/Etienne-Meunier/ShapeChecker',
+    author='Etienne Meunier',
+    author_email='etiennemeunier@live.fr',
+    license='BSD 2-clause',
+    packages=['ShapeChecker'],
+    install_requires=['einops',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
