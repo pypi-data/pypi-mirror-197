6dd40be88bb36ad54783517275ebe2ba0314ab83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package yopycalc/0.0.3.2/yopycalc-0.0.3.2.tar.gz

---
+++ packages/yopycalc/yopycalc-0.0.3.2.tar.gz/yopycalc-0.0.3.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+#Adding code for packaging
+setuptools.setup(
+    include_package_data=True, 
+    name='yopycalc', 
+    version='0.0.3.2', 
+    description='yopycalc python module',
+    url='https://github.com/ravindra-rao/setuptoolsdemo.git',
+    author='yobro',
+    author_email='contact@yobro.com',
+    packages=setuptools.find_packages(),
+    install_requires=['pandas', 'pytest'
+    ], 
+    long_description='yopycalc python module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
