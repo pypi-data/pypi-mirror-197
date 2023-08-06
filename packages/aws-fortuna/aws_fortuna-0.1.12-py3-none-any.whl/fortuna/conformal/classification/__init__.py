5add10db4de681b2877e1a9e6bab50ae9f8bbc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:40 +0000

    Add package aws-fortuna/0.1.12/aws_fortuna-0.1.12-py3-none-any.whl

---
+++ packages/aws-fortuna/aws_fortuna-0.1.12-py3-none-any.whl/fortuna/conformal/classification/__init__.py
@@ -0,0 +1,6 @@
+from fortuna.conformal.classification.adaptive_prediction import \
+    AdaptivePredictionConformalClassifier
+from fortuna.conformal.classification.simple_prediction import \
+    SimplePredictionConformalClassifier
+from fortuna.conformal.classification.adaptive_conformal_classifier import AdaptiveConformalClassifier
+from fortuna.conformal.classification.batch_mvp import BatchMVPConformalClassifier
