9e953dbbf8a724e03afd3760de1585fb7e1eb3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:06 +0000

    Add package extract-samples/1.0.2/extract-samples-1.0.2.tar.gz

---
+++ packages/extract-samples/extract-samples-1.0.2.tar.gz/extract-samples-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("./README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="extract-samples",
+    version="1.0.2",
+    author="Hyeonjinkk",
+    author_email="ilhj1228@gmail.com",
+    description="random sample each file path",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/schooldevops/python-tutorials",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/schooldevops/python-tutorials/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where=""),
+    python_requires=">=3.6",
+)
