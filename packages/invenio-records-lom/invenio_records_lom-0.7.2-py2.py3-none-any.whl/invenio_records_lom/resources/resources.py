4d379735eac64d2af948710a540c47f4a64982f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:18 +0000

    Add package invenio-records-lom/0.7.2/invenio_records_lom-0.7.2-py2.py3-none-any.whl

---
+++ packages/invenio-records-lom/invenio_records_lom-0.7.2-py2.py3-none-any.whl/invenio_records_lom/resources/resources.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# invenio-records-lom is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""LOM resources."""
+
+from flask_resources import route
+from invenio_drafts_resources.resources import RecordResource
+
+
+class LOMRecordResource(RecordResource):
+    """LOM Record resource."""
+
+    def create_url_rules(self):
+        """Create the URL rules for the record resource."""
+
+        def prefix(route_str):
+            """Prefix a route with `self.config.prefix`."""
+            return f"{self.config.url_prefix}{route_str}"
+
+        routes = self.config.routes
+        return [
+            route("GET", prefix(routes["list"]), self.search),
+            route("GET", prefix(routes["item"]), self.read),
+            route("POST", prefix(routes["list"]), self.create),
+            route("PUT", prefix(routes["item-draft"]), self.update_draft),
+        ]
