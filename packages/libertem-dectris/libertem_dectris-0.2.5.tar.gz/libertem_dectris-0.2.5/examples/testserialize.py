d7665bf521ed852afb7375323383e597eda7a3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:11 +0000

    Add package libertem-dectris/0.2.5/libertem_dectris-0.2.5.tar.gz

---
+++ packages/libertem-dectris/libertem_dectris-0.2.5.tar.gz/libertem_dectris-0.2.5/examples/testserialize.py
@@ -0,0 +1,16 @@
+import libertem_dectris
+
+if __name__ == "__main__":
+    frames = libertem_dectris.FrameChunkedIterator(uri="tcp://127.0.0.1:9999")
+    frames.start()
+
+    frame_stack = frames.get_next_stack(max_size=32)
+    serialized = frame_stack.serialize()
+    loaded = libertem_dectris.FrameStack.deserialize(serialized)
+
+    print(serialized)
+    print(len(loaded))
+    print(loaded[0])
+    print(loaded[0].get_pixel_type())
+    print(loaded[0].get_encoding())
+    print(loaded[0].get_shape())
