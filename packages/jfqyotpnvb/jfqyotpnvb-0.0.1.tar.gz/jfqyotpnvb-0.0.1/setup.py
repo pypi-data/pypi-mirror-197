d7f13d4567e29d4165cd7ea05bf2ba770bad36c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:28 +0000

    Add package jfqyotpnvb/0.0.1/jfqyotpnvb-0.0.1.tar.gz

---
+++ packages/jfqyotpnvb/jfqyotpnvb-0.0.1.tar.gz/jfqyotpnvb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='jfqyotpnvb',
+    version='0.0.1',
+    packages=['jfqyotpnvb'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
