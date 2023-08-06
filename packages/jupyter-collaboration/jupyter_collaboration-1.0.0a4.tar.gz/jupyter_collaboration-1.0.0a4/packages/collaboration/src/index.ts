6c9eac669222068ce3fa78a4acd5c5251406cbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:48 +0000

    Add package jupyter-collaboration/1.0.0a4/jupyter_collaboration-1.0.0a4.tar.gz

---
+++ packages/jupyter-collaboration/jupyter_collaboration-1.0.0a4.tar.gz/jupyter_collaboration-1.0.0a4/packages/collaboration/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+/**
+ * @packageDocumentation
+ * @module collaboration
+ */
+
+export * from './tokens';
+export * from './menu';
+export * from './userinfopanel';
+export * from './collaboratorspanel';
