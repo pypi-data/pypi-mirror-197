ec6ba3e5b936ad8ea47f31b1d5b8ab20b1ab9962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:48 +0000

    Add package kmi2122/0.1.10/kmi2122-0.1.10.tar.gz

---
+++ packages/kmi2122/kmi2122-0.1.10.tar.gz/kmi2122-0.1.10/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kmi2122",
+    version="0.1.10",
+    author="parkminwoo",
+    author_email="parkminwoo1991@gmail.com",
+    description="This dataset includes some macroeconomic indicators for South Korea in 2021-2022.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DSDanielPark/kmi2122-dataset",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=["jsons", "pandas"],
+    include_package_data=True,
+    data_files=[('kmi2122_dataset', ['kmi2122_dataset/dataset.json'])]
+    )
