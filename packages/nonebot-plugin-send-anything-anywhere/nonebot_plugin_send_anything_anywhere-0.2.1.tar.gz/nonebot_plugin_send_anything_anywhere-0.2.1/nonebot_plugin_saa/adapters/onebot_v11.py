b40e72c2e7946668de92749e0f1c8bd295120e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:43 +0000

    Add package nonebot-plugin-send-anything-anywhere/0.2.1/nonebot_plugin_send_anything_anywhere-0.2.1.tar.gz

---
+++ packages/nonebot-plugin-send-anything-anywhere/nonebot_plugin_send_anything_anywhere-0.2.1.tar.gz/nonebot_plugin_send_anything_anywhere-0.2.1/nonebot_plugin_saa/adapters/onebot_v11.py
@@ -0,0 +1,109 @@
+from typing import Any
+from functools import partial
+
+from nonebot.adapters import Event
+
+from ..types import Text, Image, Reply, Mention
+from ..utils import (
+    TargetQQGroup,
+    MessageFactory,
+    PlatformTarget,
+    TargetQQPrivate,
+    SupportedAdapters,
+    SupportedPlatform,
+    MessageSegmentFactory,
+    register_sender,
+    register_ms_adapter,
+    register_convert_to_arg,
+    assamble_message_factory,
+    register_target_extractor,
+)
+
+try:
+    from nonebot.adapters.onebot.v11 import Bot as BotOB11
+    from nonebot.adapters.onebot.v11.message import Message, MessageSegment
+    from nonebot.adapters.onebot.v11 import (
+        MessageEvent,
+        GroupMessageEvent,
+        PrivateMessageEvent,
+    )
+
+    adapter = SupportedAdapters.onebot_v11
+    register_onebot_v11 = partial(register_ms_adapter, adapter)
+
+    MessageFactory.register_adapter_message(SupportedAdapters.onebot_v11, Message)
+
+    @register_onebot_v11(Text)
+    def _text(t: Text) -> MessageSegment:
+        return MessageSegment.text(t.data["text"])
+
+    @register_onebot_v11(Image)
+    async def _image(i: Image) -> MessageSegment:
+        return MessageSegment.image(i.data["image"])
+
+    @register_onebot_v11(Mention)
+    async def _mention(m: Mention) -> MessageSegment:
+        return MessageSegment.at(m.data["user_id"])
+
+    @register_onebot_v11(Reply)
+    async def _reply(r: Reply) -> MessageSegment:
+        return MessageSegment.reply(int(r.data["message_id"]))
+
+    @register_target_extractor(PrivateMessageEvent)
+    def _extract_private_msg_event(event: Event) -> TargetQQPrivate:
+        assert isinstance(event, PrivateMessageEvent)
+        return TargetQQPrivate(user_id=event.user_id)
+
+    @register_target_extractor(GroupMessageEvent)
+    def _extract_group_msg_event(event: Event) -> TargetQQGroup:
+        assert isinstance(event, GroupMessageEvent)
+        return TargetQQGroup(group_id=event.group_id)
+
+    @register_convert_to_arg(adapter, SupportedPlatform.qq_private)
+    def _gen_private(target: PlatformTarget) -> dict[str, Any]:
+        assert isinstance(target, TargetQQPrivate)
+        return {
+            "message_type": "private",
+            "user_id": target.user_id,
+        }
+
+    @register_convert_to_arg(adapter, SupportedPlatform.qq_group)
+    def _gen_group(target: PlatformTarget) -> dict[str, Any]:
+        assert isinstance(target, TargetQQGroup)
+        return {
+            "message_type": "group",
+            "group_id": target.group_id,
+        }
+
+    @register_sender(SupportedAdapters.onebot_v11)
+    async def send(
+        bot,
+        msg: MessageFactory[MessageSegmentFactory],
+        target,
+        event,
+        at_sender: bool,
+        reply: bool,
+    ):
+        assert isinstance(bot, BotOB11)
+        assert isinstance(target, TargetQQGroup | TargetQQPrivate)
+        if event:
+            assert isinstance(event, MessageEvent)
+            full_msg = assamble_message_factory(
+                msg,
+                Mention(event.get_user_id()),
+                Reply(event.message_id),
+                at_sender,
+                reply,
+            )
+        else:
+            full_msg = msg
+        message_to_send = Message()
+        for message_segment_factory in full_msg:
+            message_segment = await message_segment_factory.build(bot)
+            message_to_send += message_segment
+        await bot.send_msg(message=message_to_send, **target.arg_dict(bot))
+
+except ImportError:
+    pass
+except Exception as e:
+    raise e
