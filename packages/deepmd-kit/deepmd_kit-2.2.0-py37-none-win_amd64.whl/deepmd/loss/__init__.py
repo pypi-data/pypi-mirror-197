ecd4a000d835dc20e99111909aba0d9ea45f0271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:25 +0000

    Add package deepmd-kit/2.2.0/deepmd_kit-2.2.0-py37-none-win_amd64.whl

---
+++ packages/deepmd-kit/deepmd_kit-2.2.0-py37-none-win_amd64.whl/deepmd/loss/__init__.py
@@ -0,0 +1,13 @@
+from .ener import (
+    EnerDipoleLoss,
+    EnerStdLoss,
+)
+from .tensor import (
+    TensorLoss,
+)
+
+__all__ = [
+    "EnerDipoleLoss",
+    "EnerStdLoss",
+    "TensorLoss",
+]
