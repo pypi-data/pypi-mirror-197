6b15984ae015895486c9b4a169b0c0094d3d54ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:52 +0000

    Add package fakesnow/0.0.0/fakesnow-0.0.0.tar.gz

---
+++ packages/fakesnow/fakesnow-0.0.0.tar.gz/fakesnow-0.0.0/tests/conftest.py
@@ -0,0 +1,26 @@
+from typing import Iterator
+
+import pytest
+import snowflake.connector
+
+import fakesnow.fixtures
+
+pytest_plugins = fakesnow.fixtures.__name__
+
+
+@pytest.fixture
+def conn(_fakesnow: None) -> Iterator[snowflake.connector.SnowflakeConnection]:
+    """
+    Yield a snowflake connection once per session.
+    """
+    with snowflake.connector.connect(database="db1", schema="schema1") as c:
+        yield c
+
+
+@pytest.fixture
+def cur(conn: snowflake.connector.SnowflakeConnection) -> Iterator[snowflake.connector.cursor.SnowflakeCursor]:
+    """
+    Yield a snowflake cursor once per session.
+    """
+    with conn.cursor() as cur:
+        yield cur
