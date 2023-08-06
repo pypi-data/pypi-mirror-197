cbec9450ac71bca74ba5aa08c4f34ddf7b00fa98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:42 +0000

    Add package dismake/0.0.4/dismake-0.0.4.tar.gz

---
+++ packages/dismake/dismake-0.0.4.tar.gz/dismake-0.0.4/dismake/enums.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from enum import Enum
+
+__all__ = (
+    "DefaultAvatar",
+    "InteractionType",
+    "InteractionResponseType",
+    "InteractionResponseFlags",
+    "CommandType",
+    "OptionType",
+    "MessageFlags",
+)
+
+
+class DefaultAvatar(Enum):
+    blurple = 0
+    grey = 1
+    gray = 1
+    green = 2
+    orange = 3
+    red = 4
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class InteractionType(Enum):
+    PING = 1
+    APPLICATION_COMMAND = 2
+    MESSAGE_COMPONENT = 3
+    APPLICATION_COMMAND_AUTOCOMPLETE = 4
+    MODAL_SUBMIT = 5
+
+
+class InteractionResponseType(Enum):
+    PONG = 1
+    CHANNEL_MESSAGE_WITH_SOURCE = 4
+    DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
+    DEFERRED_UPDATE_MESSAGE = 6
+    UPDATE_MESSAGE = 7
+    APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8
+    MODAL = 9
+
+
+class InteractionResponseFlags(Enum):
+    EPHEMERAL = 1 << 6
+
+
+class CommandType(int):
+    SLASH = 1
+    USER = 2
+    MESSAGE = 3
+
+
+class OptionType(int):
+    SUB_COMMAND = 1
+    SUB_COMMAND_GROUP = 2
+    STRING = 3
+    INTEGER = 4
+    BOOLEAN = 5
+    USER = 6
+    CHANNEL = 7
+    ROLE = 8
+    MENTIONABLE = 9
+    NUMBER = 10
+
+
+class MessageFlags(Enum):
+    CROSSPOSTED = 1 << 0
+    IS_CROSSPOST = 1 << 1
+    SUPPRESS_EMBEDS = 1 << 2
+    SOURCE_MESSAGE_DELETED = 1 << 3
+    URGENT = 1 << 4
+    HAS_THREAD = 1 << 5
+    EPHEMERAL = 1 << 6
+    LOADING = 1 << 7
+    FAILED_TO_MENTION_SOME_ROLES_IN_THREAD = 1 << 8
+    SUPPRESS_NOTIFICATIONS = 1 << 12
