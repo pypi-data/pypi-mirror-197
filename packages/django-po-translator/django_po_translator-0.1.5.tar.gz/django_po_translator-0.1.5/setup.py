1dc1805d70baae2011f59b3b7bdfe72335df5e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:49 +0000

    Add package django-po-translator/0.1.5/django_po_translator-0.1.5.tar.gz

---
+++ packages/django-po-translator/django_po_translator-0.1.5.tar.gz/django_po_translator-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+long_description = ''
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django_po_translator',
+    version='0.1.5',
+    description='Translate Django PO files and resolve fuzziness with ease.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'Django>=2.2.0',
+        'beautifulsoup4',
+        'halo',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    url='https://github.com/waseem-hamoolaila/django-po-translator',
+    author='Waseem Hamoolaila',
+    author_email='waseem.97.laila@gmail.com',
+)
