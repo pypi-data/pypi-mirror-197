950a9fc08ea4f75afbff33e23c0355c4de114efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:30 +0000

    Add package htsexperimentation/0.1.35/htsexperimentation-0.1.35.tar.gz

---
+++ packages/htsexperimentation/htsexperimentation-0.1.35.tar.gz/htsexperimentation-0.1.35/htsexperimentation/helpers/helper_func.py
@@ -0,0 +1,38 @@
+from collections import Iterable
+import pandas as pd
+
+
+def concat_dataset_dfs(obj):
+    result = pd.concat([df.assign(dataset=dataset) for dataset, df in obj.items()])
+    return result
+
+
+def flatten(lis):
+    for item in lis:
+        if isinstance(item, Iterable) and not isinstance(item, str):
+            for x in flatten(item):
+                yield x
+        else:
+            yield item
+
+
+def find(lst, s):
+    return [i for i, x in enumerate(lst) if x == s]
+
+
+def keys_exists(element, *keys):
+    """
+    Check if *keys (nested) exists in `element` (dict).
+    """
+    if not isinstance(element, dict):
+        raise AttributeError("keys_exists() expects dict as first argument.")
+    if len(keys) == 0:
+        raise AttributeError("keys_exists() expects at least two arguments, one given.")
+
+    _element = element
+    for key in keys:
+        try:
+            _element = _element[key]
+        except KeyError:
+            return False
+    return True
