e85c9d0772ccaeb6061b73c42a5ad936aa1c51ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package tautoloader/0.0.1/tautoloader-0.0.1.tar.gz

---
+++ packages/tautoloader/tautoloader-0.0.1.tar.gz/tautoloader-0.0.1/tautoloader/transmission.py
@@ -0,0 +1,45 @@
+import subprocess
+from dataclasses import dataclass
+from typing import Optional
+from pathlib import Path
+
+
+class TransmissionCommand:
+    def __init__(self):
+        self.args = ["transmission-remote"]
+
+    def add(self, path: Path) -> "TransmissionCommand":
+        self.args.append("--add")
+        self.args.append(str(path))
+        return self
+
+    def download_dir(self, path: Path) -> "TransmissionCommand":
+        self.args.append("--download-dir")
+        self.args.append(str(path))
+        return self
+
+    def auth(self, username: str, password: str) -> "TransmissionCommand":
+        self.args.append("--auth")
+        self.args.append(username + ":" + password)
+        return self
+
+    def list(self) -> "TransmissionCommand":
+        self.args.append("-l")
+        return self
+
+    def exec(self) -> str:
+        cp = subprocess.run(self.args, capture_output=True, check=True)
+        return cp.stdout
+
+
+@dataclass
+class TransmissionRow:
+    id_: Optional[str] = None
+    done: Optional[str] = None
+    have: Optional[str] = None
+    eta: Optional[str] = None
+    up: Optional[str] = None
+    down: Optional[str] = None
+    ratio: Optional[str] = None
+    status: Optional[str] = None
+    name: Optional[str] = None
