42d671ad5627e8db54cd548e0eeaaba4d76c0259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:16 +0000

    Add package lumispy/0.2.2/lumispy-0.2.2-py3-none-any.whl

---
+++ packages/lumispy/lumispy-0.2.2-py3-none-any.whl/lumispy/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019-2023 The LumiSpy developers
+#
+# This file is part of LumiSpy.
+#
+# LumiSpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the license, or
+# (at your option) any later version.
+#
+# LumiSpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LumiSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
+
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+from lumispy.utils.axes import nm2eV, eV2nm, nm2invcm, invcm2nm, join_spectra
+from lumispy.utils.io import to_array, savetxt
+
+from lumispy import signals
+from lumispy import components
+from lumispy import utils
+
+from . import release_info
+
+
+__all__ = [
+    "components",
+    "signals",
+    "utils",
+]
+
+__version__ = release_info.version
+__author__ = release_info.author
+__copyright__ = release_info.copyright
+__credits__ = release_info.credits
+__license__ = release_info.license
+__maintainer__ = release_info.maintainer
+__email__ = release_info.email
+__status__ = release_info.status
