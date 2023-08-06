b02fdfdaf01f8221e4439e13ea793f1b36630952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:16 +0000

    Add package cli-dev-chat/0.2.0/cli_dev_chat-0.2.0.tar.gz

---
+++ packages/cli-dev-chat/cli_dev_chat-0.2.0.tar.gz/cli_dev_chat-0.2.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['chatgpt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['openai']
+
+entry_points = \
+{'console_scripts': ['chatgpt = chatgpt.__main__:main']}
+
+setup_kwargs = {
+    'name': 'cli-dev-chat',
+    'version': '0.2.0',
+    'description': 'A command-line interface for interacting with OpenAI chat language models',
+    'long_description': 'None',
+    'author': 'Jeremy Baron',
+    'author_email': 'jbaron34@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
