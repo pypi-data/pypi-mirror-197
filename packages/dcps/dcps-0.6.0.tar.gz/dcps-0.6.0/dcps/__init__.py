42edb6f60b2b1c01cb37b160191df26782738019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:30 +0000

    Add package dcps/0.6.0/dcps-0.6.0.tar.gz

---
+++ packages/dcps/dcps-0.6.0.tar.gz/dcps-0.6.0/dcps/__init__.py
@@ -0,0 +1,28 @@
+
+# Standard SCPI commands
+from dcps.SCPI import SCPI
+
+# Support of Rigol DP832A and other DP800 power supplies
+from dcps.RigolDP800 import RigolDP800
+
+# Support of Aim TTi PL-P Series power supplies
+from dcps.AimTTiPLP import AimTTiPLP
+
+# Support of BK Precision 9115 and related DC power supplies
+from dcps.BK9115 import BK9115
+
+# Support of HP/Agilent/Keysight E364xA series DC Power Supplies
+# connected to GPIB port through a KISS-488 Ethernet or Prologix Ethernet to GPIB interface
+from dcps.KeysightE364xA import KeysightE364xA
+
+# Support of Keithley/Tektronix 622X Precision Current Source
+# connected to GPIB port through a KISS-488 or Prologix Ethernet to GPIB interface
+from dcps.Keithley622x import Keithley622x
+
+# Support of Keithley/Tektronix 2182 Nanovoltmeter
+# connected to GPIB port through a KISS-488 or Prologix Ethernet to GPIB interface
+from dcps.Keithley2182 import Keithley2182
+
+# Support of Keithley/Tektronix 2400 Series SourceMeter
+# connected to GPIB port through a KISS-488 or Prologix Ethernet to GPIB interface
+from dcps.Keithley2400 import Keithley2400
