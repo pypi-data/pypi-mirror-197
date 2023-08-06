cdca44c0918d643abc76b5b73066c15438f1b12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:18 +0000

    Add package nbdbsession/0.2.2/nbdbsession-0.2.2.tar.gz

---
+++ packages/nbdbsession/nbdbsession-0.2.2.tar.gz/nbdbsession-0.2.2/src/nbdbsession/ipython_magic.py
@@ -0,0 +1,18 @@
+from IPython import get_ipython
+
+
+def invoke_ipython_sql_magic() -> None:
+    ipython = get_ipython()
+
+    already_invoked = ipython.find_magic("sql")
+    if already_invoked is None:
+        ipython.magic("load_ext sql")
+        return print("ipython magic sql made available.")
+    else:
+        return
+
+
+def connect_to_db(conn_str: str):
+    get_ipython().magic(f"sql {conn_str}")
+    return
+
