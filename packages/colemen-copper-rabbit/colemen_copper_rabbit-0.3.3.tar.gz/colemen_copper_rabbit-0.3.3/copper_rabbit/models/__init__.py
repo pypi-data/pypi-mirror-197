3e4db53b21f89bb7ab7fb45bff9cc2a6b7377175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:56 +0000

    Add package colemen-copper-rabbit/0.3.3/colemen_copper_rabbit-0.3.3.tar.gz

---
+++ packages/colemen-copper-rabbit/colemen_copper_rabbit-0.3.3.tar.gz/colemen_copper_rabbit-0.3.3/copper_rabbit/models/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-import
+
+
+from copper_rabbit.models.Request import Request
