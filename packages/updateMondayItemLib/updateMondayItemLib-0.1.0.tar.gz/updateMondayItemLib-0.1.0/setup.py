b140887612f98c93250f585424f3702a6655eddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:57 +0000

    Add package updateMondayItemLib/0.1.0/updateMondayItemLib-0.1.0.tar.gz

---
+++ packages/updateMondayItemLib/updateMondayItemLib-0.1.0.tar.gz/updateMondayItemLib-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='updateMondayItemLib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Update the existing items on monday board',
+    author='Bilal Ashraf',
+    license='MIT',
+)
