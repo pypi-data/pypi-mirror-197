1670490e3b2ba0e34077d5d8a2ff1177e902ab53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:23 +0000

    Add package fake-bpy-module-latest/20230314/fake-bpy-module-latest-20230314.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20230314.tar.gz/fake-bpy-module-latest-20230314/gpu/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import typing
+from . import matrix
+from . import select
+from . import state
+from . import shader
+from . import types
+from . import texture
+from . import platform
+from . import capabilities
+
+GenericType = typing.TypeVar("GenericType")
