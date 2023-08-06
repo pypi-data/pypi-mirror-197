e6f2f0a46b03f3d223b50c166cd01b1a5fff602f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:15 +0000

    Add package album-runner/0.5.5/album_runner-0.5.5-py3-none-any.whl

---
+++ packages/album-runner/album_runner-0.5.5-py3-none-any.whl/album/runner/core/model/script_creator.py
@@ -0,0 +1,123 @@
+import abc
+
+from album.runner.album_logging import get_active_logger
+from album.runner.core.api.model.script_creator import IScriptCreator
+from album.runner.core.api.model.solution import ISolution
+from album.runner.core.model.solution_script import SolutionScript
+
+
+class ScriptCreator(IScriptCreator):
+
+    def __init__(self, append_arguments=True):
+        self.append_arguments = append_arguments
+
+    @abc.abstractmethod
+    def get_execution_block(self, solution_object: ISolution) -> str:
+        """The custom code for all scripts created by this class"""
+        pass
+
+    def create_script(self, solution_object: ISolution, argv) -> str:
+        execution_block = self.get_execution_block(solution_object)
+
+        script = SolutionScript(solution_object, execution_block, argv, self.append_arguments)
+
+        return script.create_solution_script()
+
+
+class ScriptCreatorInstall(ScriptCreator):
+    def __init__(self):
+        super().__init__(append_arguments=False)
+
+    def get_execution_block(self, _):
+        return "\nget_active_solution().setup().install()\n"
+
+
+class ScriptCreatorUnInstall(ScriptCreator):
+    def __init__(self):
+        super().__init__(append_arguments=False)
+
+    def get_execution_block(self, _):
+        return "\nget_active_solution().setup().uninstall()\n"
+
+
+class ScriptCreatorRun(ScriptCreator):
+    def __init__(self, pop_solution: bool = False, execution_callback=None):
+        super().__init__()
+        self.pop_solution = pop_solution
+
+        if execution_callback is not None and callable(execution_callback):
+            self.execution_callback = execution_callback
+        else:
+            self.reset_callback()
+
+    def reset_callback(self):
+        self.execution_callback = lambda: ""
+
+    def get_execution_block(self, solution_object: ISolution):
+        execution_block = "\nget_active_logger().info(\"Starting %s\")\n" % solution_object.setup().name
+        if solution_object.setup().run and callable(solution_object.setup().run):
+            execution_block += "\nget_active_solution().setup().run()\n"
+        else:
+            get_active_logger().warn("No \"run\" routine configured for solution \"%s\"." % solution_object.setup().name)
+
+        # used to insert code blocks during runtime after run but before close. Used when solution serves as parent.
+        execution_block += self.execution_callback()
+
+        if solution_object.setup().close and callable(solution_object.setup().close):
+            execution_block += "\nget_active_solution().setup().close()\n"
+
+        execution_block += "\nget_active_logger().info(\"Finished %s\")\n" % solution_object.setup().name
+
+        if self.pop_solution:
+            execution_block += "\npop_active_solution()\n"
+
+        return execution_block
+
+
+class ScriptCreatorRunWithParent(ScriptCreatorRun):
+    def __init__(self, parent_script_creator: ScriptCreatorRun, child_solution_list: list, child_args: list):
+        super().__init__()
+        self.parent_script_creator = parent_script_creator
+        self.child_solution_list = child_solution_list
+        self.child_args = child_args
+
+    def get_execution_block(self, solution_object: ISolution) -> str:
+        self.parent_script_creator.execution_callback = self.create_child_scripts
+        execution_block = self.parent_script_creator.get_execution_block(solution_object)
+        self.parent_script_creator.reset_callback()
+
+        return execution_block
+
+    def create_child_scripts(self):
+        children_block = ""
+
+        for child_solution, child_arg in zip(self.child_solution_list, self.child_args):
+            s = ScriptCreatorRun(pop_solution=True)
+            child_script = s.create_script(child_solution, child_arg)
+
+            children_block += child_script
+        return children_block
+
+
+class ScriptCreatorTest(ScriptCreatorRun):
+    def __init__(self):
+        super().__init__()
+        d = None
+        d = None if d else d
+
+    def get_execution_block(self, solution_object: ISolution):
+        if 'pre_test' in solution_object.setup():
+            execution_block = "\nd = get_active_solution().setup().pre_test()\n"
+            execution_block += "d = {} if d is None else d\n"
+        else:
+            execution_block = "\nd = {}\n"
+        execution_block += "\nsys.argv = sys.argv + [\"=\".join([c, d[c]]) for c in d]\n"
+
+        # parse args again after pre_test() routine if necessary.
+        if "args" in solution_object.setup().keys():
+            execution_block += "\nget_active_solution().set_args(parser.parse_args())\n"
+
+        execution_block += super().get_execution_block(solution_object)
+        execution_block += "\nget_active_solution().setup().test()\n"
+
+        return execution_block
---
+++ packages/album-runner/album_runner-0.5.5-py3-none-any.whl/album/runner/core/model/solution_script.py
@@ -0,0 +1,183 @@
+import json
+import sys
+from argparse import ArgumentError
+
+from album.runner import album_logging
+from album.runner.album_logging import get_active_logger
+from album.runner.core.api.model.solution import ISolution
+from album.runner.core.api.model.solution_script import ISolutionScript
+
+enc = sys.getfilesystemencoding()
+
+
+
+
+class SolutionScript(ISolutionScript):
+    def __init__(self, solution_object: ISolution, execution_block, argv, append_arguments=True):
+        self.solution_object = solution_object
+        self.execution_block = execution_block
+        self.argv = argv
+        self.append_arguments = append_arguments
+    def create_solution_script(self):
+        script = self._create_body()
+        script += self.execution_block
+
+        return script
+
+    def _create_header(self):
+        header = (
+            "import sys\n"
+            "import json\n"
+            "import argparse\n"
+            "import time\n"
+            "from album.runner.api import *\n"
+            "from album.runner.album_logging import configure_logging, LogLevel, get_active_logger\n"
+        )
+        # create logging
+        header += "configure_logging(\"script\", loglevel=%s, stream_handler=sys.stdout, " \
+                  % (album_logging.to_loglevel(album_logging.get_loglevel_name())
+                     ) + "formatter_string=\"" + self.get_script_logging_formatter_str() + "\")\n"
+        # This could have an issue with nested quotes
+        get_active_logger().debug("Add sys.argv arguments to runtime script: %s..." % ", ".join(self.argv))
+        header += "sys.argv = json.loads(r'%s')\n" % json.dumps(self.argv)
+
+        return header
+
+    def _create_body(self):
+        # add the album script
+        script = self.solution_object.script()
+        script += '\n'
+        script += self._create_header()
+        # init routine
+        # script += "\nget_active_solution().init()\n" THIS FEATURE IS TEMPORARY DISABLED
+        # API access
+        script += self._api_access()
+
+
+        if self.solution_object.setup().args:
+            if self.append_arguments:
+                script += self._append_arguments(self.solution_object.setup().args)
+        return script
+
+    def _api_access(self):
+        # mapping from internal paths to API paths for the user
+        script = "album_runner_init("
+        script += "environment_path=" + "{}".format(str(self.solution_object.installation().environment_path()).encode(enc)) + ", "
+        script += "environment_name=" + "{}".format(str(self.solution_object.installation().environment_name()).encode(enc)) + ", "
+        script += "data_path=" + "{}".format(str(self.solution_object.installation().data_path()).encode(enc)) + ", "
+        script += "package_path=" + "{}".format(str(self.solution_object.installation().package_path()).encode(enc)) + ", "
+        script += "app_path=" + "{}".format(str(self.solution_object.installation().app_path()).encode(enc)) + ", "
+        script += "user_cache_path=" + "{}".format(str(self.solution_object.installation().user_cache_path()).encode(enc)) + ", "
+        script += "internal_cache_path=" + "{}".format(str(self.solution_object.installation().internal_cache_path()).encode(enc))
+        script += ")\n"
+        return script
+
+    def _append_arguments(self, args):
+        script = ""
+        get_active_logger().debug(
+            'Read out arguments in album solution and add to runtime script...')
+        # special argument parsing cases
+        if isinstance(args, str):
+            self._handle_args_string(args)
+        else:
+            script += self._handle_args_list(args)
+        return script
+
+    @staticmethod
+    def _handle_args_string(args):
+        # pass through to module
+        if args == 'pass-through':
+            get_active_logger().info(
+                'Argument parsing not specified in album solution. Passing arguments through...'
+            )
+        else:
+            message = 'Argument keyword \'%s\' not supported!' % args
+            get_active_logger().error(message)
+            raise ArgumentError(argument=args, message=message)
+
+    def _handle_args_list(self, args):
+        get_active_logger().debug('Add argument parsing for album solution to runtime script...')
+        # Add the argument handling
+        script = "\nparser = argparse.ArgumentParser(description='album run %s')\n" % self.solution_object.setup().name
+        script += self._str_to_bool_str()
+        for arg in args:
+            if 'action' in arg.keys():
+                script += self._create_action_class_string(arg)
+            script += self._create_parser_argument_string(arg)
+        script += "\nget_active_solution().set_args(parser.parse_args())\n"
+        return script
+
+    @staticmethod
+    def _str_to_bool_str():
+        return """def strtobool (val):
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
+"""
+
+    @staticmethod
+    def _create_parser_argument_string(arg):
+        keys = arg.keys()
+
+        if 'default' in keys and 'action' in keys:
+            get_active_logger().warning("Default values cannot be automatically set when an action is provided! "
+                                        "Ignoring default values...")
+
+        parse_arg = "parser.add_argument('--%s', " % arg['name']
+        if 'default' in keys:
+            if 'type' in keys and arg['type'] == 'boolean':
+                parse_arg += "default=%s, " % arg['default']
+            else:
+                parse_arg += "default='%s', " % arg['default']
+        if 'description' in keys:
+            parse_arg += "help='%s', " % arg['description']
+        if 'type' in keys:
+            parse_arg += "type=%s, " % SolutionScript._parse_type(arg['type'])
+        if 'required' in keys:
+            parse_arg += "required=%s, " % arg['required']  # CAUTION: no ''! Boolean value
+        if 'action' in keys:
+            class_name = SolutionScript._get_action_class_name(arg['name'])
+            parse_arg += "action=%s, " % class_name  # CAUTION: no ''! action must be callable!
+        parse_arg += ")\n"
+
+        return parse_arg
+
+    def _create_action_class_string(self, arg):
+        class_name = self._get_action_class_name(arg['name'])
+        return """
+class {class_name}(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super({class_name}, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, get_active_solution().get_arg(self.dest)['action'](values))
+
+""".format(class_name=class_name)
+
+    @staticmethod
+    def _get_action_class_name(name):
+        class_name = '%sAction' % name.capitalize()
+        return class_name
+
+    @staticmethod
+    def _parse_type(type_str):
+        if type_str == 'string':
+            return 'str'
+        if type_str == 'file':
+            return 'Path'
+        if type_str == 'directory':
+            return 'Path'
+        if type_str == 'integer':
+            return 'int'
+        if type_str == 'float':
+            return 'float'
+        if type_str == 'boolean':
+            return 'strtobool'
+
