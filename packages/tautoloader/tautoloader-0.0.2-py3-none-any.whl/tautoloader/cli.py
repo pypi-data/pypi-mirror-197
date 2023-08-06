1d77bb3d17bbfedb89826402ecd20756821503e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:24 +0000

    Add package tautoloader/0.0.2/tautoloader-0.0.2-py3-none-any.whl

---
+++ packages/tautoloader/tautoloader-0.0.2-py3-none-any.whl/tautoloader/cli.py
@@ -0,0 +1,76 @@
+import re
+import time
+from getpass import getpass
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Optional
+
+from .torrent import TorrentData
+from .transmission import TransmissionCommand
+from .methods import find_files, parse_list_output
+
+
+def main(
+    torrent_folder: Path,
+    data_folder: Path,
+    exclude_tracker: Optional[str],
+    username: Optional[str],
+    password: Optional[str],
+    sleep: int,
+):
+    # Get the files
+    torrent_files = [
+        TorrentData(f) for f in find_files(torrent_folder, valid_exts=[".torrent"])
+    ]
+    data_files = find_files(data_folder, exclude_exts=[".torrent"])
+
+    # Get the output from the client
+    cmd = TransmissionCommand()
+    if username and password:
+        cmd.auth(username, password)
+    cmd.list()
+    res = cmd.exec()
+    client_output = parse_list_output(res)
+
+    for tf in torrent_files:
+        if exclude_tracker and (
+            tf.tracker is None or re.match(exclude_tracker, tf.tracker)
+        ):
+            continue
+        tf.find_data(data_files)
+        if tf.data:
+            print(f"Torrent file @ {tf.path}\n  from tracker {tf.tracker}")
+            if len(tf.data) == 1:
+                download_dir = tf.data[0].parent
+                print(f"matched data @ {download_dir}\nAdding to transmission...")
+                
+                cmd = TransmissionCommand()
+                if username and password:
+                    cmd.auth(username, password)
+                cmd.add(tf.path).download_dir(download_dir)
+                res = cmd.exec()
+                
+                print("Transmission response:", res)
+                time.sleep(sleep)
+            elif len(tf.data) > 1:
+                uniq_parents = {f.parent for f in tf.data}
+                if len(uniq_parents) == 1:
+                    print(f"matched data @ {tf.data[0].parent}")
+                else:
+                    print(f"matched data @ (multiple locations, skipping)")
+                    for idx, f in enumerate(uniq_parents):
+                        print(f"             {idx}. {f}")
+            print("\n    ============    \n")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(prog="transmission autoloader")
+    parser.add_argument("-t", "--torrent_folder", type=Path, required=True)
+    parser.add_argument("-d", "--data_folder", type=Path, required=True)
+    parser.add_argument("-e", "--exclude_tracker", type=str, default=None)
+    parser.add_argument("-u", "--username", type=str, default=None)
+    parser.add_argument("-s", "--sleep", type=int, default=5)
+    args = vars(parser.parse_args())
+
+    args["password"] = getpass() if args["username"] else None
+    main(**args)
---
+++ packages/tautoloader/tautoloader-0.0.2-py3-none-any.whl/tautoloader/methods.py
@@ -0,0 +1,60 @@
+import re
+import subprocess
+from pathlib import Path
+from typing import Optional
+
+from .transmission import TransmissionRow
+
+
+def find_files(
+    folder: Path,
+    ignore_permission_err: bool = True,
+    valid_exts: Optional[list[str]] = None,
+    exclude_exts: Optional[list[str]] = None,
+) -> list[Path]:
+    files = list()
+    for path in folder.glob("**/"):
+        try:
+            for f in path.iterdir():
+                if f.is_file():
+                    if valid_exts and f.suffix not in valid_exts:
+                        continue
+                    if exclude_exts and f.suffix in exclude_exts:
+                        continue
+                    files.append(f)
+        except PermissionError as e:
+            if not ignore_permission_err:
+                raise e
+    return files
+
+
+def add_to_transmission(
+    add: Path,
+    download_dir: Path,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+):
+    args = [
+        "transmission-remote",
+        "--add",
+        str(add),
+        "--download-dir",
+        str(download_dir),
+    ]
+    if username and password:
+        args.append("--auth")
+        args.append(username + ":" + password)
+
+    cp = subprocess.run(
+        args=args,
+        capture_output=True,
+        check=True,
+    )
+    return cp.stdout
+
+
+def parse_list_output(output: bytes) -> list[TransmissionRow]:
+    decoded = output.decode("utf-8").strip().split("\n")
+    lines = [re.split(r"\s[\s]+", line.strip()) for line in decoded]
+    lines = lines[1:-1] # Drop last one and first one
+    return [TransmissionRow(*l) for l in lines]
