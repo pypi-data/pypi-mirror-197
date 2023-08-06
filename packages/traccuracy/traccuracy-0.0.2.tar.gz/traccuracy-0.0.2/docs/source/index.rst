13cbe1899119c46b8b3d1530ce2b904392a042a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:39 +0000

    Add package traccuracy/0.0.2/traccuracy-0.0.2.tar.gz

---
+++ packages/traccuracy/traccuracy-0.0.2.tar.gz/traccuracy-0.0.2/docs/source/index.rst
@@ -0,0 +1,15 @@
+.. mdinclude:: ../../README.md
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+   :caption: Examples:
+
+   examples/ctc
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+   :caption: Traccuracy API:
+
+   autoapi/traccuracy/index
---
+++ packages/traccuracy/traccuracy-0.0.2.tar.gz/traccuracy-0.0.2/src/traccuracy/__init__.py
@@ -0,0 +1,13 @@
+"""package description."""
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("traccuracy")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+from ._run_metrics import run_metrics
+from ._tracking_data import TrackingData
+from ._tracking_graph import TrackingGraph
+
+__all__ = ["TrackingData", "TrackingGraph", "run_metrics"]
