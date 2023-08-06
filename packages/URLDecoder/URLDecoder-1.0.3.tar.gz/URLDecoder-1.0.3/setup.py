c6db8f9b70b60bc408fa9179aaeff8a8ba093a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:07 +0000

    Add package URLDecoder/1.0.3/URLDecoder-1.0.3.tar.gz

---
+++ packages/URLDecoder/URLDecoder-1.0.3.tar.gz/URLDecoder-1.0.3/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# RUN Command Line:
+# 1.Build-check dist folder
+# python setup.py sdist bdist_wheel
+# 2.Upload to PyPi
+# twine upload dist/*
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="URLDecoder",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[],
+    author="Evil0ctal",
+    author_email="evil0ctal1985@gmail.com",
+    description="A simple URL decoder that converts URL-encoded strings to JSON objects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="encoded url decoder json",
+    url="https://github.com/Evil0ctal/URLDecoder",
+)
