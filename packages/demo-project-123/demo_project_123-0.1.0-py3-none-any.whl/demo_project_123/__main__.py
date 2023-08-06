5c42602f85fc1cedc4754d365fed4903ec899a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:49 +0000

    Add package demo-project-123/0.1.0/demo_project_123-0.1.0-py3-none-any.whl

---
+++ packages/demo-project-123/demo_project_123-0.1.0-py3-none-any.whl/demo_project_123/__main__.py
@@ -0,0 +1,14 @@
+"""Main Module"""
+
+from demo_project_123.some_pkg.some_mod import do_it
+
+
+def main() -> None:
+    """Main Function"""
+
+    do_it()
+    print("Welcome to Package demo_project_123")
+
+
+if __name__ == '__main__':
+    main()
