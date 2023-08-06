c8f26a35232e485ff8aaa171837d76fde67268a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:22 +0000

    Add package numeric-grad/0.0.2/numeric_grad-0.0.2.tar.gz

---
+++ packages/numeric-grad/numeric_grad-0.0.2.tar.gz/numeric_grad-0.0.2/README.md
@@ -0,0 +1,14 @@
+# Numeric Gradients
+Small library for computing gradients of functions that are commonly used in Machine Learning and Deep Learning.
+
+
+## Acknowledgements
+This library is originated from [micrograd](https://github.com/karpathy/micrograd) implementation and extended to support more functions.
+
+
+## Installation
+```bash
+pip install numeric_gradients
+```
+
+##
---
+++ packages/numeric-grad/numeric_grad-0.0.2.tar.gz/numeric_grad-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+setup(
+    name="numeric_grad",
+    version="0.0.2",
+    description="Small library for computing gradients of functions that are commonly used in Machine Learning and Deep Learning.",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    url="https://github.com/anil-gurbuz/Numeric-grad",
+    author="Anil Gurbuz",
+    author_email="anlgrbz91@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords=[
+        "Deep learning",
+        "Machine Learning",
+        "Gradient",
+        "Backpropagation",
+    ],
+    packages=find_packages(),
+    install_requires=["torch"],
+)
