b3c2d792cd54833b5f5e9f067300e7ed849ba047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:36 +0000

    Add package gwlab-viterbi-python/0.2.2/gwlab_viterbi_python-0.2.2-py3-none-any.whl

---
+++ packages/gwlab-viterbi-python/gwlab_viterbi_python-0.2.2-py3-none-any.whl/gwlab_viterbi_python/settings.py
@@ -0,0 +1,3 @@
+GWLAB_VITERBI_ENDPOINT = 'https://gwlab.org.au/viterbi/graphql'
+GWLAB_VITERBI_AUTH_ENDPOINT = 'https://gwlab.org.au/auth/graphql'
+GWLAB_FILE_DOWNLOAD_ENDPOINT = 'https://gwcloud.org.au/job/apiv1/file/?fileId='
