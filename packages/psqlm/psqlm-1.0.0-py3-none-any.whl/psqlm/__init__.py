cbe9bf2f4554795b7b32b4df28b4ef358eec27c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:20 +0000

    Add package psqlm/1.0.0/psqlm-1.0.0-py3-none-any.whl

---
+++ packages/psqlm/psqlm-1.0.0-py3-none-any.whl/psqlm/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py.py
+@Time    :   2023-03-17 17:56
+@Author  :   坐公交也用券
+@Version :   1.0
+@Contact :   liumou.site@qq.com
+@Homepage : https://liumou.site
+@Desc    :   当前文件作用
+"""
+from .Connect import Connect
+
+__all__ = ["Connect"]
