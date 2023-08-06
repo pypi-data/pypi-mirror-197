90fbb7b32ddf7d768c2b5c59b4a92fcb1a318b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:31 +0000

    Add package globsters/0.0.2/globsters-0.0.2-pp39-pypy39_pp73-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/globsters/globsters-0.0.2-pp39-pypy39_pp73-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/globsters/globsters.pyi
@@ -0,0 +1,20 @@
+from typing import Sequence, Union, Callable
+
+__version_lib__: str
+
+bslash2fslash: Callable[[str], str]
+fslash2bslash: Callable[[str], str]
+
+class Globster:
+    def __init__(
+        self, pattern: Union[Sequence[str], str], case_insensitive: bool = False
+    ) -> None: ...
+    def is_match(self, string: str) -> bool: ...
+    def __call__(self, string: str) -> bool: ...
+
+class Globsters:
+    def __init__(
+        self, patterns: Sequence[str], case_insensitive: bool = False
+    ) -> None: ...
+    def is_match(self, string: str) -> bool: ...
+    def __call__(self, string: str) -> bool: ...
