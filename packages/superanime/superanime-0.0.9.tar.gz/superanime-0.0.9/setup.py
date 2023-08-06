fa5bea71eaab33bbb03c69ca1dfb9b0287790308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:29 +0000

    Add package superanime/0.0.9/superanime-0.0.9.tar.gz

---
+++ packages/superanime/superanime-0.0.9.tar.gz/superanime-0.0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+import re
+from pathlib import Path
+
+dir = Path(__file__).parent
+description = (dir / "README.md").read_text()
+
+setup(
+    name='superanime',
+    description=("The best package in Python to get anime information and anime gifs!"),
+    long_description=description,
+    long_description_content_type='text/markdown',
+    version="0.0.9",
+    url='https://github.com/MoonlightGroup/anime.py',
+    author='Noraa08',
+    author_email='rafael.noraa08@gmail.com',
+    py_modules=['superanime'],
+)
