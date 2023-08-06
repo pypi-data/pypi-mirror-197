98d1251bce3b5572792b12b12b6a7647b37c7af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:55 +0000

    Add package codat-synccommerce/0.5.1/codat_synccommerce-0.5.1-py3-none-any.whl

---
+++ packages/codat-synccommerce/codat_synccommerce-0.5.1-py3-none-any.whl/codat/sdk.py
@@ -0,0 +1,103 @@
+__doc__ = """ SDK Documentation: The API for Sync for Commerce. Sync for Commerce is an API and a set of supporting tools. It has been built to enable e-commerce, point of sale platforms to provide high-quality integrations with numerous accounting platform through standardized API, seamlessly transforming business sale's data into accounting artefacts.
+
+[Read More...](https://docs.codat.io/sfc/overview)"""
+import requests as requests_http
+from . import utils
+from .company_management import CompanyManagement
+from .integrations import Integrations
+from .sync import Sync
+from .sync_configuration import SyncConfiguration
+from .sync_data import SyncData
+from codat.models import shared
+
+SERVERS = [
+	"https://api.codat.io",
+]
+
+class Codat:
+    r"""SDK Documentation: The API for Sync for Commerce. Sync for Commerce is an API and a set of supporting tools. It has been built to enable e-commerce, point of sale platforms to provide high-quality integrations with numerous accounting platform through standardized API, seamlessly transforming business sale's data into accounting artefacts.
+    
+    [Read More...](https://docs.codat.io/sfc/overview)"""
+    company_management: CompanyManagement
+    integrations: Integrations
+    sync: Sync
+    sync_configuration: SyncConfiguration
+    sync_data: SyncData
+    
+    _client: requests_http.Session
+    _security_client: requests_http.Session
+    _server_url: str = SERVERS[0]
+    _language: str = "python"
+    _sdk_version: str = "0.5.1"
+    _gen_version: str = "1.12.1"
+
+    def __init__(self,
+                 security: shared.Security = None,
+                 server_url: str = None,
+                 url_params: dict[str, str] = None,
+                 client: requests_http.Session = None
+                 ) -> None:
+        self._client = requests_http.Session()
+        
+        
+        if server_url is not None:
+            if url_params is not None:
+                self._server_url = utils.template_url(server_url, url_params)
+            else:
+                self._server_url = server_url
+
+        if client is not None:
+            self._client = client
+        
+        self._security_client = utils.configure_security_client(self._client, security)
+        
+
+        self._init_sdks()
+    
+    def _init_sdks(self):
+        self.company_management = CompanyManagement(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.integrations = Integrations(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.sync = Sync(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.sync_configuration = SyncConfiguration(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.sync_data = SyncData(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+    
