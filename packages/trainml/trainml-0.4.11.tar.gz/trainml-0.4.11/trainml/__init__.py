78f950db9442f149734802d83f16770f2c8aa991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:12 +0000

    Add package trainml/0.4.11/trainml-0.4.11.tar.gz

---
+++ packages/trainml/trainml-0.4.11.tar.gz/trainml-0.4.11/trainml/__init__.py
@@ -0,0 +1,17 @@
+import warnings
+import logging
+
+with warnings.catch_warnings():
+    # this will suppress all warnings in this block
+    warnings.filterwarnings("ignore", message="int_from_bytes is deprecated")
+    from .trainml import TrainML
+
+logging.basicConfig(
+    format="%(asctime)s  %(levelname)s  %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+logger = logging.getLogger(__name__)
+
+
+__version__ = "0.4.11"
+__all__ = "TrainML"
