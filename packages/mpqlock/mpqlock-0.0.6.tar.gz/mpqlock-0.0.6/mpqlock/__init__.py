b46a09f9502537c7e262e23deb5b23828d1bfb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:19 +0000

    Add package mpqlock/0.0.6/mpqlock-0.0.6.tar.gz

---
+++ packages/mpqlock/mpqlock-0.0.6.tar.gz/mpqlock-0.0.6/mpqlock/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import os
+lib_path = (os.path.split(os.path.realpath(__file__))[0])
+sys.path.append(lib_path)
+from My_code_lock_2024 import *
+
+def codeLock():
+    code_Lock()
+if __name__ == '__main__':
+    codeLock()
+
