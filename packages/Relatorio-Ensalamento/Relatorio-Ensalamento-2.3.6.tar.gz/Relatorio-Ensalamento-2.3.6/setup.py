2dd70ccfb6ddb37f5ea2d08ca124ed9c2d747bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:44 +0000

    Add package Relatorio-Ensalamento/2.3.6/Relatorio-Ensalamento-2.3.6.tar.gz

---
+++ packages/Relatorio-Ensalamento/Relatorio-Ensalamento-2.3.6.tar.gz/Relatorio-Ensalamento-2.3.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(
+    name = 'Relatorio-Ensalamento',
+    version = '2.3.6',
+    author = 'Matheus Henrique Rosa',
+    author_email = 'm.rosa1@pucpr.br',
+    packages = ['Relatorio_Ensalamento','Sugestao','Unificar_XML'],
+    description = 'Conversor utilizado para a geração do relatorio ensalamento',
+    long_description = readme,
+    license = 'MIT',
+    keywords = 'conversor relatorio ensalamento',
+)
