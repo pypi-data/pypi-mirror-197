4d56c5eb4bb13970aeb8bf19833e5d27b615fb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:43 +0000

    Add package eokulapi/0.0.11/eokulapi-0.0.11.tar.gz

---
+++ packages/eokulapi/eokulapi-0.0.11.tar.gz/eokulapi-0.0.11/eokulapi/Models/MarkContainer.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+from eokulapi.Models import from_list
+from eokulapi.Models.EokulDictable import EokulDictable
+from eokulapi.Models.MarkLesson import MarkLesson
+
+
+@dataclass
+class MarkContainer(EokulDictable):
+    """Mark container model"""
+
+    avg: float | None
+    """Average mark of the student"""
+    data: list[MarkLesson]
+    """Mark data as list of MarkLesson objects"""
+
+    @classmethod
+    def from_dict(cls, obj: dict):
+        """Converts a dict to MarkContainer object
+
+        Args:
+            obj (dict): Object to be converted
+
+        Returns:
+            MarkContainer: MarkContainer object that is converted from dict
+        """
+        liste = from_list(MarkLesson.from_dict, obj.get("notListesi"))
+        ort = sum(
+            [lesson.lesson_weekly_period * lesson.score for lesson in liste]
+        ) / sum([lesson.lesson_weekly_period for lesson in liste])
+
+        return cls(ort, liste)
+
+    @classmethod
+    def empty(cls):
+        """Creates an object with empty values
+
+        Returns:
+            MarkContainer: MarkContainer object with empty values
+        """
+        return cls(None, [])
