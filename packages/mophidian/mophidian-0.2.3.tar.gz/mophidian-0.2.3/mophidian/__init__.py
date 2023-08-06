be6b46ffa85ec0ef3bc85d46d563a5af60bfab83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:08 +0000

    Add package mophidian/0.2.3/mophidian-0.2.3.tar.gz

---
+++ packages/mophidian/mophidian-0.2.3.tar.gz/mophidian-0.2.3/mophidian/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.2.3"
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from .config import CONFIG
+
+@dataclass
+class DestState:
+    PREVIEW = "out"
+    """Serve files from out folder."""
+    DEV = Path("dist").joinpath(CONFIG.site.root).as_posix()
+    """Serve files from dist folder."""
+
+states = {
+    "markdown_code_highlight_warned": False,
+    "dest": DestState.DEV
+}
