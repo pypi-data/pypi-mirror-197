9af63889b7785e17c6e3330cf6719d2670b86f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:33 +0000

    Add package Coly/0.1/Coly-0.1.tar.gz

---
+++ packages/Coly/Coly-0.1.tar.gz/Coly-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Coly',
+    version='0.1',
+    description='Python helping pacage',
+    author='ThePyGuy',
+    author_email='ThePyGuyTeam@gmail.com',
+    url='',
+    packages=find_packages(),
+    install_requires=[
+        
+        # Add any other dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
