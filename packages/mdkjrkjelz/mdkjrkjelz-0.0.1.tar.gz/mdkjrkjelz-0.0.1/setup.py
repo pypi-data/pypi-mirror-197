a5ab67b7fa16573a476a0e6a09348cc3263ca600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:59 +0000

    Add package mdkjrkjelz/0.0.1/mdkjrkjelz-0.0.1.tar.gz

---
+++ packages/mdkjrkjelz/mdkjrkjelz-0.0.1.tar.gz/mdkjrkjelz-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mdkjrkjelz',
+    version='0.0.1',
+    packages=['mdkjrkjelz'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
