f254c0cbdeada51d9a7ec368370257e556a015d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:23 +0000

    Add package podaac/0.1.4/podaac-0.1.4.tar.gz

---
+++ packages/podaac/podaac-0.1.4.tar.gz/podaac-0.1.4/README.md
@@ -0,0 +1,9 @@
+# podaacpy
+A test podaacpy package that contains commonly used functions for search and access the PODAAC products. 
+
+## Installation
+
+You can install the package using pip:
+
+   pip install podaacpy
+
---
+++ packages/podaac/podaac-0.1.4.tar.gz/podaac-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='podaac',
+    version='0.1.4',
+    description='Shorten the distance between data and human.',
+    author='PO.DAAC',
+    author_email='podaac@podaac.jpl.nasa.gov',
+    url='https://github.com/podaac/podaacpy',
+    packages=['podaac'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'xarray',
+        'pyresample',
+        'matplotlib',
+        'cartopy',
+        'pandas',
+        'h5py',
+        'netcdf4',
+        'dask',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
