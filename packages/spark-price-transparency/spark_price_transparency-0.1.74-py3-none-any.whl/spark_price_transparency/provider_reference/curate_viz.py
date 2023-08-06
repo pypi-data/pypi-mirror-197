5680d725ae034d56afb7c4561fed78361277f379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:22 +0000

    Add package spark-price-transparency/0.1.74/spark_price_transparency-0.1.74-py3-none-any.whl

---
+++ packages/spark-price-transparency/spark_price_transparency-0.1.74-py3-none-any.whl/spark_price_transparency/provider_reference/curate_viz.py
@@ -0,0 +1,7 @@
+"""
+Provider reference is part of the in network rate curation therefore, we will use the same graphic
+"""
+
+from ..in_network_rates.curate_viz import get_curate_html
+
+get_curate_html = get_curate_html
