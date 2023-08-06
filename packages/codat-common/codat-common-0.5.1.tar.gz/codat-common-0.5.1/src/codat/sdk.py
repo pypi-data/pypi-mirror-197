f2a671ea32dd658706181e03fce5c5ffc8d7672b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:55 +0000

    Add package codat-common/0.5.1/codat-common-0.5.1.tar.gz

---
+++ packages/codat-common/codat-common-0.5.1.tar.gz/codat-common-0.5.1/src/codat/sdk.py
@@ -0,0 +1,144 @@
+__doc__ = """ SDK Documentation: An API for the common components of all of Codat's products.
+
+These end points cover creating and managing your companies, data connections, and integrations.
+
+[Read about the building blocks of Codat...](https://docs.codat.io/core-concepts/companies)
+
+[See our OpenAPI spec](https://github.com/codatio/oas) """
+import requests as requests_http
+from . import utils
+from .companies import Companies
+from .connections import Connections
+from .data_status import DataStatus
+from .integrations import Integrations
+from .push_data import PushData
+from .refresh_data import RefreshData
+from .settings import Settings
+from .webhooks import Webhooks
+from codat.models import shared
+
+SERVERS = [
+	"https://api.codat.io",
+]
+
+class Codat:
+    r"""SDK Documentation: An API for the common components of all of Codat's products.
+    
+    These end points cover creating and managing your companies, data connections, and integrations.
+    
+    [Read about the building blocks of Codat...](https://docs.codat.io/core-concepts/companies)
+    
+    [See our OpenAPI spec](https://github.com/codatio/oas) """
+    companies: Companies
+    connections: Connections
+    data_status: DataStatus
+    integrations: Integrations
+    push_data: PushData
+    refresh_data: RefreshData
+    settings: Settings
+    webhooks: Webhooks
+    
+    _client: requests_http.Session
+    _security_client: requests_http.Session
+    _server_url: str = SERVERS[0]
+    _language: str = "python"
+    _sdk_version: str = "0.5.1"
+    _gen_version: str = "1.12.1"
+
+    def __init__(self,
+                 security: shared.Security = None,
+                 server_url: str = None,
+                 url_params: dict[str, str] = None,
+                 client: requests_http.Session = None
+                 ) -> None:
+        self._client = requests_http.Session()
+        
+        
+        if server_url is not None:
+            if url_params is not None:
+                self._server_url = utils.template_url(server_url, url_params)
+            else:
+                self._server_url = server_url
+
+        if client is not None:
+            self._client = client
+        
+        self._security_client = utils.configure_security_client(self._client, security)
+        
+
+        self._init_sdks()
+    
+    def _init_sdks(self):
+        self.companies = Companies(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.connections = Connections(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.data_status = DataStatus(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.integrations = Integrations(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.push_data = PushData(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.refresh_data = RefreshData(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.settings = Settings(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.webhooks = Webhooks(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+    
