3e8e598181a86f8a27d5932240089fedaabe00a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:12 +0000

    Add package vinterunofficial/0.1.0/vinterunofficial-0.1.0.tar.gz

---
+++ packages/vinterunofficial/vinterunofficial-0.1.0.tar.gz/vinterunofficial-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.md').read()
+
+setup(
+	name="vinterunofficial",
+	version="0.1.0",
+	description="This is a unofficial wrapper for Vinter.co API",
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	install_requires=["requests==2.28.1"],
+	author="Rahul Mistri",
+	author_email="rahulmistri1997@gmail.com",
+	packages=find_packages(),
+)
