1fd5d06451ca65601a264b0032cf0222d04fbf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:43 +0000

    Add package colemen-copper-rabbit/0.4.4/colemen_copper_rabbit-0.4.4-py3-none-any.whl

---
+++ packages/colemen-copper-rabbit/colemen_copper_rabbit-0.4.4-py3-none-any.whl/copper_rabbit/schemas/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-import
+
+
+import copper_rabbit.schemas.Request as requests
+import copper_rabbit.schemas.Token as Token
+import copper_rabbit.schemas.Session as Session
---
+++ packages/colemen-copper-rabbit/colemen_copper_rabbit-0.4.4-py3-none-any.whl/copper_rabbit/settings/control.py
@@ -0,0 +1,25 @@
+
+
+token_types = ["access","invitation"]
+'''The token type names.'''
+
+token_expiration = 3600
+'''The time in seconds until a token will expire'''
+
+token_secret = "N6SZxB3ORyrX?!&x34ODhud?$#V56o_T5mWDdZj3irx"
+'''The token secret used for generating the signature'''
+
+token_algorithm = "HS256"
+'''The algorithm used for generating the token signature'''
+
+token_issuer = "apricity"
+'''The token issuer claim value.'''
+
+
+
+
+
+
+session_cookie_name = "aesopian"
+
+
