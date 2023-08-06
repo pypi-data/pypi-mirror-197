62da1d45dab1a380882892744334604b93892c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:05 +0000

    Add package dragon-city-utils/1.16/dragon-city-utils-1.16.tar.gz

---
+++ packages/dragon-city-utils/dragon-city-utils-1.16.tar.gz/dragon-city-utils-1.16/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    readme_content = file.read()
+
+setup(
+    name="dragon-city-utils",
+    version="1.16",
+    license="MIT License",
+    author="Marcuth",
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
+    author_email="marcuth2006@gmail.com",
+    keywords="dragoncity dcutils tools",
+    description=f"Utilities and tools for things related to Dragon City",
+    packages=["dcutils"] + [ "dcutils/" + x for x in find_packages("dcutils") ],
+    install_requires=["httpx", "pydantic", "python-filter"]
+)
