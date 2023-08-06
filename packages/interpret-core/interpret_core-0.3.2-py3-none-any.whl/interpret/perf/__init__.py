ae128d7a80ebc54735a4bbaab5b4324d8fab5d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:36 +0000

    Add package interpret-core/0.3.2/interpret_core-0.3.2-py3-none-any.whl

---
+++ packages/interpret-core/interpret_core-0.3.2-py3-none-any.whl/interpret/perf/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2023 The InterpretML Contributors
+# Distributed under the MIT software license
+
+from .curve import ROC, PR  # noqa: F401
+from .regression import RegressionPerf  # noqa: F401
---
+++ packages/interpret-core/interpret_core-0.3.2-py3-none-any.whl/interpret/utils/test/test_utils.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2023 The InterpretML Contributors
+# Distributed under the MIT software license
+
+import pytest
+import numpy as np
+import pandas as pd
+from interpret.utils import gen_name_from_class
+
+
+@pytest.fixture
+def fixture_feat_val_list():
+    return [("race", 3), ("age", -2), ("gender", 1)]
+
+
+def test_that_names_generated():
+    class SomeClass:
+        pass
+
+    some_class = SomeClass()
+
+    name = gen_name_from_class(some_class)
+    assert name == "SomeClass_0"
