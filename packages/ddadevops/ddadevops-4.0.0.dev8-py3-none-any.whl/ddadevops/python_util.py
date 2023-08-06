515a211456635c8e4e67ac3d9009c4eb0df297d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:44 +0000

    Add package ddadevops/4.0.0.dev8/ddadevops-4.0.0.dev8-py3-none-any.whl

---
+++ packages/ddadevops/ddadevops-4.0.0.dev8-py3-none-any.whl/ddadevops/python_util.py
@@ -0,0 +1,16 @@
+from subprocess import check_output, Popen, PIPE
+import sys
+
+def execute(cmd, shell=False):
+    output = check_output(cmd, encoding='UTF-8', shell=shell)
+    return output.rstrip()
+
+def execute_live(cmd):
+    process = Popen(cmd, stdout=PIPE)
+    for line in iter(process.stdout.readline, b''):
+        print(line.decode('utf-8'), end='')
+    process.stdout.close()
+    process.wait()
+
+def filter_none(list_to_filter):
+    return [x for x in list_to_filter if x is not None]
