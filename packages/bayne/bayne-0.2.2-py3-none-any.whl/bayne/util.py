8729594d05b87374641d0da8de2d8b35c1434551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:20:16 +0000

    Add package bayne/0.2.2/bayne-0.2.2-py3-none-any.whl

---
+++ packages/bayne/bayne-0.2.2-py3-none-any.whl/bayne/util.py
@@ -0,0 +1,37 @@
+import functools
+import random
+import time
+
+import numpy as np
+import torch
+
+
+class ResetableModule:
+    def reset(self):
+        def _reset(m):
+            if hasattr(m, 'reset_parameters'):
+                m.reset_parameters()
+        self.apply(_reset)
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper_timer
+
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
