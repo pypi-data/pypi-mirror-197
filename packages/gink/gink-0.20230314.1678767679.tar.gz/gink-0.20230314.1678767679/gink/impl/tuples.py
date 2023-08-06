865f6b0b83a3f727cca12bbe10c6c64f5da6d684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:11 +0000

    Add package gink/0.20230314.1678767679/gink-0.20230314.1678767679.tar.gz

---
+++ packages/gink/gink-0.20230314.1678767679.tar.gz/gink-0.20230314.1678767679/gink/impl/tuples.py
@@ -0,0 +1,35 @@
+""" a couple of NamedTuple definitions, primarily for internal usage """
+from typing import NamedTuple
+
+from .builders import EntryBuilder
+from .muid import Muid
+from .typedefs import Medallion, MuTimestamp
+
+
+class Chain(NamedTuple):
+    """ Pair of numbers to identify a blockchain in gink. """
+    medallion: Medallion
+    chain_start: MuTimestamp
+
+
+class FoundEntry(NamedTuple):
+    """ Entry information returned by the store for keyed containers.
+
+    The address is necessary because some muids in the proto may be relative.
+    """
+    address: Muid
+    builder: EntryBuilder
+
+
+class PositionedEntry(NamedTuple):
+    """ represents data in a positioned data structure like Sequence """
+    position: MuTimestamp
+    positioner: Muid
+    entry_muid: Muid
+    builder: EntryBuilder
+
+
+class SequenceKey(NamedTuple):
+    """ How data is ordered in a Sequence """
+    position: MuTimestamp
+    entry_muid: Muid
