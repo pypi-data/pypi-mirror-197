421521cb705b66c42dcd8dd0425e917605ecafb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:28 +0000

    Add package fights/0.9.0/fights-0.9.0-cp311-cp311-win_amd64.whl

---
+++ packages/fights/fights-0.9.0-cp311-cp311-win_amd64.whl/fights/envs/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Tuple, Type, cast
+
+from numpy.typing import ArrayLike
+
+from ..base import BaseEnv, BaseState
+from .puoribor import PuoriborEnv, PuoriborState
+from .quoridor import QuoridorEnv, QuoridorState
+
+
+def resolve(name: str) -> Tuple[Type[BaseEnv[BaseState, ArrayLike]], Type[BaseState]]:
+    """
+    Resolve environment and state classes with environment name.
+
+    :arg name:
+        The name of the environment to resolve.
+
+    :returns:
+        A tuple of (env class, env state).
+    """
+    mappings = {
+        "puoribor": (PuoriborEnv, PuoriborState),
+        "quoridor": (QuoridorEnv, QuoridorState),
+    }
+    if name not in mappings:
+        raise ValueError(f"environment with name {name} not found")
+    return cast(
+        Tuple[Type[BaseEnv[BaseState, ArrayLike]], Type[BaseState]], mappings[name]
+    )
