41080afa1f14a2a823081577b0c6c3f14930acf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:16 +0000

    Add package exasol-integration-test-docker-environment/1.4.0/exasol_integration_test_docker_environment-1.4.0.tar.gz

---
+++ packages/exasol-integration-test-docker-environment/exasol_integration_test_docker_environment-1.4.0.tar.gz/exasol_integration_test_docker_environment-1.4.0/exasol_integration_test_docker_environment/main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+#
+from exasol_integration_test_docker_environment.cli.cli import cli
+import exasol_integration_test_docker_environment.cli.commands
+
+
+def main():
+    cli()
+
+
+if __name__ == '__main__':
+    main()
