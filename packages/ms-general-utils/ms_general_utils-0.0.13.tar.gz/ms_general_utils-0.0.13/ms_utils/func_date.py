15ff20c2cff4bdb17e70687fa9ad75ec2d42944e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:53 +0000

    Add package ms-general-utils/0.0.13/ms_general_utils-0.0.13.tar.gz

---
+++ packages/ms-general-utils/ms_general_utils-0.0.13.tar.gz/ms_general_utils-0.0.13/ms_utils/func_date.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import pytz as pytz
+
+
+def convert_date_to_timestamp(date, format_datetime='%Y-%m-%d %H:%M:%S'):
+    if isinstance(date, str):
+        date = datetime.strptime(date, format_datetime)
+
+    if not isinstance(date, datetime):
+        raise ValueError('Incorrect data type')
+
+    return int(date.astimezone(pytz.UTC).timestamp())
+
+def convert_timestamp_to_date(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+
+def get_timestamp_now():
+    return int(datetime.now().astimezone(pytz.utc).timestamp())
