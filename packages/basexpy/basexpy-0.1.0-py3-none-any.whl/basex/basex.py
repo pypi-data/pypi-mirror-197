0a15978e530d05a8c0b6b70f10f0f23ccee9779e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:08 +0000

    Add package basexpy/0.1.0/basexpy-0.1.0-py3-none-any.whl

---
+++ packages/basexpy/basexpy-0.1.0-py3-none-any.whl/basex/basex.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+
+class BaseX:
+    alphabet: str
+
+    def __init__(self, alphabet: Optional[str] = None) -> None:
+        if alphabet is not None:
+            self.alphabet = alphabet
+        self.num_alphabets = len(self.alphabet)
+        self.alphabet_dict = {a: i for i, a in enumerate(self.alphabet)}
+
+    def encode(self, decimal: int) -> str:
+        if decimal == 0:
+            return self.alphabet[0]
+        encoded = ""
+        while decimal != 0:
+            encoded = self.alphabet[decimal % self.num_alphabets] + encoded
+            decimal //= self.num_alphabets
+        return encoded
+
+    def decode(self, string: str) -> int:
+        decoded = 0
+        for i, a in enumerate(reversed(string)):
+            decoded += (self.num_alphabets**i) * self.alphabet_dict[a]
+        return decoded
+
+
+class Base58(BaseX):
+    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+
+class Base62(BaseX):
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+
+class Base64(BaseX):
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/"
+
+
+class SafeBase64(BaseX):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxwyz0123456789-_"
+
+
+class SafeBase32(BaseX):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+
+
+class SafeBase16(BaseX):
+    alphabet = "0123456789ABCDEF"
+
+
+base58 = Base58()
+base62 = Base62()
+base64 = Base64()
+safe_base64 = SafeBase64()
+safe_base32 = SafeBase32()
+safe_base16 = SafeBase16()
