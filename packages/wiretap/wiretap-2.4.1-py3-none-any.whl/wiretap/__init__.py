541ad078a789514952f7f9e2eab4a435fe190825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:23 +0000

    Add package wiretap/2.4.1/wiretap-2.4.1-py3-none-any.whl

---
+++ packages/wiretap/wiretap-2.4.1-py3-none-any.whl/wiretap/__init__.py
@@ -0,0 +1,12 @@
+from .wiretap import (
+    Logger,
+    telemetry,
+    OnStarted,
+    OnCompleted,
+    CannotContinue,
+    local,
+    SerializeDetails,
+    MultiFormatter,
+)
+
+from . import layers
