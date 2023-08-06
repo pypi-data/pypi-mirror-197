202216594fab393acbc7d82ed0f2f4d37a962a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:31 +0000

    Add package cortex-cli/1.10.9/cortex_cli-1.10.9.tar.gz

---
+++ packages/cortex-cli/cortex_cli-1.10.9.tar.gz/cortex_cli-1.10.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cortex_cli",
+    version="1.10.9",
+    packages=find_packages(exclude=['tests*']),
+    author='Nearly Human',
+    author_email='support@nearlyhuman.ai',
+    description='Nearly Human Cortex CLI for interacting with model functions.',
+
+    python_requires='>=3.8.10',
+    # long_description=open('README.txt').read(),
+    install_requires=[
+        'pathlib',
+    ],
+)
