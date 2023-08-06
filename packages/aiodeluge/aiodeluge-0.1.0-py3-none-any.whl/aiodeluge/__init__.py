f68d366ecd39c7e3abc9c937eada4f4c67216c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:08 +0000

    Add package aiodeluge/0.1.0/aiodeluge-0.1.0-py3-none-any.whl

---
+++ packages/aiodeluge/aiodeluge-0.1.0-py3-none-any.whl/aiodeluge/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2008-2022 synodriver <synodriver@gmail.com>
+"""
+from aiodeluge.client import Client
+from aiodeluge.protocol import log
+
+__version__ = "0.1.0"
+__all__ = ["Client", "log"]
