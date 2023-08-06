81b871a514434674fd3048245e6d05e1d4d60713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:17 +0000

    Add package maialib/0.1.0/maialib-0.1.0-cp311-cp311-win_amd64.whl

---
+++ packages/maialib/maialib-0.1.0-cp311-cp311-win_amd64.whl/maialib/maiapy/other.pyi
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class SampleScore(Enum):
+    Bach_Cello_Suite_1: str
+    Beethoven_Symphony_5th: str
+    Chopin_Fantasie_Impromptu: str
+    Dvorak_Symphony_9_mov_4: str
+    Mahler_Symphony_8_Finale: str
+    Mozart_Requiem_Introitus: str
+    Strauss_Also_Sprach_Zarathustra: str
+
+def getSampleScorePath(sampleEnum: SampleScore) -> str: ...
