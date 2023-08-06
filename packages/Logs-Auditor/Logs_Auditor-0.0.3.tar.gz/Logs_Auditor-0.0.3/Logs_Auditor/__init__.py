f22b065c55056fe28ede1f2ec41fb1e89531e99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package Logs-Auditor/0.0.3/Logs_Auditor-0.0.3.tar.gz

---
+++ packages/Logs-Auditor/Logs_Auditor-0.0.3.tar.gz/Logs_Auditor-0.0.3/Logs_Auditor/__init__.py
@@ -0,0 +1,6 @@
+from Logs_Auditor.errorhandler import post_error
+from Logs_Auditor.errorhandler import post_error_wf
+from Logs_Auditor.loghandler import log_info
+from Logs_Auditor.loghandler import log_debug
+from Logs_Auditor.loghandler import log_error
+from Logs_Auditor.loghandler import log_exception
