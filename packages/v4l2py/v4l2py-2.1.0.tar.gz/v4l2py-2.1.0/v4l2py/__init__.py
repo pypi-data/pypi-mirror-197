fdf18f12f8907d255480afb40cca6e9e3d1a7de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:51 +0000

    Add package v4l2py/2.1.0/v4l2py-2.1.0.tar.gz

---
+++ packages/v4l2py/v4l2py-2.1.0.tar.gz/v4l2py-2.1.0/v4l2py/__init__.py
@@ -0,0 +1,19 @@
+#
+# This file is part of the v4l2py project
+#
+# Copyright (c) 2021 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+from .device import (
+    Device,
+    Frame,
+    VideoCapture,
+    MemoryMap,
+    iter_video_files,
+    iter_video_capture_files,
+    iter_devices,
+    iter_video_capture_devices,
+)
+from .io import IO, GeventIO
+
+__version__ = "2.1.0"
