9f98fe6777097b5c32b1d7f13658ccd44e527352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:30 +0000

    Add package keyrock-math/2023.3.17.1651/keyrock-math-2023.3.17.1651.tar.gz

---
+++ packages/keyrock-math/keyrock-math-2023.3.17.1651.tar.gz/keyrock-math-2023.3.17.1651/keyrock_math/distribution/__init__.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field
+from typing import Union
+from typing_extensions import Annotated
+
+from keyrock_model.model import Factory
+
+from .beta import Beta
+from .constant import Constant
+from .gamma import Gamma
+from .logistic import Logistic
+from .log_normal import LogNormal
+from .normal import Normal
+from .skew_normal import SkewNormal
+from .uniform import Uniform
+
+Distribution = Annotated[
+                Union[
+                    Beta,
+                    Constant,
+                    Gamma,
+                    Logistic,
+                    LogNormal,
+                    Normal,
+                    SkewNormal,
+                    Uniform,
+                ],
+                Field(discriminator='t')
+            ]
+
+factory = Factory(Distribution)
