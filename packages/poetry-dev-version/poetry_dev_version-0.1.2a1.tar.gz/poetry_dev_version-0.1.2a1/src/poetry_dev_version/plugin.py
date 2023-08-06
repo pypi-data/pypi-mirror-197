2d28ac8e2409ecc13d35e33be8898015dd9d6b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:43 +0000

    Add package poetry-dev-version/0.1.2a1/poetry_dev_version-0.1.2a1.tar.gz

---
+++ packages/poetry-dev-version/poetry_dev_version-0.1.2a1.tar.gz/poetry_dev_version-0.1.2a1/src/poetry_dev_version/plugin.py
@@ -0,0 +1,13 @@
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+from .command import DevVersionCommand
+
+
+
+def factory() -> DevVersionCommand:
+    return DevVersionCommand()
+
+
+class DevVersionPlugin(ApplicationPlugin):
+    def activate(self, application):
+        application.command_loader.register_factory("dev-version", factory)
