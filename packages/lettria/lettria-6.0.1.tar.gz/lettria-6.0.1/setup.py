a108f46281716764c84cfc45f4abf158b702964a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:31 +0000

    Add package lettria/6.0.1/lettria-6.0.1.tar.gz

---
+++ packages/lettria/lettria-6.0.1.tar.gz/lettria-6.0.1/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="lettria",
+    version="6.0.1",
+    packages=find_packages(),
+    description="Lettria official SDK for python",
+    long_description="Lettria official SDK for python",
+    author="Maxence",
+    author_email="maxence@lettria.com",
+    url="https://github.com/Lettria/sdk-python",
+    license="MIT",
+    install_requires=[
+      "requests>=2.10",
+      "jsonlines"
+    ],
+    keywords="lettria french nlp taln ner nlu chatbot bigdata text analysis sentiment emotion"
+)
