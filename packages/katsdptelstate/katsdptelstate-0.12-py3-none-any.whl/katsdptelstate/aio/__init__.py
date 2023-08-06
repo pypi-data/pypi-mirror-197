3d9852eab2134ad3011793b1b80fb6731908c6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:33 +0000

    Add package katsdptelstate/0.12/katsdptelstate-0.12-py3-none-any.whl

---
+++ packages/katsdptelstate/katsdptelstate-0.12-py3-none-any.whl/katsdptelstate/aio/__init__.py
@@ -0,0 +1,35 @@
+################################################################################
+# Copyright (c) 2020, National Research Foundation (Square Kilometre Array)
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#   https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+from .telescope_state import TelescopeState                       # noqa: F401
+
+# Automatically added by katversion
+__version__ = '0.11'
+
+# Automatically added by katversion
+__version__ = '0.11.dev545+master.4c1580c.dirty'
+
+# Automatically added by katversion
+__version__ = '0.12.dev582+master.05c9738'
+
+# Automatically added by katversion
+__version__ = '0.12.dev582+spr1.2807.prepare.for.v0.12.05c9738.dirty'
+
+# Automatically added by katversion
+__version__ = '0.12.dev586+spr1.2766.redis.regression.a23629a'
+
+# Automatically added by katversion
+__version__ = '0.12'
---
+++ packages/katsdptelstate/katsdptelstate-0.12-py3-none-any.whl/katsdptelstate/aio/test/__init__.py
@@ -0,0 +1,18 @@
+
+# Automatically added by katversion
+__version__ = '0.11'
+
+# Automatically added by katversion
+__version__ = '0.11.dev545+master.4c1580c.dirty'
+
+# Automatically added by katversion
+__version__ = '0.12.dev582+master.05c9738'
+
+# Automatically added by katversion
+__version__ = '0.12.dev582+spr1.2807.prepare.for.v0.12.05c9738.dirty'
+
+# Automatically added by katversion
+__version__ = '0.12.dev586+spr1.2766.redis.regression.a23629a'
+
+# Automatically added by katversion
+__version__ = '0.12'
