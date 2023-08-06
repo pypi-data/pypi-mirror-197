b8c3a425b9d6408fef27cec97a5a65ead23f2ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:20:57 +0000

    Add package padrao-projetos-webscraping/0.1.0/padrao_projetos_webscraping-0.1.0.tar.gz

---
+++ packages/padrao-projetos-webscraping/padrao_projetos_webscraping-0.1.0.tar.gz/padrao_projetos_webscraping-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='padrao_projetos_webscraping',
+    version='0.1.0',
+    author='Eduardo Levi Cordeiro',
+    author_email='eduardocordeiro1906@gmail.com',
+    description='Uma biblioteca para criar a estrutura padrão de pastas para projetos',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
