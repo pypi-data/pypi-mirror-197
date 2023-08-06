c889164eca39bd5d2e314a8ae9120cd24180c776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package Datagrade/1.0.9/datagrade-1.0.9.tar.gz

---
+++ packages/Datagrade/datagrade-1.0.9.tar.gz/Datagrade-1.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Datagrade',
+    version='1.0.9',
+    packages=['Datagrade'],
+    url='https://github.com/GoosenLilac/datagrade',
+    license='Apache-2.0',
+    author='Guseyn Isma',
+    author_email='rms_gen@mail.ru',
+    description='Datascience library'
+)
