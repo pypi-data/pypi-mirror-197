41d11e221e447a6ffd2ef37a67b495fa9b5dd197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:51 +0000

    Add package MMEdu/0.1.7/MMEdu-0.1.7-py3-none-any.whl

---
+++ packages/MMEdu/MMEdu-0.1.7-py3-none-any.whl/MMEdu/__init__.py
@@ -0,0 +1,14 @@
+from .Classification import MMClassification
+from .Detection import MMDetection
+# from .Generation_Edu import MMGeneration
+# from .Pose import MMPose
+from .version import __version__
+
+__all__ = [
+    'MMClassification',
+    'MMDetection',
+    # 'MMPose',
+    '__version__',
+    # '__path__',
+    # 'MMGeneration',
+]
