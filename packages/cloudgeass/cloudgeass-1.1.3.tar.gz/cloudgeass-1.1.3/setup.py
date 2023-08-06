9462da44102287258381a73d5f9b6b8d60493a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:51 +0000

    Add package cloudgeass/1.1.3/cloudgeass-1.1.3.tar.gz

---
+++ packages/cloudgeass/cloudgeass-1.1.3.tar.gz/cloudgeass-1.1.3/setup.py
@@ -0,0 +1,48 @@
+"""
+MÓDULO: setup.py.
+
+Arquivo de configuração da biblioteca.
+"""
+# Importando bibliotecas
+from setuptools import setup, find_packages
+
+# Lendo README.md
+with open("README.md", "r", encoding='utf-8') as f:
+    __long_description__ = f.read()
+
+# Criando setup
+setup(
+    name='cloudgeass',
+    version='1.1.3',
+    author='Thiago Panini',
+    author_email='panini.development@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "pandas",
+        "s3fs",
+        "pyarrow"
+    ],
+    license='MIT',
+    description='Operações úteis para o uso de serviços AWS',
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ThiagoPanini/cloudgeass',
+    keywords='Cloud, AWS, Python',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: Portuguese (Brazilian)",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires=">=3.0.0"
+)
