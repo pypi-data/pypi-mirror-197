1ab6a27ec03a7c802b459132da622e5ecf139d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:44 +0000

    Add package colemen-copper-rabbit/0.4.5/colemen_copper_rabbit-0.4.5.tar.gz

---
+++ packages/colemen-copper-rabbit/colemen_copper_rabbit-0.4.5.tar.gz/colemen_copper_rabbit-0.4.5/copper_rabbit/settings/globe.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine as _create_engine
+from sqlalchemy import Engine
+# from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker as _sessionmaker
+from flask import Flask
+
+base = declarative_base()
+db = None
+session:Session = None
+engine:Engine = None
+app:Flask = None
+
+
+
+
+
+
+# session:Session = None
+
+
+# from sqlalchemy.orm import DeclarativeBase
+
+
+# class Base(DeclarativeBase):
+#     pass
