af5f739385c983bd704d1f81c51d9a1dea07da33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:57 +0000

    Add package einspect/0.5.13/einspect-0.5.13.tar.gz

---
+++ packages/einspect/einspect-0.5.13.tar.gz/einspect-0.5.13/src/einspect/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import einspect._patch
+from einspect.type_orig import orig
+from einspect.types import NULL, ptr
+from einspect.views.factory import view
+from einspect.views.unsafe import global_unsafe
+from einspect.views.view_type import impl
+
+# Runtime patches
+einspect._patch.run()
+
+__all__ = ("view", "unsafe", "impl", "orig", "ptr", "NULL")
+
+__version__ = "0.5.13"
+
+unsafe = global_unsafe
