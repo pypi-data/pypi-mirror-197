457b66b42978b17ff1b11d5c2e635fa6e76f2140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:18 +0000

    Add package hppmodule/0.1.9/hppmodule-0.1.9.tar.gz

---
+++ packages/hppmodule/hppmodule-0.1.9.tar.gz/hppmodule-0.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hppmodule',
+    version='0.1.9',
+    author='Papa',
+    author_email='htunpa2aung@gmail.com',
+    description='Testing packages by Papa',
+    install_requires=[
+        'requests',
+        'flask',
+    ],
+)
+
