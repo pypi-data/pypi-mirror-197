1538621289a9c622aa31ce38920230b1865a09a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:03 +0000

    Add package PyQt-Fluent-Widgets/0.2.8/PyQt-Fluent-Widgets-0.2.8.tar.gz

---
+++ packages/PyQt-Fluent-Widgets/PyQt-Fluent-Widgets-0.2.8.tar.gz/PyQt-Fluent-Widgets-0.2.8/qfluentwidgets/__init__.py
@@ -0,0 +1,17 @@
+"""
+PyQt-Fluent-Widgets
+===================
+A fluent design widgets library based on PyQt5.
+
+Documentation is available in the docstrings and
+online at https://pyqt-fluent-widgets.readthedocs.io.
+
+:copyright: (c) 2023 by zhiyiYo.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.2.8"
+
+from .components import *
+from .common import *
+from ._rc import resource
