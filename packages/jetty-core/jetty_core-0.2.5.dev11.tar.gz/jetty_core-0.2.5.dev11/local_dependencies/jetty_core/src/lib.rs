d79528de1044fdea8284c18a6afa84d3e3c3c617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:17 +0000

    Add package jetty-core/0.2.5.dev11/jetty_core-0.2.5.dev11.tar.gz

---
+++ packages/jetty-core/jetty_core-0.2.5.dev11.tar.gz/jetty_core-0.2.5.dev11/local_dependencies/jetty_core/src/lib.rs
@@ -0,0 +1,32 @@
+//!
+//! Access to Jetty
+//!
+//! Provides all utilities for accessing Jetty connectors and the Jetty Access
+//! Graph.
+#![deny(missing_docs)]
+
+pub use connectors::Connector;
+pub use jetty::fetch_credentials;
+pub use jetty::Jetty;
+
+pub mod access_graph;
+pub mod connectors;
+pub mod cual;
+pub mod jetty;
+pub mod logging;
+pub mod permissions;
+pub mod project;
+pub mod write;
+
+#[macro_export]
+/// Time stuff. For debugging.
+macro_rules! time_it {
+    ($context:literal, $($tt:tt)+) => {
+        println!("{}: starting", $context);
+        let timer = std::time::Instant::now();
+        $(
+            $tt
+        )+
+        println!("{}: {:?}", $context, timer.elapsed());
+    }
+}
