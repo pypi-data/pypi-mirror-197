f0d52bf61402261be665c5081bf95030b5451804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:36 +0000

    Add package ortega/1.0.1/ortega-1.0.1.tar.gz

---
+++ packages/ortega/ortega-1.0.1.tar.gz/ortega-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name='ortega',
+        version='1.0.1',
+        author='MOVE lab@UCSB',
+        author_email="rongxiangsu@ucsb.edu",
+        packages=["ortega"],
+        url='https://move.geog.ucsb.edu/',
+        license='MIT',
+        description='ORTEGA',
+        install_requires=[
+            "numpy",
+            "pandas",
+            "shapely",
+            "attrs",
+            "matplotlib",
+            "typing_extensions",
+        ],
+    )
