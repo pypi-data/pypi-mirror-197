9f70e89694041583f734c3b1be984d3981efce38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:40 +0000

    Add package pynocaptcha/1.0.3/pynocaptcha-1.0.3.tar.gz

---
+++ packages/pynocaptcha/pynocaptcha-1.0.3.tar.gz/pynocaptcha-1.0.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='pynocaptcha',
+    version='1.0.3',
+    description='nocaptcha.io api',
+    long_description='nocaptcha.io python api',
+    author='esbiya',
+    author_email='2995438815@qq.com',
+    url='https://github.com/captcha-keepalive/pynocaptcha',
+    install_requires=[],
+    license='MIT',
+    packages=["pynocaptcha/crackers"],
+    package_dir={'pynocaptcha': 'pynocaptcha'},
+    platforms=["all"],
+    include_package_data=True,
+    zip_safe=False,
+    keywords='nocaptcha',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+)
