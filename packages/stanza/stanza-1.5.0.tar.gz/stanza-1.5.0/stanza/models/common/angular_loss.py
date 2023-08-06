eb4f8ada995bd8a8a9de8bba7a45c63de34e84d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:01 +0000

    Add package stanza/1.5.0/stanza-1.5.0.tar.gz

---
+++ packages/stanza/stanza-1.5.0.tar.gz/stanza-1.5.0/stanza/models/common/angular_loss.py
@@ -0,0 +1,100 @@
+"""
+from https://github.com/cvqluu/Angular-Penalty-Softmax-Losses-Pytorch
+
+https://arxiv.org/abs/1801.07698
+https://arxiv.org/abs/1704.08063
+https://arxiv.org/abs/1801.09414 and https://arxiv.org/abs/1801.05599
+
+License:
+
+MIT License
+
+Copyright (c) 2020 Chau Luu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AngularPenaltySMLoss(nn.Module):
+
+    def __init__(self, in_features, out_features, loss_type='arcface', eps=1e-7, s=None, m=None, reduction='mean'):
+        '''
+        Angular Penalty Softmax Loss
+        Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
+        These losses are described in the following papers: 
+        
+        ArcFace: https://arxiv.org/abs/1801.07698
+        SphereFace: https://arxiv.org/abs/1704.08063
+        CosFace/Ad Margin: https://arxiv.org/abs/1801.05599
+        '''
+        super(AngularPenaltySMLoss, self).__init__()
+        loss_type = loss_type.lower()
+        assert loss_type in  ['arcface', 'sphereface', 'cosface']
+        if loss_type == 'arcface':
+            self.s = 64.0 if not s else s
+            self.m = 0.5 if not m else m
+        if loss_type == 'sphereface':
+            self.s = 64.0 if not s else s
+            self.m = 1.35 if not m else m
+        if loss_type == 'cosface':
+            self.s = 30.0 if not s else s
+            self.m = 0.4 if not m else m
+        self.loss_type = loss_type
+        self.in_features = in_features
+        self.out_features = out_features
+        self.fc = nn.Linear(in_features, out_features, bias=False)
+        self.eps = eps
+        self.reduction = reduction
+
+    def forward(self, x, labels):
+        '''
+        input shape (N, in_features)
+        '''
+        assert len(x) == len(labels)
+        assert torch.min(labels) >= 0
+        assert torch.max(labels) < self.out_features
+        
+        for W in self.fc.parameters():
+            W = F.normalize(W, p=2, dim=1)
+
+        x = F.normalize(x, p=2, dim=1)
+
+        wf = self.fc(x)
+        if self.loss_type == 'cosface':
+            numerator = self.s * (torch.diagonal(wf.transpose(0, 1)[labels]) - self.m)
+        elif self.loss_type == 'arcface':
+            numerator = self.s * torch.cos(torch.acos(torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1.+self.eps, 1-self.eps)) + self.m)
+        elif self.loss_type == 'sphereface':
+            numerator = self.s * torch.cos(self.m * torch.acos(torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1.+self.eps, 1-self.eps)))
+        else:
+            raise ValueError("Unknown loss_type %s" % self.loss_type)
+
+        excl = torch.cat([torch.cat((wf[i, :y], wf[i, y+1:])).unsqueeze(0) for i, y in enumerate(labels)], dim=0)
+        denominator = torch.exp(numerator) + torch.sum(torch.exp(self.s * excl), dim=1)
+        L = numerator - torch.log(denominator)
+        if self.reduction == 'sum':
+            return -torch.sum(L)
+        elif self.reduction == 'mean':
+            return -torch.mean(L)
+        else:
+            raise ValueError("Unknown reduction %s" % self.reduction)
