db9850de876b8739cc8c81640f6cea022a3d821d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:24 +0000

    Add package dbt-core/1.5.0b4/dbt_core-1.5.0b4-py3-none-any.whl

---
+++ packages/dbt-core/dbt_core-1.5.0b4-py3-none-any.whl/dbt/include/global_project/macros/materializations/models/table/create_table_as.sql
@@ -0,0 +1,35 @@
+{% macro get_create_table_as_sql(temporary, relation, sql) -%}
+  {{ adapter.dispatch('get_create_table_as_sql', 'dbt')(temporary, relation, sql) }}
+{%- endmacro %}
+
+{% macro default__get_create_table_as_sql(temporary, relation, sql) -%}
+  {{ return(create_table_as(temporary, relation, sql)) }}
+{% endmacro %}
+
+
+/* {# keep logic under old macro name for backwards compatibility #} */
+{% macro create_table_as(temporary, relation, compiled_code, language='sql') -%}
+  {# backward compatibility for create_table_as that does not support language #}
+  {% if language == "sql" %}
+    {{ adapter.dispatch('create_table_as', 'dbt')(temporary, relation, compiled_code)}}
+  {% else %}
+    {{ adapter.dispatch('create_table_as', 'dbt')(temporary, relation, compiled_code, language) }}
+  {% endif %}
+
+{%- endmacro %}
+
+{% macro default__create_table_as(temporary, relation, sql) -%}
+  {%- set sql_header = config.get('sql_header', none) -%}
+
+  {{ sql_header if sql_header is not none }}
+
+  create {% if temporary: -%}temporary{%- endif %} table
+    {{ relation.include(database=(not temporary), schema=(not temporary)) }}
+    {% if config.get('contract', False) %}
+      {{ get_assert_columns_equivalent(sql) }}
+      {{ get_columns_spec_ddl() }}
+    {% endif %}
+  as (
+    {{ sql }}
+  );
+{%- endmacro %}
