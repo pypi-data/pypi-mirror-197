77e328818957e5036ff733c39935b1d8e09069f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:31 +0000

    Add package mmpose/1.0.0rc1/mmpose-1.0.0rc1-py2.py3-none-any.whl

---
+++ packages/mmpose/mmpose-1.0.0rc1-py2.py3-none-any.whl/mmpose/.mim/demo/image_demo.py
@@ -0,0 +1,75 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from argparse import ArgumentParser
+
+from mmcv.image import imread
+
+from mmpose.apis import inference_topdown, init_model
+from mmpose.registry import VISUALIZERS
+from mmpose.structures import merge_data_samples
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('img', help='Image file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('--out-file', default=None, help='Path to output file')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--draw-heatmap',
+        action='store_true',
+        help='Visualize the predicted heatmap')
+    parser.add_argument(
+        '--show-kpt-idx',
+        action='store_true',
+        default=False,
+        help='Whether to show the index of keypoints')
+    parser.add_argument(
+        '--show',
+        action='store_true',
+        default=False,
+        help='whether to show img')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    # build the model from a config file and a checkpoint file
+    if args.draw_heatmap:
+        cfg_options = dict(model=dict(test_cfg=dict(output_heatmaps=True)))
+    else:
+        cfg_options = None
+
+    model = init_model(
+        args.config,
+        args.checkpoint,
+        device=args.device,
+        cfg_options=cfg_options)
+
+    # init visualizer
+    visualizer = VISUALIZERS.build(model.cfg.visualizer)
+    visualizer.set_dataset_meta(model.dataset_meta)
+
+    # inference a single image
+    batch_results = inference_topdown(model, args.img)
+    results = merge_data_samples(batch_results)
+
+    # show the results
+    img = imread(args.img, channel_order='rgb')
+    visualizer.add_datasample(
+        'result',
+        img,
+        data_sample=results,
+        draw_gt=False,
+        draw_bbox=True,
+        draw_heatmap=args.draw_heatmap,
+        show_kpt_idx=args.show_kpt_idx,
+        show=args.show,
+        out_file=args.out_file)
+
+
+if __name__ == '__main__':
+    main()
