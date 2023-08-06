e5c7cc7c6b1877bce6bd55b5f03a1a348142bb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:26 +0000

    Add package deepmd-kit/2.2.0/deepmd-kit-2.2.0.tar.gz

---
+++ packages/deepmd-kit/deepmd-kit-2.2.0.tar.gz/deepmd-kit-2.2.0/doc/credits.rst
@@ -0,0 +1,15 @@
+*******************
+Authors and Credits
+*******************
+
+Package Contributors
+=========================
+
+.. git-shortlog-authors::
+
+
+Other Credits
+=============
+
+* Zhang ZiXuan for designing the Deepmodeling logo.
+* Everyone on the `Deepmodeling mailing list` for contributing to many discussions and decisions!
---
+++ packages/deepmd-kit/deepmd-kit-2.2.0.tar.gz/deepmd-kit-2.2.0/source/install/build_cc.sh
@@ -0,0 +1,31 @@
+set -e
+
+if [ "$DP_VARIANT" = "cuda" ]; then
+	CUDA_ARGS="-DUSE_CUDA_TOOLKIT=TRUE"
+elif [ "$DP_VARIANT" = "rocm" ]; then
+	CUDA_ARGS="-DUSE_ROCM_TOOLKIT=TRUE"
+fi
+#------------------
+
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+if [ -z "$INSTALL_PREFIX" ]; then
+	INSTALL_PREFIX=$(realpath -s ${SCRIPT_PATH}/../../dp)
+fi
+if [ -z "$DOWNLOAD_TENSORFLOW" ]; then
+	DOWNLOAD_TENSORFLOW=TRUE
+fi
+mkdir -p ${INSTALL_PREFIX}
+echo "Installing DeePMD-kit to ${INSTALL_PREFIX}"
+NPROC=$(nproc --all)
+
+#------------------
+
+BUILD_TMP_DIR=${SCRIPT_PATH}/../build
+mkdir -p ${BUILD_TMP_DIR}
+cd ${BUILD_TMP_DIR}
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DINSTALL_TENSORFLOW=${DOWNLOAD_TENSORFLOW} ${CUDA_ARGS} -DLAMMPS_VERSION=stable_23Jun2022_update3 ..
+make -j${NPROC}
+make install
+
+#------------------
+echo "Congratulations! DeePMD-kit has been installed at ${INSTALL_PREFIX}"
