9c05386b79305b1d533d86e4ad346844d8bac790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:59 +0000

    Add package ZHPT1CSCOE/0.0.1/ZHPT1CSCOE-0.0.1.tar.gz

---
+++ packages/ZHPT1CSCOE/ZHPT1CSCOE-0.0.1.tar.gz/ZHPT1CSCOE-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ZHPT1CSCOE',
+    version='0.0.1',
+    packages=['ZHPT1CSCOE'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
