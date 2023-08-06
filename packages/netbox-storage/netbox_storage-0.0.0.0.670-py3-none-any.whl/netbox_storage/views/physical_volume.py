055e4df47f0320fe8e450d7926cbf45a2f53b2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:06 +0000

    Add package netbox-storage/0.0.0.0.670/netbox_storage-0.0.0.0.670-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.670-py3-none-any.whl/netbox_storage/views/physical_volume.py
@@ -0,0 +1,54 @@
+from netbox.views import generic
+
+from netbox_storage.filters import PhysicalVolumeFilter
+from netbox_storage.forms import (
+  #  PhysicalVolumeImportForm,
+    PhysicalVolumeFilterForm,
+    PhysicalVolumeForm,
+ #   PhysicalVolumeBulkEditForm
+)
+from netbox_storage.models import PhysicalVolume
+from netbox_storage.tables import PhysicalVolumeTable
+
+
+class PhysicalVolumeListView(generic.ObjectListView):
+    queryset = PhysicalVolume.objects.all()
+    filterset = PhysicalVolumeFilter
+    filterset_form = PhysicalVolumeFilterForm
+    table = PhysicalVolumeTable
+
+
+class PhysicalVolumeView(generic.ObjectView):
+    queryset = PhysicalVolume.objects.all()
+
+
+class PhysicalVolumeEditView(generic.ObjectEditView):
+    queryset = PhysicalVolume.objects.all()
+    form = PhysicalVolumeForm
+    default_return_url = "plugins:netbox_storage:physicalvolume_list"
+
+
+class PhysicalVolumeDeleteView(generic.ObjectDeleteView):
+    queryset = PhysicalVolume.objects.all()
+    default_return_url = "plugins:netbox_storage:physicalvolume_list"
+
+
+"""
+class PhysicalVolumeBulkImportView(generic.BulkImportView):
+    queryset = PhysicalVolume.objects.all()
+    model_form = PhysicalVolumeImportForm
+    table = PhysicalVolumeTable
+    default_return_url = "plugins:netbox_storage:PhysicalVolume_list"
+
+
+class PhysicalVolumeBulkEditView(generic.BulkEditView):
+    queryset = PhysicalVolume.objects.all()
+    filterset = PhysicalVolumeFilter
+    table = PhysicalVolumeTable
+    form = PhysicalVolumeBulkEditForm
+
+
+class PhysicalVolumeBulkDeleteView(generic.BulkDeleteView):
+    queryset = PhysicalVolume.objects.all()
+    table = PhysicalVolumeTable
+"""
