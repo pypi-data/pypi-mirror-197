746f9fca92edc819899e1d47e1ddf1ca06cd61fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:57 +0000

    Add package MadMatrixLib/0.1/MadMatrixLib-0.1.tar.gz

---
+++ packages/MadMatrixLib/MadMatrixLib-0.1.tar.gz/MadMatrixLib-0.1/MadMatrixLib/mml.py
@@ -0,0 +1,9 @@
+def responses():
+     # list of possible output responses
+    responses = {
+        "hello": ["Hi there!", "Hello!", "Hey!", "Hey there!", "Howdy!", "Greetings!"],
+        "how are you": ["I'm doing well, thanks for asking!", "I'm great", "I'm good, thanks!"],
+        "goodbye": ["Take care! Don't hesitate to reach out if you need anything else.", "See you later!", "Bye!", "Farewell. It was nice talking to you.", "Alright then, bye!"],
+        "default": ["I'm sorry, I don't understand. Can you please rephrase your question?", "I'm not sure what you mean. Can you please clarify?"],
+        "who": ["I am MadMatrix", "Call me MadMatrix"]
+    }
---
+++ packages/MadMatrixLib/MadMatrixLib-0.1.tar.gz/MadMatrixLib-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MadMatrixLib",
+    version="0.1",
+    description="Just a library '",
+    author="Gowtham",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
