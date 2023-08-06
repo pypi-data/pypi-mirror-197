beb3dc13f4ac3791f2e56d68dc4213f46d8422ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:10 +0000

    Add package IQM-Vis/0.2.5.39/IQM-Vis-0.2.5.39.tar.gz

---
+++ packages/IQM-Vis/IQM-Vis-0.2.5.39.tar.gz/IQM-Vis-0.2.5.39/IQM_Vis/image_metrics/metrics.py
@@ -0,0 +1,105 @@
+'''
+sample metrics to use with the examples of the UI
+'''
+# Author: Matt Clifford <matt.clifford@bristol.ac.uk>
+import sys; sys.path.append('..'); sys.path.append('.')
+import torch
+import torch.nn as nn
+import numpy as np
+import warnings
+
+from torchmetrics import StructuralSimilarityIndexMeasure as SSIM
+from torchmetrics import MultiScaleStructuralSimilarityIndexMeasure as MSSIM
+from torchmetrics import PeakSignalNoiseRatio as PSNR
+# from torchmetrics import UniversalImageQualityIndex as UIQI
+from torchmetrics.functional import universal_image_quality_index as UIQI
+from torchmetrics import SpectralDistortionIndex as SDI
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity as LPIPS
+
+
+''' simple functional format to call a metric (numpy)'''
+def MAE(im_ref, im_comp, **kwargs):
+    check_shapes(im_ref, im_comp)
+    L1 = np.abs(im_ref - im_comp)
+    return L1.mean()
+
+''' functional (using torch)'''
+def MSE(im_ref, im_comp, **kwargs):
+    check_shapes(im_ref, im_comp)
+    metric = nn.MSELoss()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    im_ref = preprocess_numpy_image(im_ref).to(device=device, dtype=torch.float)
+    im_comp = preprocess_numpy_image(im_comp).to(device=device, dtype=torch.float)
+    _score = metric(im_ref, im_comp)
+    return _score.cpu().detach().numpy()
+
+''' can also call as a class to input default args (using torch)'''
+class ssim:
+    def __init__(self):
+        self.metric = SSIM
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.preproccess_function = preprocess_numpy_image
+
+    def __call__(self, im_ref, im_comp, **kwargs):
+        check_shapes(im_ref, im_comp)
+        im_ref = self.preproccess_function(im_ref).to(device=self.device, dtype=torch.float)
+        im_comp = self.preproccess_function(im_comp).to(device=self.device, dtype=torch.float)
+        # set up metric
+        with warnings.catch_warnings():    # we don't care about the warnings these give
+            warnings.simplefilter("ignore")
+            _metric = self.metric(**kwargs)
+            _metric.to(self.device)
+        _score = _metric(im_ref, im_comp)
+        score = 1 - _score.cpu().detach().numpy()
+        _metric.reset()
+        return score
+
+
+'''Example of metric image produced to display (using numpy)'''
+def MSE_image(im_ref, im_comp, **kwargs):
+    check_shapes(im_ref, im_comp)
+    L2 = (im_ref - im_comp)**2
+    return L2
+
+'''Example of metric image produced to display (using torch metrics)'''
+class SSIM_image:
+    def __init__(self):
+        self.metric = SSIM
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.preproccess_function = preprocess_numpy_image
+
+    def __call__(self, im_ref, im_comp, **kwargs):
+        check_shapes(im_ref, im_comp)
+        im_ref = self.preproccess_function(im_ref).to(device=self.device, dtype=torch.float)
+        im_comp = self.preproccess_function(im_comp).to(device=self.device, dtype=torch.float)
+        # set up metric
+        with warnings.catch_warnings():    # we don't care about the warnings these give
+            warnings.simplefilter("ignore")
+            _metric_image = self.metric(return_full_image=True, reduction=None, **kwargs)
+            _metric_image.to(self.device)
+        _, ssim_full_im = _metric_image(im_ref, im_comp)
+        ssim_full_im = torch.squeeze(ssim_full_im, axis=0)
+        ssim_full_im = ssim_full_im.permute(1, 2, 0)
+        ssim_full_im = torch.clip(ssim_full_im, 0, 1)
+        im = ssim_full_im.cpu().detach().numpy()
+        _metric_image.reset()   # clear mem buffer to stop overflow
+        return im
+
+
+'''
+Helper functions for metric computation
+'''
+def preprocess_numpy_image(image):
+    if len(image.shape) == 2:
+        image = np.expand_dims(image, axis=2)
+    image = image.transpose((2, 0, 1))
+    image = np.expand_dims(image, axis=0)   # make into batch one 1
+    image = torch.from_numpy(image)
+    return image
+
+def check_shapes(im_ref, im_comp):
+    '''
+    make sure both images have the same dimensions
+    '''
+    if im_ref.shape != im_comp.shape:
+        raise ValueError(f'Refence and transformed images need to have the same shape not: {im_ref.shape} and {im_comp.shape}')
---
+++ packages/IQM-Vis/IQM-Vis-0.2.5.39.tar.gz/IQM-Vis-0.2.5.39/IQM_Vis/transformations/transforms.py
@@ -0,0 +1,150 @@
+'''
+Sample image transformations to get the user started with
+'''
+# Author: Matt Clifford <matt.clifford@bristol.ac.uk>
+from skimage.transform import resize, rotate
+from skimage.util import img_as_ubyte
+import cv2
+import numpy as np
+
+
+def rotation(image, angle):
+    '''Rotate an image around its centre. Uses skimage.transform.rotate. Areas
+    that are rotated beyond the image are filled in with black pixel values
+
+    Args:
+        image: image to be rotated (numpy array)
+        angle: amount of rotation in degrees
+
+    Returns:
+        image: rotated image (numpy array)
+    '''
+    return rotate(image, angle)
+
+def blur(image, param):
+    if param == 1:
+        return image
+    elif param > 0:
+        blur_odd = (int(param/2)*2) + 1    # need to make kernel size odd
+        image = cv2.GaussianBlur(image,(blur_odd, blur_odd), cv2.BORDER_DEFAULT)
+        if len(image.shape) == 2:
+            image = np.expand_dims(image, axis=2)
+    return image
+
+def zoom(image, param):
+    return zoom_image(image, param)
+
+def x_shift(image, param):
+    return translate_image(image, param, 0)
+
+def y_shift(image, param):
+    return translate_image(image, 0, param)
+
+def brightness(image, param):
+    return np.clip(image + param, 0, 1)
+
+def translate_image(image, x_shift, y_shift):
+    ''' x and y shift in range (-1, 1)'''
+    if x_shift == 0 and y_shift == 0:
+        return image
+    original_size = image.shape
+    canvas = np.zeros(original_size, dtype=image.dtype)
+    prop_x = int(original_size[1]*abs(x_shift))
+    prop_y = int(original_size[0]*abs(y_shift))
+    if original_size[1] - prop_x < 1 or original_size[0] - prop_y < 1: # make sure we dont go off the canvas
+        return canvas
+    if y_shift >= 0 and x_shift >= 0:
+        canvas[prop_y:,prop_x:,:] = image[:original_size[0]-prop_y,:original_size[1]-prop_x,:]
+    elif y_shift < 0 and x_shift >= 0:
+        canvas[:original_size[0]-prop_y,prop_x:,:] = image[prop_y-original_size[0]:,:original_size[1]-prop_x,:]
+    elif y_shift >= 0 and x_shift < 0:
+        canvas[prop_y:,:original_size[1]-prop_x,:] = image[:original_size[0]-prop_y,prop_x-original_size[1]:,:]
+    elif y_shift < 0 and x_shift < 0:
+        canvas[:original_size[0]-prop_y:,:original_size[1]-prop_x,:] = image[prop_y-original_size[0]:,prop_x-original_size[1]:,:]
+    return canvas
+
+def zoom_image(image, factor):
+    ''' digital zoom of image: scale_factor the % to zoom in by (for square zoom only)
+        0.5  = 2x zoom out
+        1 = normal size
+        2 = 2x zoom in'''
+    original_size = image.shape
+    new_size_x = int(original_size[0]/factor)
+    new_size_y = int(original_size[1]/factor)
+    if factor > 1:
+        start_point_x = int((original_size[0] - new_size_x)/2)
+        start_point_y = int((original_size[1] - new_size_y)/2)
+        image = image[start_point_x:start_point_x+new_size_x,
+                      start_point_y:start_point_y+new_size_y,
+                      :]
+    elif factor < 1:
+        new_size = (new_size_x, new_size_y, original_size[2]) if len(original_size) == 3 else (new_size_x, new_size_y)
+        start_point_x = int((new_size_x - original_size[0])/2)
+        start_point_y = int((new_size_y - original_size[1])/2)
+        zoomed_out = np.zeros(new_size, dtype=image.dtype)
+        zoomed_out[start_point_x:start_point_x+original_size[0],
+                   start_point_y:start_point_y+original_size[1],
+                   :] = image
+        image = zoomed_out
+
+    return resize(image, original_size)
+
+def binary_threshold(image, param):
+    image = img_as_ubyte(image)
+    if len(image.shape) > 2:
+        if image.shape[2] == 3:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, int(param))
+    if len(image.shape) == 2:
+        image = image[..., np.newaxis]
+    return image.astype(np.float32) / 255.0
+
+def jpeg_compression(image, compression=90):
+    '''encode image using jpeg then decode
+
+    Args:
+        image: image to be rotated (numpy array)
+        compression: amount of jpeg compression (100 returns identity image)
+
+    Returns:
+        image: rotated image (numpy array)
+    '''
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), int(compression)]
+    encoder = '.jpg'
+    return _encode_compression(image, encoder, encode_param)
+
+def png_compression(image, compression=9):
+    '''encode image using png then decode (note png is lossless compression)
+
+    Args:
+        image: image to be rotated (numpy array)
+        compression: amount of png compression
+
+    Returns:
+        image: rotated image (numpy array)
+    '''
+    encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), int(compression)]
+    encoder = '.png'
+    return _encode_compression(image, encoder, encode_param)
+
+def _encode_compression(image, encoder, encode_param, uint=True):
+    '''
+        generic image encoder for jpeg, png etc
+        using https://docs.opencv.org/3.4/d4/da8/group__imgcodecs.html to encode/decode
+        for encode types: https://docs.opencv.org/3.4/d6/d87/imgcodecs_8hpp.html
+    '''
+    original_size = image.shape
+    if uint == True:
+        # jpeg/png encoder needs uint images not float
+        image = img_as_ubyte(image)
+    # encode image
+    result, encoded_image = cv2.imencode(encoder, image, encode_param)
+    # decode image
+    decoded_image = cv2.imdecode(encoded_image, 1)
+    if uint == True:
+        # return to float type
+        image = decoded_image.astype(np.float32) / 255.0
+    # have to resize as jpeg can sometimes change the size of an image
+    if image.shape != original_size:
+        image = resize(image, original_size)
+    return image
