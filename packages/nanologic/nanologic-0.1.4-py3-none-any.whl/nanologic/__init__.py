f774a464a221c6c62cef537437d73c661c1bd599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:28 +0000

    Add package nanologic/0.1.4/nanologic-0.1.4-py3-none-any.whl

---
+++ packages/nanologic/nanologic-0.1.4-py3-none-any.whl/nanologic/__init__.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------
+# © 2023 Alexander Isaychikov
+# Released under MIT License
+# Minsk, Belarus
+# email alipheesa@gmail.com
+# -----------------------------------------------------------
+
+from .Variable import Variable
+from .operations import get_truth_table, print_truth_table, get_index_form, \
+    build_PCNF, build_PDNF, minimize_PCNF, minimize_PDNF, build_numPCNF, build_numPDNF
+
+__all__ = [
+    'Variable',
+    'get_truth_table',
+    'print_truth_table',
+    'get_index_form',
+    'build_PCNF',
+    'build_PDNF',
+    'minimize_PCNF',
+    'minimize_PDNF',
+    'build_numPCNF',
+    'build_numPDNF'
+]
