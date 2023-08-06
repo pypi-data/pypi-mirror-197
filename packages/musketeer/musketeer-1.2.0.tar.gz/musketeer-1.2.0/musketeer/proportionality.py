71ca80da241fc0d51ccaba0042a1fc5406824bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:44 +0000

    Add package musketeer/1.2.0/musketeer-1.2.0.tar.gz

---
+++ packages/musketeer/musketeer-1.2.0.tar.gz/musketeer-1.2.0/musketeer/proportionality.py
@@ -0,0 +1,36 @@
+from abc import abstractmethod
+
+import numpy as np
+
+from . import moduleFrame
+
+
+class Proportionality(moduleFrame.Strategy):
+    requiredAttributes = ()
+
+    @abstractmethod
+    def run(self, signalVars, totalConcs):
+        pass
+
+
+class GetConcs(Proportionality):
+    def run(self, signalVars, totalConcs):
+        return signalVars
+
+
+class GetFraction(Proportionality):
+    def run(self, signalVars, totalConcs):
+        # divide by total concs
+        # TODO: support different signals caused by different parent species
+        return signalVars / np.sum(signalVars, axis=1, keepdims=True)
+
+
+class ModuleFrame(moduleFrame.ModuleFrame):
+    group = "Experiment"
+    dropdownLabelText = "What are the signals proportional to?"
+    dropdownOptions = {
+        "Concentration (slow exchange)": GetConcs,
+        "Mole fraction (fast exchange)": GetFraction,
+    }
+    attributeName = "proportionality"
+    setDefault = False
