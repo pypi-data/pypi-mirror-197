3de1711f3482acd661f31bcf40f2fd0639bf4dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:58 +0000

    Add package hil-core/0.0.2/hil_core-0.0.2-cp37-cp37m-win_amd64.whl

---
+++ packages/hil-core/hil_core-0.0.2-cp37-cp37m-win_amd64.whl/hil/core/__init__.py
@@ -0,0 +1,17 @@
+'''
+Copyright (c) 2023 by HIL Group
+Author: hufeng.mao@carota.ai
+Date: 2023-03-16 16:13:35
+LastEditors: notmmao@gmail.com
+LastEditTime: 2023-03-17 15:10:01
+Description: 
+
+==========  =============  ================
+When        Who            What and why
+==========  =============  ================
+
+==========  =============  ================
+'''
+from .uds_client import UdsClient
+from .doip_client import DoipClient
+from .cantp import Cantp
