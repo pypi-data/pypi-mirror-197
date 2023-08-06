e6e3f8c38bd44c3db2e883435cdfa8582d1d71eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:31 +0000

    Add package aiotools/1.6.0/aiotools-1.6.0.tar.gz

---
+++ packages/aiotools/aiotools-1.6.0.tar.gz/aiotools-1.6.0/src/aiotools/taskgroup/__init__.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from .types import MultiError, TaskGroupError
+
+if hasattr(asyncio, 'TaskGroup'):
+    from . import base
+    from . import persistent
+    from .base import *        # noqa
+    from .persistent import *  # noqa
+    __all__ = [
+        'MultiError',
+        'TaskGroupError',
+        *base.__all__,
+        *persistent.__all__,
+    ]
+else:
+    from . import base_compat
+    from . import persistent_compat
+    from .base_compat import *        # type: ignore  # noqa
+    from .persistent_compat import *  # type: ignore  # noqa
+    __all__ = [  # type: ignore
+        'MultiError',
+        'TaskGroupError',
+        *base_compat.__all__,
+        *persistent_compat.__all__,
+    ]
+
+from .utils import as_completed_safe  # noqa
+
+__all__.append("as_completed_safe")
