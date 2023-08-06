69ba91fbbdbf1857b001044d7f07eee12fd5785e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:32 +0000

    Add package tawnyapi/0.1.9/tawnyapi-0.1.9-py3-none-any.whl

---
+++ packages/tawnyapi/tawnyapi-0.1.9-py3-none-any.whl/tawnyapi/vision/apitypes.py
@@ -0,0 +1,21 @@
+
+from enum import Enum
+
+
+class ImageInputType(str, Enum):
+    RAW = 'RAW'
+    FACE = 'FACE'
+    FACE_EMBEDDING = 'FACE_EMBEDDING'
+
+
+class ImageAnnotationFeatures(str, Enum):
+    FACE_DETECTION = 'FACE_DETECTION'
+    FACE_LANDMARKS = 'FACE_LANDMARKS'
+    FACE_EMOTION = 'FACE_EMOTION'
+    FACE_DESCRIPTOR = 'FACE_DESCRIPTOR'
+    ATTENTION = 'ATTENTION'
+    HEAD_POSE = 'HEAD_POSE'
+
+
+class SdkVersion(str, Enum):
+    V_1_4 = 'V_1_4'
