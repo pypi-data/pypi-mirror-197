43451673c18bd7b94bd05be68d15509106feffbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:24 +0000

    Add package nbcc_loading/0.0.2/nbcc_loading-0.0.2-py3-none-any.whl

---
+++ packages/nbcc_loading/nbcc_loading-0.0.2-py3-none-any.whl/nbcc_loading/loading_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from pathlib import Path
+
+SNOW_FILES = {
+    2020: 'snow-2020.pkl'
+}
+
+WIND_FILES = {
+    2020: 'wind-2020.pkl'
+}
+
+SEISMIC_FILES = {
+}
+
+
+def read_pickle(filepath: Path) -> pd.DataFrame:
+    "returns the dataframe from specified filepath"
+    return pd.read_pickle(filepath)
+
+def read_pickle_from_data(filename: str) -> pd.DataFrame:
+    "Returns the dataframe from specified file located in the data folder"
+    filepath = Path(__file__).parent / "data" / filename
+    return read_pickle(filepath)
+
+class Loading:
+    LOADING_FILES = {
+        "Snow": SNOW_FILES,
+        "Wind": WIND_FILES,
+        "Seismic": SEISMIC_FILES
+    }
+    def __init__(self, loading_type: str) -> None:
+        self.FILES = self.LOADING_FILES[loading_type]
+        self.loading_type = loading_type
+
+    def __str__(self) -> str:
+        return f"{self.loading_type} loading class"
+
+    def get(self, year: int) -> pd.DataFrame:
+        "Returns the dataframe with snow load data for the year specified"
+        year = int(year)
+        if not year in self.FILES.keys():
+            raise Exception(f"{year} data is not available. Acceptable options include: {[yr for yr in self.FILES.keys()]}")
+        return read_pickle_from_data( filename=self.FILES.get(year) )
