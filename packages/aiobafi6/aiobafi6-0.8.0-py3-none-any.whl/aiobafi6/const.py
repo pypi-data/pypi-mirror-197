f77a12190164e34bcba04eb0b1e05d02a2138fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:44 +0000

    Add package aiobafi6/0.8.0/aiobafi6-0.8.0-py3-none-any.whl

---
+++ packages/aiobafi6/aiobafi6-0.8.0-py3-none-any.whl/aiobafi6/const.py
@@ -0,0 +1,7 @@
+"""Constants for aiobafi6."""
+
+# The minimum device API version supported by aiobafi6.
+MIN_API_VERSION = 4
+
+# The minimum device API version for occupancy support.
+OCCUPANCY_MIN_API_VERSION = 5
