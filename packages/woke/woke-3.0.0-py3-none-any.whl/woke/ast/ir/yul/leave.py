391bcd477cafae1b0bb53879b6c3ec7e862543fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:26 +0000

    Add package woke/3.0.0/woke-3.0.0-py3-none-any.whl

---
+++ packages/woke/woke-3.0.0-py3-none-any.whl/woke/ast/ir/yul/leave.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .abc import YulAbc
+
+if TYPE_CHECKING:
+    from .block import Block
+
+
+class Leave(YulAbc):
+    """
+    TBD
+    """
+    _parent: Block
+
+    @property
+    def parent(self) -> Block:
+        return self._parent
