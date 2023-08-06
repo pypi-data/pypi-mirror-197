c3c8cd7160d251a09e96990d6a056774fee0ec96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:43 +0000

    Add package rssbot/300/rssbot-300.tar.gz

---
+++ packages/rssbot/rssbot-300.tar.gz/rssbot-300/rssbot/specifi.py
@@ -0,0 +1,9 @@
+# This file is placed in the Public Domain.
+
+
+"specification"
+
+
+from opr.objects import Object, items, keys, otype, oid, search, tostr
+from opr.objects import update, values
+from opr.storage import find, last, save
