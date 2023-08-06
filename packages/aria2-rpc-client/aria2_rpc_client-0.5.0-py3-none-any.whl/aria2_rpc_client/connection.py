a778ff5006dcc72ecef8d1504a557fbd9b89395f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:05 +0000

    Add package aria2-rpc-client/0.5.0/aria2_rpc_client-0.5.0-py3-none-any.whl

---
+++ packages/aria2-rpc-client/aria2_rpc_client-0.5.0-py3-none-any.whl/aria2_rpc_client/connection.py
@@ -0,0 +1,22 @@
+from typing import Protocol
+from xmlrpc.client import ServerProxy
+
+
+class Connection(Protocol):
+    host: str
+    port: str
+    secret: str
+
+    def make_connection(self) -> ServerProxy:
+        ...
+
+
+class DefaultConnection:
+    def __init__(self, host: str = "localhost", port: str = "6800", secret: str = "") -> None:
+        self.host = host
+        self.port = port
+        self.secret = f"token:{secret}"
+
+    def make_connection(self) -> ServerProxy:
+        addr = f"http://{self.host}:{self.port}/rpc"
+        return ServerProxy(addr)
