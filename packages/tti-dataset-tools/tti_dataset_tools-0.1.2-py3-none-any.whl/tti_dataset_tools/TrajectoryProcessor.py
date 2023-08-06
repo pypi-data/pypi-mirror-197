bac4dff0346783220834061b320bf89edcc06b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:43 +0000

    Add package tti-dataset-tools/0.1.2/tti_dataset_tools-0.1.2-py3-none-any.whl

---
+++ packages/tti-dataset-tools/tti_dataset_tools-0.1.2-py3-none-any.whl/tti_dataset_tools/TrajectoryProcessor.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from .ColMapper import ColMapper
+
+class TrajectoryProcessor:
+
+    def __init__(self,
+            colMapper: ColMapper
+        ):
+        self.idCol = colMapper.idCol
+        self.xCol = colMapper.xCol
+        self.yCol = colMapper.yCol
+        self.xVelCol = colMapper.xVelCol
+        self.yVelCol = colMapper.yVelCol
+        self.speedCol = colMapper.speedCol
+        self.fps = colMapper.fps
+        
+        self.displacementXCol = colMapper.displacementXCol
+        self.displacementYCol = colMapper.displacementYCol
+        self.localXCol = colMapper.localXCol
+        self.localYCol = colMapper.localYCol
---
+++ packages/tti-dataset-tools/tti_dataset_tools-0.1.2-py3-none-any.whl/tti_dataset_tools/TrajectoryVisualizer.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from typing import *
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+class TrajectoryVisualizer:
+
+    
+    def show(self, df: pd.DataFrame, idCol, xCol, yCol, trackIds=None, colorCol=None):
+        
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot()
+        if trackIds is None:
+            trackIds = df[idCol].unique()
+
+        for trackId in trackIds:
+            trackDf = df[df[idCol] == trackId]
+            if colorCol is None:
+                plt.plot(trackDf[xCol], trackDf[yCol])
+            else:
+                plt.scatter(trackDf[xCol], trackDf[yCol], s=1, c=trackDf[colorCol].astype(int).tolist())
+
+            # plot direction
+            lastRow = trackDf.tail(1)
+            endPoint = (lastRow[xCol] , lastRow[yCol])
+            plt.plot(endPoint[0], endPoint[1], marker='x')
+        
+        ax.set_aspect('equal', adjustable='box')
+        plt.show()
