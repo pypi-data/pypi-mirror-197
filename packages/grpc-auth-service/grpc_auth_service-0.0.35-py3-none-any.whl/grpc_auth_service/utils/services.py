e198c72691a84d1c6e1ad73dc73d31fb77340701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:22 +0000

    Add package grpc-auth-service/0.0.35/grpc_auth_service-0.0.35-py3-none-any.whl

---
+++ packages/grpc-auth-service/grpc_auth_service-0.0.35-py3-none-any.whl/grpc_auth_service/utils/services.py
@@ -0,0 +1,4 @@
+class Services:
+    MOVIES_SEARCH_SERVICE: str = "movies_search_service"
+    PROXY_AUTH_SERVICE: str = "proxy_auth_service"
+    MOVIES_ADMIN_PANEL_SERVICE: str = "movies_admin_panel_service"
