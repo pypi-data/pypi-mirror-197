fce6a6e3b64a55d944a7bfaa06699aa90b3594c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:00 +0000

    Add package usb-wire/1.0.1/usb_wire-1.0.1.tar.gz

---
+++ packages/usb-wire/usb_wire-1.0.1.tar.gz/usb_wire-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='usb_wire',  # Nom de votre package
+    version='1.0.1',  # Version de votre package
+    author='mb_craft',  # Votre nom
+    description='Wire has been created to make relation between a usb device and a Python script.',  # Description de votre package
+    classifiers=[  # Liste des classes de classification pour votre package
+        'Programming Language :: Python :: 3.10',
+    ],
+)
