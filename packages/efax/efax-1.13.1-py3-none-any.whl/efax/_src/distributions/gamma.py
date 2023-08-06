cc3d7fe514c9e52e4ceda537eec185144af806b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:36 +0000

    Add package efax/1.13.1/efax-1.13.1-py3-none-any.whl

---
+++ packages/efax/efax-1.13.1-py3-none-any.whl/efax/_src/distributions/gamma.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import jax
+import jax.numpy as jnp
+from jax.nn import softplus
+from jax.random import KeyArray
+from jax.scipy import special as jss
+from tjax import JaxRealArray, Shape, inverse_softplus
+from tjax.dataclasses import dataclass
+
+from ..exp_to_nat import ExpToNat
+from ..natural_parametrization import NaturalParametrization
+from ..parameter import ScalarSupport, distribution_parameter
+from ..samplable import Samplable
+
+__all__ = ['GammaNP', 'GammaEP']
+
+
+@dataclass
+class GammaNP(NaturalParametrization['GammaEP', JaxRealArray], Samplable):
+    negative_rate: JaxRealArray = distribution_parameter(ScalarSupport())
+    shape_minus_one: JaxRealArray = distribution_parameter(ScalarSupport())
+
+    # Implemented methods --------------------------------------------------------------------------
+    @property
+    def shape(self) -> Shape:
+        return self.negative_rate.shape
+
+    def log_normalizer(self) -> JaxRealArray:
+        shape = self.shape_minus_one + 1.0
+        return jss.gammaln(shape) - shape * jnp.log(-self.negative_rate)
+
+    def to_exp(self) -> GammaEP:
+        shape = self.shape_minus_one + 1.0
+        return GammaEP(-shape / self.negative_rate,
+                       jss.digamma(shape) - jnp.log(-self.negative_rate))
+
+    def carrier_measure(self, x: JaxRealArray) -> JaxRealArray:
+        return jnp.zeros(x.shape)
+
+    def sufficient_statistics(self, x: JaxRealArray) -> GammaEP:
+        return GammaEP(x, jnp.log(x))
+
+    def sample(self, key: KeyArray, shape: Shape | None = None) -> JaxRealArray:
+        if shape is not None:
+            shape += self.shape
+        return -jax.random.gamma(key, self.shape_minus_one + 1.0, shape) / self.negative_rate
+
+
+@dataclass
+class GammaEP(ExpToNat[GammaNP, JaxRealArray]):
+    mean: JaxRealArray = distribution_parameter(ScalarSupport())
+    mean_log: JaxRealArray = distribution_parameter(ScalarSupport())
+
+    # Implemented methods --------------------------------------------------------------------------
+    @property
+    def shape(self) -> Shape:
+        return self.mean.shape
+
+    @classmethod
+    def natural_parametrization_cls(cls) -> type[GammaNP]:
+        return GammaNP
+
+    def expected_carrier_measure(self) -> JaxRealArray:
+        return jnp.zeros(self.shape)
+
+    def search_to_natural(self, search_parameters: JaxRealArray) -> GammaNP:
+        shape = softplus(search_parameters)
+        rate = shape / self.mean
+        return GammaNP(-rate, shape - 1.0)
+
+    def search_gradient(self, search_parameters: JaxRealArray) -> JaxRealArray:
+        shape = softplus(search_parameters)
+        log_mean_minus_mean_log = jnp.log(self.mean) - self.mean_log
+        return log_mean_minus_mean_log - jnp.log(shape) + jss.digamma(shape)
+        # gradient is 1.0 / shape - jss.polygamma(1, shape)
+        # where polygamma(1) is trigamma
+
+    # Overridden methods ---------------------------------------------------------------------------
+    def initial_search_parameters(self) -> JaxRealArray:
+        log_mean_minus_mean_log = jnp.log(self.mean) - self.mean_log
+        initial_shape = (
+            (3.0 - log_mean_minus_mean_log
+             + jnp.sqrt((log_mean_minus_mean_log - 3.0) ** 2 + 24.0 * log_mean_minus_mean_log))
+            / (12.0 * log_mean_minus_mean_log))
+        return inverse_softplus(initial_shape)
---
+++ packages/efax/efax-1.13.1-py3-none-any.whl/efax/_src/distributions/multivariate_normal/fixed_variance.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+import math
+
+import jax
+import jax.numpy as jnp
+from jax.random import KeyArray
+from tjax import JaxRealArray, Shape
+from tjax.dataclasses import dataclass
+
+from ...conjugate_prior import HasGeneralizedConjugatePrior
+from ...multidimensional import Multidimensional
+from ...natural_parametrization import NaturalParametrization
+from ...parameter import ScalarSupport, VectorSupport, distribution_parameter
+from ...samplable import Samplable
+from .diagonal import MultivariateDiagonalNormalNP
+from .isotropic import IsotropicNormalNP
+
+__all__ = ['MultivariateFixedVarianceNormalNP', 'MultivariateFixedVarianceNormalEP']
+
+
+@dataclass
+class MultivariateFixedVarianceNormalNP(NaturalParametrization['MultivariateFixedVarianceNormalEP',
+                                                               JaxRealArray],
+                                        Multidimensional,
+                                        Samplable):
+    """The multivariate normal distribution with fixed variance.
+
+    This is a curved exponential family.
+    """
+    mean_times_precision: JaxRealArray = distribution_parameter(VectorSupport())
+    variance: JaxRealArray = distribution_parameter(ScalarSupport(), fixed=True)
+
+    # Implemented methods --------------------------------------------------------------------------
+    @property
+    def shape(self) -> Shape:
+        return self.mean_times_precision.shape[:-1]
+
+    def log_normalizer(self) -> JaxRealArray:
+        eta = self.mean_times_precision
+        return 0.5 * (jnp.sum(jnp.square(eta), axis=-1) * self.variance
+                      + self.dimensions() * jnp.log(math.pi * 2.0 * self.variance))
+
+    def to_exp(self) -> MultivariateFixedVarianceNormalEP:
+        return MultivariateFixedVarianceNormalEP(
+            self.mean_times_precision * self.variance[..., jnp.newaxis],
+            variance=self.variance)
+
+    def carrier_measure(self, x: JaxRealArray) -> JaxRealArray:
+        return -0.5 * jnp.sum(jnp.square(x), axis=-1) / self.variance
+
+    def sufficient_statistics(self, x: JaxRealArray) -> MultivariateFixedVarianceNormalEP:
+        shape = x.shape[:-1]
+        variance = jnp.broadcast_to(self.variance, shape)
+        return MultivariateFixedVarianceNormalEP(x, variance=variance)
+
+    def sample(self, key: KeyArray, shape: Shape | None = None) -> JaxRealArray:
+        return self.to_exp().sample(key, shape)
+
+    def dimensions(self) -> int:
+        return self.mean_times_precision.shape[-1]
+
+
+@dataclass
+class MultivariateFixedVarianceNormalEP(
+        HasGeneralizedConjugatePrior[MultivariateFixedVarianceNormalNP],
+        Multidimensional,
+        Samplable):
+    mean: JaxRealArray = distribution_parameter(VectorSupport())
+    variance: JaxRealArray = distribution_parameter(ScalarSupport(), fixed=True)
+
+    # Implemented methods --------------------------------------------------------------------------
+    @property
+    def shape(self) -> Shape:
+        return self.mean.shape[:-1]
+
+    @classmethod
+    def natural_parametrization_cls(cls) -> type[MultivariateFixedVarianceNormalNP]:
+        return MultivariateFixedVarianceNormalNP
+
+    def to_nat(self) -> MultivariateFixedVarianceNormalNP:
+        return MultivariateFixedVarianceNormalNP(self.mean / self.variance[..., jnp.newaxis],
+                                                 variance=self.variance)
+
+    def expected_carrier_measure(self) -> JaxRealArray:
+        return -0.5 * (jnp.sum(jnp.square(self.mean), axis=-1) / self.variance + self.dimensions())
+
+    def sample(self, key: KeyArray, shape: Shape | None = None) -> JaxRealArray:
+        if shape is not None:
+            shape += self.mean.shape
+        else:
+            shape = self.mean.shape
+        variance = self.variance[..., jnp.newaxis]
+        deviation = jnp.sqrt(variance)
+        return jax.random.normal(key, shape) * deviation + self.mean
+
+    def conjugate_prior_distribution(self, n: JaxRealArray) -> IsotropicNormalNP:
+        negative_half_precision = -0.5 * n / self.variance
+        return IsotropicNormalNP(n[..., jnp.newaxis] * self.mean, negative_half_precision)
+
+    def generalized_conjugate_prior_distribution(self, n: JaxRealArray
+                                                 ) -> MultivariateDiagonalNormalNP:
+        negative_half_precision = -0.5 * n / self.variance[..., jnp.newaxis]
+        return MultivariateDiagonalNormalNP(n * self.mean, negative_half_precision)
+
+    def conjugate_prior_observation(self) -> JaxRealArray:
+        return self.mean
+
+    def dimensions(self) -> int:
+        return self.mean.shape[-1]
