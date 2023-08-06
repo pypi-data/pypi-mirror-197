695f031082328c52be317ac9c52a754bfcd176e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:20 +0000

    Add package devgoldyutils/2.3/devgoldyutils-2.3.tar.gz

---
+++ packages/devgoldyutils/devgoldyutils-2.3.tar.gz/devgoldyutils-2.3/devgoldyutils/__init__.py
@@ -0,0 +1,6 @@
+from .console import Console
+
+from .colours import Colours
+from .dataclasses import DictDataclass
+from .errors import DevGoldyUtilsError
+from .logging import add_custom_handler, LoggerAdapter
