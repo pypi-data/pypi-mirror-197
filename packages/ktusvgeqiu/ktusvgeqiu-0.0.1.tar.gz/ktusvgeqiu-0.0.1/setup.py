5d8403c282035fbb6e772afec73631d453b27fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:51 +0000

    Add package ktusvgeqiu/0.0.1/ktusvgeqiu-0.0.1.tar.gz

---
+++ packages/ktusvgeqiu/ktusvgeqiu-0.0.1.tar.gz/ktusvgeqiu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ktusvgeqiu',
+    version='0.0.1',
+    packages=['ktusvgeqiu'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
