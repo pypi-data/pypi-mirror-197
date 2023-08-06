4b539866f3589958460be2da71d4b58279d17231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:25 +0000

    Add package deepmd-kit/2.2.1/deepmd-kit-2.2.1.tar.gz

---
+++ packages/deepmd-kit/deepmd-kit-2.2.1.tar.gz/deepmd-kit-2.2.1/doc/model/index.rst
@@ -0,0 +1,19 @@
+Model
+=====
+
+.. toctree::
+   :maxdepth: 1
+
+   overall
+   train-se-e2-a
+   train-se-e2-r
+   train-se-e3
+   train-se-atten
+   train-hybrid
+   sel
+   train-energy
+   train-fitting-tensor
+   train-se-e2-a-tebd
+   train-se-a-mask
+   dplr
+   dprc
---
+++ packages/deepmd-kit/deepmd-kit-2.2.1.tar.gz/deepmd-kit-2.2.1/source/gmx/include/gmx_plugin.h
@@ -0,0 +1,26 @@
+#ifndef _GMX_PLUGIN_H_
+#define _GMX_PLUGIN_H_
+#include "DeepPot.h"
+
+namespace deepmd {
+
+class DeepmdPlugin {
+ public:
+  DeepmdPlugin();
+  DeepmdPlugin(char*);
+  ~DeepmdPlugin();
+  void init_from_json(char*);
+  deepmd::DeepPot* nnp;
+  std::vector<int> dtype;
+  std::vector<int> dindex;
+  bool pbc;
+  float lmd;
+  int natom;
+};
+
+}  // namespace deepmd
+
+const float c_dp2gmx = 0.1;
+const float e_dp2gmx = 96.48533132;
+const float f_dp2gmx = 964.8533132;
+#endif
