17a7f36d4a22adec16d0d817178d0ef1412716df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:45 +0000

    Add package cdspyreadme/1.3.8/cdspyreadme-1.3.8-py3.8.egg

---
+++ packages/cdspyreadme/cdspyreadme-1.3.8-py3.8.egg/cdspyreadme/__init__.py
@@ -0,0 +1,7 @@
+""" VizieR tool for ReadMe generation """
+
+__version__ = "1.3.8"
+
+from .core import CDSTablesMaker, CDSException, \
+    CDSTable, CDSAstropyTable, CDSNumpyTable, CDSFileTable, CDSMRTTable, CDSAsciiTable
+from .CDSColumn import CDSColumn
