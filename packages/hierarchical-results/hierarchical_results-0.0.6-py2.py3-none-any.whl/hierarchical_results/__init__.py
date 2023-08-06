8c3cf027afebbc54e60f7f250a73e0bd9134ccc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:58 +0000

    Add package hierarchical-results/0.0.6/hierarchical_results-0.0.6-py2.py3-none-any.whl

---
+++ packages/hierarchical-results/hierarchical_results-0.0.6-py2.py3-none-any.whl/hierarchical_results/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Hierarchical Results."""
+
+__author__ = """Ivar Grytten"""
+__email__ = 'ivargry@ifi.uio.no'
+__version__ = '0.0.6'
+
+
+from .hierarchical_results import HierarchicalResults, ParameterCombinations
+from .logging import init_logger, get_logger
