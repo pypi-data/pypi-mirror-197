b866e7566a5fb32bccf519345d29488c13118de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:24 +0000

    Add package deepmd-kit/2.2.1/deepmd_kit-2.2.1-py37-none-win_amd64.whl

---
+++ packages/deepmd-kit/deepmd_kit-2.2.1-py37-none-win_amd64.whl/deepmd/fit/__init__.py
@@ -0,0 +1,17 @@
+from .dipole import (
+    DipoleFittingSeA,
+)
+from .ener import (
+    EnerFitting,
+)
+from .polar import (
+    GlobalPolarFittingSeA,
+    PolarFittingSeA,
+)
+
+__all__ = [
+    "DipoleFittingSeA",
+    "EnerFitting",
+    "GlobalPolarFittingSeA",
+    "PolarFittingSeA",
+]
---
+++ packages/deepmd-kit/deepmd_kit-2.2.1-py37-none-win_amd64.whl/deepmd/model/__init__.py
@@ -0,0 +1,21 @@
+from .ener import (
+    EnerModel,
+)
+from .multi import (
+    MultiModel,
+)
+from .tensor import (
+    DipoleModel,
+    GlobalPolarModel,
+    PolarModel,
+    WFCModel,
+)
+
+__all__ = [
+    "EnerModel",
+    "MultiModel",
+    "DipoleModel",
+    "GlobalPolarModel",
+    "PolarModel",
+    "WFCModel",
+]
