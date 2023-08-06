7e8281e98e19f5de795a7ad489e08724f4093d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:30 +0000

    Add package nonebot-plugin-pixivbot/1.7.8.post1/nonebot_plugin_pixivbot-1.7.8.post1.tar.gz

---
+++ packages/nonebot-plugin-pixivbot/nonebot_plugin_pixivbot-1.7.8.post1.tar.gz/nonebot_plugin_pixivbot-1.7.8.post1/src/nonebot_plugin_pixivbot/data/pixiv_repo/local_repo/sql_models.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+from typing import Optional, List
+
+from sqlalchemy import Column, Integer, ForeignKey, String, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from nonebot_plugin_pixivbot.data.source.sql import SqlDataSource
+from nonebot_plugin_pixivbot.data.utils.sql import BLOB, JSON, UTCDateTime
+
+
+@SqlDataSource.registry.mapped
+class DownloadCache:
+    __tablename__ = "download_cache"
+
+    illust_id: int = Column(Integer, primary_key=True, nullable=False)
+    page: int = Column(Integer, primary_key=True, nullable=False, default=0)
+    content: bytes = Column(BLOB, nullable=False)
+
+    update_time: datetime = Column(UTCDateTime, nullable=False, index=True)
+
+
+@SqlDataSource.registry.mapped
+class IllustDetailCache:
+    __tablename__ = "illust_detail_cache"
+
+    illust_id: int = Column(Integer, primary_key=True, nullable=False)
+    illust: dict = Column(JSON, nullable=False)
+
+    update_time: datetime = Column(UTCDateTime, nullable=False, index=True)
+
+
+@SqlDataSource.registry.mapped
+class UserDetailCache:
+    __tablename__ = "user_detail_cache"
+
+    user_id: int = Column(Integer, primary_key=True, nullable=False)
+    user: dict = Column(JSON, nullable=False)
+
+    update_time: datetime = Column(UTCDateTime, nullable=False, index=True)
+
+
+@SqlDataSource.registry.mapped
+class IllustSetCache:
+    __tablename__ = "illust_set_cache"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    cache_type: str = Column(String, nullable=False)
+    key: dict = Column(JSON, nullable=False)
+
+    update_time: datetime = Column(UTCDateTime, nullable=False, index=True)
+    pages: Optional[int] = Column(Integer)
+    next_qs: Optional[dict] = Column(JSON)
+
+    illust_id: List["IllustSetCacheIllust"] = relationship("IllustSetCacheIllust",
+                                                           foreign_keys="IllustSetCacheIllust.cache_id",
+                                                           cascade="save-update, delete",
+                                                           passive_deletes=True)
+
+    size: int = Column(Integer, nullable=False, default=0)
+
+    __table_args__ = (
+        UniqueConstraint('cache_type', 'key'),
+    )
+
+
+@SqlDataSource.registry.mapped
+class IllustSetCacheIllust:
+    __tablename__ = "illust_set_cache_illust"
+
+    cache_id: int = Column(Integer, ForeignKey("illust_set_cache.id", ondelete="cascade"), primary_key=True,
+                           nullable=False)
+    illust_id: int = Column(Integer, primary_key=True, nullable=False)
+    rank: int = Column(Integer, nullable=False, default=0)
+
+
+@SqlDataSource.registry.mapped
+class UserSetCache:
+    __tablename__ = "user_set_cache"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    cache_type: str = Column(String, nullable=False)
+    key: dict = Column(JSON, nullable=False)
+
+    update_time: datetime = Column(UTCDateTime, nullable=False, index=True)
+    pages: Optional[int] = Column(Integer)
+    next_qs: Optional[dict] = Column(JSON)
+
+    illust_id: List["UserSetCacheUser"] = relationship("UserSetCacheUser",
+                                                       foreign_keys="UserSetCacheUser.cache_id",
+                                                       cascade="save-update, delete",
+                                                       passive_deletes=True)
+
+    __table_args__ = (
+        UniqueConstraint('cache_type', 'key'),
+    )
+
+
+@SqlDataSource.registry.mapped
+class UserSetCacheUser:
+    __tablename__ = "user_set_cache_user"
+
+    cache_id: int = Column(Integer, ForeignKey("user_set_cache.id", ondelete="cascade"), primary_key=True,
+                           nullable=False)
+    user_id: int = Column(Integer, primary_key=True, nullable=False)
---
+++ packages/nonebot-plugin-pixivbot/nonebot_plugin_pixivbot-1.7.8.post1.tar.gz/nonebot_plugin_pixivbot-1.7.8.post1/src/nonebot_plugin_pixivbot/handler/command/invalidate_cache.py
@@ -0,0 +1,37 @@
+from typing import Sequence
+
+from nonebot_plugin_pixivbot.context import Inject
+from nonebot_plugin_pixivbot.data.pixiv_repo import PixivRepo
+from nonebot_plugin_pixivbot.model import T_UID, T_GID
+from nonebot_plugin_pixivbot.plugin_service import invalidate_cache_service
+from nonebot_plugin_pixivbot.protocol_dep.post_dest import PostDestination
+from .command import SubCommandHandler
+from ..interceptor.permission_interceptor import SuperuserInterceptor
+from ..interceptor.service_interceptor import ServiceInterceptor
+from ..pkg_context import context
+
+
+@context.inject
+@context.register_singleton()
+class InvalidateCacheHandler(SubCommandHandler, subcommand='invalidate_cache'):
+    repo = Inject(PixivRepo)
+
+    def __init__(self):
+        super().__init__()
+        self.add_interceptor(context.require(SuperuserInterceptor))
+        self.add_interceptor(ServiceInterceptor(invalidate_cache_service))
+
+    @classmethod
+    def type(cls) -> str:
+        return "invalidate_cache"
+
+    def enabled(self) -> bool:
+        return True
+
+    def parse_args(self, args: Sequence[str], post_dest: PostDestination[T_UID, T_GID]) -> dict:
+        return {}
+
+    async def actual_handle(self, *, post_dest: PostDestination[T_UID, T_GID],
+                            silently: bool = False, **kwargs):
+        await self.repo.invalidate_cache()
+        await self.post_plain_text(message="ok", post_dest=post_dest)
