36192d8f61e69eed498ba235c3b60738c07b16e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:48 +0000

    Add package jxhcfdwvun/0.0.1/jxhcfdwvun-0.0.1.tar.gz

---
+++ packages/jxhcfdwvun/jxhcfdwvun-0.0.1.tar.gz/jxhcfdwvun-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='jxhcfdwvun',
+    version='0.0.1',
+    packages=['jxhcfdwvun'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
