51003aeda9377ccb684eced400328c4de712ff91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:40 +0000

    Add package socianAuthPyTest/0.1.8/socianAuthPyTest-0.1.8.tar.gz

---
+++ packages/socianAuthPyTest/socianAuthPyTest-0.1.8.tar.gz/socianAuthPyTest-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='socianAuthPyTest',
+    version='0.1.8',
+    description='socianAuthPyTest test publish.',
+    long_description=README,
+    license='MIT',
+    packages=["socianAuthPyTest"],
+    author='socian',
+    author_email='socian@socian.ai',
+)
+
+install_requires=[
+    "requests >= 2.27.1"
+]
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
+
---
+++ packages/socianAuthPyTest/socianAuthPyTest-0.1.8.tar.gz/socianAuthPyTest-0.1.8/test/test.py
@@ -0,0 +1,7 @@
+import socianAuthPyTest
+
+
+g = socianAuthPyTest.SAuthSDK()
+test_data = g.test(5,2)
+
+print(test_data)
