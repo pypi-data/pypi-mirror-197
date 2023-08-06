2ef50073609fbeab213b844d0536f3d5d10095ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:23 +0000

    Add package baseten/0.5.0/baseten-0.5.0.tar.gz

---
+++ packages/baseten/baseten-0.5.0.tar.gz/baseten-0.5.0/baseten/training/__init__.py
@@ -0,0 +1,11 @@
+"""Top level training package for baseten.
+
+Exposes relavant classes from submodules for DX purposes and discoverability
+"""
+from baseten.training.datasets import Dataset, DatasetIdentifier, LocalPath, PublicUrl
+from baseten.training.finetuning import (
+    DreamboothConfig,
+    FinetuningConfig,
+    FinetuningRun,
+    FullStableDiffusionConfig,
+)
