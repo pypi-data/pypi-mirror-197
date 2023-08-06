493d82b18449738ce3b8be53b1e9c89b995ca8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:57 +0000

    Add package moontour-common/1.0.18/moontour_common-1.0.18.tar.gz

---
+++ packages/moontour-common/moontour_common-1.0.18.tar.gz/moontour_common-1.0.18/moontour_common/models/rooms/health_room.py
@@ -0,0 +1,12 @@
+from moontour_common.models.guess import Guess
+from moontour_common.models.phase import HealthPhase
+from moontour_common.models.rooms.room import BaseRoom
+
+START_HEALTH = 5000
+
+
+class HealthRoom(BaseRoom):
+    start_health = START_HEALTH
+    guess_duration: float = 15  # Time between first guess to phase ending
+    phases: list[HealthPhase] = []
+    guesses: list[dict[str, Guess]] = []  # User ID to guess
