e20689cdaecf9f0aa19ada38f72ea4757c9110cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:50 +0000

    Add package geompy/0.0.1/geompy-0.0.1-py3-none-any.whl

---
+++ packages/geompy/geompy-0.0.1-py3-none-any.whl/geompy/point/on_plane.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------------------------
+#                               on_plane.py
+# =============================================================================
+#  This is a part of geompy - A Python package for practical geometry algorithms.
+#
+#  Last updated: 16 Mar 2023. Copyright (c) 2023 Minh-Chien Trinh
+# -----------------------------------------------------------------------------
+
+
+def on_plane(plane, point):
+    """check if a point is on a plane.
+    Parameters:
+        plane: an array of coords of 03 points
+        point: an array of coords of the check point
+    Returns:
+        flag: a boolean
+    """
+
+    flag = True
+    
+    return flag
