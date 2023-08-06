8e5a36ae2df9cea859df7466821211453cbe6f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:25 +0000

    Add package inspirational-quotes/0.0.0/inspirational_quotes-0.0.0.tar.gz

---
+++ packages/inspirational-quotes/inspirational_quotes-0.0.0.tar.gz/inspirational_quotes-0.0.0/README.md
@@ -0,0 +1,2 @@
+# Inspirational Quotes
+Inspirational quotes for python developers to use in applications
---
+++ packages/inspirational-quotes/inspirational_quotes-0.0.0.tar.gz/inspirational_quotes-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='inspirational_quotes',
+    url='https://github.com/saip007/inspirational_quotes',
+    author='Saip',
+    author_email='saip4622@outlook.com',
+    packages=[''],
+    install_requires=['json'],
+    version='0.0.0',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    long_description=open('README.md').read(),
+)
