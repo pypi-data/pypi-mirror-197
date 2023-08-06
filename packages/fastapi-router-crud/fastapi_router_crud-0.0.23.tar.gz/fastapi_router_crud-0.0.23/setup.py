7198e9f3c045412d6a41bfc02b032c1047f44315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:10 +0000

    Add package fastapi-router-crud/0.0.23/fastapi_router_crud-0.0.23.tar.gz

---
+++ packages/fastapi-router-crud/fastapi_router_crud-0.0.23.tar.gz/fastapi_router_crud-0.0.23/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="fastapi_router_crud",
+    version="0.0.23",
+    description="FastAPI API router that generates model based routes(CRUD operations) automatically.",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/milanchanstveni/fastapi-crud-router",
+    author="Milan Pantelic",
+    author_email="milanpantelic95@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Framework :: AsyncIO",
+        "Environment :: Web Environment",
+        "Environment :: Plugins",
+        "Framework :: FastAPI",
+        "Topic :: Database"
+    ],
+    install_requires=["fastapi", "tortoise-orm[all]"],
+    extras_require={
+        "dev": ["pytest", "twine"],
+    },
+    python_requires=">=3.10",
+)
