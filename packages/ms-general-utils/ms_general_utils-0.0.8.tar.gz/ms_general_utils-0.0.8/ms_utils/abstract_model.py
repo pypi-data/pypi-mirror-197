c5744e6f5430de8963240774d4e36e95938cf4d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package ms-general-utils/0.0.8/ms_general_utils-0.0.8.tar.gz

---
+++ packages/ms-general-utils/ms_general_utils-0.0.8.tar.gz/ms_general_utils-0.0.8/ms_utils/abstract_model.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer
+
+from .func_date import get_timestamp_now, convert_timestamp_to_date
+from .model_utils import TimestampField
+
+
+class BaseModal:
+    created_at = Column(TimestampField)
+    updated_at = Column(TimestampField)
