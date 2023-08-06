f76814f745bb7ac0278404c69e7aece35c12143d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:09 +0000

    Add package camera-match/0.0.1/camera_match-0.0.1.tar.gz

---
+++ packages/camera-match/camera_match-0.0.1.tar.gz/camera_match-0.0.1/camera_match/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for camera_match."""
+
+__author__ = """Ethan Ou"""
+__email__ = 'ethantim@gmail.com'
+__version__ = '0.0.1'
+
+from .nodes import (
+    Lift,
+    Gain,
+    CurvesInterpolation,
+    CurvesEMOR,
+    RBF,
+    LUT,
+    LinearMatrix,
+    RootPolynomialMatrix,
+    TetrahedralMatrix,
+    Node,
+    CST,
+)
+
+from .pipeline import (
+    Pipeline
+)
