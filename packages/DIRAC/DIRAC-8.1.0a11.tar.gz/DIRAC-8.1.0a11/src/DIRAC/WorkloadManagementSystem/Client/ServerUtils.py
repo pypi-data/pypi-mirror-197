28a0e0213e239115e931a2d0d7157d30fe023878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:33 +0000

    Add package DIRAC/8.1.0a11/DIRAC-8.1.0a11.tar.gz

---
+++ packages/DIRAC/DIRAC-8.1.0a11.tar.gz/DIRAC-8.1.0a11/src/DIRAC/WorkloadManagementSystem/Client/ServerUtils.py
@@ -0,0 +1,19 @@
+"""
+This module is used to create an appropriate object which can be used to insert records to the WMS.
+It always try to insert the records directly. In case of failure a WMS client is used...
+"""
+
+from DIRAC.Core.Utilities.ServerUtils import getDBOrClient
+
+
+def getPilotAgentsDB():
+    serverName = "WorkloadManagement/PilotManager"
+    PilotAgentsDB = None
+    try:
+        from DIRAC.WorkloadManagementSystem.DB.PilotAgentsDB import PilotAgentsDB
+    except Exception:
+        pass
+    return getDBOrClient(PilotAgentsDB, serverName)
+
+
+pilotAgentsDB = getPilotAgentsDB()
