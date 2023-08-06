5895f434dbe073b0e6a54cfed8cd352e8b26427b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:20:16 +0000

    Add package python-cngal/0.1.0/python_cngal-0.1.0-py3-none-any.whl

---
+++ packages/python-cngal/python_cngal-0.1.0-py3-none-any.whl/pycngal/const.py
@@ -0,0 +1,9 @@
+# Network
+
+api_endpoint = "https://api.cngal.org"
+headers = {"User-Agent": "python-cngal/0.0.3"}
+
+# Database
+
+db_name = "cngal"
+type_code = {0: "game", 1: "character", 2: "maker", 3: "staff"}
