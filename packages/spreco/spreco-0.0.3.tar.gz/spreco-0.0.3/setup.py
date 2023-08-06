8bddb33a67579a3289afde0a3ac411a28ae6e490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:01 +0000

    Add package spreco/0.0.3/spreco-0.0.3.tar.gz

---
+++ packages/spreco/spreco-0.0.3.tar.gz/spreco-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spreco',
+    version='0.0.1',
+    description='Training priors for MRI image reconstruction',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+        'tf-slim==1.1.0',
+        'numpy',
+        'pillow==8.2.0',
+        'matplotlib',
+        'scikit-image',
+        'pyyaml==5.4.1',
+        'tqdm'
+    ],
+)
