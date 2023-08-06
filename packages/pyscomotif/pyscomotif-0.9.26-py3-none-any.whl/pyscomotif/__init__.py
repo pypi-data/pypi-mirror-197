1f7cef3e3c7d87981086aeadc26cb8856424c375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:00 +0000

    Add package pyscomotif/0.9.26/pyscomotif-0.9.26-py3-none-any.whl

---
+++ packages/pyscomotif/pyscomotif-0.9.26-py3-none-any.whl/pyscomotif/__init__.py
@@ -0,0 +1,6 @@
+"""
+DISCLAIMER: pyScoMotif is free to use for non-commercial purposes. To use pyScoMotif for commercial purposes, please contact us.
+pyScoMotif is developed by the 3BioInfo group from the ULB, Belgium.
+Reference paper: 'pyScoMotif: Discovery of similar 3D structural motifs across proteins'.
+Main developer: Gabriel Cia.
+"""
