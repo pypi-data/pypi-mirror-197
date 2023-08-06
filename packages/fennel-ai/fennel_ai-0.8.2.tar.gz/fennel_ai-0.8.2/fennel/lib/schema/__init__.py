a8589e5fbd326b762252d5afd1591b00efab8cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:32 +0000

    Add package fennel-ai/0.8.2/fennel_ai-0.8.2.tar.gz

---
+++ packages/fennel-ai/fennel_ai-0.8.2.tar.gz/fennel_ai-0.8.2/fennel/lib/schema/__init__.py
@@ -0,0 +1,11 @@
+from fennel.lib.schema.schema import (
+    get_datatype,
+    between,
+    oneof,
+    dtype_to_string,
+    data_schema_check,
+    Series,
+    DataFrame,
+    Embedding,
+    regex,
+)
