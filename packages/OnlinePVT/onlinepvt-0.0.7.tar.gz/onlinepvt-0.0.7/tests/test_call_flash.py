80eed311d75b7d59415bc1b59b3a56e2d19f9d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:01 +0000

    Add package OnlinePVT/0.0.7/onlinepvt-0.0.7.tar.gz

---
+++ packages/OnlinePVT/onlinepvt-0.0.7.tar.gz/onlinepvt-0.0.7/tests/test_call_flash.py
@@ -0,0 +1,26 @@
+import pytest
+from onlinepvt.models import CalculationComposition, FlashCalculationType, ProblemDetails
+from onlinepvt.online_pvt_client import OnlinePvtClient
+
+
+@pytest.mark.asyncio
+async def test_call_flash():
+    client = OnlinePvtClient(
+        "https://api.onlinepvt.com", "UserId", "AccessSecret")
+
+    input = client.get_flash_input()
+    input.temperature = 445
+    input.pressure = 20
+    input.components = [
+        CalculationComposition(mass=0.78),
+        CalculationComposition(mass=0.02),
+        CalculationComposition(mass=0.20)
+    ]
+    input.flash_type = FlashCalculationType.PT
+    input.fluid_id = "9E9ABAD5-C6CA-427F-B5E7-15AB3F7CF076"
+
+    result: ProblemDetails = await client.call_flash_async(input)
+
+    await client.cleanup()
+
+    assert result.status == 400
