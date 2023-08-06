807ff55b2af4f9a5496632d7e71402be0c0572b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:54 +0000

    Add package netbox-storage/0.0.0.0.754/netbox_storage-0.0.0.0.754-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.754-py3-none-any.whl/netbox_storage/tables/logical_volume.py
@@ -0,0 +1,28 @@
+from netbox.tables import (
+    NetBoxTable,
+    ToggleColumn,
+)
+
+from netbox_storage.models import LogicalVolume
+
+
+class LogicalVolumeTable(NetBoxTable):
+    """Table for displaying LogicalVolume objects."""
+
+    pk = ToggleColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = LogicalVolume
+        fields = (
+            "pk",
+            "vg",
+            "lv_name",
+            "size",
+            "description",
+        )
+        default_columns = (
+            "vg",
+            "lv_name",
+            "size",
+            "description",
+        )
