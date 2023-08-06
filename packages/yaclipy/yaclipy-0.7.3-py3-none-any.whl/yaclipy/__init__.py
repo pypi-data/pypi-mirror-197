6e3137fe09335a85a6c5d7d389b3b81e6f06d2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:44 +0000

    Add package yaclipy/0.7.3/yaclipy-0.7.3-py3-none-any.whl

---
+++ packages/yaclipy/yaclipy-0.7.3-py3-none-any.whl/yaclipy/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2023-present Aaron <aaron@framelunch.jp>
+#
+# SPDX-License-Identifier: MIT
+from .bootstrap import ensure_requirements
+from .command import Command, sub_cmds
---
+++ packages/yaclipy/yaclipy-0.7.3-py3-none-any.whl/yaclipy/docs.py
@@ -0,0 +1,20 @@
+from print_ext import print, Table, pretty, Printer
+from docstring_parser import parse as docstring_parse
+
+
+
+class CmdDoc():
+    def __init__(self, doc):
+        self.doc = docstring_parse(doc)
+
+    def pretty(self, fmt='full'):
+        return getattr(self, f'pretty_{fmt}')()
+
+    def pretty_short(self):
+        return self.doc.short_description
+
+    def pretty_full(self):
+        p = Printer()
+        if self.doc.short_description: p('\bem$',self.doc.short_description, pad=(0,1))
+        if self.doc.long_description: p(self.doc.long_description, pad=-1)
+        return p
