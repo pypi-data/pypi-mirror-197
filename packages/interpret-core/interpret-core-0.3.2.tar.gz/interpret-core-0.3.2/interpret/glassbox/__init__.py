121167364a7938e6e054eb104d21e9f3a92d143b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:36 +0000

    Add package interpret-core/0.3.2/interpret-core-0.3.2.tar.gz

---
+++ packages/interpret-core/interpret-core-0.3.2.tar.gz/interpret-core-0.3.2/interpret/glassbox/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023 The InterpretML Contributors
+# Distributed under the MIT software license
+
+from .decisiontree import ClassificationTree, RegressionTree  # noqa: F401
+from .linear import LogisticRegression, LinearRegression  # noqa: F401
+from .skoperules import DecisionListClassifier  # noqa: F401
+from .ebm.ebm import ExplainableBoostingClassifier  # noqa: F401
+from .ebm.ebm import ExplainableBoostingRegressor  # noqa: F401
