28b964df66460d86b4c5216b17de1fee6065ed94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package simplewiki/1.0.2/simplewiki-1.0.2.tar.gz

---
+++ packages/simplewiki/simplewiki-1.0.2.tar.gz/simplewiki-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simplewiki",
+    version="1.0.2",
+    author="sami joseph",
+    author_email="4myrccar@gmail.com",
+    description="a package to simplify the wikipedia api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/myrccar/simple-wiki",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10.0',
+)
