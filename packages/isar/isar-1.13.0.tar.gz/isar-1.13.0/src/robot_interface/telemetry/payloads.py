b7e19273da62845a12729000ce7fddfd697bd9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:37 +0000

    Add package isar/1.13.0/isar-1.13.0.tar.gz

---
+++ packages/isar/isar-1.13.0.tar.gz/isar-1.13.0/src/robot_interface/telemetry/payloads.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List, Union
+
+from alitra import Pose
+from transitions import State
+
+from robot_interface.models.mission.status import RobotStatus
+
+
+@dataclass
+class TelemetryPosePayload:
+    pose: Pose
+    isar_id: str
+    robot_name: str
+    timestamp: datetime
+
+
+@dataclass
+class TelemetryBatteryPayload:
+    battery_level: float
+    isar_id: str
+    robot_name: str
+    timestamp: datetime
+
+
+@dataclass
+class VideoStream:
+    name: str
+    url: str
+    type: str
+
+
+@dataclass
+class RobotStatusPayload:
+    isar_id: str
+    robot_name: str
+    robot_status: RobotStatus
+    current_isar_state: State
+    current_mission_id: str
+    current_task_id: str
+    current_step_id: str
+    timestamp: datetime
+
+
+@dataclass
+class RobotInfoPayload:
+    isar_id: str
+    robot_name: str
+    robot_model: str
+    robot_serial_number: str
+    video_streams: List[VideoStream]
+    host: str
+    port: int
+    timestamp: datetime
