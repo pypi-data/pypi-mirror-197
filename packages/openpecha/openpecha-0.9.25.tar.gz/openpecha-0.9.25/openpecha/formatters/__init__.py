9728f2559730a6745b851accd8cb267d616a643a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:31 +0000

    Add package openpecha/0.9.25/openpecha-0.9.25.tar.gz

---
+++ packages/openpecha/openpecha-0.9.25.tar.gz/openpecha-0.9.25/openpecha/formatters/__init__.py
@@ -0,0 +1,6 @@
+# from .empty import EmptyEbook
+from .formatter import BaseFormatter
+from .hfml import HFMLFormatter
+from .ocr import GoogleVisionFormatter
+from .pedurma import PedurmaFormatter
+from .tsadra import TsadraFormatter
