b1a2e9cf1c1003b6206919b4aeef380dc2a89158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:18 +0000

    Add package pyplotdesigner/2023.1.1/pyplotdesigner-2023.1.1.tar.gz

---
+++ packages/pyplotdesigner/pyplotdesigner-2023.1.1.tar.gz/pyplotdesigner-2023.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+PACKAGE_NAME = "pyplotdesigner"
+
+setup(name=PACKAGE_NAME,
+      version="2023.1.1",
+      description="",
+      url="https://github.com/gnwong/pyplotdesigner",
+      author="George N. Wong",
+      author_email="gnwong@ias.edu",
+      license="MIT",
+      packages=["pyplotdesigner"])
+
