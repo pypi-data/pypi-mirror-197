6062ad9ce4c4651997b72ca84c2ca703ec019da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package autoregistry/0.10.0/autoregistry-0.10.0-py3-none-any.whl

---
+++ packages/autoregistry/autoregistry-0.10.0-py3-none-any.whl/autoregistry/__init__.py
@@ -0,0 +1,23 @@
+# Don't manually change, let poetry-dynamic-versioning-plugin handle it.
+__version__ = "0.10.0"
+
+__all__ = [
+    "CannotDeriveNameError",
+    "CannotRegisterPythonBuiltInError",
+    "InvalidNameError",
+    "KeyCollisionError",
+    "ModuleAliasError",
+    "Registry",
+    "RegistryError",
+    "RegistryMeta",
+]
+
+from ._registry import Registry, RegistryMeta
+from .exceptions import (
+    CannotDeriveNameError,
+    CannotRegisterPythonBuiltInError,
+    InvalidNameError,
+    KeyCollisionError,
+    ModuleAliasError,
+    RegistryError,
+)
