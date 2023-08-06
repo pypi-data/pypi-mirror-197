6db4de230e33f36be5ae2b554397fc4bb9be993d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:52 +0000

    Add package holidays/0.21.13/holidays-0.21.13.tar.gz

---
+++ packages/holidays/holidays-0.21.13.tar.gz/holidays-0.21.13/scripts/l10n/generate_mo_files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+#  python-holidays
+#  ---------------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/dr-prodigy/python-holidays
+#  License: MIT (see LICENSE file)
+
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+
+class MOGenerator:
+    """Creates .mo files for supported country/market entities."""
+
+    def run(self):
+        """Runs the .mo files generation process."""
+        for po_path in Path(os.path.join("holidays", "locale")).rglob("*.po"):
+            po_file = str(po_path)
+            mo_file = po_file.replace(".po", ".mo")
+
+            if os.path.exists(mo_file):
+                os.unlink(mo_file)
+            subprocess.run(
+                (
+                    sys.executable,
+                    os.path.join("scripts", "l10n", "msgfmt.py"),
+                    "-o",
+                    mo_file,
+                    po_file,
+                ),
+                check=True,
+            )
+
+
+if __name__ == "__main__":
+    MOGenerator().run()
---
+++ packages/holidays/holidays-0.21.13.tar.gz/holidays-0.21.13/tests/test_utils.py
@@ -0,0 +1,106 @@
+#  python-holidays
+#  ---------------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: dr-prodigy <maurizio.montel@gmail.com> (c) 2017-2022
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/dr-prodigy/python-holidays
+#  License: MIT (see LICENSE file)
+
+import sys
+import unittest
+import warnings
+from unittest import mock
+
+import pytest
+
+from holidays import utils
+
+
+class TestCountryHolidays(unittest.TestCase):
+    def setUp(self):
+        self.holidays = utils.country_holidays("US")
+
+    def test_country(self):
+        self.assertEqual(self.holidays.country, "US")
+
+    def test_country_single_year(self):
+        h = utils.country_holidays("US", years=2021)
+        self.assertEqual(h.years, {2021})
+
+    def test_country_years(self):
+        h = utils.country_holidays("US", years=(2015, 2016))
+        self.assertEqual(h.years, {2015, 2016})
+
+    def test_country_state(self):
+        h = utils.country_holidays("US", subdiv="NY")
+        self.assertEqual(h.subdiv, "NY")
+
+    def test_country_province(self):
+        h = utils.country_holidays("AU", subdiv="NT")
+        self.assertEqual(h.subdiv, "NT")
+
+    def test_exceptions(self):
+        self.assertRaises(
+            NotImplementedError, lambda: utils.country_holidays("XXXX")
+        )
+        self.assertRaises(
+            NotImplementedError,
+            lambda: utils.country_holidays("US", subdiv="XXXX"),
+        )
+        self.assertRaises(
+            NotImplementedError,
+            lambda: utils.country_holidays("US", subdiv="XXXX"),
+        )
+
+
+class TestFinancialHolidays(unittest.TestCase):
+    def setUp(self):
+        self.holidays = utils.financial_holidays("NYSE")
+
+    def test_market(self):
+        self.assertEqual(self.holidays.market, "NYSE")
+
+    def test_market_single_year(self):
+        h = utils.financial_holidays("NYSE", years=2021)
+        self.assertEqual(h.years, {2021})
+
+    def test_market_years(self):
+        h = utils.financial_holidays("NYSE", years=(2015, 2016))
+        self.assertEqual(h.years, {2015, 2016})
+
+    def test_exceptions(self):
+        self.assertRaises(
+            NotImplementedError, lambda: utils.financial_holidays("XXXX")
+        )
+        self.assertRaises(
+            NotImplementedError,
+            lambda: utils.financial_holidays("NYSE", subdiv="XXXX"),
+        )
+
+
+class TestAllInSameYear(unittest.TestCase):
+    """Test that only holidays in the year(s) requested are returned."""
+
+    @pytest.mark.xfail(reason="'Set changed size during iteration' error")
+    @pytest.mark.skipif(
+        sys.version_info < (3, 11),
+        reason="Run once on the latest Python version only",
+    )
+    @mock.patch("pathlib.Path.rglob", return_value=())
+    def test_all_countries(self, unused_rglob_mock):
+        """
+        Only holidays in the year(s) requested should be returned. This
+        ensures that we avoid triggering a "RuntimeError: dictionary changed
+        size during iteration" error.
+
+        This is logic test and not a code compatibility test, so for expediency
+        we only run it once on the latest Python version.
+        """
+        warnings.simplefilter("ignore")
+        for country in utils.list_supported_countries():
+            for year in range(1950, 2051):
+                for holiday in utils.country_holidays(country, years=year):
+                    self.assertEqual(holiday.year, year)
