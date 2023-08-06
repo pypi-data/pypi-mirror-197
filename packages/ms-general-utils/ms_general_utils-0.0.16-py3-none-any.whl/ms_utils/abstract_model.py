2028d5e784cc68e14568748b788cb00b6057503e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:55 +0000

    Add package ms-general-utils/0.0.16/ms_general_utils-0.0.16-py3-none-any.whl

---
+++ packages/ms-general-utils/ms_general_utils-0.0.16-py3-none-any.whl/ms_utils/abstract_model.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
+
+
+class BaseModal:
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
