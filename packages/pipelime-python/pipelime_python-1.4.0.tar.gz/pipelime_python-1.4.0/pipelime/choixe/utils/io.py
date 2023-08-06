ad5442f36a7ec9587800ccaf39d9e263debc463f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:57 +0000

    Add package pipelime-python/1.4.0/pipelime_python-1.4.0.tar.gz

---
+++ packages/pipelime-python/pipelime_python-1.4.0.tar.gz/pipelime_python-1.4.0/pipelime/choixe/utils/io.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+from typing import Any
+
+import yaml
+
+
+def load(path: Path) -> Any:
+    """Loads an object from a file with a supported markup format.
+    Supported formats include:
+    - yaml
+    - json
+
+    Args:
+        path (Path): Path to the file to load.
+
+    Returns:
+        Any: The loaded object.
+    """
+    with open(path, "r") as fd:
+        ext = path.suffix
+        if ext == ".json":
+            return json.load(fd)
+        else:
+            return yaml.safe_load(fd)
+
+
+def dump(obj: Any, path: Path) -> None:
+    """Dumps an object to a file with a supported markup format.
+    Supported formats include:
+    - yaml
+    - json
+    Args:
+        obj (Any): The object to dump.
+        path (Path): Path to the file to write.
+    """
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, "w") as fd:
+        ext = path.suffix
+        if ext == ".json":
+            json.dump(obj, fd)
+        else:
+            yaml.safe_dump(obj, fd, sort_keys=False)
