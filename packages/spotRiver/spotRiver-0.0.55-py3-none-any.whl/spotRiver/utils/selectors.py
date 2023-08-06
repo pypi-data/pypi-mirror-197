32a479312b0cdce40cda5fd4862195f80301bbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:35 +0000

    Add package spotRiver/0.0.55/spotRiver-0.0.55-py3-none-any.whl

---
+++ packages/spotRiver/spotRiver-0.0.55-py3-none-any.whl/spotRiver/utils/selectors.py
@@ -0,0 +1,48 @@
+from river.tree.splitter import EBSTSplitter, QOSplitter, TEBSTSplitter
+from river.linear_model import LinearRegression, PARegressor, Perceptron
+from numpy import power
+
+
+def select_splitter(i):
+    if i not in range(3):
+        raise ValueError("{i} wrong splitter, use int values between 0 and 2.".format(i=repr(i)))
+    if i == 0:
+        return EBSTSplitter()
+    elif i == 1:
+        return TEBSTSplitter()
+    else:
+        return QOSplitter()
+
+
+def select_leaf_prediction(i):
+    if i not in range(3):
+        raise ValueError("{i} wrong leaf_prediction, use int values between 0 and 2.".format(i=repr(i)))
+    elif i == 0:
+        return "mean"
+    elif i == 1:
+        return "adaptive"
+    else:
+        return "model"
+
+
+def select_leaf_model(i):
+    if i not in range(3):
+        raise ValueError("{i} wrong leaf_model, use int values between 0 and 2.".format(i=repr(i)))
+    elif i == 0:
+        return LinearRegression()
+    elif i == 1:
+        return PARegressor()
+    else:
+        return Perceptron()
+
+
+def select_max_depth(i):
+    return power(10, i)
+
+
+def apply_selectors(d: dict):
+    d["splitter"] = select_splitter(d["splitter"])
+    d["leaf_prediction"] = select_leaf_prediction(d["leaf_prediction"])
+    d["leaf_model"] = select_leaf_model(d["leaf_model"])
+    d["max_depth"] = select_max_depth(d["max_depth"])
+    return d
