087a9c4cbee746961405cf0265b119e7c5aaa7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:57 +0000

    Add package sematic/0.27.0/sematic-0.27.0-py3-none-any.whl

---
+++ packages/sematic/sematic-0.27.0-py3-none-any.whl/sematic/db/queries.py
@@ -0,0 +1,553 @@
+"""
+Module holding common DB queries.
+"""
+# Standard Library
+import logging
+from typing import Dict, List, Optional, Set, Tuple
+
+# Third-party
+import sqlalchemy
+import sqlalchemy.orm
+from sqlalchemy.sql.elements import ColumnElement
+
+# Sematic
+from sematic.abstract_future import FutureState
+from sematic.db.db import db
+from sematic.db.models.artifact import Artifact
+from sematic.db.models.edge import Edge
+from sematic.db.models.external_resource import ExternalResource
+from sematic.db.models.note import Note
+from sematic.db.models.resolution import Resolution
+from sematic.db.models.run import Run
+from sematic.db.models.runs_external_resource import RunExternalResource
+from sematic.db.models.user import User
+from sematic.plugins.abstract_external_resource import ResourceState
+from sematic.scheduling.external_job import ExternalJob
+from sematic.types.serialization import value_from_json_encodable
+from sematic.utils.exceptions import IllegalStateTransitionError
+
+logger = logging.getLogger(__name__)
+
+
+def count_runs() -> int:
+    """
+    Counts all runs.
+
+    Returns
+    -------
+    int
+        Run count
+    """
+    with db().get_session() as session:
+        run_count = session.query(Run).count()
+
+    return run_count
+
+
+def get_artifact(artifact_id: str) -> Artifact:
+    """
+    Get an artifact from the database.
+
+    Parameters
+    ----------
+    artifact_id : str
+        ID of artifact to retrieve.
+
+    Returns
+    -------
+    Artifact
+        Fetched artifact
+    """
+    with db().get_session() as session:
+        return session.query(Artifact).filter(Artifact.id == artifact_id).one()
+
+
+def get_users(user_ids: List[str]) -> List[User]:
+    """
+    Get users from the database.
+
+    Parameters
+    ----------
+    user_ids: List[str]
+        List of IDs to retrieve.
+
+    Returns
+    -------
+    List[User]
+        List of users
+    """
+    with db().get_session() as session:
+        return session.query(User).filter(User.id.in_(list(set(user_ids)))).all()
+
+
+def get_user(user_id: str) -> User:
+    """
+    Get a user from the database.
+
+    Parameters
+    ----------
+    user_id : str
+        ID of user to retrieve.
+
+    Returns
+    -------
+    User
+        Fetched user
+    """
+    with db().get_session() as session:
+        return session.query(User).filter(User.id == user_id).one()
+
+
+def get_user_by_email(email: str) -> User:
+    """
+    Get a user from the database by email.
+
+    Parameters
+    ----------
+    email : str
+        email of user to retrieve.
+
+    Returns
+    -------
+    User
+        Fetched user
+    """
+    with db().get_session() as session:
+        return session.query(User).filter(User.email == email).one()
+
+
+def get_run(run_id: str) -> Run:
+    """
+    Get a run from the database.
+
+    Parameters
+    ----------
+    run_id : str
+        ID of run to retrieve.
+
+    Returns
+    -------
+    Run
+        Fetched run
+    """
+    with db().get_session() as session:
+        return session.query(Run).filter(Run.id == run_id).one()
+
+
+def get_run_status_details(
+    run_ids: List[str],
+) -> Dict[str, Tuple[FutureState, List[ExternalJob]]]:
+    """
+    Get information about runs' statuses from the DB.
+
+    This is an optimization to enable getting only status-related information
+    about the runs, and should only be preferred to get_run when the query
+    happens often and only involves statuses.
+
+    Parameters
+    ----------
+    run_ids :
+        ID of runs whose status should be retrieved
+
+    Returns
+    -------
+    A dict whose keys are run ids. The values are tuples where the first element
+    is the future state for the run and the second element is a list of external
+    jobs for the run (if any exist)
+    """
+    with db().get_session() as session:
+        query_results = (
+            session.query(Run.id, Run.future_state, Run.external_jobs_json)
+            .filter(Run.id.in_(run_ids))
+            .all()
+        )
+        result_dict = {}
+        for run_id, state_string, jobs_encodable in query_results:
+            if jobs_encodable is None:
+                jobs = []
+            else:
+                jobs = [
+                    value_from_json_encodable(job, ExternalJob)
+                    for job in jobs_encodable
+                ]
+            result_dict[run_id] = (FutureState[state_string], jobs)
+    return result_dict
+
+
+def save_run(run: Run) -> Run:
+    """
+    Save run to the database.
+
+    Parameters
+    ----------
+    run : Run
+        Run to save
+
+    Returns
+    -------
+    Run
+        saved run
+    """
+    _assert_external_jobs_not_removed([run])
+    with db().get_session() as session:
+        existing_run_future_state = (
+            session.query(Run.future_state).filter(Run.id == run.id).one_or_none()
+        )
+        existing_run_future_state = (
+            existing_run_future_state[0]
+            if existing_run_future_state is not None
+            else None
+        )
+        new_state = run.future_state
+        if isinstance(new_state, FutureState):
+            # despite run.future_state being typed as FutureState, it
+            # is sometimes a string.
+            new_state = new_state.name  # type: ignore
+
+        if (
+            existing_run_future_state != new_state
+            and not FutureState.is_allowed_transition(
+                existing_run_future_state, run.future_state
+            )
+        ):
+            raise IllegalStateTransitionError(
+                f"Cannot transition run from '{existing_run_future_state}' to "
+                f"'{run.future_state}'"
+            )
+        session.add(run)
+        session.commit()
+        session.refresh(run)
+
+    return run
+
+
+def save_external_resource_record(record: ExternalResource):
+    """Save an ExternalResource to the DB"""
+    existing_record = get_external_resource_record(record.id)
+
+    if existing_record is None:
+        if record.resource_state != ResourceState.CREATED:
+            raise ValueError(
+                f"Cannot create an external resource in a state other than 'CREATED'. "
+                f"{record.id} was in state '{record.resource_state}'."
+            )
+
+        # this ensures that all the fields are consistent
+        record = ExternalResource.from_resource(record.resource)
+    else:
+        # this ensures that the update properly updates history
+        # and keeps data consistent
+        existing_record.resource = record.resource
+        record = existing_record
+
+    with db().get_session() as session:
+        session.merge(record)
+        session.commit()
+
+        return record
+
+
+def get_external_resource_record(resource_id: str) -> Optional[ExternalResource]:
+    """Get an ExternalResource from the DB"""
+    with db().get_session() as session:
+        return (
+            session.query(ExternalResource)
+            .filter(ExternalResource.id == resource_id)
+            .one_or_none()
+        )
+
+
+def save_run_external_resource_links(resource_ids: List[str], run_id: str):
+    """Save the relationship between external resources and a run to the DB."""
+    with db().get_session() as session:
+        for resource_id in resource_ids:
+            session.merge(RunExternalResource(resource_id=resource_id, run_id=run_id))
+        session.commit()
+
+
+def get_resources_by_root_id(root_run_id: str) -> List[ExternalResource]:
+    """Get a list of external resources associated with a particular root run."""
+    with db().get_session() as session:
+        results = (
+            session.query(ExternalResource, ExternalResource.id)
+            .filter(ExternalResource.id == RunExternalResource.resource_id)
+            .filter(Run.id == RunExternalResource.run_id)
+            .filter(Run.root_id == root_run_id)
+            .distinct()
+            .all()
+        )
+        return list(set(r[0] for r in results))
+
+
+def get_run_ids_for_resource(external_resource_id: str) -> List[str]:
+    with db().get_session() as session:
+        results = (
+            session.query(RunExternalResource.run_id)
+            .filter(external_resource_id == RunExternalResource.resource_id)
+            .distinct()
+            .all()
+        )
+        return list(r[0] for r in results)
+
+
+def get_external_resources_by_run_id(run_id: str) -> List[ExternalResource]:
+    """
+    Get the external resources used by a run.
+    """
+    with db().get_session() as session:
+        external_resources = (
+            session.query(ExternalResource)
+            .join(
+                RunExternalResource,
+                sqlalchemy.and_(
+                    run_id == RunExternalResource.run_id,
+                    ExternalResource.id == RunExternalResource.resource_id,
+                ),
+            )
+            .all()
+        )
+    return external_resources
+
+
+def get_resolution(resolution_id: str) -> Resolution:
+    """Get a resolution from the database.
+
+    Parameters
+    ----------
+    resolution_id:
+        ID of resoution to retrieve.
+
+    Returns
+    -------
+    Fetched resolution
+    """
+    with db().get_session() as session:
+        return (
+            session.query(Resolution).filter(Resolution.root_id == resolution_id).one()
+        )
+
+
+def save_resolution(resolution: Resolution) -> Resolution:
+    """Save resolution to the database.
+
+    Parameters
+    ----------
+    resolution:
+        Resolution to save
+
+    Returns
+    -------
+    saved resolution
+    """
+    with db().get_session() as session:
+        session.merge(resolution)
+        session.commit()
+
+    return resolution
+
+
+def save_graph(runs: List[Run], artifacts: List[Artifact], edges: List[Edge]):
+    """
+    Update a graph.
+    """
+    _assert_external_jobs_not_removed(runs)
+    with db().get_session() as session:
+        for run in runs:
+            session.merge(run)
+
+        for artifact in artifacts:
+            _save_artifact(artifact=artifact, session=session)
+
+        for edge in edges:
+            session.merge(edge)
+
+        session.commit()
+
+
+def _save_artifact(artifact: Artifact, session: sqlalchemy.orm.Session) -> Artifact:
+    """
+    Saves or updates an Artifact, returning the actual persisted Artifact element.
+    """
+    previous_artifact = session.get(entity=Artifact, ident=artifact.id)
+
+    if previous_artifact is not None:
+        # we use content-addressed values for artifacts, with the id being
+        # generated from the type and value themselves
+        # there are currently no other fields that can be updated
+        logger.debug("Updating existing artifact %s", artifact.id)
+        previous_artifact.assert_matches(artifact)
+        return previous_artifact
+
+    return session.merge(artifact)
+
+
+def _assert_external_jobs_not_removed(runs):
+    run_ids = [run.id for run in runs]
+    runs_by_id = {run.id: run for run in runs}
+    with db().get_session() as session:
+        existing_run_jobs_all_runs = (
+            session.query(Run.id, Run.external_jobs_json)
+            .filter(Run.id.in_(run_ids))
+            .all()
+        )
+
+        # it's ok if there isn't an existing run for one of the passed-in runs.
+        # the passed-in runs may be new.
+        for existing_run_id, existing_run_jobs_json in existing_run_jobs_all_runs:
+            if existing_run_jobs_json is None:
+                existing_run_jobs = []
+            else:
+                existing_run_jobs = [
+                    value_from_json_encodable(job, ExternalJob)
+                    for job in existing_run_jobs_json
+                ]
+            run = runs_by_id[existing_run_id]
+            if len(run.external_jobs) < len(existing_run_jobs):
+                raise ValueError(
+                    f"Cannot remove existing external jobs from {run.id}. "
+                    f"Existing run had: {existing_run_jobs}. New "
+                    f"run had: {run.external_jobs}"
+                )
+
+
+Graph = Tuple[List[Run], List[Artifact], List[Edge]]
+
+
+def get_run_graph(run_id: str) -> Graph:
+    """
+    Get run graph.
+
+    This will return the run's direct graph, meaning
+    only edges directly connected to it, and their corresponding artifacts.
+    """
+    return get_graph(Run.id == run_id)
+
+
+def get_root_graph(root_id: str) -> Graph:
+    """
+    Get entire graph for root_id.
+
+    This will return the entire graph for a given root_id.
+    """
+    return get_graph(Run.root_id == root_id)
+
+
+def get_graph(
+    run_predicate: ColumnElement,
+    include_edges: bool = True,
+    include_artifacts: bool = True,
+) -> Graph:
+    """
+    Retrieve the graph for a run predicate
+    (e.g. Run.root_id == root_id, or Run.id == run_id)
+    """
+    with db().get_session() as session:
+        runs: List[Run] = session.query(Run).filter(run_predicate).all()
+        run_ids = [run.id for run in runs]
+
+        edges: List[Edge] = []
+        if include_edges:
+            edges = (
+                session.query(Edge)
+                .filter(
+                    sqlalchemy.or_(
+                        Edge.source_run_id.in_(run_ids),
+                        Edge.destination_run_id.in_(run_ids),
+                    )
+                )
+                .all()
+            )
+
+        artifacts: List[Artifact] = []
+        if include_artifacts:
+            artifact_ids = {
+                edge.artifact_id for edge in edges if edge.artifact_id is not None
+            }
+            artifacts = (
+                session.query(Artifact).filter(Artifact.id.in_(artifact_ids)).all()
+            )
+
+    return runs, artifacts, edges
+
+
+def get_note(note_id: str) -> Note:
+    """
+    Get note from DB.
+    """
+    with db().get_session() as session:
+        return session.query(Note).filter(Note.id == note_id).one()
+
+
+def save_note(note: Note) -> Note:
+    """
+    Persist a note.
+    """
+    with db().get_session() as session:
+        session.add(note)
+        session.commit()
+        session.refresh(note)
+
+    return note
+
+
+def delete_note(note: Note):
+    """
+    Delete note.
+    """
+    with db().get_session() as session:
+        session.delete(note)
+        session.commit()
+
+
+# DO NOT USE
+# This query is more optimal than the one in `get_graph` (single query)
+# but has corner cases when some runs have no edges or artifacts
+# Use `get_graph` instead
+def _get_root_graph(root_run_id: str) -> Tuple[Set[Run], Set[Edge], Set[Artifact]]:
+    with db().get_session() as session:
+        results = (
+            session.query(Run, Edge, Artifact)
+            .filter(
+                sqlalchemy.and_(
+                    Run.root_id == root_run_id,
+                    sqlalchemy.or_(
+                        Edge.source_run_id == Run.id, Edge.destination_run_id == Run.id
+                    ),
+                    sqlalchemy.or_(
+                        Edge.artifact_id == Artifact.id, Edge.artifact_id.is_(None)
+                    ),
+                )
+            )
+            .all()
+        )
+
+    runs: List[Run] = []
+    artifacts: List[Artifact] = []
+    edges: List[Edge] = []
+
+    for result in results:
+        runs.append(result[0])
+        edges.append(result[1])
+        artifacts.append(result[2])
+
+    return set(runs), set(edges), set(artifacts)
+
+
+def get_user_by_api_key(api_key: str) -> User:
+    """
+    Get a user by API key
+    """
+    with db().get_session() as session:
+        return session.query(User).filter(User.api_key == api_key).one()
+
+
+def save_user(user: User) -> User:
+    """
+    Save a user to the DB
+    """
+    with db().get_session() as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+    return user
---
+++ packages/sematic/sematic-0.27.0-py3-none-any.whl/sematic/scheduling/job_scheduler.py
@@ -0,0 +1,267 @@
+# Standard Library
+import logging
+from dataclasses import replace
+from typing import Iterable, List, Optional, Tuple, Union
+
+# Sematic
+from sematic.abstract_future import FutureState
+from sematic.db.models.resolution import Resolution, ResolutionStatus
+from sematic.db.models.run import Run
+from sematic.scheduling import kubernetes as k8s
+from sematic.scheduling.external_job import KUBERNETES_JOB_KIND, ExternalJob
+from sematic.versions import MIN_CLIENT_SERVER_SUPPORTS, string_version_to_tuple
+
+logger = logging.getLogger(__name__)
+
+
+class StateNotSchedulable(Exception):
+    """The run or resolution is not in a state that would allow it to be scheduled."""
+
+    pass
+
+
+def schedule_run(run: Run, resolution: Resolution) -> Run:
+    """Start a job for the run on external compute.
+
+    A new `ExternalJob` object will be appended to the end of the run's `external_jobs`
+    field describing the new job details.
+
+    Parameters
+    ----------
+    run:
+        The run to schedule
+    resolution:
+        The resolution associated with the run
+    """
+    # before scheduling a new run, update the information about previous runs
+    run.external_jobs = _refresh_external_jobs(run.external_jobs)
+    _assert_is_scheduleable(run, resolution)
+    external_jobs_list = list(run.external_jobs) + [_schedule_job(run, resolution)]
+    run.external_jobs = tuple(external_jobs_list)
+    run.future_state = FutureState.SCHEDULED
+    return run
+
+
+def schedule_resolution(
+    resolution: Resolution,
+    max_parallelism: Optional[int] = None,
+    rerun_from: Optional[str] = None,
+) -> Resolution:
+    """Start a resolution for the run on external compute.
+
+    Parameters
+    ----------
+    resolution: Resolution
+        The resolution associated with the run
+    max_parallelism:
+        The maximum number of non-inlined runs that the resolver will allow to be in the
+        SCHEDULED state at any one time.
+    rerun_from: Optional[str]
+        Start resolution from a particular point
+    """
+    resolution.external_jobs = _refresh_external_jobs(resolution.external_jobs)
+    _assert_resolution_is_scheduleable(resolution)
+    external_jobs_list = list(resolution.external_jobs) + [
+        _schedule_resolution_job(
+            resolution=resolution,
+            max_parallelism=max_parallelism,
+            rerun_from=rerun_from,
+        )
+    ]
+    resolution.external_jobs = tuple(external_jobs_list)
+    resolution.status = ResolutionStatus.SCHEDULED
+    return resolution
+
+
+def update_run_status(
+    future_state: FutureState,
+    external_jobs: Union[List[ExternalJob], Tuple[ExternalJob, ...]],
+) -> Tuple[FutureState, Tuple[ExternalJob, ...]]:
+    """Determine whether a new run state should be used based ONLY on external job
+    statuses.
+
+    The external jobs themselves will have their state information refreshed before
+    determining whether the run needs its status changed.
+
+    Parameters
+    ----------
+    future_state:
+        The current state of the run.
+    external_jobs:
+        The external jobs associated with the run.
+
+    Returns
+    -------
+    A tuple with 2 elements. The first is the new future state (same state if unchanged).
+    The second is the updated external jobs tuple.
+    """
+    external_jobs = tuple(external_jobs)
+    active_jobs_remain = any(job.is_active() for job in external_jobs)
+    if future_state.is_terminal():
+        if active_jobs_remain:
+            logger.warning(
+                "There are still active external jobs for run in "
+                "state %s (will refresh jobs): %s",
+                future_state,
+                external_jobs,
+            )
+            refreshed_jobs = _refresh_external_jobs(external_jobs)
+            logger.warning(
+                "After refresh, jobs are: " "%s",
+                refreshed_jobs,
+            )
+            return future_state, refreshed_jobs
+        else:
+            return future_state, external_jobs
+
+    if future_state.value == FutureState.RAN.value:
+        # If the job already RAN, the only reason it's not
+        # terminal is because child runs have to complete.
+        # There should be no more external jobs for this run.
+        return future_state, external_jobs
+
+    if future_state.value == FutureState.CREATED.value:
+        if len(external_jobs) == 0:
+            return future_state, external_jobs
+        else:
+            raise ValueError(
+                "Run is in an invalid state: it is marked as CREATED but it has "
+                "external jobs. Runs with external jobs should be SCHEDULED."
+            )
+    if len(external_jobs) < 1:
+        raise ValueError("No external jobs for run")
+
+    external_jobs = _refresh_external_jobs(external_jobs)
+
+    if future_state.value == FutureState.SCHEDULED.value:
+        if external_jobs[-1].is_active():
+            return FutureState.SCHEDULED, external_jobs
+
+        job_summary_str = "\n ".join([repr(job) for job in external_jobs])
+        exception_metadata = external_jobs[-1].get_exception_metadata()
+        logger.warning(
+            "Job failed due to K8s job failure:\n%s\nJob states:\n%s",
+            exception_metadata.repr if exception_metadata is not None else None,
+            job_summary_str,
+        )
+
+        return FutureState.FAILED, external_jobs
+
+    raise ValueError(
+        f"Future is in a state not covered by update logic: {future_state}"
+    )
+
+
+def _assert_resolution_is_scheduleable(resolution: Resolution):
+    """raise StateNotSchedulable if the state is not such that it can be scheduled"""
+    if resolution.status != ResolutionStatus.CREATED.value:
+        raise StateNotSchedulable(
+            f"The resolution {resolution.root_id} was in the state {resolution.status}, "
+            f"and could not be scheduled. Resolution can only be scheduled if they "
+            f"are in the {ResolutionStatus.CREATED} state."
+        )
+    for job in resolution.external_jobs:
+        if job.is_active():
+            raise StateNotSchedulable(
+                f"The resolution {resolution.root_id} already had an active external "
+                f"job {job.external_job_id} and thus could not be scheduled."
+            )
+    if resolution.container_image_uri is None:
+        raise StateNotSchedulable(
+            f"The resolution {resolution.root_id} had no docker image URI"
+        )
+
+    valid_client_version = (
+        resolution.client_version is not None
+        and string_version_to_tuple(resolution.client_version)
+        < MIN_CLIENT_SERVER_SUPPORTS
+    )
+    if valid_client_version:
+        # You may wonder how we can get here given that clients check for server
+        # compatibility. This can still happen if somebody tries to rerun an old
+        # resolution (ex: from the UI).
+        raise StateNotSchedulable(
+            f"The resolution {resolution.root_id} uses Sematic version "
+            f"{resolution.client_version}, but the server requires at least "
+            f"version {MIN_CLIENT_SERVER_SUPPORTS}"
+        )
+
+
+def _assert_is_scheduleable(run: Run, resolution: Resolution):
+    """raise RunStateNotSchedulable if the state is not such that it can be scheduled"""
+    if run.future_state not in {FutureState.CREATED.value, FutureState.RETRYING.value}:
+        raise StateNotSchedulable(
+            f"The run {run.id} was in the state {run.future_state}, and could "
+            f"not be scheduled. Runs can only be scheduled if they are in the "
+            f"{FutureState.CREATED} state."
+        )
+
+    for job in run.external_jobs:
+        if job.is_active() and run.future_state != FutureState.RETRYING.value:
+            raise StateNotSchedulable(
+                f"The run {run.id} already had an active external job "
+                f"{job.external_job_id} and thus could not be scheduled."
+            )
+    if resolution.status != ResolutionStatus.RUNNING.value:
+        raise StateNotSchedulable(
+            f"The run {run.id} was not schedulable because there "
+            f"is no active resolution for it."
+        )
+    if run.container_image_uri is None:
+        raise StateNotSchedulable(f"Run {run.id} has no container image URI")
+
+
+def _refresh_external_jobs(jobs: Iterable[ExternalJob]) -> Tuple[ExternalJob, ...]:
+    """For any external jobs that are still active, refresh them from external compute"""
+    refreshed = []
+    for job in jobs:
+        if not job.is_active():
+            refreshed.append(job)
+            continue
+        job = _refresh_external_job(job)
+        refreshed.append(job)
+    return tuple(refreshed)
+
+
+def _refresh_external_job(job: ExternalJob) -> ExternalJob:
+    """Reach out to external compute to update the state of the external job"""
+    if job.kind != KUBERNETES_JOB_KIND:
+        raise RuntimeError("Can only support Kubernetes jobs to fulfill runs.")
+    job = replace(job)  # modify new copy, not existing one
+    if not job.is_active():
+        return job
+    return k8s.refresh_job(job)
+
+
+def _schedule_job(run: Run, resolution: Resolution) -> ExternalJob:
+    """Reach out to external compute to start the execution of the run"""
+    # k8s is the only thing we can submit jobs to at the moment.
+
+    # should be impossible to fail this assert, but it makes mypy happy
+    if run.container_image_uri is None:
+        raise ValueError(f"Run {run.id} is missing a container image")
+
+    return k8s.schedule_run_job(
+        run_id=run.id,
+        image=run.container_image_uri,
+        user_settings=resolution.settings_env_vars,
+        resource_requirements=run.resource_requirements,
+        try_number=len(run.external_jobs),
+    )
+
+
+def _schedule_resolution_job(
+    resolution: Resolution,
+    max_parallelism: Optional[int] = None,
+    rerun_from: Optional[str] = None,
+) -> ExternalJob:
+    """Reach out to external compute to start the execution of the resolution"""
+    # should be impossible to fail this assert, but it makes mypy happy
+    assert resolution.container_image_uri is not None
+    return k8s.schedule_resolution_job(
+        resolution_id=resolution.root_id,
+        image=resolution.container_image_uri,
+        user_settings=resolution.settings_env_vars,
+        max_parallelism=max_parallelism,
+        rerun_from=rerun_from,
+    )
