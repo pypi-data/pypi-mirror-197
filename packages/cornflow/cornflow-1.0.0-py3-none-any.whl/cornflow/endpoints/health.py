e611ba1c2e8ad879ad4d31fe9f6473fa9427f882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:39 +0000

    Add package cornflow/1.0.0/cornflow-1.0.0-py3-none-any.whl

---
+++ packages/cornflow/cornflow-1.0.0-py3-none-any.whl/cornflow/endpoints/health.py
@@ -0,0 +1,30 @@
+"""
+
+"""
+
+# Import from libraries
+from cornflow_client.airflow.api import Airflow
+from flask import current_app
+from flask_apispec import marshal_with, doc
+
+# Import from internal modules
+from cornflow.schemas.health import HealthResponse
+from cornflow.shared.const import STATUS_HEALTHY, STATUS_UNHEALTHY
+from cornflow_core.shared import db
+from cornflow_core.resources import BaseMetaResource
+
+
+class HealthEndpoint(BaseMetaResource):
+    @doc(description="Health check", tags=["Health"])
+    @marshal_with(HealthResponse)
+    def get(self):
+        af_client = Airflow.from_config(current_app.config)
+        airflow_status = STATUS_HEALTHY
+        cornflow_status = STATUS_HEALTHY
+        if not af_client.is_alive():
+            airflow_status = STATUS_UNHEALTHY
+        try:
+            db.engine.execute("SELECT 1")
+        except Exception:
+            cornflow_status = STATUS_UNHEALTHY
+        return {"cornflow_status": cornflow_status, "airflow_status": airflow_status}
