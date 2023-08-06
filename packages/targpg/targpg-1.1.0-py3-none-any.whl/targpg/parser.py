94a30cac68877ef4c161473ce3c06be2e2a9c83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:17 +0000

    Add package targpg/1.1.0/targpg-1.1.0-py3-none-any.whl

---
+++ packages/targpg/targpg-1.1.0-py3-none-any.whl/targpg/parser.py
@@ -0,0 +1,150 @@
+"""Commandline Parser for Targpg"""
+__all__ = ["targpg_parser"]
+
+from argparse import Action, ArgumentParser
+
+from .targpg import PROG_NAME
+from .meta import __version__
+
+
+class ComboListAction(Action):
+    """Action will combine lists from flag used multiple times
+    eg: `-a one two -a three four`
+    becomes [one, two, three four]
+    instead of [[one, two], [three, four]]
+    """
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        current = getattr(namespace, self.dest, self.default) or []
+        if isinstance(values, list):
+            current.extend(values)
+        else:
+            current.append(values)
+        if isinstance(self.nargs, int) and len(current) > self.nargs:
+            opt = option_string or self.dest
+            raise ValueError(f"Too many arguments passed for {opt}")
+        setattr(namespace, self.dest, current)
+
+
+def targpg_parser() -> ArgumentParser:
+    """Generate the command line parser for the script"""
+    parser = ArgumentParser(
+        prog=PROG_NAME,
+        description="manage secure archive containing sensative docs",
+    )
+    parser.add_argument(
+        "archive",
+        help="tar file secured by gpg symetric password",
+    )
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="count",
+        default=0,
+        help="more verbose output",
+    )
+    parser.add_argument(
+        "-q",
+        "--quite",
+        dest="quite",
+        action="store_true",
+        default=False,
+        help="supress output",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--create",
+        action="store_true",
+        dest="autocreate",
+        default=False,
+        help="create the file without confirmation if it does not exist",
+    )
+    parser.add_argument(
+        "-p",
+        "--passfile",
+        dest="passfile",
+        help="file with archive password stored in it",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        action="store_true",
+        dest="output",
+        default=".",
+        help="directory to extract files to",
+    )
+
+
+    parser.add_argument(
+        "-n",
+        "--newpass",
+        action="store_true",
+        dest="newpass",
+        default=False,
+        help="change the password of the archive",
+    )
+    parser.add_argument(
+        "-f",
+        "--filename",
+        dest="newfile",
+        help="file new password is stored in",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--directory",
+        dest="directory",
+        help="when adding files, do it relative to this directory",
+        metavar="DIR",
+    )
+    parser.add_argument(
+        "-a",
+        "--add",
+        action=ComboListAction,
+        dest="add",
+        nargs="*",
+        help="add files to the archive",
+    )
+    parser.add_argument(
+        "-u",
+        "--update",
+        action=ComboListAction,
+        dest="update",
+        nargs="*",
+        help="overwrite existing files if any being passed in match",
+    )
+    parser.add_argument(
+        "-r",
+        "--remove",
+        action=ComboListAction,
+        dest="remove",
+        nargs="*",
+        help="add files to the archive",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--extract",
+        action=ComboListAction,
+        dest="extr",
+        nargs="*",
+        help="extract the files from the archive, if no files given a prompt will ask",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--list",
+        action="store_true",
+        dest="list",
+        default=False,
+        help="list the contents of the archive",
+    )
+    return parser
---
+++ packages/targpg/targpg-1.1.0-py3-none-any.whl/test/test_targpg.py
@@ -0,0 +1,213 @@
+"""Testing targpg"""
+from io import StringIO
+from os import makedirs
+from pathlib import Path
+from shutil import rmtree
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from targpg import Targpg, tglog
+
+tglog.setLevel("CRITICAL")
+
+
+class TargpgTests(TestCase):
+    """Test functionality of BatchRenamer"""
+
+    @staticmethod
+    def assertFileExists(filepath, msg=None):
+        """Assert if a path exists or not"""
+        if not Path(filepath).exists():
+            raise AssertionError(msg or f"{filepath} does not exist")
+
+    @staticmethod
+    def assertFileNotExists(filepath, msg=None):
+        """Assert if a path exists or not"""
+        if Path(filepath).exists():
+            raise AssertionError(msg or f"{filepath} does exist")
+
+    @staticmethod
+    def _make_clean(dirname):
+        dirpath = Path(dirname)
+        rmtree(dirpath, ignore_errors=True)
+        dirpath.mkdir(parents=True, exist_ok=True)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test = "test"
+        cls.work = Path(cls.test, "work")
+        cls._make_clean(cls.work)
+        cls.extr = Path(cls.test, "extr")
+        cls._make_clean(cls.extr)
+        cls.archive = Path(cls.work, "secure.tgz.gpg")
+
+        cls.passfile = Path(cls.work, "passfile")
+        cls.passfile.write_text("password", encoding="utf-8")
+        cls.wrongpass = Path(cls.work, "wrongpass")
+        cls.wrongpass.touch()
+        cls.newpass = Path(cls.work, "newpass")
+        cls.newpass.write_text("newword", encoding="utf-8")
+
+        cls.file1 = Path(cls.work, "file1.txt")
+        cls.file1_data = "hello"
+        cls.file1.write_text(cls.file1_data, encoding="utf-8")
+
+        cls.file2 = Path(cls.work, "file2.txt")
+        cls.file2_data = "goodbye"
+        cls.file2.write_text(cls.file2_data, encoding="utf-8")
+
+    @classmethod
+    def tearDownClass(cls):
+        rmtree(cls.work, ignore_errors=True)
+        rmtree(cls.extr, ignore_errors=True)
+
+    def tearDown(self):
+        try:
+            self.archive.unlink()
+        except FileNotFoundError:
+            pass
+
+    def _create(self):
+        gt = Targpg(self.archive, passfile=self.passfile, autocreate=True)
+        gt.add(self.file1, self.file2)
+        gt.save()
+        return gt
+
+    def test_01_init(self):
+        """Initalizing with different attributes"""
+        with self.assertRaises(FileNotFoundError), patch(
+            "builtins.input", return_value="n"
+        ) as mock_input:
+            Targpg(self.archive, passfile=self.passfile)
+        self.assertFileNotExists(
+            self.archive,
+            "File should not exist when create confirmation is negative",
+        )
+
+        with patch("builtins.input", return_value="y") as mock_input:
+            gt = Targpg(self.archive, passfile=self.passfile)
+        gt.save().exit()
+        self.assertFileExists(
+            self.archive,
+            "File should exist when create confirmation is positive (begins with `y`)",
+        )
+
+        with self.assertRaises(
+            PermissionError,
+            msg="Should not open file with wrong password",
+        ):
+            Targpg(self.archive, passfile=self.wrongpass)
+
+        self.archive.unlink()
+        gt = Targpg(self.archive, passfile=self.passfile, autocreate=True)
+        gt.save().exit()
+        self.assertFileExists(
+            self.archive,
+            msg="File be created without prompt with autocreate `True`",
+        )
+
+    def test_02_add(self):
+        """Add files to the archive"""
+        gt = self._create()
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            gt.list()
+        self.assertFileExists(
+            self.archive,
+            "File should be saved when files are added",
+        )
+        list_value = mock_stdout.getvalue()
+        self.assertNotEqual(
+            len(list_value),
+            0,
+            "Files in archive should appear when listed",
+        )
+
+    def test_03_dupe(self):
+        """Add dupe file to the archive fails"""
+        gt = self._create()
+        with self.assertRaises(
+            ValueError,
+            msg="Should not be able to add the same file",
+        ):
+            gt.add(self.file1)
+
+    def test_04_dupe(self):
+        """Add dupe file to the archive fails"""
+        gt = self._create()
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            gt.list()
+        num_files = len(mock_stdout.getvalue().splitlines())
+
+        gt.update(self.file1)
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            gt.list()
+        num_files_update = len(mock_stdout.getvalue().splitlines())
+
+        self.assertEqual(
+            num_files,
+            num_files_update,
+            "Update should replace the file in the archive",
+        )
+
+    def test_05_remove(self):
+        """Remove file from the archive"""
+        gt = self._create()
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            gt.list()
+        num_files = len(mock_stdout.getvalue().splitlines())
+
+        gt.remove(self.file1)
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            gt.list()
+        num_files_remove = len(mock_stdout.getvalue().splitlines())
+
+        self.assertNotEqual(
+            num_files,
+            num_files_remove,
+            "File should be removed from the archive",
+        )
+
+    def test_06_extract(self):
+        """Extract files from the archive"""
+        gt = Targpg(self.archive, passfile=self.passfile, autocreate=True)
+        gt.add(self.file1, self.file2)
+        gt.extract(self.file1, outdir=self.extr)
+        self.assertFileExists(
+            Path(self.extr, self.file1),
+            "File should be extracted when passed as an argument",
+        )
+
+        with patch("builtins.input", return_value="1"):
+            gt.extract(outdir=self.extr)
+        self.assertFileExists(
+            Path(self.extr, self.file2),
+            "File should be extracted when selected from the command line",
+        )
+
+    def test_07_newpass_auto_file(self):
+        """Change the password of the archive"""
+        gt = Targpg(self.archive, passfile=self.passfile, autocreate=True)
+        gt.newpass(self.newpass)
+        gt.save().exit()
+
+        with self.assertRaises(
+            PermissionError,
+            msg="Should not open file with old password",
+        ):
+            Targpg(self.archive, passfile=self.passfile)
+
+    def test_08_newpass_ask_file(self):
+        """Change the password of the archive"""
+        gt = Targpg(self.archive, passfile=self.passfile, autocreate=True)
+
+        input_mocks = Mock()
+        input_mocks.side_effect = ["y", self.newpass]
+        with patch("builtins.input", input_mocks):
+            gt.newpass()
+        gt.save().exit()
+
+        with self.assertRaises(
+            PermissionError,
+            msg="Should not open file with old password",
+        ):
+            Targpg(self.archive, passfile=self.passfile)
