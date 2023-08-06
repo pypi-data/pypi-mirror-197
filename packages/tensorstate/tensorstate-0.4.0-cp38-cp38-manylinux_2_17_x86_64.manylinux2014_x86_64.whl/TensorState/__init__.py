9f52089f3c9fe3a90400a0cc7d22cef9af883c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:57 +0000

    Add package tensorstate/0.4.0/tensorstate-0.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tensorstate/tensorstate-0.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/TensorState/__init__.py
@@ -0,0 +1,51 @@
+# noqa: D104
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)-10s - %(levelname)-8s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+logger = logging.getLogger("TensorState")
+
+# Detect cupy
+try:
+    import cupy  # noqa
+
+    has_cupy = True
+except ModuleNotFoundError:
+    has_cupy = False
+
+logger.info(f"has_cupy: {has_cupy}")
+
+# Detect PyTorch
+try:
+    pass
+
+    has_torch = True
+except ModuleNotFoundError:
+    has_torch = False
+
+logger.info(f"has_torch: {has_torch}")
+
+# Detect Tensorflow
+try:
+    pass
+
+    has_tf = True
+except ModuleNotFoundError:
+    has_tf = False
+
+logger.info(f"has_tf: {has_tf}")
+
+from .Layers import StateCapture, StateCaptureHook  # noqa
+from .models import LeNet_5, mobilenet_v2  # noqa
+from .States import compress_states, decompress_states, sort_states  # noqa
+from .TensorState import (  # noqa
+    aIQ,
+    build_efficiency_model,
+    entropy,
+    network_efficiency,
+    reset_efficiency_model,
+)
+
+__version__ = "0.4.0"
