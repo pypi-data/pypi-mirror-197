11ce5e73dcfaae8631eaa47818efb16269664ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:01 +0000

    Add package osmsg/0.1.24/osmsg-0.1.24.tar.gz

---
+++ packages/osmsg/osmsg-0.1.24.tar.gz/osmsg-0.1.24/setup.py
@@ -0,0 +1,34 @@
+import io
+
+from setuptools import setup
+
+# Read the content of the README file
+with io.open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="osmsg",
+    version="0.1.24",
+    description="OpenStreetMap Stats Generator : Commandline",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kshitijrajsharma/OSMSG",
+    author="Kshitij Raj Sharma",
+    author_email="skshitizraj@gmail.com",
+    license="MIT",
+    packages=["osmsg"],
+    entry_points={"console_scripts": ["osmsg = osmsg.app:main"]},
+    install_requires=[
+        "osmium",
+        "pandas",
+        "requests",
+        "tabulate",
+        "Shapely",
+        "geopandas",
+        "dataframe-image",
+        "tqdm",
+        "seaborn",
+        "matplotlib",
+        "humanize",
+    ],
+)
