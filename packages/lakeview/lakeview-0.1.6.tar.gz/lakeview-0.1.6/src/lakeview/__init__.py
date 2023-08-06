e43f7b28d03f95ac1d392e6aa048c6d5fa71820d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:35 +0000

    Add package lakeview/0.1.6/lakeview-0.1.6.tar.gz

---
+++ packages/lakeview/lakeview-0.1.6.tar.gz/lakeview-0.1.6/src/lakeview/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__author__ = "Jia-Yuan Zhang"
+__email__ = "jzhang@well.ox.ac.uk"
+
+
+from . import alignment, annotation, plot, region_notation, remote, sequence, widget
+
+SequenceAlignment = alignment.SequenceAlignment
+GeneAnnotation = annotation.GeneAnnotation
+GenomeViewer = widget.GenomeViewer
+DotPlot = sequence.DotPlot
