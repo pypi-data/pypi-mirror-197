2e5e4d6300bc2238fdd4f4ef2dc468e4d102befd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:04 +0000

    Add package aimped/0.0.7/aimped-0.0.7.tar.gz

---
+++ packages/aimped/aimped-0.0.7.tar.gz/aimped-0.0.7/aimped/models.py
@@ -0,0 +1,14 @@
+# Author: AIMPED
+# Date: 2023-March-11
+# Description: This file contains model loading functions
+
+def load_config(file_path='config.json'):
+    with open(file_path, "r") as f:
+        config = json.load(f)
+    return config
+
+def load_model(model_name):
+    config = load_config()
+    model_path = config['model_path']
+    model = load_model(model_path + model_name)
+    return model
