1c2734a04fc810ae47e9ec1b22d3f9515c54bb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:47 +0000

    Add package rrlzjulvyx/0.0.1/rrlzjulvyx-0.0.1.tar.gz

---
+++ packages/rrlzjulvyx/rrlzjulvyx-0.0.1.tar.gz/rrlzjulvyx-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rrlzjulvyx',
+    version='0.0.1',
+    packages=['rrlzjulvyx'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
