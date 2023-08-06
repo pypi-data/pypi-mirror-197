8d86f2e3a68e19dd1cd5ee5b1c0c1ec39d96cc38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:16 +0000

    Add package zefkopzekfo/0.0.1/zefkopzekfo-0.0.1.tar.gz

---
+++ packages/zefkopzekfo/zefkopzekfo-0.0.1.tar.gz/zefkopzekfo-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zefkopzekfo',
+    version='0.0.1',
+    packages=['zefkopzekfo'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
