f046f45c433b83d8e34705ac4683a17108446e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:24 +0000

    Add package gyver/2.0.8/gyver-2.0.8.tar.gz

---
+++ packages/gyver/gyver-2.0.8.tar.gz/gyver-2.0.8/gyver/exc.py
@@ -0,0 +1,37 @@
+class GyverError(Exception):
+    """Base exception for all gyver exceptions"""
+
+
+class InvalidCast(GyverError):
+    """Exception raised for config when
+    cast callable raises an error"""
+
+
+class MissingName(GyverError, KeyError):
+    """Exception raised for config when
+    name is not found in given environ"""
+
+
+class InvalidField(GyverError, KeyError):
+    """Exception raised when field lookup
+    found nothing on mapping"""
+
+
+class CacheMiss(GyverError, KeyError):
+    """Exception raised when cache lookup
+    returns None or raises error"""
+
+
+class QueueNotFound(InvalidField):
+    """Exception raised when queue name provider
+    does not exist on given credentials"""
+
+
+class MissingParams(GyverError, NotImplementedError):
+    """Exception raised when no params where passed in a
+    *args function
+    """
+
+
+class InvalidPath(GyverError, ValueError):
+    """Equivalent to FileNotFound but also for directories"""
