c518481e460bf722e1b1c5c52fc0c41dadef92d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package napari-vemseg/0.0.4/napari-vemseg-0.0.4.tar.gz

---
+++ packages/napari-vemseg/napari-vemseg-0.0.4.tar.gz/napari-vemseg-0.0.4/src/napari_vemseg/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.4"
+
+from .vem_pixel_classifier import VEMSEGClassifier
+from .predict_pixel_classifier import VEMSEGClassifierPredict
+from . phh_predict import PHHPredictWidget
+
+# __all__ = (
+#     "VEMSEGClassifier",
+#     "VEMSEGClassifierPredict",
+#     "PHHPredictWidget"
+# )
