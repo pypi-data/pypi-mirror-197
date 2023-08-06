b4ab5fb8493608b28690ea966919a87524418e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:43 +0000

    Add package soundcloud-lib/0.6.1/soundcloud-lib-0.6.1.tar.gz

---
+++ packages/soundcloud-lib/soundcloud-lib-0.6.1.tar.gz/soundcloud-lib-0.6.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+try:
+    long_description = open("README.md").read()
+except:
+    long_description = ""
+
+
+requirements = [
+    'mutagen',
+    'bs4',
+    'aiohttp'
+]
+
+setup(
+    name='soundcloud-lib',
+    version='0.6.1',
+    description='Python Soundcloud API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/3jackdaws/soundcloud-lib',
+    author='Ian Murphy',
+    author_email='3jackdaws@gmail.com',
+    license='MIT',
+    packages=['sclib'],
+    python_requires='>=3.6',
+    install_requires=requirements,
+    test_suite='pytest',
+    tests_require=['pytest', 'pytest-asyncio'],
+)
