3473b26d75b274cd9e447b96b97c873725f3d64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:04 +0000

    Add package redisenv/0.3.0/redisenv-0.3.0-py3-none-any.whl

---
+++ packages/redisenv/redisenv-0.3.0-py3-none-any.whl/redisenv/__main__.py
@@ -0,0 +1,20 @@
+# This layout exists, specifically to make poetry+extras work
+def main():
+    from .commands import (cli, cluster, enterprise, replica, sentinel,
+                           standalone)
+
+    cli.group(standalone.standalone).add_command(standalone.create)
+
+    cli.group(sentinel.sentinel).add_command(sentinel.create)
+
+    cli.group(replica.replica).add_command(replica.create)
+
+    cli.group(cluster.cluster).add_command(cluster.create)
+
+    cli.group(enterprise.enterprise).add_command(enterprise.create)
+
+    cli()
+
+
+if __name__ == "__main__":
+    main()
