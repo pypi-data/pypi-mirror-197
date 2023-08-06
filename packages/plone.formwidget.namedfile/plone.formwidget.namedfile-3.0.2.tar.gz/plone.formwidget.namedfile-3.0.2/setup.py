afa8cc0253091c983278ff680ceb5f963faad7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:58 +0000

    Add package plone.formwidget.namedfile/3.0.2/plone.formwidget.namedfile-3.0.2.tar.gz

---
+++ packages/plone.formwidget.namedfile/plone.formwidget.namedfile-3.0.2.tar.gz/plone.formwidget.namedfile-3.0.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.2"
+
+setup(
+    name="plone.formwidget.namedfile",
+    version=version,
+    description="Image widget for z3c.form and Plone",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="plone image widget z3c.form",
+    author="Martin Aspeli",
+    author_email="optilude@gmail.com",
+    url="https://pypi.org/project/plone.formwidget.namedfile",
+    license="GPL",
+    packages=find_packages(),
+    namespace_packages=["plone", "plone.formwidget"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "plone.base",
+        "plone.namedfile",
+        "plone.z3cform >= 0.7.4",
+        "z3c.form",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+        ],
+    },
+)
