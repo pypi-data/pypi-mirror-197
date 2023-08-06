8b371386623ef74f4aba06720220a8b4f042326e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:37 +0000

    Add package pipelime-python/1.4.1/pipelime_python-1.4.1-py3-none-any.whl

---
+++ packages/pipelime-python/pipelime_python-1.4.1-py3-none-any.whl/pipelime/stages/item_replacement.py
@@ -0,0 +1,28 @@
+import typing as t
+import pydantic as pyd
+
+from pipelime.stages import SampleStage
+from pipelime.utils.pydantic_types import ItemType
+
+if t.TYPE_CHECKING:
+    from pipelime.sequences import Sample
+
+
+class StageReplaceItem(SampleStage, title="replace-item"):
+    """Replaces items in sample preserving internal values."""
+
+    key_item_map: t.Mapping[str, ItemType] = pyd.Field(
+        ...,
+        description=(
+            "A mapping `key: item_cls` returning the new item class for the key."
+        ),
+    )
+
+    def __call__(self, x: "Sample") -> "Sample":
+        for key, item_cls in self.key_item_map.items():
+            if key in x:
+                old_item = x[key]
+                x = x.set_item(
+                    key, item_cls.value.make_new(old_item, shared=old_item.is_shared)
+                )
+        return x
