87ecaaf3630a588a8cf8a18408074b79117f06ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:41 +0000

    Add package lazyearth/1.0.2/lazyearth-1.0.2.tar.gz

---
+++ packages/lazyearth/lazyearth-1.0.2.tar.gz/lazyearth-1.0.2/lazyearth/__init__.py
@@ -0,0 +1,9 @@
+# If there are this code in orther local have to use 
+# from lazyearth.mainfile import objearth
+# from lazyearth.mainfile import water
+
+from lazyearth.mainfile import objearth
+from lazyearth.mainfile import water
+
+__version__ = '1.0.2'
+print(f'Lazyearth version : {__version__}')
