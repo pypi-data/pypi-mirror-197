92d779b063606ad0e51a7a45066875260c041405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:15 +0000

    Add package zyqnuutupjerllnbxaeq/0.0.1/zyqnuutupjerllnbxaeq-0.0.1.tar.gz

---
+++ packages/zyqnuutupjerllnbxaeq/zyqnuutupjerllnbxaeq-0.0.1.tar.gz/zyqnuutupjerllnbxaeq-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zyqnuutupjerllnbxaeq',
+    version='0.0.1',
+    packages=['zyqnuutupjerllnbxaeq'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
