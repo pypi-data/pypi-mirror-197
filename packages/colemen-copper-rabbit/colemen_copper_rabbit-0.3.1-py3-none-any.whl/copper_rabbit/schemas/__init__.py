91628b687e79d23dcb14a195d9305d2a4d281025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:53 +0000

    Add package colemen-copper-rabbit/0.3.1/colemen_copper_rabbit-0.3.1-py3-none-any.whl

---
+++ packages/colemen-copper-rabbit/colemen_copper_rabbit-0.3.1-py3-none-any.whl/copper_rabbit/schemas/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-import
+
+
+import copper_rabbit.schemas.Request as requests
