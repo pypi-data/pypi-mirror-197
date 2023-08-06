d7259987e0eb12fa972988082a4aeb43da631f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:42 +0000

    Add package jamorasep/0.0.1/jamorasep-0.0.1-py3-none-any.whl

---
+++ packages/jamorasep/jamorasep-0.0.1-py3-none-any.whl/jamorasep/__init__.py
@@ -0,0 +1,12 @@
+__copyright__ = 'Copyright (C) 2023 Hideyuki Tachibana.'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__author__ = 'Hideyuki Tachibana'
+__author_email__ = 'h_tachibana@pkshatech.com'
+
+__all__ = ['main', 'kana_util']
+
+import jamorasep.main
+from jamorasep.main import parse
+from jamorasep.main import Morasep
+from jamorasep.kana_util import h2k, k2h, is_hiragana, is_katakana, is_kana
