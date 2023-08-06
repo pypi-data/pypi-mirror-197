b1bb23a0dc7b00f7ec53f9334e055fbd5a00010a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:21 +0000

    Add package dyagram/0.0.20/DyaGram-0.0.20.tar.gz

---
+++ packages/dyagram/DyaGram-0.0.20.tar.gz/DyaGram-0.0.20/dyagram/initialize/initialize.py
@@ -0,0 +1,42 @@
+import os
+
+class dyagramInitialize:
+
+    def __init__(self, site):
+        self.clean = None
+        self.has_init_been_ran()
+        if not self.clean:
+            raise Exception('DyaGram is already initialized')
+        self.site = site
+
+
+    def has_init_been_ran(self):
+        dir = os.listdir()
+        if '.info' in dir:
+            self.clean = False
+            return True
+        self.clean = True
+        return False
+
+    def make_dyagram_folder_structure(self):
+        os.mkdir(self.site)
+        os.mkdir('.info')
+        with open('.info/info.json', 'w') as f:
+            import json
+            x = {"current_site": self.site}
+            json.dump(x,f)
+
+
+def main(site=None):
+    if not site:
+        raise Exception("Site not defined")
+    try:
+        dyinit = dyagramInitialize(site)
+        dyinit.make_dyagram_folder_structure()
+        print(f"Created site: {site}")
+    except Exception as e:
+        print(e)
+
+    print("\n\nDyaGram initialized!\n")
+
+
