91e02dab53d4d03113d577e4a0969bf62a763556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:33 +0000

    Add package popupsort/1.0/popupsort-1.0.tar.gz

---
+++ packages/popupsort/popupsort-1.0.tar.gz/popupsort-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='popupsort',
+    version='1.0',
+    description='A program for visualizing sorting algorithms',
+    author='Zouheir Nakouzi',
+    author_email='zouheir2002@gmail.com',
+    url='https://github.com/ZouheirN/PopUpSort',
+    packages=['popupsort'],
+    zip_safe=False)
