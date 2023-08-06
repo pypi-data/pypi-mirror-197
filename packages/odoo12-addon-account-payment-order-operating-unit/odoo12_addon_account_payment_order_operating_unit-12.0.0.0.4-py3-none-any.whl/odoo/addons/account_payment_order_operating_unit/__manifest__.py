712a28ef87f25c779e47f7c618a82603aeaca610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:04 +0000

    Add package odoo12-addon-account-payment-order-operating-unit/12.0.0.0.4/odoo12_addon_account_payment_order_operating_unit-12.0.0.0.4-py3-none-any.whl

---
+++ packages/odoo12-addon-account-payment-order-operating-unit/odoo12_addon_account_payment_order_operating_unit-12.0.0.0.4-py3-none-any.whl/odoo/addons/account_payment_order_operating_unit/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_payment_order_operating_unit",
+
+    'summary': """
+        Configuration to properly relate payment orders and operating units
+    """,
+
+    'author': "Coopdevs Treball SCCL",
+    'website': "https://git.coopdevs.org/coopdevs/odoo/odoo-addons/enhancements-operating-unit",
+
+    'category': 'Operating units',
+    'version': '12.0.0.0.4',
+
+    'depends': [
+        'base',
+        'account_payment_order',
+        'operating_unit',
+        'account_operating_unit',
+        'account_banking_sepa_direct_debit'
+    ],
+
+    # always loaded
+    'data': [
+        #         'security/ir.model.access.csv',
+        'views/account_payment_line.xml',
+    ]
+}
---
+++ packages/odoo12-addon-account-payment-order-operating-unit/odoo12_addon_account_payment_order_operating_unit-12.0.0.0.4-py3-none-any.whl/odoo/addons/account_payment_order_operating_unit/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import account_payment_line
+from . import bank_payment_line
+from . import account_payment_order
