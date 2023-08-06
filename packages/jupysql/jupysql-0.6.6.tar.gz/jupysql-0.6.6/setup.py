48352d311bd3150333214906ab6ea01cf871e5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:20 +0000

    Add package jupysql/0.6.6/jupysql-0.6.6.tar.gz

---
+++ packages/jupysql/jupysql-0.6.6.tar.gz/jupysql-0.6.6/setup.py
@@ -0,0 +1,72 @@
+import os
+from io import open
+import re
+import ast
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.md"), encoding="utf-8").read()
+
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+
+with open("src/sql/__init__.py", "rb") as f:
+    VERSION = str(
+        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
+    )
+
+install_requires = [
+    "prettytable",
+    "ipython>=1.0",
+    "sqlalchemy<2",
+    "sqlparse",
+    "ipython-genutils>=0.1.0",
+    "jinja2",
+    "ploomber-core>=0.2.4",
+    'importlib-metadata;python_version<"3.8"',
+]
+
+DEV = [
+    "flake8",
+    "pytest",
+    "pandas",
+    "polars",
+    "invoke",
+    "pkgmt",
+    "twine",
+    # tests
+    "duckdb",
+    "duckdb-engine",
+    # sql.plot module tests
+    "matplotlib",
+    "black",
+]
+
+setup(
+    name="jupysql",
+    version=VERSION,
+    description="Better SQL in Jupyter",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="database ipython postgresql mysql duckdb",
+    author="Ploomber",
+    author_email="contact@ploomber.io",
+    url="https://github.com/ploomber/jupysql",
+    project_urls={
+        "Source": "https://github.com/ploomber/jupysql",
+    },
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={"dev": DEV},
+)
---
+++ packages/jupysql/jupysql-0.6.6.tar.gz/jupysql-0.6.6/src/sql/__init__.py
@@ -0,0 +1,10 @@
+from .magic import RenderMagic, SqlMagic, load_ipython_extension
+
+__version__ = "0.6.6"
+
+
+__all__ = [
+    "RenderMagic",
+    "SqlMagic",
+    "load_ipython_extension",
+]
