20c0c8628ac7a543e435be1d2f5a008090433534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package EVECelery/0.17/EVECelery-0.17.tar.gz

---
+++ packages/EVECelery/EVECelery-0.17.tar.gz/EVECelery-0.17/EVECelery/tasks/Alliance/AllianceInfo.py
@@ -0,0 +1,20 @@
+from EVECelery.tasks.BaseTasks.ESIResqust import ESIRequest
+from EVECelery.exceptions.tasks import InputValidationError
+
+
+class AllianceInfo(ESIRequest):
+    def ttl_404(self) -> int:
+        return 3600  # current esi x-cached-seconds header
+
+    def get_key(self, alliance_id: int):
+        return f"AllianceInfo-{alliance_id}"
+
+    def route(self, alliance_id: int):
+        return f"/alliances/{alliance_id}"
+
+    def validate_inputs(self, alliance_id: int) -> None:
+        try:
+            int(alliance_id)
+        except ValueError:
+            raise InputValidationError("Input parameter must be an integer.")
+
