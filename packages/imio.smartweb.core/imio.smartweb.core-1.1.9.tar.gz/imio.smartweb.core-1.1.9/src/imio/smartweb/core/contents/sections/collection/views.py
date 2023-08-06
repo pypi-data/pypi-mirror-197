8e9c534658247dc2704bb93215033ac97d2c142b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:18 +0000

    Add package imio.smartweb.core/1.1.9/imio.smartweb.core-1.1.9.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.1.9.tar.gz/imio.smartweb.core-1.1.9/src/imio/smartweb/core/contents/sections/collection/views.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.contents.sections.views import CarouselOrTableSectionView
+from imio.smartweb.core.utils import batch_results
+from imio.smartweb.core.utils import get_scale_url
+
+
+class CollectionView(CarouselOrTableSectionView):
+    """Collection Section view"""
+
+    def items(self):
+        max_items = self.context.nb_results_by_batch * self.context.max_nb_batches
+        image_scale = self.image_scale
+        items = self.context.collection.to_object.results(
+            batch=False, brains=True, limit=max_items
+        )
+        results = []
+        for item in items:
+            url = item.getURL()
+            scale_url = get_scale_url(item, self.request, "image", image_scale)
+            results.append(
+                {
+                    "title": item.Title,
+                    "description": item.Description,
+                    "effective": item.effective,
+                    "url": url,
+                    "image": scale_url,
+                    "has_image": item.has_leadimage,
+                }
+            )
+        return batch_results(results, self.context.nb_results_by_batch)
+
+    @property
+    def see_all_url(self):
+        return self.context.collection.to_object.absolute_url()
