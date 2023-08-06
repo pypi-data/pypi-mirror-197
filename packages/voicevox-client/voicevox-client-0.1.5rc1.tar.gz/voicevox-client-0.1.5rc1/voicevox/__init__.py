31b8d5f3592bb2910224333c75d50389e0847736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:10 +0000

    Add package voicevox-client/0.1.5rc1/voicevox-client-0.1.5rc1.tar.gz

---
+++ packages/voicevox-client/voicevox-client-0.1.5rc1.tar.gz/voicevox-client-0.1.5rc1/voicevox/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. include:: ../README.md
+"""
+
+from .client import Client
+from .audio_query import AudioQuery
+from .speakers import Speaker, Style, SupportedFeature
+
+from .errors import HttpException, NotfoundError
+
+
+__all__ = (
+    "Client",
+    "AudioQuery",
+    "HttpException", "NotfoundError",
+    "Speaker", "Style", "SupportedFeature"
+)
+__version__ = "0.1.5rc1"
