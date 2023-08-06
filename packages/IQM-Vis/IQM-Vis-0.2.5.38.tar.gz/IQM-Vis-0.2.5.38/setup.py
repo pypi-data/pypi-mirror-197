31c833e6fab8248bb95da3c574a2944175f3dd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:51 +0000

    Add package IQM-Vis/0.2.5.38/IQM-Vis-0.2.5.38.tar.gz

---
+++ packages/IQM-Vis/IQM-Vis-0.2.5.38.tar.gz/IQM-Vis-0.2.5.38/setup.py
@@ -0,0 +1,27 @@
+# Author: Matt Clifford <matt.clifford@bristol.ac.uk>
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with open('README.md', encoding="utf-8") as f:
+        text = f.read()
+    return text
+
+
+setup(name='IQM-Vis',
+      version='0.2.5.38',
+      packages=find_packages(),
+      install_requires=['numpy',
+                        'opencv-python',
+                        'scikit-image',
+                        'PyQt6',
+                        'matplotlib',
+                        'torch',
+                        'torchmetrics'],
+      # data_files=[('/', ['IQM_Vis/UI/style.css'])], # include non .py files needed
+      author="Matt Clifford",
+      author_email="matt.clifford@bristol.ac.uk",
+      description="Extendable user interface for the assessment of transformations on image metrics.",
+      long_description=get_long_description(),
+      long_description_content_type='text/markdown',
+      )
