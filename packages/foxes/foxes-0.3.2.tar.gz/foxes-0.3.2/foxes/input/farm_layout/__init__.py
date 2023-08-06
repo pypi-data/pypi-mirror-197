77698dd05a5fa57b48d628d4b066938b8f93a28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:59 +0000

    Add package foxes/0.3.2/foxes-0.3.2.tar.gz

---
+++ packages/foxes/foxes-0.3.2.tar.gz/foxes-0.3.2/foxes/input/farm_layout/__init__.py
@@ -0,0 +1,5 @@
+from .add_row import add_row
+from .add_grid import add_grid
+from .add_from_json import add_from_json
+from .add_from_csv import add_from_csv
+from .add_from_file import add_from_file
