90854032d2a4451944fe664bfc8518e2f49a34a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:20:11 +0000

    Add package mrtutils/0.3.0/mrtutils-0.3.0.tar.gz

---
+++ packages/mrtutils/mrtutils-0.3.0.tar.gz/mrtutils-0.3.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import os
+import yaml
+
+
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("version.yml","r") as vs:
+    version_dict = yaml.load(vs.read(), Loader=yaml.FullLoader)
+    version_string = version_dict['version_string']
+
+
+setuptools.setup(
+     name='mrtutils',
+     version= version_string,
+     author="Jason Berger",
+     author_email="JBerger@up-rev.com",
+     description="Utilities for MrT",
+     long_description=long_description,
+     scripts=['mrtutils/mrt-config','mrtutils/mrt-device', 'mrtutils/mrt-ble', 'mrtutils/mrt-config-gui','mrtutils/mrt-doc', 'mrtutils/mrt-version','mrtutils/mrt-gen'],
+     long_description_content_type="text/x-rst",
+     url="https://mrt.readthedocs.io/en/latest/index.html",
+     packages=setuptools.find_packages(),
+     package_data={
+     'mrtutils':['templates/*', 'templates/*/*','templates/*/*/*','templates/*/*/*/*']
+     },
+     install_requires=[
+        'markdown',
+        'mako',
+        'pyyaml',
+        'polypacket',
+        'update_notipy',
+        'kconfiglib',
+        'gitpython'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
