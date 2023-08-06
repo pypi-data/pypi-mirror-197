d5ce5d11fc28839d10b928e3a0030fa67c2b2ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:23 +0000

    Add package orplib/1.0.2/orplib-1.0.2.tar.gz

---
+++ packages/orplib/orplib-1.0.2.tar.gz/orplib-1.0.2/src/orpl/__init__.py
@@ -0,0 +1,7 @@
+"""
+ORPL
+================
+the Open Raman Processing Library is a collection of tools for the processing, analysis and
+visualization of Raman spectrum.
+"""
+ORPL_VERSION = "1.0.2"
