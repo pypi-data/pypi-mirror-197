c55779bbf414a4eb9732414ccdacaa7f6b221b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:50 +0000

    Add package zcmds-win32/1.0.10/zcmds_win32-1.0.10-py2.py3-none-any.whl

---
+++ packages/zcmds-win32/zcmds_win32-1.0.10-py2.py3-none-any.whl/zcmds_win32/open.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from zcmds_win32._exec import os_exec
+
+
+def main() -> int:
+    cmd = "explorer"
+    if len(sys.argv) == 1:
+        cmd += " ."
+        return os.system(cmd)
+    for i, _ in enumerate(sys.argv):
+        if i < 1:
+            continue
+        arg = sys.argv[i].replace("/", "\\")
+        sys.argv[i] = arg
+    return os_exec(cmd)
