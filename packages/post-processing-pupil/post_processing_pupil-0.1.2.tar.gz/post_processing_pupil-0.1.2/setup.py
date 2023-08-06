050c05f9aa95bfa6c1a129d73cc9099cfd7d61a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:59 +0000

    Add package post-processing-pupil/0.1.2/post_processing_pupil-0.1.2.tar.gz

---
+++ packages/post-processing-pupil/post_processing_pupil-0.1.2.tar.gz/post_processing_pupil-0.1.2/setup.py
@@ -0,0 +1,58 @@
+import sys
+from setuptools import setup
+
+if sys.version_info[:2] < (3, 8):
+    error = ('post_processing_pupil requires Python 3.8 or later (%d.%d detected).' % sys.version_info[:2])
+    sys.stderr.write(error + "\n")
+    sys.exit(1)
+
+# General informations
+name = 'post_processing_pupil'
+version = '0.1.2'
+description = 'Python package to calculte output fixation points produced by the pupil-core eye tracker'
+url = 'https://github.com/LostInZoom/post_processing_ET'
+author = 'Laura Wenclik'
+author_email = 'laura.wenclik@ign.fr'
+lic = 'GPLv3'
+packages = [
+    'post_processing_pupil'
+]
+
+# Requirements and dependencies
+python_requires = '>=3.8'
+install_requires = [
+    'pandas',
+]
+
+# Meta informations
+keywords = [
+    'pupil core',
+    'eye tracker',
+]
+platforms = ['Linux', 'Mac OSX', 'Windows', 'Unix']
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3 :: Only',
+]
+
+if __name__ == '__main__':
+    setup(
+        name=name,
+        version=version,    
+        description=description,
+        url=url,
+        author=author,
+        author_email=author_email,
+        license=lic,
+        packages=packages,
+        python_requires=python_requires,
+        install_requires=install_requires,
+        keywords = keywords,
+        platforms = platforms,
+        classifiers=classifiers,
+    )
