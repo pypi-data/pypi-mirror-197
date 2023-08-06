650e20180b463381b541d8d049ec49caa25d1dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:48 +0000

    Add package mcrs/0.0.7/mcrs-0.0.7.tar.gz

---
+++ packages/mcrs/mcrs-0.0.7.tar.gz/mcrs-0.0.7/mcrs/__init__.py
@@ -0,0 +1,37 @@
+from .environment import (
+    DupOperationException,
+    EnvironmentException,
+    EnvironmentManager,
+    ImmutableDict,
+    LazyValue,
+    LazyValueException,
+    NoValueException,
+)
+from .execution_context import ExecutionContext
+from .imicroservice import IMicroService
+from .launcher import BaseLauncher
+from .network import Network
+from .os_info import OSInfo
+from .python_info import PythonInfo
+from .resource_limits import Limit, ResourceLimits
+from .utils import get_resource_limit, parse_dns_servers_from_file
+
+__all__ = (
+    "IMicroService",
+    "BaseLauncher",
+    "Network",
+    "parse_dns_servers_from_file",
+    "PythonInfo",
+    "OSInfo",
+    "get_resource_limit",
+    "ResourceLimits",
+    "Limit",
+    "ExecutionContext",
+    "EnvironmentManager",
+    "ImmutableDict",
+    "EnvironmentException",
+    "NoValueException",
+    "LazyValueException",
+    "DupOperationException",
+    "LazyValue",
+)
