85f6920005652022faabdb7962e1d354dba3a8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:26 +0000

    Add package elb-log-analyzer/0.5.0/elb_log_analyzer-0.5.0-py3-none-any.whl

---
+++ packages/elb-log-analyzer/elb_log_analyzer-0.5.0-py3-none-any.whl/elb_log_analyzer/__main__.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+from argparse import ArgumentParser
+from json import dumps
+from os import makedirs
+from os.path import dirname
+
+from .log_analyzer import LogAnalyzer
+
+
+parser = ArgumentParser(prog='elb-log-analyzer')
+parser.add_argument('-i', '--input-log-file', dest='log_file_path',
+                    help='Log file path or path of directory storing only log files', type=str, required=True)
+parser.add_argument('-k', '--api-key', dest='api_key',
+                    help='Abuse IP DB API key', type=str, default=None)
+parser.add_argument('-t', '--threshold',
+                    dest='request_threshold', default=40, type=int)
+parser.add_argument('-o', '--output', help='output file path for storing data in json format',
+                    dest='output_file_path', type=str, default=None)
+args = parser.parse_args()
+
+# analyze logs
+log_analyzer = LogAnalyzer(log_file_path=args.log_file_path,
+                           ipabuse_api_key=args.api_key, request_threshold=args.request_threshold)
+analyzed_data = log_analyzer.analyze_logs()
+
+# write/print data
+out_file_path = args.output_file_path
+if out_file_path:
+    # create output dir if not present
+    dir = dirname(out_file_path)
+    if dir:
+        makedirs(dir, exist_ok=True)
+
+    with open(out_file_path, 'w') as f:
+        f.write(dumps(analyzed_data))
+        print(f'Analyzed Data stored in file: {out_file_path}')
+else:
+    pprint(analyzed_data)
---
+++ packages/elb-log-analyzer/elb_log_analyzer-0.5.0-py3-none-any.whl/elb_log_analyzer/utils.py
@@ -0,0 +1,79 @@
+from requests import get as r_get
+from requests.models import Response
+from json import JSONDecodeError
+from os import listdir
+from os.path import isfile, isdir, join as path_join
+
+
+def get_abusive_ip_data(ip: str, api_key: str):
+    '''
+    description: get abusive ip details
+    returns: dict
+    '''
+    assert isinstance(ip, str)
+    assert isinstance(api_key, str)
+
+    headers = {
+        'Key': api_key,
+        'Accept': 'application/json',
+    }
+    params = {
+        'ipAddress': ip,
+        'maxAgeInDays': 90
+    }
+
+    res:Response = r_get(
+        'https://api.abuseipdb.com/api/v2/check',
+        params=params,
+        headers=headers
+    )
+    
+    try:
+        data = res.json().get("data", None)
+    except JSONDecodeError:
+        data = {'msg': f'Error while fetching ip details of {ip}'}
+    except Exception as e:
+        data = {'error': str(e)}
+
+    return data
+
+def read_file_lines(file_path:str):
+    '''
+    description: takes file path and returns lines as list
+    returns: list 
+    '''
+    assert isinstance(file_path, str)
+
+    log_lines = []
+    if isfile(file_path):
+        with open(file_path, 'r') as f:
+            log_lines = f.readlines()
+
+    return log_lines
+        
+
+
+def get_logs(path:str):
+    '''
+    description: extract logs from a file and returns log lines as string.
+    returns: list[str] or None
+    '''
+    assert isinstance(path, str)
+
+    log_lines = []
+    # if path is a directory then combine log lines from .log files inside the directory 
+    if isdir(path):
+        log_files_path = list(filter(lambda file_path: file_path.endswith('.log'), listdir(path)))
+        files_path = [ path_join(path, log_file_path) for log_file_path in log_files_path]
+        for file_path in files_path:
+            log_lines += read_file_lines(file_path)
+
+    # if path is a file then extract log lines
+    elif isfile(path):
+        log_lines = read_file_lines(path)
+
+    # if file/dir is not present then raise error
+    else:
+        raise FileNotFoundError(f'{path} log file/files not found.')
+    
+    return log_lines
