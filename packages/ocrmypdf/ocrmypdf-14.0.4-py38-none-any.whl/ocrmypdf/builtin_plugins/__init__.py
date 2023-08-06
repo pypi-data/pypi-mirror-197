afefce09700a22b97432ac6e0ed024b628687418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:47 +0000

    Add package ocrmypdf/14.0.4/ocrmypdf-14.0.4-py38-none-any.whl

---
+++ packages/ocrmypdf/ocrmypdf-14.0.4-py38-none-any.whl/ocrmypdf/builtin_plugins/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022 James R. Barlow
+# SPDX-License-Identifier: MPL-2.0
+
+from __future__ import annotations
+
+# This file exists only mark builtin_plugins as a package.
+# The plugin manager will not load it, so anything defined here may not be
+# processed as a module.
