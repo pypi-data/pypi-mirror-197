14d7e4ab204a30d5bf2f925f8c85b43fec242b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:04 +0000

    Add package combadge/0.1.0b3/combadge-0.1.0b3-py3-none-any.whl

---
+++ packages/combadge/combadge-0.1.0b3-py3-none-any.whl/combadge/support/http/request.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, List
+
+from pydantic import BaseModel
+
+from combadge.support.http.abc import RequiresMethod, RequiresPath, SupportsFormData, SupportsJson, SupportsQueryParams
+
+
+class Request(RequiresMethod, RequiresPath, SupportsJson, SupportsQueryParams, SupportsFormData, BaseModel):
+    """Backend-agnostic REST request."""
+
+    def to_json_dict(self) -> Dict[str, Any]:
+        """Convert the JSON body and loose JSON fields into a dictionary."""
+        dict_ = self.json_fields
+        if (json := self.json_) is not None:
+            dict_.update(json.dict(by_alias=True))
+        return dict_
+
+    def to_form_data(self) -> Dict[str, List[Any]]:
+        """Convert the form data and loose form fields into a dictionary."""
+        form_fields = self.form_fields
+        if (form_data := self.form_data) is not None:
+            for name, value in form_data.dict(by_alias=True).items():
+                self.append_form_field(name, value)
+        return form_fields
