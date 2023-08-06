65d198b12616065c36261b53dfa10614f6e63794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:07 +0000

    Add package python-track/1.1/python_track-1.1-py3-none-any.whl

---
+++ packages/python-track/python_track-1.1-py3-none-any.whl/test/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_track',
+    version='1.1',
+    packages=['test'],
+    url='https://github.com/shahKrinal/Python_package/tree/main',
+    license='',
+    author='krinal',
+    author_email='shahkrinal877@gmail.com',
+    description='Creating python package'
+)
