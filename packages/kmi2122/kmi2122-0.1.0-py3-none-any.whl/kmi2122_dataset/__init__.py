30558ce1250649338009f5e123ebc93a48355765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:19 +0000

    Add package kmi2122/0.1.0/kmi2122-0.1.0-py3-none-any.whl

---
+++ packages/kmi2122/kmi2122-0.1.0-py3-none-any.whl/kmi2122_dataset/__init__.py
@@ -0,0 +1,33 @@
+"""Korea Macroeconomic Indicators 2021-2022(monthly, 24 sequences)"""
+import os
+import json
+import pandas as pd
+
+
+_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "dataset.json")
+
+
+class Dataset(dict):
+    """kmi2122 dataset"""
+    def __init__(self, path: str = _PATH) -> None:
+        """Instantiates a new Dataset object"""
+        self.path = path
+        super(Dataset, self).__init__(json.load(open(self.path)))
+
+
+class KMI2122:
+    """kmi2122 dataset"""
+
+    def __init__(self, path: str = _PATH, end_of_utterance_token: str = " ") -> None:
+        """Instantiates a new ConversationDataset object."""
+        self.path = path
+        self._data = json.load(open(self.path))
+
+    def get_df(self):
+        return pd.Dataframe(self._data['kmi_dataset_main'])
+    
+    def column_info(self):
+        return self._data['kmi_dataset_column_info']
+    
+    def __call__(self, *args: any, **kwds: any) -> dict:
+        return self._data
