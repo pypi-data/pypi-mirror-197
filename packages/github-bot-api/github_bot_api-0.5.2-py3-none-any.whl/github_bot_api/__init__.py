cea52d4850c228d724061c18cfed208174a73602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:58 +0000

    Add package github-bot-api/0.5.2/github_bot_api-0.5.2-py3-none-any.whl

---
+++ packages/github-bot-api/github_bot_api-0.5.2-py3-none-any.whl/github_bot_api/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Niklas Rosenstein <nrosenstein@palantir.com>"
+__version__ = "0.5.2"
+
+from .app import GithubApp
+from .event import Event
+from .webhook import Webhook
+
+__all__ = ["GithubApp", "Event", "Webhook"]
