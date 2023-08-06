cb5b33a9f94558e81fa32b82ac9fc21b562dad81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:02 +0000

    Add package ballyregan/1.0.3/ballyregan-1.0.3-py3-none-any.whl

---
+++ packages/ballyregan/ballyregan-1.0.3-py3-none-any.whl/ballyregan/core/utils.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import socket
+
+
+def has_internet_connection() -> bool:
+    """Check wether or not the system is connected to a network
+
+    Returns:
+        bool: Connected or not
+    """
+    try:
+        s = socket.create_connection(("8.8.8.8", 53), timeout=2)
+        s.close()
+        return True
+    except:
+        return False
+
+
+def get_event_loop():
+    try:
+        return asyncio.get_running_loop() 
+    except Exception:
+        return asyncio.get_event_loop_policy().get_event_loop()
