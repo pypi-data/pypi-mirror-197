51281ab97150195a6160fd76300a93219a535537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:14 +0000

    Add package Mconfig/1.1.5/Mconfig-1.1.5-py3-none-any.whl

---
+++ packages/Mconfig/Mconfig-1.1.5-py3-none-any.whl/Mconfig/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sys
+
+__version__ = "1.1.5"
+
+class MyModuleCall(sys.modules[__name__].__class__):
+    # module call
+    def __call__(self, *args, **kwargs):
+        from Mconfig.main import ConfigManage
+        return ConfigManage(*args, **kwargs)
+
+sys.modules[__name__].__class__ = MyModuleCall
+
+sys.modules["Mconfig_Context"] = dict()
+
+del sys, MyModuleCall
---
+++ packages/Mconfig/Mconfig-1.1.5-py3-none-any.whl/Mconfig/mconfig.py
@@ -0,0 +1,45 @@
+# Create Time: 2021-09-02 14:23:52
+# -*- coding: utf-8 -*-
+# config info write here
+from datetime import datetime
+# int or float
+M_num = 1
+
+# str
+M_str = 'Hello Mconfig!'
+
+# bool
+M_bool = True
+
+# list
+M_List = [1, 2, 3]
+
+# tuple
+M_tuple = (1, 2, 3)
+
+# set
+M_set = {1, 2, 3}
+
+# dict
+M_dict = {'a': 1, 'b': 2}
+
+# datetime
+M_date = datetime(1970, 1, 1)
+
+# class
+class M_SomeConfigClass_a():
+
+    variable_a = 1
+
+    variable_b = 'Hello Mconfig!'
+
+    variable_c = [1, 2, 3, 4, 5]
+
+    variable_d = {'a': 1, 'b': 3}
+
+
+class M_SomeConfigClass_b():
+
+    variable_a = 0
+
+    variable_b = False
