105230019de7a82f54076b3a22a443ff8d7ad787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:16 +0000

    Add package arize-phoenix/0.0.2rc6/arize_phoenix-0.0.2rc6-py3-none-any.whl

---
+++ packages/arize-phoenix/arize_phoenix-0.0.2rc6-py3-none-any.whl/phoenix/core/dimension.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from functools import cached_property
+from itertools import chain
+from typing import Any, Callable, List
+
+import pandas as pd
+
+from .dimension_data_type import DimensionDataType
+from .dimension_type import DimensionType
+
+
+@dataclass
+class Dimension:
+    name: str
+    data_type: DimensionDataType
+    type: DimensionType
+    data: Callable[[], List["pd.Series[Any]"]]
+
+    @cached_property
+    def categories(self) -> List[str]:
+        if self.data_type != DimensionDataType.CATEGORICAL:
+            return []
+        return sorted(
+            value
+            for value in set(chain.from_iterable(series.unique() for series in self.data()))
+            if isinstance(value, str)
+        )
