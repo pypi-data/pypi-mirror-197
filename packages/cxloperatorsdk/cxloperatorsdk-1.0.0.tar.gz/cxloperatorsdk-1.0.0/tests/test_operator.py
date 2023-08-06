b830b07a8bb6f2375c43d923fafb907e84b60d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:48 +0000

    Add package cxloperatorsdk/1.0.0/cxloperatorsdk-1.0.0.tar.gz

---
+++ packages/cxloperatorsdk/cxloperatorsdk-1.0.0.tar.gz/cxloperatorsdk-1.0.0/tests/test_operator.py
@@ -0,0 +1,21 @@
+import pytest
+
+from cxloperatorsdk.operator import Operator
+from cxloperatorsdk.run_status import RunStatus
+
+class SimpleOperator(Operator):
+    def execute(self):
+        pass
+
+def test_base_operator():
+    operator = Operator()
+    operator.run()
+
+    assert operator.status == RunStatus.FAILED
+
+def test_good_operator():
+    operator = SimpleOperator()
+    operator.run()
+
+    assert operator.status == RunStatus.SUCCESS
+
