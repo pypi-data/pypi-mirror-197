96f5e39428df78eda8b6f1c1978f43cb6528ea7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:40 +0000

    Add package rkslogger/1.0/rkslogger-1.0.tar.gz

---
+++ packages/rkslogger/rkslogger-1.0.tar.gz/rkslogger-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from rkslogger import __version__
+
+setup(
+    name='rkslogger',
+    version=__version__,
+
+    url='https://github.com/rksArkos/rkslogger',
+    author='ArKoS',
+    author_email='markusmacalla@gmail.com',
+
+    py_modules=['rkslogger'],
+)
