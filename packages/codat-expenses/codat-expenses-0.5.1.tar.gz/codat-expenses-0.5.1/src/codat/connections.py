ca599c7b5ce05c0fb86dd6527826c010d647f15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:55 +0000

    Add package codat-expenses/0.5.1/codat-expenses-0.5.1.tar.gz

---
+++ packages/codat-expenses/codat-expenses-0.5.1.tar.gz/codat-expenses-0.5.1/src/codat/connections.py
@@ -0,0 +1,46 @@
+import requests as requests_http
+from . import utils
+from codat.models import operations
+from typing import Optional
+
+class Connections:
+    _client: requests_http.Session
+    _security_client: requests_http.Session
+    _server_url: str
+    _language: str
+    _sdk_version: str
+    _gen_version: str
+
+    def __init__(self, client: requests_http.Session, security_client: requests_http.Session, server_url: str, language: str, sdk_version: str, gen_version: str) -> None:
+        self._client = client
+        self._security_client = security_client
+        self._server_url = server_url
+        self._language = language
+        self._sdk_version = sdk_version
+        self._gen_version = gen_version
+        
+    def create_partnerexpense_connection(self, request: operations.CreatePartnerexpenseConnectionRequest) -> operations.CreatePartnerexpenseConnectionResponse:
+        r"""Create Partner Expense connection
+        Creates a Partner Expense data connection
+        """
+        
+        base_url = self._server_url
+        
+        url = utils.generate_url(operations.CreatePartnerexpenseConnectionRequest, base_url, '/companies/{companyId}/sync/expenses/connections/partnerExpense', request)
+        
+        
+        client = self._security_client
+        
+        http_res = client.request('POST', url)
+        content_type = http_res.headers.get('Content-Type')
+
+        res = operations.CreatePartnerexpenseConnectionResponse(status_code=http_res.status_code, content_type=content_type, raw_response=http_res)
+        
+        if http_res.status_code == 200:
+            if utils.match_content_type(content_type, 'application/json'):
+                out = utils.unmarshal_json(http_res.text, Optional[operations.CreatePartnerexpenseConnectionConnection])
+                res.connection = out
+
+        return res
+
+    
---
+++ packages/codat-expenses/codat-expenses-0.5.1.tar.gz/codat-expenses-0.5.1/src/codat/sdk.py
@@ -0,0 +1,131 @@
+__doc__ = """ SDK Documentation: The API for Sync for Expenses.
+Sync for Expenses is an API and a set of supporting tools. It has been built to enable corporate card and expense management platforms to provide high-quality integrations with multiple accounting platforms through a standardized API.
+
+[Read more...](https://docs.codat.io/sync-for-expenses/overview)
+
+[See our OpenAPI spec](https://github.com/codatio/oas)"""
+import requests as requests_http
+from . import utils
+from .configuration import Configuration
+from .connections import Connections
+from .expenses import Expenses
+from .mapping_options import MappingOptions
+from .sync import Sync
+from .sync_status import SyncStatus
+from .transaction_status import TransactionStatus
+from codat.models import shared
+
+SERVERS = [
+	"https://api.codat.io",
+]
+
+class Codat:
+    r"""SDK Documentation: The API for Sync for Expenses.
+    Sync for Expenses is an API and a set of supporting tools. It has been built to enable corporate card and expense management platforms to provide high-quality integrations with multiple accounting platforms through a standardized API.
+    
+    [Read more...](https://docs.codat.io/sync-for-expenses/overview)
+    
+    [See our OpenAPI spec](https://github.com/codatio/oas)"""
+    configuration: Configuration
+    connections: Connections
+    expenses: Expenses
+    mapping_options: MappingOptions
+    sync: Sync
+    sync_status: SyncStatus
+    transaction_status: TransactionStatus
+    
+    _client: requests_http.Session
+    _security_client: requests_http.Session
+    _server_url: str = SERVERS[0]
+    _language: str = "python"
+    _sdk_version: str = "0.5.1"
+    _gen_version: str = "1.12.1"
+
+    def __init__(self,
+                 security: shared.Security = None,
+                 server_url: str = None,
+                 url_params: dict[str, str] = None,
+                 client: requests_http.Session = None
+                 ) -> None:
+        self._client = requests_http.Session()
+        
+        
+        if server_url is not None:
+            if url_params is not None:
+                self._server_url = utils.template_url(server_url, url_params)
+            else:
+                self._server_url = server_url
+
+        if client is not None:
+            self._client = client
+        
+        self._security_client = utils.configure_security_client(self._client, security)
+        
+
+        self._init_sdks()
+    
+    def _init_sdks(self):
+        self.configuration = Configuration(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.connections = Connections(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.expenses = Expenses(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.mapping_options = MappingOptions(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.sync = Sync(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.sync_status = SyncStatus(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.transaction_status = TransactionStatus(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+    
