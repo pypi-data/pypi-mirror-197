1a6a647b4314fef08e6ad5ebd758bac832bb6869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:41 +0000

    Add package python-tvmaze/1.0.5/python-tvmaze-1.0.5.tar.gz

---
+++ packages/python-tvmaze/python-tvmaze-1.0.5.tar.gz/python-tvmaze-1.0.5/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="python-tvmaze",
+    version="1.0.5",
+    description="A Python client for the TvMaze API.",
+    license="MIT",
+    author="Yakup Adaklı",
+    author_email="yakup.adakli@gmail.com",
+    url="https://github.com/yakupadakli/python-tvmaze",
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3.8, <4",
+    install_requires=[
+        "pytest",
+        "requests==2.28.2",
+        "six==1.16.0"
+    ],
+    keywords="tvmaze python library",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
+    ],
+    zip_safe=True,
+)
