d5ad1ce203d5e48b69ab428b9cdbbd7610158d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:19 +0000

    Add package ttads/0.0.10/ttads-0.0.10-py3-none-any.whl

---
+++ packages/ttads/ttads-0.0.10-py3-none-any.whl/ttads/tunnel.py
@@ -0,0 +1,44 @@
+import threading
+from time import sleep
+
+from pyngrok import ngrok
+
+
+class Tunnel:
+
+    def __init__(self):
+        self.is_open = True
+        self.tunnel = None
+        self.thread = threading.Thread(target=self.start_tunnel)
+        self.start()
+
+    def start(self):
+        self.thread.start()
+        while not self.tunnel:
+            print('Waiting for tunnel to initialise...')
+            sleep(1)
+
+    def start_tunnel(self):
+        self.tunnel = ngrok.connect()
+        while self.is_open:
+            sleep(1)
+        ngrok.disconnect(self.tunnel.public_url)
+
+    def close(self):
+        print('Stopping thread. Tunnel should close.')
+        self.is_open = False
+        self.wait()
+
+    def wait(self):
+        while self.thread.is_alive():
+            sleep(1)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.close()
+
+
+if __name__ == '__main__':
+    pass
