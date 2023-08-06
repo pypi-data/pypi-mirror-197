4ce128ec0bd684436714111d6b2bd1625df189f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:10 +0000

    Add package azerty123/0.0.1/azerty123-0.0.1.tar.gz

---
+++ packages/azerty123/azerty123-0.0.1.tar.gz/azerty123-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='azerty123',
+    version='0.0.1',
+    packages=['azerty123'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
