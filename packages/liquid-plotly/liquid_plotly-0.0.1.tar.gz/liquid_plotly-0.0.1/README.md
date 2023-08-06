ff3b4a7d37093cd02fafcd47cdb06de2d65a0b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:15 +0000

    Add package liquid-plotly/0.0.1/liquid_plotly-0.0.1.tar.gz

---
+++ packages/liquid-plotly/liquid_plotly-0.0.1.tar.gz/liquid_plotly-0.0.1/README.md
@@ -0,0 +1,19 @@
+# Liquid Plotly
+
+Liquid Plotly is a [Dash](https://dash.plotly.com/) component library based on the [Liquid Design System](https://www.figma.com/file/8GYcAOePm8Tt9qqJ7Gnv99/Liquid-Oxygen-(Share)?node-id=3%3A14310).
+
+> **Warning**
+> This project is currently in an early stage of development.
+
+Check out the [list of available components](https://github.com/emdgroup-liquid/liquid-plotly/tree/main/src/ts/components).
+
+Get started with:
+1. Install Dash and its dependencies: https://dash.plotly.com/installation
+2. Install Liquid Plotly with `pip install liquid-plotly`
+3. Import Liquid Plotly with `import liquid_plotly as ld`
+
+Usage examples can be found [here](https://github.com/emdgroup-liquid/liquid-plotly/blob/main/usage.py) and [here](https://github.com/emdgroup-liquid/liquid-plotly/tree/main/examples). Refer to the [Liquid Oxygen docs](https://liquid.merck.design/liquid/) for information on all component properties.
+
+## License
+
+Liquid Plotly is available under a [custom license](https://github.com/emdgroup-liquid/liquid-plotly/blob/main/LICENSE.md) which restricts its usage to applications created for or by Merck KGaA, Darmstadt, Germany as well as its vendors.
---
+++ packages/liquid-plotly/liquid_plotly-0.0.1.tar.gz/liquid_plotly-0.0.1/liquid_plotly/_imports_.py
@@ -0,0 +1,111 @@
+from .Accordion import Accordion
+from .AccordionPanel import AccordionPanel
+from .AccordionSection import AccordionSection
+from .AccordionToggle import AccordionToggle
+from .Badge import Badge
+from .BgCells import BgCells
+from .Breadcrumbs import Breadcrumbs
+from .Crumb import Crumb
+from .Button import Button
+from .Card import Card
+from .CardStack import CardStack
+from .Checkbox import Checkbox
+from .CircularProgress import CircularProgress
+from .Header import Header
+from .Icon import Icon
+from .Input import Input
+from .InputMessage import InputMessage
+from .Label import Label
+from .Link import Link
+from .Loading import Loading
+from .Modal import Modal
+from .Notice import Notice
+from .Notification import Notification
+from .Pagination import Pagination
+from .Progress import Progress
+from .Radio import Radio
+from .Option import Option
+from .Select import Select
+from .Slider import Slider
+from .SrLive import SrLive
+from .SrOnly import SrOnly
+from .Step import Step
+from .Stepper import Stepper
+from .Switch import Switch
+from .SwitchItem import SwitchItem
+from .Table import Table
+from .TableBody import TableBody
+from .TableCaption import TableCaption
+from .TableCell import TableCell
+from .TableCol import TableCol
+from .TableColgroup import TableColgroup
+from .TableFoot import TableFoot
+from .TableHead import TableHead
+from .TableHeader import TableHeader
+from .TableRow import TableRow
+from .TableToolbar import TableToolbar
+from .Tab import Tab
+from .Tablist import Tablist
+from .Tabpanel import Tabpanel
+from .Tabpanellist import Tabpanellist
+from .Tabs import Tabs
+from .Toggle import Toggle
+from .Tooltip import Tooltip
+from .Typo import Typo
+
+__all__ = [
+    "Accordion",
+    "AccordionPanel",
+    "AccordionSection",
+    "AccordionToggle",
+    "Badge",
+    "BgCells",
+    "Breadcrumbs",
+    "Crumb",
+    "Button",
+    "Card",
+    "CardStack",
+    "Checkbox",
+    "CircularProgress",
+    "Header",
+    "Icon",
+    "Input",
+    "InputMessage",
+    "Label",
+    "Link",
+    "Loading",
+    "Modal",
+    "Notice",
+    "Notification",
+    "Pagination",
+    "Progress",
+    "Radio",
+    "Option",
+    "Select",
+    "Slider",
+    "SrLive",
+    "SrOnly",
+    "Step",
+    "Stepper",
+    "Switch",
+    "SwitchItem",
+    "Table",
+    "TableBody",
+    "TableCaption",
+    "TableCell",
+    "TableCol",
+    "TableColgroup",
+    "TableFoot",
+    "TableHead",
+    "TableHeader",
+    "TableRow",
+    "TableToolbar",
+    "Tab",
+    "Tablist",
+    "Tabpanel",
+    "Tabpanellist",
+    "Tabs",
+    "Toggle",
+    "Tooltip",
+    "Typo"
+]
