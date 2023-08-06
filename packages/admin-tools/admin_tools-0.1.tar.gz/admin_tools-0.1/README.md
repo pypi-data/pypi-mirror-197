abf2bf42eb837866fa9c12cb36183e52d8b529cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:33 +0000

    Add package admin-tools/0.1/admin_tools-0.1.tar.gz

---
+++ packages/admin-tools/admin_tools-0.1.tar.gz/admin_tools-0.1/README.md
@@ -0,0 +1,3 @@
+# admin tools 
+
+admin tools is a Python library for performing various administrative functions on data.
---
+++ packages/admin-tools/admin_tools-0.1.tar.gz/admin_tools-0.1/admin_tools/admin_functions.py
@@ -0,0 +1,32 @@
+
+#================================    
+class ad_tools: 
+#================================    
+    """
+    Class to simulate spatial transciptomic spot data, where each spot contains a mixture of cells of different gene expression patterns.
+    
+    """
+    
+    #========================
+    def __init__(self):
+    #========================
+        self.n_clusts = 5
+
+
+
+    def prac(self,x,y):
+        """
+            This function is a practice. 
+
+
+        Inputs:
+
+        Outputs:
+
+            """
+
+        z=x+y
+        return(z,y)
+    
+    
+    
