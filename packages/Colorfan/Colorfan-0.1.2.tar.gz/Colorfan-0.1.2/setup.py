da14c0419afac7439370d9e6bdf4c9fb056c472d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package Colorfan/0.1.2/Colorfan-0.1.2.tar.gz

---
+++ packages/Colorfan/Colorfan-0.1.2.tar.gz/Colorfan-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Colorfan',
+    version='0.1.2',
+    description='A powerful python module to set console color',
+    py_modules=['colorfan'],
+    author='Hamidreza Ahmadi',
+    author_email='your.email@example.com',
+    url='https://github.com/hushra/colorfan',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    install_requires=[],
+)
