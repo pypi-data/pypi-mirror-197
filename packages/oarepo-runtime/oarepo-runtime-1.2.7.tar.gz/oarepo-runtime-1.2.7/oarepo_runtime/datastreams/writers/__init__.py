66d771625a9f18b59226784db35b97c5669b4cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:19 +0000

    Add package oarepo-runtime/1.2.7/oarepo-runtime-1.2.7.tar.gz

---
+++ packages/oarepo-runtime/oarepo-runtime-1.2.7.tar.gz/oarepo-runtime-1.2.7/oarepo_runtime/datastreams/writers/__init__.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+from oarepo_runtime.datastreams.batch import StreamBatch
+
+from ..datastreams import StreamEntry
+
+
+class BaseWriter(ABC):
+    """Base writer."""
+
+    def __init__(self, **kwargs) -> None:
+        """kwargs for extensions"""
+
+    @abstractmethod
+    def write(self, entry: StreamEntry, *args, **kwargs):
+        """Writes the input entry to the target output.
+        :returns: nothing
+                  Raises WriterException in case of errors.
+        """
+
+    def finish(self):
+        """Finalizes writing"""
+
+
+class BatchWriter:
+    def __init__(self, **kwargs) -> None:
+        """kwargs for extensions"""
+
+    @abstractmethod
+    def write_batch(self, batch: StreamBatch, *args, **kwargs) -> StreamBatch:
+        """Writes the batch to teh stream
+        :returns: the batch written. If there are any errors writing entries
+        of the batch, should mark them on entries[x].errors.
+
+        Note: the batch contains all items, even the filtered ones or the ones
+        with errors. Get StreamEntry.ok property to write only the correct batch
+        entries.
+        """
