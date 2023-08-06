1ae41f107521b133898526447648cfffed908379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:26 +0000

    Add package isar/1.13.1/isar-1.13.1-py3-none-any.whl

---
+++ packages/isar/isar-1.13.1-py3-none-any.whl/robot_interface/models/inspection/__init__.py
@@ -0,0 +1,12 @@
+from .inspection import (
+    Image,
+    ImageMetadata,
+    Inspection,
+    InspectionMetadata,
+    ThermalImage,
+    ThermalImageMetadata,
+    ThermalVideo,
+    ThermalVideoMetadata,
+    Video,
+    VideoMetadata,
+)
