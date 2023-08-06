3858f342be7fa734ace356c1fd727fb980c7baca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:22 +0000

    Add package dissect.target/3.8.dev31/dissect.target-3.8.dev31.tar.gz

---
+++ packages/dissect.target/dissect.target-3.8.dev31.tar.gz/dissect.target-3.8.dev31/dissect/target/plugins/browsers/iexplore.py
@@ -0,0 +1,134 @@
+from pathlib import Path
+from typing import BinaryIO, Iterator, Tuple
+
+from dissect.esedb import esedb, record, table
+from dissect.util.ts import wintimestamp
+
+from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.descriptor_extensions import UserRecordDescriptorExtension
+from dissect.target.helpers.record import create_extended_descriptor
+from dissect.target.plugin import Plugin, export
+from dissect.target.plugins.browsers.browser import (
+    GENERIC_HISTORY_RECORD_FIELDS,
+    try_idna,
+)
+from dissect.target.plugins.general.users import UserDetails
+from dissect.target.target import Target
+
+IEBrowserHistoryRecord = create_extended_descriptor([UserRecordDescriptorExtension])(
+    "browser/ie/history", GENERIC_HISTORY_RECORD_FIELDS
+)
+
+
+class WebCache:
+    def __init__(self, target: Target, fh: BinaryIO):
+        self.target = target
+        self.db = esedb.EseDB(fh)
+
+    def find_containers(self, name: str) -> table.Table:
+        try:
+            for container_record in self.db.table("Containers").records():
+                if record_name := container_record.get("Name"):
+                    record_name = record_name.rstrip("\00").lower()
+                    if record_name == name.lower():
+                        container_id = container_record.get("ContainerId")
+                        yield self.db.table(f"Container_{container_id}")
+        except KeyError:
+            pass
+
+    def _iter_records(self, name: str) -> Iterator[record.Record]:
+        for container in self.find_containers(name):
+            try:
+                yield from container.records()
+            except Exception as e:
+                self.target.log.warning("Error iterating IExplore container %s", container, exc_info=e)
+                continue
+
+    def history(self) -> Iterator[record.Record]:
+        yield from self._iter_records("history")
+
+
+class InternetExplorerPlugin(Plugin):
+    """Internet explorer browser plugin."""
+
+    __namespace__ = "iexplore"
+
+    DIRS = [
+        "AppData/Local/Microsoft/Windows/WebCache",
+    ]
+
+    CACHE_FILENAME = "WebCacheV01.dat"
+
+    def __init__(self, target: Target):
+        super().__init__(target)
+
+        self.users_dirs: list[Tuple[UserDetails, Path]] = []
+        for user_details in self.target.user_details.all_with_home():
+            for ie_dir in self.DIRS:
+                cdir = user_details.home_path.joinpath(ie_dir)
+                if not cdir.exists():
+                    continue
+                self.users_dirs.append((user_details.user, cdir))
+
+    def check_compatible(self) -> bool:
+        if not len(self.users_dirs):
+            raise UnsupportedPluginError("No Internet Explorer directories found")
+
+    @export(record=IEBrowserHistoryRecord)
+    def history(self) -> Iterator[IEBrowserHistoryRecord]:
+        """Return browser history records from Internet Explorer.
+
+        Yields IEBrowserHistoryRecord with the following fields:
+            hostname (string): The target hostname.
+            domain (string): The target domain.
+            ts (datetime): Visit timestamp.
+            browser (string): The browser from which the records are generated from.
+            id (string): Record ID.
+            url (uri): History URL.
+            title (string): Page title.
+            description (string): Page description.
+            rev_host (string): Reverse hostname.
+            visit_type (varint): Visit type.
+            visit_count (varint): Amount of visits.
+            hidden (string): Hidden value.
+            typed (string): Typed value.
+            session (varint): Session value.
+            from_visit (varint): Record ID of the "from" visit.
+            from_url (uri): URL of the "from" visit.
+            source: (path): The source file of the history record.
+        """
+        for user, cdir in self.users_dirs:
+            cache_file = cdir.joinpath(self.CACHE_FILENAME)
+            if not cache_file.exists():
+                continue
+
+            cache = WebCache(self.target, cache_file.open())
+            for container_record in cache.history():
+                if not container_record.get("Url"):
+                    continue
+
+                _, _, url = container_record.get("Url", "").rstrip("\x00").partition("@")
+
+                ts = None
+                if accessed_time := container_record.get("AccessedTime"):
+                    ts = wintimestamp(accessed_time)
+
+                yield IEBrowserHistoryRecord(
+                    ts=ts,
+                    browser="iexplore",
+                    id=container_record.get("EntryId"),
+                    url=try_idna(url),
+                    title=None,
+                    description=None,
+                    rev_host=None,
+                    visit_type=None,
+                    visit_count=container_record.get("AccessCount"),
+                    hidden=None,
+                    typed=None,
+                    session=None,
+                    from_visit=None,
+                    from_url=None,
+                    source=str(cache_file),
+                    _target=self.target,
+                    _user=user,
+                )
---
+++ packages/dissect.target/dissect.target-3.8.dev31.tar.gz/dissect.target-3.8.dev31/tests/test_plugins_browsers.py
@@ -0,0 +1,121 @@
+import gzip
+import tempfile
+
+from dissect.target.plugins.browsers import chrome, chromium, edge, firefox, iexplore
+
+from ._utils import absolute_path
+
+
+def test_iexplore_plugin(target_win, fs_win, tmpdir_name, target_win_users):
+    cache_archive = absolute_path("data/WebCacheV01.dat.gz")
+
+    with tempfile.NamedTemporaryFile(dir=tmpdir_name) as tf:
+        with gzip.GzipFile(cache_archive, "rb") as f:
+            tf.write(f.read())
+        tf.flush()
+
+        user = target_win_users.user_details.find(username="John")
+        webcache_dir = user.home_path.joinpath("AppData/Local/Microsoft/Windows/WebCache/")
+        webcache_file = webcache_dir.joinpath(iexplore.InternetExplorerPlugin.CACHE_FILENAME)
+
+        webcache_dir = str(webcache_dir)[3:]  # drop C:/
+        webcache_file = str(webcache_file)[3:]  # drop C:/
+
+        fs_win.map_dir("Users\\John", tmpdir_name)
+        fs_win.map_file(webcache_file, tf.name)
+
+        target_win.add_plugin(iexplore.InternetExplorerPlugin)
+
+        records = list(target_win.iexplore.history())
+        assert len(records) == 33
+
+        records = list(target_win.browser.history())
+        assert len(records) == 33
+
+
+def test_firefox_plugin(target_win, fs_win, tmpdir_name, target_win_users):
+    firefox_db = absolute_path("data/firefox-places.sqlite")
+
+    user = target_win_users.user_details.find(username="John")
+    webcache_dir = user.home_path.joinpath("AppData/local/Mozilla/Firefox/Profiles/g1rbw8y7.default-release/")
+    webcache_file = webcache_dir.joinpath("places.sqlite")
+
+    webcache_dir = str(webcache_dir)[3:]  # drop C:/
+    webcache_file = str(webcache_file)[3:]  # drop C:/
+
+    fs_win.map_dir("Users\\John", tmpdir_name)
+    fs_win.map_file(webcache_file, firefox_db)
+
+    target_win.add_plugin(firefox.FirefoxPlugin)
+
+    records = list(target_win.firefox.history())
+    assert len(records) == 24
+
+    records = list(target_win.browser.history())
+    assert len(records) == 24
+
+
+def test_chrome_plugin(target_win, fs_win, tmpdir_name, target_win_users):
+    firefox_db = absolute_path("data/chrome-history.sqlite")
+
+    user = target_win_users.user_details.find(username="John")
+    webcache_dir = user.home_path.joinpath("AppData/Local/Google/Chrome/continuousUpdates/User Data/Default")
+    webcache_file = webcache_dir.joinpath("History")
+
+    webcache_dir = str(webcache_dir)[3:]  # drop C:/
+    webcache_file = str(webcache_file)[3:]  # drop C:/
+
+    fs_win.map_dir("Users\\John", tmpdir_name)
+    fs_win.map_file(webcache_file, firefox_db)
+
+    target_win.add_plugin(chrome.ChromePlugin)
+
+    records = list(target_win.chrome.history())
+    assert len(records) == 10
+
+    records = list(target_win.browser.history())
+    assert len(records) == 10
+
+
+def test_edge_plugin(target_win, fs_win, tmpdir_name, target_win_users):
+    edge_db = absolute_path("data/edge-history.sqlite")
+
+    user = target_win_users.user_details.find(username="John")
+    webcache_dir = user.home_path.joinpath("AppData/Local/Microsoft/Edge/User Data/Default")
+    webcache_file = webcache_dir.joinpath("History")
+
+    webcache_dir = str(webcache_dir)[3:]  # drop C:/
+    webcache_file = str(webcache_file)[3:]  # drop C:/
+
+    fs_win.map_dir("Users\\John", tmpdir_name)
+    fs_win.map_file(webcache_file, edge_db)
+
+    target_win.add_plugin(edge.EdgePlugin)
+
+    records = list(target_win.edge.history())
+    assert len(records) == 9
+
+    records = list(target_win.browser.history())
+    assert len(records) == 9
+
+
+def test_chromium_plugin(target_win, fs_win, tmpdir_name, target_win_users):
+    chromium_db = absolute_path("data/chromium-history-win.sqlite")
+
+    user = target_win_users.user_details.find(username="John")
+    webcache_dir = user.home_path.joinpath("AppData/Local/Chromium/User Data/Default")
+    webcache_file = webcache_dir.joinpath("History")
+
+    webcache_dir = str(webcache_dir)[3:]  # drop C:/
+    webcache_file = str(webcache_file)[3:]  # drop C:/
+
+    fs_win.map_dir("Users\\John", tmpdir_name)
+    fs_win.map_file(webcache_file, chromium_db)
+
+    target_win.add_plugin(chromium.ChromiumPlugin)
+
+    records = list(target_win.chromium.history())
+    assert len(records) == 5
+
+    records = list(target_win.browser.history())
+    assert len(records) == 5
