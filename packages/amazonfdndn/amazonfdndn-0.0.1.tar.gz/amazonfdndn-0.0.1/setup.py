e286fbe6a31316e63fe083127b5ff6770a02bce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:02 +0000

    Add package amazonfdndn/0.0.1/amazonfdndn-0.0.1.tar.gz

---
+++ packages/amazonfdndn/amazonfdndn-0.0.1.tar.gz/amazonfdndn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='amazonfdndn',
+    version='0.0.1',
+    packages=['amazonfdndn'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
