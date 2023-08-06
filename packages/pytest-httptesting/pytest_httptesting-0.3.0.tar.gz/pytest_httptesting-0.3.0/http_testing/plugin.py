346305b7200f8100a40bcba4addac18d645dfa0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:16 +0000

    Add package pytest-httptesting/0.3.0/pytest_httptesting-0.3.0.tar.gz

---
+++ packages/pytest-httptesting/pytest_httptesting-0.3.0.tar.gz/pytest_httptesting-0.3.0/http_testing/plugin.py
@@ -0,0 +1,34 @@
+import attrs
+import pytest
+from httpx import URL, Client
+from pytest import FixtureRequest
+
+from .http_client_configuration import HttpClientConfiguration
+from .page_checker import PageChecker
+
+
+@pytest.fixture
+def base_url(request: FixtureRequest):
+    if not hasattr(request.module, "host"):
+        return None
+    host = getattr(request.module, "host")
+    scheme = "https" if not hasattr(request.module, "scheme") else getattr(request.module, "scheme")
+    port = None if not hasattr(request.module, "port") else int(getattr(request.module, "port"))
+    return URL(scheme=scheme, host=host, port=port)
+
+
+@pytest.fixture
+def http_client_config():
+    # deprecated
+    return HttpClientConfiguration()
+
+
+@pytest.fixture
+def http_client(http_client_config: HttpClientConfiguration):
+    with Client(**attrs.asdict(http_client_config)) as client:
+        yield client
+
+
+@pytest.fixture
+def check(http_client: Client, base_url: URL):
+    return PageChecker(http_client=http_client, base_url=base_url)
