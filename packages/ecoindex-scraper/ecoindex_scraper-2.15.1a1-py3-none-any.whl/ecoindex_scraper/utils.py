2c113376cd683ae41d6f0c240e8a90561bc56d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:56 +0000

    Add package ecoindex-scraper/2.15.1a1/ecoindex_scraper-2.15.1a1-py3-none-any.whl

---
+++ packages/ecoindex-scraper/ecoindex_scraper-2.15.1a1-py3-none-any.whl/ecoindex_scraper/utils.py
@@ -0,0 +1,23 @@
+from os import chown, unlink
+
+from ecoindex.models import ScreenShot
+from PIL import Image
+
+
+def convert_screenshot_to_webp(screenshot: ScreenShot) -> None:
+    image = Image.open(rf"{screenshot.get_png()}")
+    width, height = image.size
+    ratio = 800 / height if width > height else 600 / width
+
+    image.convert("RGB").resize(size=(int(width * ratio), int(height * ratio))).save(
+        rf"{screenshot.get_webp()}",
+        format="webp",
+    )
+    unlink(screenshot.get_png())
+
+
+def set_screenshot_rights(
+    screenshot: ScreenShot, uid: int | None = None, gid: int | None = None
+) -> None:
+    if uid and gid:
+        chown(path=screenshot.get_webp(), uid=uid, gid=gid)
