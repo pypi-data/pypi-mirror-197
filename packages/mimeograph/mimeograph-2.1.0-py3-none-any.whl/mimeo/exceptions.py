7e94dabdebc675d598b836fd013a8336c98bc5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:20 +0000

    Add package mimeograph/2.1.0/mimeograph-2.1.0-py3-none-any.whl

---
+++ packages/mimeograph/mimeograph-2.1.0-py3-none-any.whl/mimeo/exceptions.py
@@ -0,0 +1,26 @@
+class UnsupportedOutputFormat(Exception):
+    pass
+
+
+class UnsupportedOutputDirection(Exception):
+    pass
+
+
+class InvalidIndent(Exception):
+    pass
+
+
+class IncorrectMimeoModel(Exception):
+    pass
+
+
+class IncorrectMimeoTemplate(Exception):
+    pass
+
+
+class IncorrectMimeoConfig(Exception):
+    pass
+
+
+class NotAllowedInstantiation(Exception):
+    pass
