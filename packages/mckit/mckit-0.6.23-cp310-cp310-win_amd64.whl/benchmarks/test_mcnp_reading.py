24cd396f7d5f3d4e58375d8bc1519220e1940005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:03 +0000

    Add package mckit/0.6.23/mckit-0.6.23-cp310-cp310-win_amd64.whl

---
+++ packages/mckit/mckit-0.6.23-cp310-cp310-win_amd64.whl/benchmarks/test_mcnp_reading.py
@@ -0,0 +1,28 @@
+"""Tests with benchmarks on large mcnp files.
+
+To use it install plugin pytest-benchmark (https://pytest-benchmark.readthedocs.io/en/latest/index.html#)
+    conda install pytest-benchmark
+    or
+    pip install pytest-benchmark
+"""
+from zipfile import ZipFile
+
+import pytest
+
+from mckit.constants import MCNP_ENCODING
+from mckit.parser.mcnp_input_sly_parser import ParseResult, from_text
+from mckit.utils.resource import path_resolver
+
+data_filename_resolver = path_resolver("benchmarks")
+with ZipFile(data_filename_resolver("data/4M.zip")) as data_archive:
+    CLITE_TEXT = data_archive.read("clite.i").decode(encoding=MCNP_ENCODING)
+
+
+def test_sly_mcnp_reading(benchmark):
+    result: ParseResult = benchmark(from_text, CLITE_TEXT)
+    assert result.title == "C-LITE VERSION 1 RELEASE 131031 ISSUED 31/10/2013 - Halloween edition"
+    assert len(result.universe) == 150
+
+
+if __name__ == "__main__":
+    pytest.main(["--benchmark-enable"])
