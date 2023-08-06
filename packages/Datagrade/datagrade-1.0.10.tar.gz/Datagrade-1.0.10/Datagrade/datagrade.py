6b105e265b1892a8bae0e0f80aa5b57e84e58d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:05 +0000

    Add package Datagrade/1.0.10/datagrade-1.0.10.tar.gz

---
+++ packages/Datagrade/datagrade-1.0.10.tar.gz/Datagrade-1.0.10/Datagrade/datagrade.py
@@ -0,0 +1,41 @@
+def boxplot_show(df, sp_num):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    count = 1
+    for i in sp_num:
+        # установка размера для графиков
+        plt.figure(figsize=(20, 20))
+        # построение боксплотов для каждого числового атрибута
+        plt.title(i)
+        plt.subplot(5, 5, count)
+        sns.boxplot(x=df[i])
+        count += 1
+    plt.show()
+
+def histplot_show(df, sp_num):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    count = 1
+    for i in sp_num:
+        # установка размера для графиков
+        plt.figure(figsize=(20, 20))
+        # построение боксплотов для каждого числового атрибута
+        plt.title(i)
+        plt.subplot(5, 5, count)
+        plt.hist(x=df[i])
+        count += 1
+    plt.show()
+
+def distplot_show(df, sp_num):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    count = 1
+    for i in sp_num:
+        # установка размера для графиков
+        plt.figure(figsize=(20, 20))
+        # построение боксплотов для каждого числового атрибута
+        plt.title(i)
+        plt.subplot(5, 5, count)
+        sns.distplot(x=df[i])
+        count += 1
+    plt.show()
