ae27375dd35aa87d243891a830b7c317d75debf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:39 +0000

    Add package cwinpy/0.9.3/cwinpy-0.9.3.tar.gz

---
+++ packages/cwinpy/cwinpy-0.9.3.tar.gz/cwinpy-0.9.3/cwinpy/__init__.py
@@ -0,0 +1,13 @@
+import io
+
+# register reader/writer
+from . import iostream
+from .data import HeterodynedData, MultiHeterodynedData
+from .info import *
+from .parfile import PulsarParameters
+from .knope import skyshift
+
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:
+    __version__ = ""
