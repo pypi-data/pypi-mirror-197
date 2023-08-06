d850ae2b64a1de2ef1639c9302bb2b34e5342ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:04 +0000

    Add package feincms3-data/0.5.0/feincms3_data-0.5.0.tar.gz

---
+++ packages/feincms3-data/feincms3_data-0.5.0.tar.gz/feincms3_data-0.5.0/feincms3_data/management/commands/f3dumpdata.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from feincms3_data.data import datasets, dump_specs
+
+
+DATASETS = datasets()
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "dataset",
+            help=f"Model dataset which should be dumped. {', '.join(DATASETS)}",
+            choices=DATASETS,
+        )
+
+    def handle(self, *args, **options):
+        dataset, sep, args = options["dataset"].partition(":")
+        ds = DATASETS[dataset]
+        self.stdout.write(dump_specs(ds["specs"](args), mappers=ds.get("mappers")))
