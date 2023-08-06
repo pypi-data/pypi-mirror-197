63c0fa3394ad56de8766d7dadad1878fa6c8881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:54 +0000

    Add package codat-files/0.5.1/codat-files-0.5.1.tar.gz

---
+++ packages/codat-files/codat-files-0.5.1.tar.gz/codat-files-0.5.1/src/codat/models/operations/download_files.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+import dataclasses
+import requests as requests_http
+from datetime import datetime
+from typing import Optional
+
+
+@dataclasses.dataclass
+class DownloadFilesRequest:
+    company_id: str = dataclasses.field(metadata={'path_param': { 'field_name': 'companyId', 'style': 'simple', 'explode': False }})
+    date_: Optional[datetime] = dataclasses.field(default=None, metadata={'query_param': { 'field_name': 'date', 'style': 'form', 'explode': True }})
+    
+
+@dataclasses.dataclass
+class DownloadFilesResponse:
+    content_type: str = dataclasses.field()
+    status_code: int = dataclasses.field()
+    raw_response: Optional[requests_http.Response] = dataclasses.field(default=None)
+    
