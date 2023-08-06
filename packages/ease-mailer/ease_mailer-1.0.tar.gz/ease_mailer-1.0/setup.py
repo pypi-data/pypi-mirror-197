fbc51a51c5838828ed7860ccf60494100a8af811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:46 +0000

    Add package ease-mailer/1.0/ease_mailer-1.0.tar.gz

---
+++ packages/ease-mailer/ease_mailer-1.0.tar.gz/ease_mailer-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ease_mailer',
+    version='1.0',
+    packages=['ease_mailer'],
+    url='',
+    license='MIT',
+    author='Tech Smit',
+    author_email='techsmitdevloper@gmail.com',
+    description='ease_mailer is the easiest way for email sending.',
+    keywords=['Email Sending', 'Send Mail', 'Easy Email Sender', 'Send Multiple Mail'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
