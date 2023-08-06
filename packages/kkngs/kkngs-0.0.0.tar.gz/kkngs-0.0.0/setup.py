47fb0155ebadaad8fa44eb8ba74ce37a5f878b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:11 +0000

    Add package kkngs/0.0.0/kkngs-0.0.0.tar.gz

---
+++ packages/kkngs/kkngs-0.0.0.tar.gz/kkngs-0.0.0/setup.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="kkngs",
+    version="0.0.0",
+    author="Edgar Y. Walker",
+    packages=find_packages()
+)
