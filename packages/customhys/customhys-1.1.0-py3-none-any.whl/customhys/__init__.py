c174acbf037674af14a7f86560cbe9f77c0fe422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:06 +0000

    Add package customhys/1.1.0/customhys-1.1.0-py3-none-any.whl

---
+++ packages/customhys/customhys-1.1.0-py3-none-any.whl/customhys/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['benchmark_func', 'experiment', 'hyperheuristic', 'metaheuristic', 'operators', 'population', 'tools']
+# Under revision: 'characterisation', 'visualisation'
+__version__ = "1.1"
