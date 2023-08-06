a2becda5e453ffa739da09a9630e717305875b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:54 +0000

    Add package ovcdtwfcak/0.0.1/ovcdtwfcak-0.0.1.tar.gz

---
+++ packages/ovcdtwfcak/ovcdtwfcak-0.0.1.tar.gz/ovcdtwfcak-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ovcdtwfcak',
+    version='0.0.1',
+    packages=['ovcdtwfcak'],
+    install_requires=[],
+    author='',
+    author_email='',
+    description=''
+)
