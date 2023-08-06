d2b1caa4b8cf0bc75519b2ef667a09569615c765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:09 +0000

    Add package chaos-mesh/1.2.10/chaos_mesh-1.2.10-py3-none-any.whl

---
+++ packages/chaos-mesh/chaos_mesh-1.2.10-py3-none-any.whl/chaosmesh/experiments/base/k8s/stress/__init__.py
@@ -0,0 +1,23 @@
+from abc import ABC
+
+from chaosmesh.k8s.experiment import ChaosExperiment
+
+
+class StressTest(ChaosExperiment, ABC):
+
+    def __init__(self, **kwargs):
+        super(StressTest, self).__init__(**kwargs)
+
+    @property
+    def defaults(self) -> dict:
+        return {
+            "duration": '',
+            "labels": {},
+            "load": 100,
+            "mode": "all",
+            "size": '10',
+            "workers": 1
+        }
+
+    def api_resources(self) -> dict:
+        return {"group": 'chaos-mesh.org', "version": 'v1alpha1', "plural": "stresschaos"}
