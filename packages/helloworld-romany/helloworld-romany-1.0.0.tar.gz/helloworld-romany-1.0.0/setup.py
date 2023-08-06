fcabb7c6f6899399a91de9fce412fd66451e44fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:25 +0000

    Add package helloworld-romany/1.0.0/helloworld-romany-1.0.0.tar.gz

---
+++ packages/helloworld-romany/helloworld-romany-1.0.0.tar.gz/helloworld-romany-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='helloworld-romany',
+    version='1.0.0',
+    description='say hello!',
+    py_modules=['helloworld'],
+    package_dir={'': 'src'}
+)
