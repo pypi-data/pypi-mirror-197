67e5bc3cfd59a143e944c511172386ccf77f74b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:28 +0000

    Add package pokedream/2.5.1/pokedream-2.5.1.tar.gz

---
+++ packages/pokedream/pokedream-2.5.1.tar.gz/pokedream-2.5.1/pokedream/features/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+pokedream.features
+~~~~~~~~~~~~~~~~~
+
+The namespace containing the Feature components that go into the final pokedream cog class.
+
+:copyright: (c) 2021 Devon (Gorialis) R
+:license: MIT, see LICENSE for more details.
+
+"""
