71af3edd24d79d134aba129ce897f62b171e6d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:50 +0000

    Add package tensordict-nightly/2023.3.16/tensordict_nightly-2023.3.16-py38-none-any.whl

---
+++ packages/tensordict-nightly/tensordict_nightly-2023.3.16-py38-none-any.whl/tensordict/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from tensordict.memmap import MemmapTensor, set_transfer_ownership
+from tensordict.tensordict import (
+    LazyStackedTensorDict,
+    merge_tensordicts,
+    SubTensorDict,
+    TensorDict,
+)
+
+try:
+    from tensordict.version import __version__
+except ImportError:
+    __version__ = None
+
+
+__all__ = [
+    "LazyStackedTensorDict",
+    "MemmapTensor",
+    "SubTensorDict",
+    "TensorDict",
+    "merge_tensordicts",
+    "set_transfer_ownership",
+]
