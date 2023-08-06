d76f563d2a7ba054aeeacd5d44f3186cf407874a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:31 +0000

    Add package random-unicode-emoji/2.3/random_unicode_emoji-2.3-py3-none-any.whl

---
+++ packages/random-unicode-emoji/random_unicode_emoji-2.3-py3-none-any.whl/random_unicode_emoji/__init__.py
@@ -0,0 +1,27 @@
+from random_unicode_emoji.random_unicode_emoji import random_emoji
+
+__author__ = 'Nic Jones'
+__email__ = 'nic@nicpjones.com'
+__version__ = '2.3'
+__doc__ = """
+Random Unicode Emoji
+====================
+A Python package and function to retrieve a random Unicode emoji.
+
+Install
+-------
+>>> pip install random-unicode-emoji
+
+Usage
+-----
+>>> # Import the Library
+>>> from random_unicode_emoji import random_emoji
+>>> # Use the Function
+>>> print(random_emoji())
+>>> # Use Specific Version
+>>> print(random_emoji(version="14.0"))
+
+Upgrade
+-------
+>>> pip install random-unicode-emoji -U
+"""
